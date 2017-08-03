6152308fe4b82aa753aa4db5d41320948ef026e2
Narayanan Iyer <nars@yottadb.com>
2017-08-03 13:15:54 -0400

    Update copyrights for all modules changed by YottaDB since V6.3-001A

--- sr_port/gvcst_search.c
+++ sr_port/gvcst_search.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2015 Fidelity National Information 	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
--- sr_unix/offset.csh
+++ sr_unix/offset.csh
@@ -3,6 +3,9 @@
 #								#
 #	Copyright 2001, 2010 Fidelity Information Services, Inc	#
 #								#
+# Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	#
+# All rights reserved.						#
+#								#
 #	This source code contains the intellectual property	#
 #	of its copyright holder(s), and is made available	#
 #	under a license.  If you do not know the terms of	#
