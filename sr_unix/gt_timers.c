ee8bc972c3df382e48b986949e9141da35269d0d
Narayanan Iyer <nars@yottadb.com>
2017-10-10 15:34:07 -0400

    [YDB#61] Skip timer-overdue core on ARM as it is usually short in memory so system load can arbitrarily delay processes

--- sr_unix/gt_timers.c
+++ sr_unix/gt_timers.c
@@ -715,13 +715,13 @@ STATICFNDEF void timer_handler(int why)
 		cmp = abs_time_comp(&at, (ABS_TIME *)&tpop->expir_time);
 		if (cmp < 0)
 			break;
-#		if defined(DEBUG) && !defined(_AIX)
+#		if defined(DEBUG) && !defined(_AIX) && !defined(__armv7l__)
 		if (tpop->safe && (TREF(continue_proc_cnt) == last_continue_proc_cnt)
 			&& !(gtm_white_box_test_case_enabled
 				&& (WBTEST_SIGTSTP_IN_JNL_OUTPUT_SP == gtm_white_box_test_case_number)))
 		{	/* Check if the timer is extremely overdue, with the following exceptions:
 			 *	- Unsafe timers can be delayed indefinitely.
-			 *	- AIX systems tend to arbitrarily delay processes when loaded.
+			 *	- AIX and ARM systems tend to arbitrarily delay processes when loaded.
 			 *	- WBTEST_SIGTSTP_IN_JNL_OUTPUT_SP stops the process from running.
 			 *	- Some other mechanism causes a SIGSTOP/SIGCONT, bumping continue_proc_cnt.
 			 */
