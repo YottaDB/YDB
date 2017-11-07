3b7131577ff411cc65df05a7ddc5f06be9b36583
Narayanan Iyer <nars@yottadb.com>
2017-11-07 16:27:33 -0500

    [NARS1] [estess] lvtreenode_newblock should grow exponentially (not linearly) as it affects local array performance when the number of nodes is large (e.g. millions) #80

--- sr_port/lv_getslot.c
+++ sr_port/lv_getslot.c
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2014 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2014 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -138,7 +141,7 @@ lvTreeNode *lvtreenode_getslot(symval *sym)
 					numElems = p->numAlloc;
 				else
 					numElems = LV_NEWBLOCK_INIT_ALLOC;
-				lvtreenode_newblock(sym, numElems > 64 ? 128 : numElems * 2);
+				lvtreenode_newblock(sym, (numElems > MAXINT4) ? MAXINT4 : (numElems * 2));
 				p = sym->lvtreenode_first_block;
 				assert(NULL != p);
 			}
