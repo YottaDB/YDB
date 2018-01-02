1abde2b2558d20f0497e3bda6f914d67037a10c6
Narayanan Iyer <nars@yottadb.com>
2018-01-02 11:29:24 -0500

    [#59] Add 2018 copyright to all modules till now changed in this project

--- sr_x86_64/make_dmode_sp.h
+++ sr_x86_64/make_dmode_sp.h
@@ -1,15 +1,15 @@
 /****************************************************************
- *                                                              *
- * Copyright 2007, 2011 Fidelity Information Services, Inc 	*
  *								*
- * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * Copyright 2007, 2011 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017-2018 YottaDB LLC. and/or its subsidiaries.*
  * All rights reserved.						*
- *                                                              *
- *      This source code contains the intellectual property     *
- *      of its copyright holder(s), and is made available       *
- *      under a license.  If you do not know the terms of       *
- *      the license, please stop and do not read further.       *
- *                                                              *
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
  ****************************************************************/
 
 #define CODEBUF_TYPE	char
