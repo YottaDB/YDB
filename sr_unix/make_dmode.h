24fbdefe0641f363e9491af8b5aa84947ca9bad5
Narayanan Iyer <nars@yottadb.com>
2018-01-02 11:29:24 -0500

    [#59] Add 2018 copyright to all modules till now changed in this project

--- sr_unix/make_dmode.h
+++ sr_unix/make_dmode.h
@@ -2,7 +2,7 @@
  *								*
  * Copyright 2003, 2009 Fidelity Information Services, Inc	*
  *								*
- * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * Copyright (c) 2017-2018 YottaDB LLC. and/or its subsidiaries.*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
