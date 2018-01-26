113fc283bd6acd8a46fc8625e5946af81cbaa33d
Narayanan Iyer <nars@yottadb.com>
2018-01-26 13:41:37 -0500

    [#120] READ command on files in /proc filesystem now work

--- sr_unix/iorm_open.c
+++ sr_unix/iorm_open.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -220,7 +223,8 @@ short	iorm_open(io_log_name *dev_name, mval *pp, int fd, mval *mspace, int4 time
 					}
 				}
 			}
-			if (size == statbuf.st_size)
+			/* Note that for /proc files, statbuf.st_size is 0. We should not incorrectly set $ZEOF=1 in that case. */
+			if ((size == statbuf.st_size) && size)
 				iod->dollar.zeof = TRUE;
 		} else
 		{
