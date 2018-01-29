e3e5500d27a4c6ba2474b8a31ab848cc7899d221
Narayanan Iyer <nars@yottadb.com>
2018-01-29 11:49:48 -0500

    [#59] Fix few more multi-year copyrights to use - syntax (not , syntax)

--- sr_port/emit_code.c
+++ sr_port/emit_code.c
@@ -3,10 +3,10 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017,2018 YottaDB LLC. and/or its subsidiaries.*
+ * Copyright (c) 2017-2018 YottaDB LLC. and/or its subsidiaries.*
  * All rights reserved.						*
  *								*
- * Copyright (c) 2017,2018 Stephen L Johnson.			*
+ * Copyright (c) 2017-2018 Stephen L Johnson.			*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
