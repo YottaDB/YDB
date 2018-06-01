c28d9d8f18b1eb14218393e5ec5e773d91df7d41
Steve Johnson <sljohnson1@verizon.net>
2018-06-01 11:43:59 -0400

    [AARCH64] Add a new definition for AArch64

--- sr_unix/extract_signal_info.c
+++ sr_unix/extract_signal_info.c
@@ -140,6 +140,8 @@ void extract_signal_info(int sig, siginfo_t *info, gtm_sigcontext_t *context, gt
 					gtmsi->int_iadr = (caddr_t)context->uc_mcontext.psw.addr;
 #    elif defined(__armv6l__) || defined(__armv7l__)
 					gtmsi->int_iadr = (caddr_t)context->uc_mcontext.arm_pc;
+#    elif defined(__aarch64__)
+					gtmsi->int_iadr = (caddr_t)context->uc_mcontext.pc;
 #    else
 #      error "Unsupported Linux Platform"
 #    endif
