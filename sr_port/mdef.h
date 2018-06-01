aa5336f0d83c12b7ac905d870d2acf31e2566043
Steve Johnson <sljohnson1@verizon.net>
2018-06-01 11:36:36 -0400

    [AARCH64] Add AArch64 changes     - Include AArch64 definitions with armv6 and armv7 since it behaves the same     - Add new categories to the RISC_ONLY definition         * NON_AARCH64_RISC_ONLY 	* AARCH64_RISC_ONLY       The new category is needed because AARCH64 doesn't generate code the same       as the other RISC architectures. In fact, it works the same as the other       NON_RISC_ONLY architectures, but adding AARCH64 to NON_RISC_ONLY seemed       wrong so a new macro was created.

--- sr_port/mdef.h
+++ sr_port/mdef.h
@@ -211,7 +211,8 @@ typedef UINTPTR_T uintszofptr_t;
 #	define UNALIGNED_ACCESS_SUPPORTED
 #endif
 
-#if defined(__i386) || defined(__x86_64__) || defined(_AIX) || defined (__sun) || defined(__armv6l__) || defined(__armv7l__)
+#if defined(__i386) || defined(__x86_64__) || defined(_AIX) || defined (__sun)							\
+		|| defined(__armv6l__) || defined(__armv7l__) || defined(__aarch64__)
 #	define GTM_PTHREAD
 #	define GTM_PTHREAD_ONLY(X) X
 #	define NON_GTM_PTHREAD_ONLY(X)
@@ -249,36 +250,37 @@ typedef UINTPTR_T uintszofptr_t;
 #define NON_X86_64_ONLY(x)    		x
 #endif /* __x86_64__ */
 
-#if defined(__armv6l__) || defined(__armv7l__)
-#define ARM32_ONLY(x)			x
-#define NON_ARM32_ONLY(x)
+#if defined(__armv6l__) || defined(__armv7l__) || defined(__aarch64__)
+#define ARM_ONLY(x)			x
+#define NON_ARM_ONLY(x)
 #else
-#define ARM32_ONLY(x)
-#define NON_ARM32_ONLY(x)    		x
-#endif /* __armv6l__ || __armv7l__ */
+#define ARM_ONLY(x)
+#define NON_ARM_ONLY(x)    		x
+#endif /* __armv6l__ || __armv7l__ || __aarch64__ */
 
-#if defined(__x86_64__) || defined(__armv6l__) || defined(__armv7l__)
-#define	X86_64_OR_ARM32_ONLY(x)		x
-#define	NON_X86_64_OR_ARM32_ONLY(x)
+#if defined(__x86_64__) || defined(__armv6l__) || defined(__armv7l__) || defined(__aarch64__)
+#define	X86_64_OR_ARM_ONLY(x)		x
+#define	NON_X86_64_OR_ARM_ONLY(x)
 #else
-#define	X86_64_OR_ARM32_ONLY(x)
-#define	NON_X86_64_OR_ARM32_ONLY(x)	x
+#define	X86_64_OR_ARM_ONLY(x)
+#define	NON_X86_64_OR_ARM_ONLY(x)	x
 #endif
 
 #if defined(__i386) || defined(__x86_64__) || defined(__ia64) || defined(__MVS__) || defined(Linux390)
-#define NON_RISC_ONLY(x)	x
-#define RISC_ONLY(x)
-#elif defined(__sparc) || defined(_AIX) || defined(__alpha) || defined(__armv6l__) || defined(__armv7l__)
-#define RISC_ONLY(x)		x
-#define NON_RISC_ONLY(x)
-#endif
-
-#if defined(__armv6l__) || defined(__armv7l__)
-#	define ARM_ONLY(x)	x
-#	define NON_ARM_ONLY(x)
-#else
-#	define ARM_ONLY(x)
-#	define NON_ARM_ONLY(x)	x
+#    define NON_RISC_ONLY(x)		x
+#    define RISC_ONLY(x)
+#    define AARCH64_RISC_ONLY(x)
+#    define NON_AARCH64_RISC_ONLY(x)
+#elif defined(__sparc) || defined(_AIX) || defined(__alpha) || defined(__armv6l__) || defined(__armv7l__) || defined(__aarch64__)
+#    define RISC_ONLY(x)		x
+#    define NON_RISC_ONLY(x)
+#    if defined(__aarch64__)
+#	define AARCH64_RISC_ONLY(x)	x
+#	define NON_AARCH64_RISC_ONLY(x)
+#    else
+#	define AARCH64_RISC_ONLY(x)
+#	define NON_AARCH64_RISC_ONLY(x)	x
+#    endif
 #endif
 
 #ifdef _AIX
@@ -350,8 +352,9 @@ typedef struct
  * initialize literal_ptr field apppropriately.
  *
  */
-#if defined(__alpha) || defined(_AIX) || defined(__hpux) || defined(__sparc) || defined(__MVS__) || (defined(__linux__) &&  \
-	(defined(__ia64) || defined(__x86_64__) || defined(__s390__) || defined(__armv6l__) || defined(__armv7l__)))
+#if defined(__alpha) || defined(_AIX) || defined(__hpux) || defined(__sparc) || defined(__MVS__)				\
+		|| (defined(__linux__) && (defined(__ia64) || defined(__x86_64__) || defined(__s390__) || defined(__armv6l__)	\
+					   || defined(__armv7l__) || defined(__aarch64__)))
 #	define HAS_LITERAL_SECT
 #endif
 
@@ -1750,7 +1753,7 @@ typedef enum
 
 /* Encryption- and TLS-related macros */
 #if defined(__ia64) || defined(__i386) || defined(__x86_64__) || defined(__sparc) || defined(_AIX) || defined(__s390__)		\
-	|| defined(__armv6l__) || defined(__armv7l__)
+	|| defined(__armv6l__) || defined(__armv7l__) || defined(__aarch64__)
 # define GTM_TLS
 # define GTMTLS_ONLY(X)			X
 # define GTMTLS_ONLY_COMMA(X)		, X
