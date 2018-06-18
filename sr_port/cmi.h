bf85f6a7d5ee0a8904710c5c34d6598cc17c1739
Narayanan Iyer <nars@yottadb.com>
2018-06-18 07:27:46 -0400

    [#287] Fix compiler warnings while porting YottaDB to MacOS/Darwin (compiler clang/llvm)

--- sr_port/cmi.h
+++ sr_port/cmi.h
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2007 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2007 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -10,7 +13,7 @@
  ****************************************************************/
 
 #ifndef CMI_INCLUDED
-#define CMI__INCLUDED
+#define CMI_INCLUDED
 
 cmi_status_t cmi_read(struct CLB *lnk);
 cmi_status_t cmi_write(struct CLB *lnk);
