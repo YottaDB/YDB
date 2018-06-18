48cd8bc764734b7c2016334fb25cd87550c35519
Christopher Edwards <Christopher.Edwards@krminc.com>
2018-06-18 17:06:05 -0400

    Change case for mumps set command to match rest of routine

--- sr_port/gdeinit.m
+++ sr_port/gdeinit.m
@@ -23,7 +23,7 @@ GDEINIT
 	s ZERO=$c(0),ONE=$c(1),TRUE=ONE,FALSE=ZERO,TAB=$c(9)
 	s endian("x86","SCO")=FALSE,endian("x86","UWIN")=FALSE,endian("x86","Linux")=FALSE,endian("x86","CYGWIN")=FALSE
 	s endian("x86_64","Linux")=FALSE
-	S endian("x86_64","Darwin")=FALSE
+	s endian("x86_64","Darwin")=FALSE
 	s glo("SCO")=384,glo("UWIN")=1024,glo("Linux")=1024,glo("CYGWIN")=1024,glo("Darwin")=1024
 	s endian("SEQUOIA_SERIES_400","VAX")=TRUE,glo("VAX")=1024
 	s endian("HP-PA","HP-UX")=TRUE,glo("HP-UX")=1024
