c934ca1fdd128e5282d84318ca62bb0cf4e6a611
Christopher Edwards <Christopher.Edwards@krminc.com>
2018-07-04 11:42:41 -0400

    Re-add if not __MVS__ to if condition

--- sr_unix_cm/omi_prc_conn.c
+++ sr_unix_cm/omi_prc_conn.c
@@ -27,7 +27,7 @@ static char rcsid[] = "$Header:$";
 
 #include "gtm_string.h"
 #include "gtm_stdio.h"
-#ifndef __APPLE__
+#if !defined(__APPLE__) && !defined(__MVS__)
 #include <crypt.h> /* for crypt(), actually it is in unistd.h */
 #endif
 #include "gtm_unistd.h" /* for crypt() */
