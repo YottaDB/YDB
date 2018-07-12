f0028a52d63d2c6a5f177a1fa501cfb371e77e2b
Steve Johnson <sljohnson1@verizon.net>
2018-07-12 16:03:52 -0400

    [AARCH64] Add code to check for each case (0 through 4 arguments) when nothing needs to be modified on the stack. For 5 arguments the stack doesn't need to be moved, but the argument needs saving.
    Use register x28 instead of sp for saving/moving values on the stack since sp wants everything 16 byte aligned. We've already ensured that sp will end up 16 byte aligned.

--- sr_aarch64/opp_tstart.s
+++ sr_aarch64/opp_tstart.s
@@ -46,30 +46,52 @@ ENTRY opp_tstart
 	mov	x27, x29					/* save entry value of frame pointer */
 	mov	x29, sp
 	CHKSTKALIGN
-	cmp	w2, #5
-	b.le	cont
+	cmp	w2, wzr
+	b.le	no_arg
+	cmp	w2, #1
+	b.eq	one_arg
+	cmp	w2, #2
+	b.eq	two_arg
+	cmp	w2, #3
+	b.eq	three_arg
+	cmp	w2, #4
+	b.eq	four_arg
 	/*
 	 * We have 1 or more local variable names to save/restore so we need some aligned space on the stack for
-	 * parameters that don't fit in the 4 parm registers. No var name parameters can fit in parm registers though
-	 * a one var was initially in a parm reg but is now to be shifted to the stack since this code adds a parm. All
+	 * parameters that don't fit in the 8 parm registers. Four var name parameters can fit in parm registers though
+	 * one var was initially in a parm reg but is now to be shifted to the stack since this code adds a parm. All
 	 * other parms must reside on the stack starting at the lowest address. So for example, if we need 7 slots, we
 	 * must allocate 8 slots to keep the stack aligned but the 7 slots used must be those with the lowest address for
 	 * this to work correctly.
 	 */
 	ADJ_STACK_ALIGN_ODD_ARGS w2
+	mov	x28, sp						/* str xnn, [sp, #-8]! only works for 16 byte aligns, so use x28.
+								 * At the end x28 will be 16 byte aligned, so sp will be OK */
+	cmp	w2, #5
+	b.eq	five_arg					/* Param in x7 doesn't get copied, but is pushed directly */
 	mov	w13, w2						/* Total number of args */
-	sub	w13, w13, #1					/* But one arg is in r3 so it doesn't get copied */
+	sub	w13, w13, #5					/* Five args were in registers so they don't get copied */
 	tst	w13, #1						/* See if count was odd or even */
 	beq	loop						/* Original count was not even so no extra word used to align sp */
 	sub	x27, x27, #8					/* Skip over the word that was used to align sp */
 loop:
 	ldr	x12, [x27, #-8]!
-	sub	sp, sp, #8
-	str	x12, [sp]
+	str	x12, [x28, #-8]!
 	subs	w13, w13, #1
 	b.ne	loop
+five_arg:
+	str	x7, [x28, #-8]!
+	mov	sp, x28						/* Put sp back where it should be */
 	CHKSTKALIGN						/* Verify stack alignment */
-cont:
+four_arg:
+	mov	x7, x6
+three_arg:
+	mov	x6, x5
+two_arg:
+	mov	x5, x4
+one_arg:
+	mov	x4, x3
+no_arg:
 	mov	w3, w2
 	mov	x2, x1
 	mov	x1, x0
