5e3d66533b999822c2726356352ff2c571f250d6
Narayanan Iyer <nars@yottadb.com>
2018-07-18 13:38:14 -0400

    [V63005] Rename new (in GT.M V6.3-005) usages of gtm_white_box_* to ydb_white_box_*

--- sr_port/t_end.c
+++ sr_port/t_end.c
@@ -1492,8 +1492,8 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 			jnl_write_reserve(csa, jrs, non_tp_jfb_ptr->rectype, non_tp_jfb_ptr->record_size, non_tp_jfb_ptr);
 		}
 #ifdef DEBUG
-		if ((WBTEST_ENABLED(WBTEST_MURUNDOWN_KILLCMT06)) && (gtm_white_box_test_case_count == 0))
-			gtm_white_box_test_case_count = 2;
+		if ((WBTEST_ENABLED(WBTEST_MURUNDOWN_KILLCMT06)) && (ydb_white_box_test_case_count == 0))
+			ydb_white_box_test_case_count = 2;
 #endif
 		UPDATE_JRS_RSRV_FREEADDR(csa, jpc, jbp, jrs, jpl, jnl_fence_ctl, replication);	/* updates jbp->rsrv_freeaddr.
 												 * Step CMT06
--- sr_port/tp_tend.c
+++ sr_port/tp_tend.c
@@ -1498,8 +1498,8 @@ boolean_t	tp_tend()
 		jbp = jpc->jnl_buff;
 		assert(JNL_ALLOWED(csa));
 #ifdef DEBUG
-		if ((WBTEST_ENABLED(WBTEST_MURUNDOWN_KILLCMT06)) && (gtm_white_box_test_case_count == 0))
-			gtm_white_box_test_case_count = 2;
+		if ((WBTEST_ENABLED(WBTEST_MURUNDOWN_KILLCMT06)) && (ydb_white_box_test_case_count == 0))
+			ydb_white_box_test_case_count = 2;
 #endif
 		if (JNL_ENABLED(csa))	/* Need "if" check to handle WAS_ON scenario */
 			UPDATE_JRS_RSRV_FREEADDR(csa, jpc, jbp, jrs, jpl, jnl_fence_ctl, replication);/* sets jbp->rsrv_freeaddr.
