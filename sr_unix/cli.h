ef043d8ddeadc143c7349c50a739acb09bf0bb14
Narayanan Iyer <nars@yottadb.com>
2018-07-20 10:00:21 -0400

    [V63005] Fixes needed to get scantypedefs script working
    The scantypedefs script that we have access to expects every character array member in a structure
    to have a size of at least 1 byte. In GT.M V6.3-005, a char array size was changed from 1 to 0 bytes
    and so this is reverted back to 1 byte to let the scantypedefs script work fine without any more changes.

--- sr_unix/cli.h
+++ sr_unix/cli.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -121,7 +124,7 @@ typedef struct
 
 	char    *tp;            /* token pointer */
 	int	buflen;		/* length of in_str */
-	char    in_str[];	/* input string buffer. The real length is computed and added to this block */
+	char    in_str[1];	/* input string buffer. The real length is computed and added to this block */
 } IN_PARMS;
 
 /* include platform independent prototypes */
