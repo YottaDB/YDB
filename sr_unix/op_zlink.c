fcace25815aed174e132dd812d10f82d77abeab7
Narayanan Iyer <nars@yottadb.com>
2018-08-19 10:59:47 -0400

    [StaticScan] sr_unix/op_zlink.c : Address 1-byte buffer overflow in memmove of srcnamebuf[]

--- sr_unix/op_zlink.c
+++ sr_unix/op_zlink.c
@@ -318,18 +318,22 @@ void op_zlink (mval *v, mval *quals)
 		if (srcdir)
 		{	/* A source directory containing routine was found by zro_search() */
 			assert(ZRO_TYPE_OBJLIB != objdir->type);
-			if (srcdir->str.len + srcnamelen > SIZEOF(srcnamebuf) - 1)
-				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(4) ERR_ZLINKFILE, 2, v->str.len, v->str.addr);
 			if (srcdir->str.len)
 			{
-				tslash = ('/' == srcdir->str.addr[srcdir->str.len - 1]) ? 0 : 1;
-				memmove(&srcnamebuf[srcdir->str.len + tslash], srcnamebuf, srcnamelen);
-				if (tslash)
-					srcnamebuf[srcdir->str.len] = '/';
-				memcpy(srcnamebuf, srcdir->str.addr, srcdir->str.len);
-				srcnamelen += srcdir->str.len + tslash;
-				srcnamebuf[srcnamelen] = 0;
-			}
+				tslash = ('/' == srcdir->str.addr[srcdir->str.len - 1]) ? 0 : 1; /* for possible '/' in between */
+				if (srcdir->str.len + srcnamelen + tslash > SIZEOF(srcnamebuf) - 1)
+					rts_error_csa(CSA_ARG(NULL) VARLSTCNT(4) ERR_ZLINKFILE, 2, v->str.len, v->str.addr);
+				else
+				{
+					memmove(&srcnamebuf[srcdir->str.len + tslash], srcnamebuf, srcnamelen);
+					if (tslash)
+						srcnamebuf[srcdir->str.len] = '/';
+					memcpy(srcnamebuf, srcdir->str.addr, srcdir->str.len);
+					srcnamelen += srcdir->str.len + tslash;
+					srcnamebuf[srcnamelen] = 0;
+				}
+			} else
+				assert(srcnamelen <= (SIZEOF(srcnamebuf) - 1));
 		}
 		if (objdir)
 		{	/* An object directory or shared library containing the routine was found by zro_search() */
