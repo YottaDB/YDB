f51896f91a65bc0334990c800bf06383ddcc65ce
Narayanan Iyer <nars@yottadb.com>
2019-02-01 14:45:21 -0500

    [AARCH64] Skip timer-overdue core on 64-bit ARM too as system load has been seen to delay timers in processes by more than 5 minutes

--- sr_unix/gt_timers.c
+++ sr_unix/gt_timers.c
@@ -784,14 +784,14 @@ STATICFNDEF void timer_handler(int why, siginfo_t *info, void *context)
 		cmp = abs_time_comp(&at, (ABS_TIME *)&tpop->expir_time);
 		if (cmp < 0)
 			break;
-#		if defined(DEBUG) && !defined(_AIX) && !defined(__armv6l__) && !defined(__armv7l__)
+#		if defined(DEBUG) && !defined(_AIX) && !defined(__armv6l__) && !defined(__armv7l__) && !defined(__aarch64__)
 		if (tpop->safe && (TREF(continue_proc_cnt) == last_continue_proc_cnt)
 			&& !(ydb_white_box_test_case_enabled
 				&& ((WBTEST_SIGTSTP_IN_JNL_OUTPUT_SP == ydb_white_box_test_case_number)
 					|| (WBTEST_EXPECT_IO_HANG == ydb_white_box_test_case_number))))
 		{	/* Check if the timer is extremely overdue, with the following exceptions:
 			 *	- Unsafe timers can be delayed indefinitely.
-			 *	- AIX and ARM systems tend to arbitrarily delay processes when loaded.
+			 *	- AIX and ARM systems (32-bit and 64-bit) tend to arbitrarily delay processes when loaded.
 			 *	- WBTEST_SIGTSTP_IN_JNL_OUTPUT_SP stops the process from running.
 			 *	- Some other mechanism causes a SIGSTOP/SIGCONT, bumping continue_proc_cnt.
 			 */
