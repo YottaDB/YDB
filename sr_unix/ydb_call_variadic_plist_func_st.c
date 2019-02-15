681dd2d6d3252f96b46bea84abeaf2f210de4d78
Narayanan Iyer <nars@yottadb.com>
2019-02-15 16:58:16 -0500

    [#420] Get go/unit_tests subtest to pass

--- sr_unix/ydb_call_variadic_plist_func_st.c
+++ sr_unix/ydb_call_variadic_plist_func_st.c
@@ -14,6 +14,8 @@
 
 #include "libyottadb_int.h"
 
+GBLREF	boolean_t	caller_func_is_stapi;
+
 /* Routine to drive ydb_call_variadic_plist_func_s() in a worker thread so YottaDB access is isolated. Note because this
  * drives an underlying SimpleAPI call, we don't do any of the exclusive access checks here. The thread management
  * itself takes care of most of that currently but also the check in LIBYOTTADB_INIT*() macro will happen in
@@ -37,7 +39,13 @@ int ydb_call_variadic_plist_func_st(uint64_t tptoken, ydb_buffer_t *errstr, ydb_
 					save_active_stapi_rtn, save_errstr, get_lock, retval);
 	if (YDB_OK == retval)
 	{
+		caller_func_is_stapi = TRUE;	/* used to inform below SimpleAPI call that caller is SimpleThreadAPI */
 		retval = ydb_call_variadic_plist_func_s(cgfunc, cvplist);
+		/* Normally the above would call "ydb_lock_s" (the only use of "ydb_call_variadic_plist_func_st" currently)
+		 * which would clear "caller_func_is_stapi". But in case the caller is not a simpleapi function, it is better
+		 * we restore this global variable back to its original state before entering this function.
+		 */
+		caller_func_is_stapi = FALSE;	/* used to inform below SimpleAPI call that caller is SimpleThreadAPI */
 		THREADED_API_YDB_ENGINE_UNLOCK(tptoken, errstr, save_active_stapi_rtn, save_errstr, get_lock);
 	}
 	return (int)retval;
