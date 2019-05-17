cce35a66b5cc8e7a8731caa1edc8dab54b40c0fe
Narayanan Iyer <nars@yottadb.com>
2019-05-17 12:15:03 -0400

    [#454] If ydb_routines/gtmroutines env var is not defined or set to "", set $zroutines to "$ydb_dist/libyottadbutil.so" if it exists and "$ydb_dist" otherwise
    Note that previously the default value of $zroutines in this case was ".".  That is being changed
    now. Although this is a backwards incompatible change with respect to GT.M, this is not considered
    an issue because we expect most users to already set ydb_routines/gtmroutines env var to the value
    they want as part of their YottaDB/GT.M startup scripts.

--- sr_port/zro_init.c
+++ sr_port/zro_init.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -21,10 +21,18 @@
 #include "io.h"
 #include "zroutines.h"
 #include "ydb_trans_log_name.h"
+#include "gtm_file_stat.h"
 
 error_def(ERR_LOGTOOLONG);
 
 #define MAX_NUMBER_FILENAMES	(256 * MAX_TRANS_NAME_LEN)
+
+/* At entry into this function, "ydb_dist" env var would have been defined (either by the user before YottaDB
+ * process startup OR by "dlopen_libyottadb" through a "setenv" at image startup. Therefore it is okay to use
+ * "$ydb_dist" in the literals below. Those will be expanded by "gtm_file_stat" below.
+ */
+#define	ZROUTINES_DEFAULT1	"$ydb_dist/libyottadbutil.so"
+#define	ZROUTINES_DEFAULT2	"$ydb_dist"
 
 void zro_init(void)
 {
@@ -32,6 +40,8 @@ void zro_init(void)
 	mstr		val, tn;
 	char		buf1[MAX_NUMBER_FILENAMES]; /* buffer to hold translated name */
 	boolean_t	is_ydb_env_match;
+	uint4		ustatus;
+	mstr		def1, def2;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -40,13 +50,24 @@ void zro_init(void)
 	status = ydb_trans_log_name(YDBENVINDX_ROUTINES, &tn, buf1, SIZEOF(buf1), IGNORE_ERRORS_FALSE, NULL);
 	assert((SS_NORMAL == status) || (SS_NOLOGNAM == status));
 	if ((0 == tn.len) || (SS_NOLOGNAM == status))
-	{
+	{	/* "ydb_routines" env var is defined and set to "" OR undefined */
 		tn.len = 1;
 		tn.addr = buf1;
-		buf1[0] = '.';
+		MSTR_CONST(def1, ZROUTINES_DEFAULT1);
+		MSTR_CONST(ext1, "");
+		if (FILE_PRESENT == gtm_file_stat(&def1, &ext1, NULL, FALSE, &ustatus))
+		{	/* "$ydb_dist/libyottadbutil.so" is present. So use it as $zroutines. */
+			tn.len = def1.len;
+			tn.addr = def1.addr;
+		} else
+		{	/* "$ydb_dist/libyottadbutil.so" is NOT present. So use "$ydb_dist" as $zroutines. */
+			MSTR_CONST(def2, ZROUTINES_DEFAULT2);
+			tn.len = def2.len;
+			tn.addr = def2.addr;
+		}
 	}
 	(TREF(dollar_zroutines)).len = tn.len;
 	(TREF(dollar_zroutines)).addr = (char *)malloc (tn.len);
-	memcpy ((TREF(dollar_zroutines)).addr, buf1, tn.len);
+	memcpy((TREF(dollar_zroutines)).addr, tn.addr, tn.len);
 	zro_load(TADR(dollar_zroutines));
 }
