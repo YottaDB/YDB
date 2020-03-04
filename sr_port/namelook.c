6e90e329e35eb23fa972e8785d3ca68a79522070
Steven Estes <steven.estes@yottadb.com>
2020-03-04 17:06:56 -0500

    [DEBUG-ONLY] Missed part of the copyright stuff in namelook.c

--- sr_port/namelook.c
+++ sr_port/namelook.c
@@ -3,6 +3,7 @@
  * Copyright 2001, 2007 Fidelity Information Services, Inc	*
  *								*
  * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
