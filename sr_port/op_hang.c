81f90d26027ec2bf31f7d2068ff1f6a9dc23a4b2
Narayanan Iyer <nars@yottadb.com>
2020-04-07 18:59:32 -0400

    [#567] MUPIP INTRPT in the middle of a HANG in a FOR loop causes fails debug-build-only assert in sr_port/op_hang.c line 154
    * The primary issue was that if the hang timeout coming into `op_hang()` was less than 1 millisecond, we
      would invoke `rel_quant()` to do a yield and return (no actual sleep for that small timeout). But in that
      case we would still check the global `outofband` (which is TRUE if say a MUPIP INTRPT got delivered
      before the `rel_quant()`) and if that is TRUE we will push an entry into the M stack to say the HANG
      has to be redone for the remaining time (which there is none in the `rel_quant()` case) which is 0 msec.
      When op_hang() gets invoked again after the outofband action gets handled, if the M code had a random hang
      time specified, the new hang invocation would come in with a `num` value that is different from the time
      stored in the M stack (from the previous op_hang() invocation). That would fail the following assert.
    
      ```c
      assert(ms == mv_zintcmd->mv_st_cont.mvs_zintcmd.ms);
      ```
    
    * An automated test case for the assert failure will be added to the `r130/ydb567` subtest.
    
    * While the above assert is the only user-visible issue that we encountered, I realize there is a potential
      issue even in the pro/release/non-debug builds where an interrupted HANG 0 can result in an arbitrary hang.
      This is because in the `0 == ms` case, if `outofband` is noticed at the end of `op_hang()` we would note
      down the `end_or_remain` time in the M-stack from the `end_time` local variable. But this variable is
      uninitialized in the `0 == ms` case. So we could end up with an arbitrary hang when the next invocation
      of `op_hang()` retrieves the `end_or_remain` time from the M-stack and uses that to complete the
      interrupted hang. But I was not able to reproduce this failure by trying lots of HANG 0 and interrupting
      it with a MUPIP INTRPT. It requires a much more heavyweight application doing this than a simple test
      case. Am going to record this possibility in the issue release note even though I have not been able to
      reproduce it with a simple test case.

--- sr_port/op_hang.c
+++ sr_port/op_hang.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -91,14 +91,15 @@ error_def(ERR_SYSCALL);
  */
 void op_hang(mval* num)
 {
-	int		ms;
+	int			ms;
 #	ifdef DEBUG
-	int		orig_ms;
+	int			orig_ms;
 #	endif
-	double		tmp;
-	mv_stent	*mv_zintcmd;
-	ABS_TIME	cur_time, end_time;
-	intrpt_state_t	prev_intrpt_state;
+	double			tmp;
+	mv_stent		*mv_zintcmd;
+	ABS_TIME		cur_time, end_time;
+	intrpt_state_t		prev_intrpt_state;
+	mvs_zintcmd_struct	*mvs_zintcmd;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -150,9 +151,15 @@ void op_hang(mval* num)
 		if (!mv_zintcmd)
 			add_int_to_abs_time(&cur_time, ms, &end_time);
 		else
-		{	/* See later comment about "xf_restartpc" on why the below assert is valid */
-			assert(ms == mv_zintcmd->mv_st_cont.mvs_zintcmd.ms);
-			end_time = mv_zintcmd->mv_st_cont.mvs_zintcmd.end_or_remain;
+		{
+			mvs_zintcmd = &mv_zintcmd->mv_st_cont.mvs_zintcmd;
+			/* We are guaranteed that the post-interrupt invocation of "op_hang" (after the jobinterrupt is handled)
+			 * will have the same "num" parameter (and hence the same value of "ms") as the pre-interrupt
+			 * invocation of "op_hang" thanks to the xf_restartpc invocation in OC_HANG in ttt.txt.
+			 * The below assert validates that guarantee.
+			 */
+			assert(ms == mvs_zintcmd->ms);
+			end_time = mvs_zintcmd->end_or_remain;
 			cur_time = sub_abs_time(&end_time, &cur_time);	/* get remaining time to sleep */
 			if (0 <= cur_time.tv_sec)
 				ms = (int4)(cur_time.tv_sec * MILLISECS_IN_SEC +
@@ -161,44 +168,43 @@ void op_hang(mval* num)
 			else
 				ms = 0;		/* all done */
 			/* restore/pop previous zintcmd_active[ZINTCMD_HANG] hints */
-			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last = mv_zintcmd->mv_st_cont.mvs_zintcmd.restart_pc_prior;
-			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last
-				= mv_zintcmd->mv_st_cont.mvs_zintcmd.restart_ctxt_prior;
+			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last = mvs_zintcmd->restart_pc_prior;
+			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last = mvs_zintcmd->restart_ctxt_prior;
 			TAREF1(zintcmd_active, ZINTCMD_HANG).count--;
 			assert(0 <= TAREF1(zintcmd_active, ZINTCMD_HANG).count);
 			if (mv_chain == mv_zintcmd)
 				POP_MV_STENT();	/* just pop if top of stack */
 			else
 			{	/* flag as not active */
-				mv_zintcmd->mv_st_cont.mvs_zintcmd.command = ZINTCMD_NOOP;
-				mv_zintcmd->mv_st_cont.mvs_zintcmd.restart_pc_check = NULL;
+				mvs_zintcmd->command = ZINTCMD_NOOP;
+				mvs_zintcmd->restart_pc_check = NULL;
 			}
 			if (0 == ms)
-				return;		/* done HANGing */
+				return;		/* done with HANG */
 		}
 		hiber_start(ms);
-	} else	/* the rel_quant below seems legitimate */
-		rel_quant();
-	if (outofband)
-	{
-		PUSH_MV_STENT(MVST_ZINTCMD);
-		DEBUG_ONLY(mv_chain->mv_st_cont.mvs_zintcmd.ms = orig_ms);
-		/* Note that "end_time" is uninitialized in case "ms" is 0 but it is okay since when it is restored,
-		 * we are guaranteed that the post-interrupt invocation of "op_hang" (after the jobinterrupt is handled)
-		 * will use the restored "end_time" only if "ms" (which will have the exact same value as the pre-interrupt
-		 * invocation of "op_hang" thanks to the xf_restartpc invocation in OC_HANG in ttt.txt) is not 0.
+		if (outofband)
+		{
+			PUSH_MV_STENT(MVST_ZINTCMD);
+			mvs_zintcmd = &mv_chain->mv_st_cont.mvs_zintcmd;
+			DEBUG_ONLY(mvs_zintcmd->ms = orig_ms);
+			mvs_zintcmd->end_or_remain = end_time;
+			mvs_zintcmd->restart_ctxt_check = frame_pointer->restart_ctxt;
+			mvs_zintcmd->restart_pc_check = frame_pointer->restart_pc;
+			/* save current information from zintcmd_active */
+			mvs_zintcmd->restart_ctxt_prior = TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last;
+			mvs_zintcmd->restart_pc_prior = TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last;
+			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last = frame_pointer->restart_pc;
+			TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last = frame_pointer->restart_ctxt;
+			TAREF1(zintcmd_active, ZINTCMD_HANG).count++;
+			mvs_zintcmd->command = ZINTCMD_HANG;
+			outofband_action(FALSE);
+		}
+	} else
+	{	/* For timeouts less than 1 millisecond, do no sleeps but just yield the current time slice.
+		 * Note: No outofband processing done in this case as we are done with the desired HANG.
 		 */
-		mv_chain->mv_st_cont.mvs_zintcmd.end_or_remain = end_time;
-		mv_chain->mv_st_cont.mvs_zintcmd.restart_ctxt_check = frame_pointer->restart_ctxt;
-		mv_chain->mv_st_cont.mvs_zintcmd.restart_pc_check = frame_pointer->restart_pc;
-		/* save current information from zintcmd_active */
-		mv_chain->mv_st_cont.mvs_zintcmd.restart_ctxt_prior = TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last;
-		mv_chain->mv_st_cont.mvs_zintcmd.restart_pc_prior = TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last;
-		TAREF1(zintcmd_active, ZINTCMD_HANG).restart_pc_last = frame_pointer->restart_pc;
-		TAREF1(zintcmd_active, ZINTCMD_HANG).restart_ctxt_last = frame_pointer->restart_ctxt;
-		TAREF1(zintcmd_active, ZINTCMD_HANG).count++;
-		mv_chain->mv_st_cont.mvs_zintcmd.command = ZINTCMD_HANG;
-		outofband_action(FALSE);
+		rel_quant();
 	}
 	return;
 }
