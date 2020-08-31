YottaDB/YDB

commit 623dc00d96c92bf4a1f799a6adb63421b4caff40

[#594] Fix ydb_incr_s() to buffer lvn value in stringpool as caller buffer is not guaranteed to persist
lvn tree should not have any nodes with values pointing to SimpleAPI caller buffers as they are not
guaranteed to persist. If those buffers are later freed, the lvn tree will be pointing to garbage
memory resulting in SIG-11 or incorrect results.

We noticed the SIG-11 in the `go/randomWalk` subtest (very rare as it happened once in hundreds of
test runs).  The incorrect result part is easily reproducible with a `ydb_dbglvl` env var of `0x40`
and is exercised by the `r130/ydb594` subtest.

sr_unix/ydb_incr_s.c
@@ -125,6 +125,9 @@ int ydb_incr_s(const ydb_buffer_t *varname, int subs_used, const ydb_buffer_t *s
 			if (!MV_DEFINED(lv_mv))
 				*lv_mv = literal_zero;
 			op_add(lv_mv, increment_mv, lv_mv);
+			s2pool(&(lv_mv->str));	/* Rebuffer in stringpool to avoid lv tree pointing to caller buffer
+						 * that is not guaranteed to stay after this call.
+						 */
 			ret_mval = *lv_mv;
 			ret_mv = &ret_mval;
 			break;

sr_unix/ydb_set_s.c
@@ -113,7 +113,6 @@ int ydb_set_s(const ydb_buffer_t *varname, int subs_used, const ydb_buffer_t *su
 			}
 			SET_MVAL_FROM_YDB_BUFF_T(&dst_lv->v, value);	/* Set value into located/created node */
 			s2pool(&(dst_lv->v.str));			/* Rebuffer in stringpool for protection */
-			RECORD_MSTR_FOR_GC(&(dst_lv->v.str));
 			break;
 		case LYDB_VARREF_GLOBAL:
 			/* Set the given global variable with the given value. We do this by:

commit 7fce0514c55809d45a4651e542036311c09b62b5

[YDB#633] [YottaDB/DBMS/YDBOcto#587] Fix regression introduced in 623dc00d (as part of fixing #594)
* This commit fixes the YDB issue described at https://gitlab.com/YottaDB/DBMS/YDBOcto/-/issues/587.

* The relevant text from there is pasted below.

  1. ydb_incr_s.c was modified as part of YottaDB/DB/YDB@623dc00d (while fixing #594) to do a `s2pool()`
     invocation on the return value of op_add(). While this is the right thing to do in case the mval
     returned by op_add() has the MV_STR bit set, it is not the right thing to do in case the mval does
     not have the MV_STR bit set. Invoking s2pool() on an mval that represents a pure number (MV_INT or
     MV_NM bits are set but MV_STR bit is not set in the mvtype member) implies we could be looking at
     garbage values in str.len and str.addr and trying to move that to the stringpool which
     could cause a SIG-11.

* An automated test case for this is taken care of by the `test_createtable/TC031` bats subtest in the
  YDBOcto repo.

sr_unix/ydb_incr_s.c
@@ -125,9 +125,10 @@ int ydb_incr_s(const ydb_buffer_t *varname, int subs_used, const ydb_buffer_t *s
 			if (!MV_DEFINED(lv_mv))
 				*lv_mv = literal_zero;
 			op_add(lv_mv, increment_mv, lv_mv);
-			s2pool(&(lv_mv->str));	/* Rebuffer in stringpool to avoid lv tree pointing to caller buffer
-						 * that is not guaranteed to stay after this call.
-						 */
+			if (MV_IS_STRING(lv_mv))
+				s2pool(&(lv_mv->str));	/* Rebuffer in stringpool to avoid lv tree pointing to caller buffer
+							 * that is not guaranteed to stay after this call.
+							 */
 			ret_mval = *lv_mv;
 			ret_mv = &ret_mval;
 			break;