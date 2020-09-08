f1b0ac2d35b9d16ccfe62fe69ac04927edf3ed5b
Joshua Nelson <joshua@yottadb.com>
2020-09-08 15:54:27 -0400

    [#615] Add clang-tidy to pipeline
    - Install clang-tidy in the pipeline
    - Only look in source directories for x86_64 Linux
    
      This avoids looking in generated `build` files, as well as spurious
      errors for code meant for other architectures.
    
    - Only run clang-tidy on modified files
      + Don't give an error if no .c files were modified
    - Don't give a warning for using `strcpy`
    
      By default, `clang-tidy` enables
      `clang-analyzer-security.insecureAPI`, which is an unhelpful warning
      that tells you to use platform-specific APIs instead of `strcpy`.
      Example output:
    
      ```
      /home/joshua/src/yottadb/YDB/sr_port/dbcertify_scan_phase.c:693:3: warning: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119 [clang-analyzer-security.insecureAPI.strcpy]
                      strcpy((char_ptr_t)intgerrmsg, emsg);
                      ^
      /home/joshua/src/yottadb/YDB/sr_port/dbcertify_scan_phase.c:693:3: note: Call to function 'strcpy' is insecure as it does not provide bounding of the memory buffer. Replace unbounded copy functions with analogous functions that support length arguments such as 'strlcpy'. CWE-119
      ```
    
    - Generate .ref file
    
      This file can be regenerated with the following commands:
    
      ```
      mkdir build
      cd build
      cmake -D CMAKE_C_COMPILER=clang-10 -D CMAKE_BUILD_TYPE=$build_type -D CMAKE_EXPORT_COMPILE_COMMANDS=ON ..
      make
      find ../sr_{linux,unix,port,x86_64} -name '*.c' \
        | xargs --no-run-if-empty parallel clang-tidy-10 \
            '--checks=-clang-analyzer-security.insecureAPI.*' -- \
            >all_tidy_warnings.txt
      ../ci/sort-warnings.sh all_tidy_warnings.txt
      mv sorted_warnings.txt ../ci/tidy_warnings.ref
      ```
    
      Running clang-tidy will take about 5-20 minutes. Using `parallel` is
      optional but greatly speeds up the runtime.
    
    - Use `parallel` in the CI pipeline to speed up run times

--- ci/build.sh
+++ ci/build.sh
@@ -70,28 +70,53 @@ fi
 compare() {
 	expected="$1"
 	actual="$2"
-	# We do not want any failures in "diff" command below to exit the script (we want to see the actual diff a few steps later).
-	# So never count this step as failing even if the output does not match.
-	diff "$expected" sorted_warnings.txt &> differences.txt || true
+	warnings="$3"
+	if [ -e "$expected" ]; then
+		# We do not want any failures in "diff" command below to exit the script (we want to see the actual diff a few steps later).
+		# So never count this step as failing even if the output does not match.
+		diff "$expected" sorted_warnings.txt &> differences.txt || true
 
-	if [ $(wc -l differences.txt | awk '{print $1}') -gt 0 ]; then
-		echo " -> Expected warnings differ from actual warnings! diff output follows"
-		echo " -> note: '<' indicates an expected warning, '>' indicates an actual warning"
-		cat differences.txt
-		echo
-		echo " -> note: these were the original warnings:"
-		grep -Ev '^(Database file |%YDB-I-DBFILECREATED, |%GDE-I-|YDB-MUMPS\[)' make_warnings.txt \
-			| grep -v '\.gld$'
-		echo
-		echo " -> note: the expected warnings are available in $expected"
-		exit 1
+		if [ $(wc -l differences.txt | awk '{print $1}') -gt 0 ]; then
+			echo " -> Expected warnings differ from actual warnings! diff output follows"
+			echo " -> note: '<' indicates an expected warning, '>' indicates an actual warning"
+			cat differences.txt
+			echo
+			echo " -> note: these were the original warnings:"
+			grep -Ev '^(Database file |%YDB-I-DBFILECREATED, |%GDE-I-|YDB-MUMPS\[)' $warnings \
+				| grep -v '\.gld$'
+			echo
+			echo " -> note: the expected warnings are available in $expected"
+			exit 1
+		fi
+	else
+		# Make sure that there are actually no warnings, not just that someone forgot to add a .ref file
+		[ $(wc -c < "$actual") = 0 ]
 	fi
 }
-if [ -e ../ci/warnings.ref ]; then
-	compare ../ci/warnings.ref sorted_warnings.txt
-else
-	# Make sure that there are actually no warnings, not just that someone forgot to add a .ref file
-	[ $(wc -c < sorted_warnings.txt) = 0 ]
-fi
+compare ../ci/warnings.ref sorted_warnings.txt make_warnings.txt
 
-# TODO(#615): add clang-tidy warnings in a follow-up MR
+# Run clang-tidy, but only on C files that have been modified.
+# This avoids ballooning the pipeline times to nearly an hour.
+# NOTE: this does *not* run on header files, because YDB style is not to
+# include headers in other headers when they were included in the original .c
+# file. So trying to run `clang-tidy` on them gives lots of errors about
+# unknown typedefs.
+# NOTE: adds `../` to start of files, since git shows them relative to the top-level directory.
+git diff --name-only HEAD~ \
+	| { grep -E 'sr_(linux|unix|port|x86_64)/.*\.c$' || true; } \
+	| sed 's#^#../#' \
+	>modified_files.txt
+
+# NOTE: doesn't run `insecureAPI` because it has many thousands of false positives
+# In particular, it warns any time you use strcpy.
+xargs < modified_files.txt --no-run-if-empty \
+	parallel clang-tidy-10 '--checks=-clang-analyzer-security.insecureAPI.*' -- \
+	>tidy_warnings.txt
+
+../ci/sort_warnings.sh tidy_warnings.txt
+# Strip `../<dir>/` from file names
+sed 's#^\.\./[^/]*/##' modified_files.txt > modified_files_stripped.txt
+# Only compare warnings from modified files
+# Don't exit with an error if there were no files modified.
+grep --fixed-strings --file modified_files_stripped.txt ../ci/tidy_warnings.ref > expected_warnings.ref || true
+compare expected_warnings.ref sorted_warnings.txt tidy_warnings.txt
