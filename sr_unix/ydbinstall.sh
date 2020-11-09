0ca6dab4ed1e896e5c0e1c5a834b19934fdb6978
Brad Westhafer <brad@yottadb.com>
2020-11-09 14:52:04 -0500

    [#654] Fix ydbinstall failures on AArch64 machines running Ubuntu 20.04 due to "missing" libtinfo.so.5
    On Ubuntu 20.04, ydbinstall currently requires libtinfo.so.5 to successfully install YottaDB. However, the script always looks for it at the path where it will be installed on x86_64 (/lib/x86_64-linux-gnu/libtinfo.so.5) even on AArch64 machines where it is installed at a different location (/lib/aarch64-linux-gnu/libtinfo.so.5). This commit fixes this by looking for the correct location for the architecture on which ydbinstall is being run.
    
    As only Raspbian and Debian are currently supported by YottaDB on ARMv6l and ARMv7l, this commit does not address the case where a user attempts to install YottaDB on Ubuntu 20.04 on those architectures. This change likely eliminates the check for libtinfo.so.5 on those platforms altogether but this has not been tested.

--- sr_unix/ydbinstall.sh
+++ sr_unix/ydbinstall.sh
@@ -461,7 +461,7 @@ if [ "N" = "$ydb_force_install" ]; then
 
 	# Use the OS variables just defined to determine if we are running on Ubuntu 18.10 or greater and make sure libtinfo5 is installed
 	if [ "ubuntu" = "${osid}" -a 1 = `expr "$osmajorver" ">" "18"` ] || [ 1 = `expr "$osmajorver" "=" "18"` -a 1 = `expr "$osminorver" ">=" "10"` ] ; then
-		if [ ! -f /lib/x86_64-linux-gnu/libtinfo.so.5 ] ; then
+		if [ "x8664" = "${ydb_flavor}" -a ! -f /lib/x86_64-linux-gnu/libtinfo.so.5 ] || [ "aarch64" = "${ydb_flavor}" -a ! -f /lib/aarch64-linux-gnu/libtinfo.so.5 ] ; then
 			echo "libtinfo5 package is required to be installed on Ubuntu 18.10 or greater using 'sudo apt-get install --no-install-recommends libtinfo5'"
 			err_exit
 		fi
