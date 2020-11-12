YottaDB/YDB

commit 6f623cc7cdee7577aad838009b7f4720c299bba9

[V63008] Merge GT.M V6.3-008 into YottaDB mainline (with conflicts)

sr_port/repl_comm.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2001-2018 Fidelity National Information	*
+ * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
  * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
@@ -176,6 +176,7 @@ int repl_send(int sock_fd, unsigned char *buff, int *send_len, int timeout GTMTL
 {
 	int		send_size, status, io_ready, save_errno, EMSGSIZE_cnt = 0, EWOULDBLOCK_cnt = 0;
   	ssize_t		bytes_sent;
+	char const	*errptr;
 
 	if (!repl_send_trace_buff)
 		repl_send_trace_buff = malloc(REPL_SEND_TRACE_BUFF_SIZE);
@@ -242,7 +243,13 @@ int repl_send(int sock_fd, unsigned char *buff, int *send_len, int timeout GTMTL
 				 * Set error status to ERR_TLSIOERROR and let caller handle it appropriately.
 				 */
 				assert(repl_tls.enabled);
+<<<<<<< HEAD
 				save_errno = ERR_TLSIOERROR;
+=======
+				errptr = gtm_tls_get_error();
+				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(8) ERR_TLSIOERROR, 2, LEN_AND_LIT("send"), ERR_TEXT, 2,
+						LEN_AND_STR(errptr));
+>>>>>>> a6cd7b01f... GT.M V6.3-008
 			}
 #			else
 			save_errno = ERRNO;
@@ -288,6 +295,7 @@ int repl_recv(int sock_fd, unsigned char *buff, int *recv_len, int timeout GTMTL
 {
 	int		status, max_recv_len, io_ready, save_errno;
 	ssize_t		bytes_recvd;
+	char const	*errptr;
 
 	if (!repl_recv_trace_buff)
 		repl_recv_trace_buff = malloc(REPL_RECV_TRACE_BUFF_SIZE);
@@ -351,8 +359,14 @@ int repl_recv(int sock_fd, unsigned char *buff, int *recv_len, int timeout GTMTL
 				 * Set error status to ERR_TLSIOERROR and let caller handle it appropriately.
 				 */
 				assert(repl_tls.enabled);
+<<<<<<< HEAD
 				save_errno = ERR_TLSIOERROR;
 				bytes_recvd = -1;	/* to ensure "save_errno" does not get overwritten a few lines later */
+=======
+				errptr = gtm_tls_get_error();
+				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(8) ERR_TLSIOERROR, 2, LEN_AND_LIT("recv"), ERR_TEXT, 2,
+						LEN_AND_STR(errptr));
+>>>>>>> a6cd7b01f... GT.M V6.3-008
 			}
 #			else
 			save_errno = ERRNO;

sr_unix/gtm_tls_impl.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2013-2018 Fidelity National Information	*
+ * Copyright (c) 2013-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
  * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
@@ -184,6 +184,11 @@ STATICDEF struct gtm_ssl_options gtm_ssl_options_list[] =
 #ifdef	SSL_OP_NO_TLSv1_2
 	DEFINE_SSL_OP(SSL_OP_NO_TLSv1_2),
 #endif
+/*	Special case for NO_TLSv1_3 so defined for pre OpenSSL 1.1.1 */
+#ifndef	SSL_OP_NO_TLSv1_3
+#	define SSL_OP_NO_TLSv1_3	0x0
+#endif
+	DEFINE_SSL_OP(SSL_OP_NO_TLSv1_3),
 #ifdef	SSL_OP_NO_TLSv1_1
 	DEFINE_SSL_OP(SSL_OP_NO_TLSv1_1),
 #endif
@@ -539,6 +544,8 @@ gtm_tls_ctx_t *gtm_tls_init(int version, int flags)
 		return NULL;
 	}
 	SSL_CTX_set_options(ctx, DEPRECATED_SSLTLS_PROTOCOLS);
+	if (0 != SSL_OP_NO_TLSv1_3)
+		SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_3);	/* until changes made to handle TLSv1.3 ciphers */
 	/* Read the configuration file for more configuration parameters. */
 	cfg = &gtm_tls_cfg;
 	config_init(cfg);
@@ -1717,7 +1724,40 @@ int gtm_tls_get_conn_info(gtm_tls_socket_t *socket, gtm_tls_conn_info *conn_info
 		if ((X509_V_OK == verify_result) || (GTMTLS_OP_ABSENT_CONFIG & tls_ctx->flags))
 		{	/* return information for Socket clients even without a config file */
 			/* SSL-Session Protocol */
+<<<<<<< HEAD
 			SNPRINTF(conn_info->protocol, SIZEOF(conn_info->protocol), "%s", SSL_get_version(ssl));
+=======
+			switch (ssl_version = SSL_version(ssl))
+			{
+				case SSL2_VERSION:
+					ssl_version_ptr = "SSLv2";
+					break;
+
+				case SSL3_VERSION:
+					ssl_version_ptr = "SSLv3";
+					break;
+
+				case TLS1_VERSION:
+					ssl_version_ptr = "TLSv1";
+					break;
+				case TLS1_1_VERSION:
+					ssl_version_ptr = "TLSv1.1";
+					break;
+				case TLS1_2_VERSION:
+					ssl_version_ptr = "TLSv1.2";
+					break;
+				case TLS1_3_VERSION:
+					ssl_version_ptr = "TLSv1.3";
+					break;
+				default:
+					ssl_version_ptr = NULL;
+					SNPRINTF(conn_info->protocol, SIZEOF(conn_info->protocol), "unknown protocol 0x%X",
+							ssl_version);
+					break;
+			}
+			if (NULL != ssl_version_ptr)
+				strncpy(conn_info->protocol, ssl_version_ptr, MAX_ALGORITHM_LEN);
+>>>>>>> a6cd7b01f... GT.M V6.3-008
 			/* SSL-Session Cipher Algorithm */
 			cipher = SSL_get_current_cipher(ssl);
 			SNPRINTF(conn_info->session_algo, SIZEOF(conn_info->session_algo), "%s", SSL_CIPHER_get_name(cipher));

commit 7e3b0b13a9833aa380100e904fa3981ae50c7757

[#657] Re-enable support for TLS 1.3 in reference TLS implementation
Background
-----------
* Support for TLS 1.3 was enabled as part of #376 and made available in YottaDB %r1.24.

* But in YottaDB %r1.30, as part of integrating GT.M V6.3-008 (which specifically disabled TLS 1.3 support
  due to http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V6.3-008_Release_Notes.html#GTM-9084), this
  support got unintentionally disabled. This meant that TLS 1.2 ended up getting used in %r1.30 even though
  the underlying OpenSSL version was 1.1.1 or higher (and hence supported TLS 1.3).

Fixes
------
There were 2 regressions.

1. `sr_unix/gtm_tls_impl.c` : The first regression was introduced in 6f623cc7cdee7577aad838009b7f4720c299bba9.
   The `SSL_OP_NO_TLSv1_3` option was enabled by default in GT.M V6.3-008 due to the `GTM-9084` changes. This
   was done because GT.M does not support TLS 1.3. But YottaDB does support TLS 1.3 (introduced in commit
   44931460201d15fe573bea3cf7e16ceaf620f803), and so this GT.M change should have be discarded.

   This change in GT.M V6.3-008 did not show up as a conflict while integrating it into the YottaDB code base
   and is most likely why it got missed out.

   The fix is to remove the `SSL_OP_NO_TLSv1_3` option from being enabled by default. To avoid similar issues
   from happening when a future GT.M version gets integrated into YottaDB, the section of code that enables
   this option is not removed but commented out inside a `#ifdef GTM_ONLY` block. This way it will show up
   as a conflict if ever this block of code gets touched again in a future GT.M version.

2. `sr_port/repl_comm.c` : As part of a prior commit (see 55a93a200c53a05933ead98c64bd9bbfc43ecced commit message
   for details), the `repl_recv()` and `repl_send()` functions were enhanced to return `ERR_TLSIOERROR`
   instead of issuing an error in case of an expired certificate (due to TLS 1.3 initial handshake

sr_port/repl_comm.c
@@ -185,7 +185,6 @@ int repl_send(int sock_fd, unsigned char *buff, int *send_len, int timeout GTMTL
 {
 	int		send_size, status, io_ready, save_errno, EMSGSIZE_cnt = 0, EWOULDBLOCK_cnt = 0;
   	ssize_t		bytes_sent;
-	char const	*errptr;
 
 	if (!repl_send_trace_buff)
 		repl_send_trace_buff = malloc(REPL_SEND_TRACE_BUFF_SIZE);
@@ -255,9 +254,7 @@ int repl_send(int sock_fd, unsigned char *buff, int *send_len, int timeout GTMTL
 				 */
 				assert(repl_tls.enabled);
 				HANDLE_EINTR_OUTSIDE_SYSTEM_CALL;
-				errptr = gtm_tls_get_error();
-				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(8) ERR_TLSIOERROR, 2, LEN_AND_LIT("send"), ERR_TEXT, 2,
-						LEN_AND_STR(errptr));
+				save_errno = ERR_TLSIOERROR;
 			}
 #			else
 			save_errno = ERRNO;
@@ -307,7 +304,6 @@ int repl_recv(int sock_fd, unsigned char *buff, int *recv_len, int timeout GTMTL
 {
 	int		status, max_recv_len, io_ready, save_errno;
 	ssize_t		bytes_recvd;
-	char const	*errptr;
 
 	if (!repl_recv_trace_buff)
 		repl_recv_trace_buff = malloc(REPL_RECV_TRACE_BUFF_SIZE);
@@ -374,9 +370,8 @@ int repl_recv(int sock_fd, unsigned char *buff, int *recv_len, int timeout GTMTL
 				 */
 				assert(repl_tls.enabled);
 				HANDLE_EINTR_OUTSIDE_SYSTEM_CALL;
-				errptr = gtm_tls_get_error();
-				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(8) ERR_TLSIOERROR, 2, LEN_AND_LIT("recv"), ERR_TEXT, 2,
-						LEN_AND_STR(errptr));
+				save_errno = ERR_TLSIOERROR;
+				bytes_recvd = -1;	/* to ensure "save_errno" does not get overwritten a few lines later */
 			}
 #			else
 			save_errno = ERRNO;

sr_unix/gtm_tls_impl.c
@@ -543,8 +543,16 @@ gtm_tls_ctx_t *gtm_tls_init(int version, int flags)
 		return NULL;
 	}
 	SSL_CTX_set_options(ctx, DEPRECATED_SSLTLS_PROTOCOLS);
+#	ifdef GTM_ONLY
+	/* Note: The below code is inherited from GT.M V6.3-008
+	 * (http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V6.3-008_Release_Notes.html#GTM-9084).
+	 * And sets the connection to not use TLS 1.3 by default. This is because GT.M does not yet
+	 * support TLS 1.3. But YottaDB does support it (fixed as part of YDB#376). Therefore
+	 * keep the below code commented out.
+	 */
 	if (0 != SSL_OP_NO_TLSv1_3)
 		SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_3);	/* until changes made to handle TLSv1.3 ciphers */
+#	endif
 	/* Read the configuration file for more configuration parameters. */
 	cfg = &gtm_tls_cfg;
 	config_init(cfg);