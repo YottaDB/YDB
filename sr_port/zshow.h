23752bd3e413cab8d2f68f91e447bdc31a4a9e84
Sam Habiel <sam@yottadb.com>
2020-12-24 09:24:02 -0500

    [#558] Different behavior between ZSHOW "*":array and ZSHOW "V":array
    The core is of the issue is that when doing zshow "*":lvn, the output of
    zshow "I" is included in the output of the zshow "V". Here's an example:
    
    ```
    YDB>kill  zshow "*":array zwrite array
    array("D",1)="/dev/pts/5 OPEN TERMINAL NOPAST NOESCA NOREADS TYPE
    WIDTH=211 LENG=53 "
    array("G",0)="GLD:*,REG:*,SET:0,KIL:0,GET:0,DTA:0,ORD:0,ZPR:0,QRY:0,LKS:0,LKF:0,CTN:0,DRD:0,DWT:0,NTW:0,NTR:0,NBW:0,NBR:0,NR0:0,NR1:0,NR2:0,NR3:0,TTW:0,TTR:0,TRB:0,TBW:0,TBR:0,TR0:0,TR1:0,TR2:0,TR3:0,TR4:0,TC0:0
    ,TC1:0,TC2:0,TC3:0,TC4:0,ZTR:0,DFL:0,DFS:0,JFL:0,JFS:0,JBB:0,JFB:0,JFW:0,JRL:0,JRP:0,JRE:0,JRI:0,JRO:0,JEX:0,DEX:0,CAT:0,CFE:0,CFS:0,CFT:0,CQS:0,CQT:0,CYS:0,CYT:0,BTD:0"
    array("I",1)="$DEVICE="""""
    array("I",2)="$ECODE="""""
    ...
    array("V",1)="array(""I"",1)=""$DEVICE="""""""""""
    array("V",2)="array(""I"",2)=""$ECODE="""""""""""
    array("V",3)="array(""I"",3)=""$ESTACK=0"""
    array("V",4)="array(""I"",4)=""$ETRAP=""""B"""""""
    array("V",5)="array(""I"",5)=""$HOROLOG=""""65737,30562"""""""
    ```
    
    Array was empty prior to running this command, and array("I") should not
    show up in array("V") as a list of variables defined in the symbol
    table.
    
    The fix is to change the order of operartions for zshow "*": do "V"
    first, then all the other stuff. This is stored in the macro ZSHOW_ALL
    in zshow.h.
    
    As discussed in the issue's comments, there will be a small
    incompatiblity with GT.M: if 'zshow "*"' is ran by itself, it outputs to
    the current $IO in the order of the macro ZSHOW_ALL. GT.M will show it
    in the previous order IVBDLGRC, vs now here in YottaDB it will show in
    the order of VIBDLGRC.

--- sr_port/zshow.h
+++ sr_port/zshow.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -20,9 +23,9 @@
 #define ZSHOW_NOPARM		-1
 
 #ifdef UNIX
-# define ZSHOW_ALL		"IVBDLGRC"
+# define ZSHOW_ALL		"VIBDLGRC"
 #else
-# define ZSHOW_ALL		"IVBDLGSC"
+# define ZSHOW_ALL		"VIBDLGSC"
 #endif
 
 #define CLEANUP_ZSHOW_BUFF				\
