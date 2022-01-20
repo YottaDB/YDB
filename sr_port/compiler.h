YottaDB/YDB

commit 7a1987cd1eaf0fccdeb682ef060ad92deb6b6829

[YDB#10] [NARS] [estess] Implement $query(glvn,-1) aka reverse $query; fixes #10 (#24)
* [YDB#10] [NARS] [estess] Implement $query(glvn,-1) aka reverse $query; Fix $query(gvn) to avoid infinite loops with null subscripts & GT.M Null Collation

Release Note
-------------
$query(glvn,-1) returns the previous subscripted local or global variable node name, independent of level, which
precedes the node specified by its argument in M collating sequence and has a data value. That is, if $query(glvn1)
returns glvn2, then $query(glvn2,-1) should return glvn1. (YDB#10)

$query(gvn) works correctly in case the global corresponding to gvn has nodes with null subscripts and the database
has Standard Null Collation set to FALSE (i.e. GT.M Null Collation is in effect). Previously, this would result in
infinite loops. (YDB#10)

Messages & Recovery Manual
---------------------------
A QUERY2 error is issued when $query(gvn,dir) is invoked with dir not evaluating to a -1 or 1 (the only permissible
values).

Test
-----
* New r100/reversequery subtest to exercise $query(gvn,-1) and $query(lvn,-1).
* Enhancements to existing gtcm_gnp/basic subtest to test $query(gvn,-1) across GTCM GNP server.
* E_ALL run many times to ensure no regressions.

README
-------
$QUERY, which was until now a 1-operand function, becomes a 2-operand function due to the addition of the
direction parameter. This meant a lot of changes in the compiler (particularly f_query.c). Added complications
were the need to handle side-effects as well as the naked indicator with the additional direction parameter.
A lot of this implementation was based on $ORDER which also allowed for an optional 2nd direction parameter.

New opcodes were added to ttt.txt and ttt.c was therefore regenerated.

Since new opcodes were added, the object file format was bumped in objlabel.h.

While implementing $query(gvn,-1), it was not clear what purpose there was to a block of code in gvcst_query.c

sr_port/compiler.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -148,8 +151,10 @@ typedef struct	tripletype
 				jmplist;	/* triples which jump to this one */
 	source_address		src;
 	int			rtaddr;		/* relative run time address of triple */
-	oprtype			operand[2],
-				destination;
+	oprtype			operand[2],	/* Note: operand[0] corresponds to val.1 in ttt.txt
+						 *	 operand[1] corresponds to val.2 in ttt.txt
+						 */
+				destination;	/* Note: corresponds to val.0 in ttt.txt */
 } triple;
 
 typedef struct
@@ -684,7 +689,8 @@ int		f_order1(oprtype *a, opctype op);
 int		f_piece(oprtype *a, opctype op);
 int		f_qlength(oprtype *a, opctype op);
 int		f_qsubscript(oprtype *a, opctype op);
-int		f_query (oprtype *a, opctype op);
+int		f_query(oprtype *a, opctype op);
+int		f_reversequery1(oprtype *a, opctype op);
 int		f_reverse(oprtype *a, opctype op);
 int		f_select(oprtype *a, opctype op);
 int		f_stack(oprtype *a, opctype op);

sr_port/indir.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -14,8 +17,15 @@
  * Note: ***	Please add new entries to end of list so as not to cause execution problems for
  *		compilations from previous versions. Yes, they should have recompiled but we can
  *		avoid exploding by simply adding entries to end of list. ***
- * This comment and the preceeding empty lines put the first item at line 20, so adding 20 to an argcode
- * places you on the line with its information or subtracting 20 from a line gives the corresponding argcode
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * This comment and the preceeding empty lines put the first item at line 30, so adding 30 to an argcode
+ * places you on the line with its information or subtracting 30 from a line gives the corresponding argcode
  */
 INDIR(indir_fndata,		f_data,		OC_FNDATA)
 ,INDIR(indir_fnnext,		f_next,		OC_FNNEXT)
@@ -65,7 +75,7 @@ INDIR(indir_fndata,		f_data,		OC_FNDATA)
 ,INDIR(indir_zwithdraw,		m_zwithdraw,	0)
 ,INDIR(indir_tstart,		m_tstart,	0)
 ,INDIR(indir_fnname,		f_name,		0)		/* f_name is really a dummy */
-,INDIR(indir_fnorder2,		f_order,	0)
+,INDIR(indir_fnorder2,		f_order,	0)	/* Is actually unused but don't want to delete per comment at top of file */
 ,INDIR(indir_fnzqgblmod,	f_zqgblmod,	OC_FNZQGBLMOD)
 ,INDIR(indir_trollback,		m_trollback,	0)
 ,INDIR(indir_devparms,		indirection,	0)
@@ -87,3 +97,4 @@ INDIR(indir_fndata,		f_data,		OC_FNDATA)
 #ifdef AUTORELINK_SUPPORTED
 ,INDIR(indir_zrupdate,		m_zrupdate,	0)
 #endif
+,INDIR(indir_fnreversequery1,	f_reversequery1,OC_FNREVERSEQUERY)

sr_port/cmmdef.h
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2009 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2009 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -91,11 +94,14 @@
 #define CMMS_C_BUFFLUSH		52	/* [0x34] */
 #define CMMS_Q_INCREMENT	53	/* [0x35] */	/* Opcode for message type sent from   client (to   server) */
 #define CMMS_R_INCREMENT	54	/* [0x36] */	/* Opcode for message type received by client (from server) */
+#define CMMS_Q_REVERSEQUERY	55	/* [0x37] */
+#define CMMS_R_REVERSEQUERY	56	/* [0x38] */
 
-#define	CMM_QUERYGET_MIN_LEVEL	"200"	/* $query works as queryget only from version "V200" onwards */
-#define	CMM_INCREMENT_MIN_LEVEL	"210"	/* $INCREMENT works only from version "V210" onwards */
+#define	CMM_QUERYGET_MIN_LEVEL		"200"	/* $query works as queryget only from version "V200" onwards */
+#define	CMM_INCREMENT_MIN_LEVEL		"210"	/* $INCREMENT works only from version "V210" onwards */
 #define	CMM_STDNULLCOLL_MIN_LEVEL	"210"	/* Standard null collation works only from version "V210" onwards */
-#define	CMM_LONGNAMES_MIN_LEVEL	"210"	/* long name works only from protocol "V210" onwards */
+#define	CMM_LONGNAMES_MIN_LEVEL		"210"	/* long name works only from protocol "V210" onwards */
+#define	CMM_REVERSEQUERY_MIN_LEVEL	"220"	/* Reverse $query works only from protocol "V220" onwards */
 
 typedef struct cm_region_list_struct
 	{
@@ -187,6 +193,7 @@ typedef struct link_info_struct
 		boolean_t			server_supports_dollar_incr;	/* decided based on server protocol levels */
 		boolean_t			server_supports_std_null_coll;	/* decided based on server protocol levels */
 		boolean_t			server_supports_long_names;	/* decided based on server protocol levels */
+		boolean_t			server_supports_reverse_query;	/* decided based on server protocol levels */
 	} link_info;
 
 typedef struct

sr_port/create_temporaries.c
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2012 Fidelity Information Services, Inc	*
+ * Copyright 2012 Fidelity Information Services, Inc		*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -54,6 +57,9 @@
  *	subscript is not also an individual target within the same compound SET. Even in that case, this function
  *	will be called to store the subscript in temporaries (as we dont know at compile time if a particular
  *	subscript is also used as a target within a compound SET).
+ * Note: The above comment was written when the only purpose of this function was to address the COMPOUND SET case.
+ *	Later, other needs for this function arose (e.g. f_order etc.) and many callers started using this without
+ *	accordingly revising this comment. That is my understanding based on the current state of the code. 2017/08 --- nars
  */
 void create_temporaries(triple *sub, opctype put_oc)
 {
@@ -67,7 +73,15 @@ void create_temporaries(triple *sub, opctype put_oc)
 	sb1 = &sub->operand[1];
 	if ((OC_GVNAME == put_oc) || (OC_PUTINDX == put_oc) || (OC_SRCHINDX == put_oc))
 	{
-		sub = sb1->oprval.tref;		/* global name */
+		sub = sb1->oprval.tref;		/* global or local name */
+		assert(OC_PARAMETER == sub->opcode);
+		sb1 = &sub->operand[1];
+	} else if (OC_FNQUERY == put_oc)
+	{
+		sub = sb1->oprval.tref;		/* OC_LIT : # of subscripts + 2 */
+		assert(OC_PARAMETER == sub->opcode);
+		sb1 = &sub->operand[1];
+		sub = sb1->oprval.tref;		/* OC_VAR : unsubscripted local variable name */
 		assert(OC_PARAMETER == sub->opcode);
 		sb1 = &sub->operand[1];
 	} else if (OC_GVEXTNAM == put_oc)

sr_port/err_check.c
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2012 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2012 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -12,20 +15,15 @@
 #include "mdef.h"
 #include "error.h"
 
-VMS_ONLY(LITREF)UNIX_ONLY(GBLREF) err_ctl merrors_ctl;
-VMS_ONLY(LITREF)UNIX_ONLY(GBLREF) err_ctl cmerrors_ctl;
-VMS_ONLY(LITREF)UNIX_ONLY(GBLREF) err_ctl cmierrors_ctl;
-VMS_ONLY(LITREF)UNIX_ONLY(GBLREF) err_ctl gdeerrors_ctl;
-#ifdef VMS
-LITREF err_ctl laerrors_ctl; /* Roger thinks that this one is obsolete */
-LITREF err_ctl lperrors_ctl; /* Roger thinks that this one may be obsolete */
-#endif
+GBLREF err_ctl merrors_ctl;
+GBLREF err_ctl cmerrors_ctl;
+GBLREF err_ctl cmierrors_ctl;
+GBLREF err_ctl gdeerrors_ctl;
+GBLREF err_ctl ydberrors_ctl;
 
 STATICDEF const err_ctl *all_errors[] = { &merrors_ctl, &gdeerrors_ctl,
 		&cmierrors_ctl, &cmerrors_ctl,
-#ifdef VMS
-		&laerrors_ctl, &lperrors_ctl,
-#endif
+		&ydberrors_ctl,
 		NULL };
 
 /* Returns the error control struct corresponding to the errornum if it is valid, otherwise

sr_port/f_order.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2015 Fidelity National Information 	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -32,32 +35,31 @@ error_def(ERR_ORDER2);
 error_def(ERR_SIDEEFFECTEVAL);
 error_def(ERR_VAREXPECTED);
 
-LITDEF	opctype order_opc[LAST_OBJECT][LAST_DIRECTION] =
-{
-	/* FORWARD	BACKWARD	TBD */
-	{ OC_GVORDER,	OC_ZPREVIOUS,	OC_GVO2		},	/* GLOBAL */
-	{ OC_FNORDER,	OC_FNZPREVIOUS, OC_FNO2		},	/* LOCAL */
-	{ OC_FNLVNAME,	OC_FNLVPRVNAME, OC_FNLVNAMEO2	},	/* LOCAL_NAME */
-	{ OC_INDFUN,	OC_INDFUN,	OC_INDO2	}	/* INDIRECT */
-};
-
 int f_order(oprtype *a, opctype op)
 {
-	boolean_t	ok, used_glvn_slot;
+	boolean_t	ok;
 	enum order_dir	direction;
 	enum order_obj	object;
 	int4		intval;
 	opctype		gv_oc;
 	oprtype		control_slot, dir_opr, *dir_oprptr, *next_oprptr;
 	short int	column;
-	triple		*oldchain, *r, *sav_dirref, *sav_gv1, *sav_gvn, *sav_lvn, *sav_ref, *share, *triptr;
+	triple		*oldchain, *r, *sav_dirref, *sav_gv1, *sav_gvn, *sav_lvn, *sav_ref, *triptr;
 	triple		*chain2, *obp, tmpchain2;
 	save_se		save_state;
+	static opctype order_opc[LAST_OBJECT][LAST_DIRECTION] =
+	{
+		/* FORWARD	BACKWARD	TBD */
+		{ OC_GVORDER,	OC_ZPREVIOUS,	OC_GVO2		},	/* GLOBAL */
+		{ OC_FNORDER,	OC_FNZPREVIOUS, OC_FNO2		},	/* LOCAL */
+		{ OC_FNLVNAME,	OC_FNLVPRVNAME, OC_FNLVNAMEO2	},	/* LOCAL_NAME */
+		{ OC_INDFUN,	OC_INDFUN,	OC_INDO2	}	/* INDIRECT */
+	};
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
+	/* Note: The below code is similar to f_order.c. Any changes here might need to be made there and vice versa */
 	oldchain = sav_dirref = NULL;			/* default to no direction and no shifting indirection */
-	used_glvn_slot = FALSE;
 	r = maketriple(OC_NOOP);			/* We'll fill in the opcode later, when we figure out what it is */
 	switch (TREF(window_token))
 	{
@@ -158,6 +160,7 @@ int f_order(oprtype *a, opctype op)
 				assert((OC_GVNAME == gv_oc) || (OC_GVNAKED == gv_oc) || (OC_GVEXTNAM == gv_oc));
 				TREF(temp_subs) = TRUE;
 				create_temporaries(sav_gvn, gv_oc);
+				ins_triple(r);
 				break;
 			case LOCAL:		/* Additionally need to move srchindx triple to after potential side effect */
 				triptr = newtriple(OC_PARAMETER);
@@ -187,10 +190,10 @@ int f_order(oprtype *a, opctype op)
 					dqins(sav_lvn, exorder, triptr);		/* NOTE: violation of info hiding */
 					next_oprptr->oprval.tref->operand[0].oprval.tref = triptr;
 				}
+				ins_triple(r);
 				break;
 			case INDIRECT:		/* Save and restore the variable lookup for true left-to-right evaluation */
 				*next_oprptr = *dir_oprptr;
-				used_glvn_slot = TRUE;
 				dqinit(&tmpchain2, exorder);
 				chain2 = setcurtchain(&tmpchain2);
 				INSERT_INDSAVGLVN(control_slot, r->operand[0], ANY_SLOT, 1);
@@ -198,18 +201,16 @@ int f_order(oprtype *a, opctype op)
 				obp = sav_ref->exorder.bl;	/* insert before second arg */
 				dqadd(obp, &tmpchain2, exorder);
 				r->operand[0] = control_slot;
+				ins_triple(r);
+				triptr = newtriple(OC_GLVNPOP);
+				triptr->operand[0] = control_slot;
 				break;
 			case LOCAL_NAME:	/* left argument is a string - side effect can't screw it up */
 				*next_oprptr = *dir_oprptr;
+				ins_triple(r);
 				break;
 			default:
 				assert(FALSE);
-			}
-			ins_triple(r);
-			if (used_glvn_slot)
-			{
-				triptr = newtriple(OC_GLVNPOP);
-				triptr->operand[0] = control_slot;
 			}
 			if (SE_WARN_ON && (TREF(side_effect_base))[TREF(expr_depth)])
 				ISSUE_SIDEEFFECTEVAL_WARNING(column - 1);

sr_port/f_order1.c
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2012 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2012 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*

commit 5ff1f4585b5bc7e82858bb82382494fdafc47df1

[#828] [#10] Fix GTMASSERT2 fatal error in $QUERY(@"a,")
Background
----------
* This is an issue identified by fuzz testing (#828).

* Below is a simple test case demonstrating the issue.

  ```
  YDB>write $query(@"a,")
  %YDB-F-GTMASSERT2, YottaDB r999 Linux x86_64 - Assert failed sr_port/f_query.c line 164 for expression (DEPTH)
  ```

Issue
-----
* This is a longstanding issue in YottaDB introduced by 7a1987cd1eaf0fccdeb682ef060ad92deb6b6829
  (implemented support for reverse $query as part of #10).

* Whenever a function supports multiple arguments and the first argument can take on an indirection usage,
  for example `f_order.c` for `$ORDER`, then the indirection invocation cannot call `f_order.c` again.
  It has to go through the 1-argument (i.e. simplified) version of `f_order.c`. This is done in `f_order1.c`.

* As part of implementing support for reverse $query, what used to be a 1-argument function had to be changed
  into one accepting an optional second argument. This meant that indirection usages of the first argument
  had to be changed to go through a 1-argument version of `f_query.c`. This was not done then. That is what
  caused the above issue.

* Since upstream/GT.M does not support reverse $query (or 2-argument $query), it does not suffer from this issue.

Fix
---
* A copy of `sr_port/f_query.c` has been taken into `sr_port/f_query1.c` with the only simplification that
  `direction` is `FORWARD` and so parsing logic to handle an optional second argument is all removed there.
  In addition, code that set variables `sav_gv1`, `sav_gvn`, `tmpa0` and `tmpa1` which were later used in
  the now-deleted second-argument-handling-code is also removed.

* `sr_port/f_query.c` has been changed to store `indir_fnquery1` as the indirection opcode.

* And finally `indir_fnquery` in `sr_port/indir.h` has been changed to `indir_fnquery1` and also to invoke
  the function `f_query1` (instead of `f_query`).

sr_port/compiler.h
@@ -890,6 +890,7 @@ int		f_piece(oprtype *a, opctype op);
 int		f_qlength(oprtype *a, opctype op);
 int		f_qsubscript(oprtype *a, opctype op);
 int		f_query(oprtype *a, opctype op);
+int		f_query1(oprtype *a, opctype op);
 int		f_reversequery1(oprtype *a, opctype op);
 int		f_reverse(oprtype *a, opctype op);
 int		f_select(oprtype *a, opctype op);

sr_port/f_query1.c
@@ -34,23 +34,19 @@ error_def(ERR_QUERY2);
 error_def(ERR_SIDEEFFECTEVAL);
 error_def(ERR_VAREXPECTED);
 
-/* Note: The below code is duplicated in "f_query1.c" and "f_reversequery1.c".
- * Any changes here might need to be made in those files too and vice versa.
+/* This function is basically a 1-argument $query call where the direction (2nd argument) is known to be forward query.
+ * This is basically a copy of "f_query.c" except some code removed because we know there is no 2nd argument to parse.
+ * Any changes here might need to be made in "f_query.c" and vice versa.
  */
-int f_query(oprtype *a, opctype op)
+int f_query1(oprtype *a, opctype op)
 {
 	boolean_t	ok;
 	enum order_dir	direction;
 	enum order_obj	object;
-	int4		intval;
-	opctype		gv_oc;
-	oprtype		*tmpa, *tmpa0, *tmpa1, tmpa2;
-	oprtype		control_slot, dir_opr, *dir_oprptr, *next_oprptr;
+	oprtype		*tmpa, *tmpa0, *tmpa1;
+	oprtype		*next_oprptr;
 	save_se		save_state;
-	short int	column;
-	triple		*chain2, *obp, tmpchain2;
-	triple		*oldchain, *r, *r0, *r1, *r2, *sav_dirref, *triptr;
-	triple		*sav_gv1, *sav_gvn, *sav_lvn, *sav_ref;
+	triple		*oldchain, *r, *r0, *r1, *sav_dirref, *triptr;
 	static opctype	query_opc[LAST_OBJECT][LAST_DIRECTION] =
 	{
 		/* FORWARD	BACKWARD		TBD */
@@ -117,24 +113,13 @@ int f_query(oprtype *a, opctype op)
 			r1 = r0->operand[1].oprval.tref;
 			r1->operand[1] = *tmpa;
 			*tmpa = put_tref(r0);
-			tmpa0 = &tmpa->oprval.tref->operand[0];
-			tmpa1 = &tmpa->oprval.tref->operand[1];
 		}
 		assert(OC_FNQUERY == tmpa->oprval.tref->opcode);
 		next_oprptr = &r->operand[1];
 		break;
 	case TK_CIRCUMFLEX:
 		object = GLOBAL;
-		sav_gv1 = TREF(curtchain);
 		ok = gvn();
-		sav_gvn = (TREF(curtchain))->exorder.bl;
-		if (OC_GVRECTARG == sav_gvn->opcode)
-		{	/* because of shifting if we need to find it, look in the expr_start chain */
-			assert(TREF(shift_side_effects));
-			assert(((sav_gvn->operand[0].oprval.tref) == TREF(expr_start)) && (NULL != TREF(expr_start_orig)));
-			sav_gv1 = TREF(expr_start_orig);
-			sav_gvn = TREF(expr_start);
-		}
 		next_oprptr = &r->operand[0];
 		break;
 	case TK_ATSIGN:
@@ -155,107 +140,7 @@ int f_query(oprtype *a, opctype op)
 		stx_error(ERR_VAREXPECTED);
 		return FALSE;
 	}
-	if (TK_COMMA != TREF(window_token))
-		direction = FORWARD;	/* default direction */
-	else
-	{	/* two argument form: ugly logic for direction */
-		advancewindow();
-		column = source_column;
-		dir_oprptr = (oprtype *)mcalloc(SIZEOF(oprtype));
-		dir_opr = put_indr(dir_oprptr);
-		sav_ref = newtriple(OC_GVSAVTARG);
-		DISABLE_SIDE_EFFECT_AT_DEPTH;		/* doing this here lets us know specifically if direction had SE threat */
-		if (EXPR_FAIL == expr(dir_oprptr, MUMPS_EXPR))
-		{
-			if (NULL != oldchain)
-				setcurtchain(oldchain);
-			return FALSE;
-		}
-		assert(TRIP_REF == dir_oprptr->oprclass);
-		triptr = dir_oprptr->oprval.tref;
-		if (OC_LIT == triptr->opcode)
-		{	/* if direction is a literal - pick it up and stop flailing about */
-			if (MV_IS_TRUEINT(&triptr->operand[0].oprval.mlit->v, &intval) && (1 == intval || -1 == intval))
-			{
-				direction = (1 == intval) ? FORWARD : BACKWARD;
-				sav_ref->opcode = OC_NOOP;
-				sav_ref = NULL;
-			} else
-			{	/* bad direction */
-				if (NULL != oldchain)
-					setcurtchain(oldchain);
-				stx_error(ERR_QUERY2);
-				return FALSE;
-			}
-		} else
-		{
-			direction = TBD;
-			sav_dirref = newtriple(OC_GVSAVTARG);		/* $R reflects direction eval even if we revisit 1st arg */
-			triptr = newtriple(OC_GVRECTARG);
-			triptr->operand[0] = put_tref(sav_ref);
-			switch (object)
-			{
-			case GLOBAL:		/* The direction may have had a side effect, so take copies of subscripts */
-				*next_oprptr = *dir_oprptr;
-				for (; sav_gvn != sav_gv1; sav_gvn = sav_gvn->exorder.bl)
-				{	/* hunt down the gv opcode */
-					gv_oc = sav_gvn->opcode;
-					if ((OC_GVNAME == gv_oc) || (OC_GVNAKED == gv_oc) || (OC_GVEXTNAM == gv_oc))
-						break;
-				}
-				assert((OC_GVNAME == gv_oc) || (OC_GVNAKED == gv_oc) || (OC_GVEXTNAM == gv_oc));
-				TREF(temp_subs) = TRUE;
-				create_temporaries(sav_gvn, gv_oc);
-				ins_triple(r);
-				break;
-			case LOCAL:		/* Additionally need to move OC_FNQUERY triple to after potential side effect */
-				sav_lvn = r->operand[0].oprval.tref;
-				assert(OC_FNQUERY == sav_lvn->opcode);
-				dqdel(sav_lvn, exorder);
-				ins_triple(sav_lvn);
-				assert(TRIP_REF == tmpa0->oprclass);
-				assert(OC_ILIT == tmpa0->oprval.tref->opcode);
-				assert(ILIT_REF == tmpa0->oprval.tref->operand[0].oprclass);
-				assert(3 <= tmpa0->oprval.tref->operand[0].oprval.ilit);
-				if (3 != tmpa0->oprval.tref->operand[0].oprval.ilit)
-				{	/* sav_lvn has subscripts that we need to create temporaries for */
-					TREF(temp_subs) = TRUE;
-					create_temporaries(sav_lvn, sav_lvn->opcode);
-				}
-				/* else: sav_lvn is unsubscripted. No need to create temporaries */
-				/* Insert direction in OC_FNQUERY triple to eventually go to OC_FNQ2 */
-				r2 = maketriple(OC_PARAMETER);
-				r2->operand[0] = dir_opr;
-				r2->operand[1] = *tmpa1;
-				*tmpa1 = put_tref(r2);
-				dqins(sav_lvn, exorder, r2);
-				/* Since the "lvn" call above happened with "0" as the 3rd ("parent") parameter, r->operand[1]
-				 * would not be filled in like it would in case of "f_order" so this part of the code is different
-				 * from that of "f_order" (there we need to account for temporaries for r->operand[1]).
-				 */
-				assert(&r->operand[1] == next_oprptr);
-				break;
-			case INDIRECT:		/* Save and restore the variable lookup for true left-to-right evaluation */
-				*next_oprptr = *dir_oprptr;
-				dqinit(&tmpchain2, exorder);
-				chain2 = setcurtchain(&tmpchain2);
-				INSERT_INDSAVGLVN(control_slot, r->operand[0], ANY_SLOT, 1);
-				setcurtchain(chain2);
-				obp = sav_ref->exorder.bl;	/* insert before second arg */
-				dqadd(obp, &tmpchain2, exorder);
-				r->operand[0] = control_slot;
-				ins_triple(r);
-				triptr = newtriple(OC_GLVNPOP);
-				triptr->operand[0] = control_slot;
-				break;
-			default:
-				assert(FALSE);
-			}
-			if (SE_WARN_ON && (TREF(side_effect_base))[TREF(expr_depth)])
-				ISSUE_SIDEEFFECTEVAL_WARNING(column - 1);
-			DISABLE_SIDE_EFFECT_AT_DEPTH;		/* usual side effect processing doesn't work for $QUERY() */
-		}
-	}
+	direction = FORWARD;	/* default direction */
 	if (LOCAL != object)
 	{
 		if (TBD != direction)

sr_port/indir.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -68,7 +68,7 @@ INDIR(indir_fndata,		f_data,		OC_FNDATA)
 ,INDIR(indir_zdeallocate,	m_zdeallocate,	0)
 ,INDIR(indir_lvarg,		indirection,	0)
 ,INDIR(indir_fnzprevious,	f_zprevious,	OC_FNZPREVIOUS)
-,INDIR(indir_fnquery, 		f_query,	OC_FNQUERY)
+,INDIR(indir_fnquery1, 		f_query1,	OC_FNQUERY)
 ,INDIR(indir_zhelp,		m_zhelp,	0)
 ,INDIR(indir_zshow,		m_zshow,	0)
 ,INDIR(indir_lvnamadr,		indirection,	0)