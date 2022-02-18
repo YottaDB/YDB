YottaDB/YDB

commit 9d6421e878d378d21a785c2a40a6ec0bc3431974

[#828] Fix SIG-11 in $QUERY(lvn) after ZSHOW "*":lvn
Background
----------
* This is an issue identified by fuzz testing.

* Below is a simple test case using `ZSHOW "VL"` that demonstrates the problem.

  ```m
  YDB>zshow "VL":x write $query(@"x(""L"",0)"),!
  %YDB-F-ASSERT, Assert failed in sr_port/op_fnquery.c line 290 for expression (NULL != lvt)
  ```

* In a Release build, one sees a SIG-11.

  ```m
  YDB>zshow "VL":x write $query(@"x(""L"",0)"),!
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 9232 has been killed by a signal 11 at address 0x00007FB401F9C169 (vaddr 0x0000000000000020)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

* Below is a different test case using `ZSHOW "*"` that demonstrates a similar issue.

  **Debug Build**
  ```m
  YDB>zshow "*":x write $query(x("R",1))
  %YDB-F-ASSERT, Assert failed in sr_port/op_fnquery.c line 290 for expression (NULL != lvt)
  ```

  **Release Build**
  ```m
  YDB>zshow "*":x write $query(x("R",1))
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 8858 has been killed by a signal 11 at address 0x00007F998849C169 (vaddr 0x0000000000000020)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* This is a longstanding issue inherited from the upstream project (GT.M).

* There was logic in `sr_port/op_zshow.c` that checked if a dangling lvn was created as part of the ZSHOW
  operation whose target is a lvn (a dangling lvn is a lvn node that was created by `op_putindx()` but not
  set to any value and so is an out-of-design structure as far as the lvn tree is concerned since all nodes
  in the tree should be there with a value or lead to a child lv node with a value). If it detected one it
  would remove such a dangling node using a call to `UNDO_ACTIVE_LV` which looks at the global variable
  `active_lv` (the most recently created lv node, maintained by `op_putindx()`).

* But this was done after processing the entire ZSHOW operation.

sr_port/op_zshow.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -215,14 +215,14 @@ void op_zshow(mval *func, int type, lv_val *lvn)
 				zshow_zwrite(&output);
 				break;
 		}
+		/* If ZSHOW was done onto a subscripted lvn but no zshow records got dumped in that lvn, it might have $data = 0.
+		 * Kill it in that case as otherwise it will create an out-of-design situation for $query(lvn).
+		 */
+		if ((type == ZSHOW_LOCAL) && (NULL != active_lv) && lcl_arg1_is_desc_of_arg2(active_lv, lvn))
+			UNDO_ACTIVE_LV(actlv_op_zshow);
 	}
 	output.code = 0;
 	output.flush = TRUE;
 	zshow_output(&output,0);
 	MAXSTR_BUFF_FINI;
-	/* If ZSHOW was done onto a subscripted lvn but no zshow records got dumped in that lvn, it might have $data = 0.
-	 * Kill it in that case as otherwise it will create an out-of-design situation for $query(lvn).
-	 */
-	if ((type == ZSHOW_LOCAL) && (NULL != active_lv) && lcl_arg1_is_desc_of_arg2(active_lv, lvn))
-		UNDO_ACTIVE_LV(actlv_op_zshow);
 }

commit 656ec6f60476cf2d59085641c25f1417faad0d6e

[#828] Fix SIG-11 in ZSHOW "*":lvn where lvn is a non-existent subscripted lvn (regression in 9d6421e8)
Background
----------
* This is an issue identified by fuzz testing.

* Below is a simple test case that demonstrates the problem.

  ```m
  YDB>zshow "*":x(1)
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 48367 has been killed by a signal 11 at address 0x00007FB86CC63836 (vaddr 0x0000000000000000)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* A prior commit 9d6421e8 fixed a related issue where a `ZSHOW "*":lvn` followed by a `$QUERY(lvn)`
  resulted in a SIG-11.

* It did this by removing dangling lv nodes after each iteration of the for loop in `op_zshow.c`
  where each iteration was for each ZSHOW action code that the user specified (e.g. "L" for locks,
  "I" for ISVs etc.).

* The fix done was okay if `lvn` is an unsubscripted lvn.

* But if it is a subscripted lvn and does not already exist, it is possible that the `UNDO_ACTIVE_LV`
  call (now done inside the for loop) kills not just the deeper lv nodes created during processing of
  the current zshow action code but also the parent node `lvn`.

* In that case, the next iteration of the for loop (for the next zshow action code) would end up with
  an issue because it expects `lvn` to exist in the lv tree even if it has a `$data` of 0. This is what
  caused the SIG-11.

Fix
---
* Each iteration of the for loop limits the kill of lv nodes to stop just before it reaches the parent
  of `lvn`. This is done by temporarily setting the `lvn` node values to `literal_null` in case it is
  undefined.

* This ensures both the following conditions at the end of each for loop iteration.
  - No dangling lv nodes in the tree.
  - `lvn` node continues to exist in the tree.

* One thing this does not handle is if `lvn` is undefined to begin with (i.e. before `op_zshow` is entered).

sr_port/op_zshow.c
@@ -31,8 +31,10 @@
 #include "gtm_maxstr.h"
 #include "svnames.h"
 
-GBLREF lv_val	*active_lv;
-GBLREF gv_key	*gv_currkey;
+GBLREF	lv_val	*active_lv;
+GBLREF	gv_key	*gv_currkey;
+
+LITREF	mval	literal_null;
 
 error_def(ERR_ZSHOWBADFUNC);
 
@@ -102,9 +104,12 @@ void op_zshow(mval *func, int type, lv_val *lvn)
 		i = func->str.len;
 	}
 	memset(&output, 0, SIZEOF(output));
-	if (type == ZSHOW_LOCAL)
+	assert((ZSHOW_LOCAL == type) || (NULL == lvn));
+	if (ZSHOW_LOCAL == type)
+	{
+		assert(NULL != lvn);
 		output.out_var.lv.lvar = lvn;
-	else if (type == ZSHOW_GLOBAL)
+	} else if (type == ZSHOW_GLOBAL)
 	{
 		output.out_var.gv.end = gv_currkey->end;
 		output.out_var.gv.prev = gv_currkey->prev;
@@ -215,14 +220,46 @@ void op_zshow(mval *func, int type, lv_val *lvn)
 				zshow_zwrite(&output);
 				break;
 		}
-		/* If ZSHOW was done onto a subscripted lvn but no zshow records got dumped in that lvn, it might have $data = 0.
-		 * Kill it in that case as otherwise it will create an out-of-design situation for $query(lvn).
+		/* If ZSHOW output was redirected to "lvn" but no zshow records got dumped in the subtree under "lvn" as part
+		 * of the current zshow action code, it is possible an "op_putindx()" call was done to create a subscripted node
+		 * under "lvn" in anticipation of actual nodes getting set under the subtree but none happened. In that case,
+		 * the currently active lvn ("active_lv" global variable maintained by "op_putindx()") would have $data = 0.
+		 * This will create an out-of-design situation for a later $query(lvn) and hence needs to be killed. Hence the
+		 * use of the UNDO_ACTIVE_LV macro below. Note though that the "op_kill" call inside UNDO_ACTIVE_LV could
+		 * potentially kill not just "active_lv" but also parent lv tree nodes until it finds a node with a non-zero $data.
+		 * But we do not want the kill to reach "lvn" as that lv node is relied upon by later iterations in this for loop
+		 * to unconditionally exist (the "op_putindx()" for that lvn would have been done by generated code). Therefore,
+		 * check if "lvn" does not have any data and if so temporarily set it to point to one ("literal_null") before
+		 * the UNDO_ACTIVE_LV and then reset it immediately afterwards.
 		 */
-		if ((type == ZSHOW_LOCAL) && (NULL != active_lv) && lcl_arg1_is_desc_of_arg2(active_lv, lvn))
+		if ((ZSHOW_LOCAL == type) && (NULL != active_lv) && lcl_arg1_is_desc_of_arg2(active_lv, lvn))
+		{
+			boolean_t	is_defined;
+
+			is_defined = LV_IS_VAL_DEFINED(lvn);
+			if (!is_defined)
+			{
+				assert(0 == lvn->v.mvtype);
+				lvn->v = literal_null;
+			}
 			UNDO_ACTIVE_LV(actlv_op_zshow);
+			if (!is_defined)
+				LV_VAL_CLEAR_MVTYPE(lvn);
+		}
 	}
 	output.code = 0;
 	output.flush = TRUE;
 	zshow_output(&output,0);
 	MAXSTR_BUFF_FINI;
+	/* If ZSHOW output was redirected to "lvn", check if it is not a base var and if it is undefined. In that case,
+	 * it is possible this is a node with $data = 0 (see comment block above explaining $query issues otherwise).
+	 * If so, we need to kill it and any potential parent nodes until a non-zero $data node is found. Therefore use
+	 * the UNDO_ACTIVE_LV macro below. Note though that "active_lv" should never point to an unsubscripted lvn hence
+	 * the check for !LV_IS_BASE_VAR below.
+	 */
+	if ((ZSHOW_LOCAL == type) && !LV_IS_BASE_VAR(lvn) && !LV_IS_VAL_DEFINED(lvn))
+	{
+		active_lv = lvn;	/* needed by the UNDO_ACTIVE_LV macro call */
+		UNDO_ACTIVE_LV(actlv_op_zshow);
+	}
 }