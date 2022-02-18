b660ee328570b848e48d734b529e86be63830191
K.S. Bhaskar <bhaskar@yottadb.com>
2022-02-18 16:02:24 -0500

    [#763] Add check in ydbinstall to ensure that libelf.so exists on the system

--- sr_unix/ydbinstall.sh
+++ sr_unix/ydbinstall.sh
@@ -54,6 +54,12 @@ do
 	check_if_util_exists $util
 done
 
+# Check whether libelf.so exists; issue an error and exit if it does not
+ldconfig -p | grep /libelf.so >/dev/null 2>&1 ; ydb_tmp_stat=$?
+if [ 0 -ne ${ydb_tmp_stat:-0} ] ; then
+	echo >&2 "library libelf.so is needed by YottaDB but not found. Exiting." ; exit $ydb_tmp_stat
+fi
+
 # Ensure this is not being sourced so that environment variables in this file do not change the shell environment
 if [ "ydbinstall" != `basename -s .sh $0` ] ; then
     echo "Please execute ydbinstall/ydbinstall.sh instead of sourcing it"
