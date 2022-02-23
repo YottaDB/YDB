YottaDB/YDB

commit a3b15a647185921f41a0718f5f2c2ecd244ce15e

[#828] Fix %YDB-F-GTMASSERT2 fatal error from $INCREMENT(@glvn,boolexpr)
Background
----------
* This is an issue identified by fuzz testing.

* Below is a simple test case that demonstrates the problem.

  ```m
  YDB>write $increment(@x,1&2)
  %YDB-F-GTMASSERT2, YottaDB r999 Linux x86_64 - Assert failed sr_port/emit_code.c line 1169 for expression (FALSE && ct->opcode)
  ```

Issue
-----
* The assert failure in `emit_code.c` is usually because of some malformed triple.

* To better investigate this, I looked at the current execution triple chain in the debugger
  just before invoking `emit_code.c`.

  ```c
  (gdb)
  Breakpoint 1, trip_gen (ct=0x625000009748) at sr_port/emit_code.c:144
  144             oprtype         **sopr, *opr;   /* triple operand */

  (gdb) p cdbg_dump_curtchain()
  .
  .
  ```

* The triple chain output was the following.

  ```
  Triple OC_NOOP
  Triple OC_LINEFETCH
  Triple OC_GVRECTARG
  Triple OC_WRITE
  Triple OC_RET
  Triple OC_ILIT
  Triple OC_NOOP
  ```

* Notice the `OC_GVRECTARG` but no corresponding `OC_SAVTARG` triple. This is the issue.
  It is like having a closing paren without an opening paren.

* Turns out the issue is in `sr_port/f_incr.c` where after evaluation of the boolean expression in
  the second argument to `$INCR(@glvn,boolexpr)` we could end up with a non-NULL value of `TREF(expr_start)`.
  This global variable maintains the chain that gets evaluated at all times even in case of boolean short
  circuiting. If this chain was NULL before evaluating the `boolexpr` argument and becomes non-NULL after
  evaluating `boolexpr`, we incorrectly used the non-NULL value of the global variable to do additions of
  other triples in the indirection case (in a later call to `dqadd()`). In this case, `dqadd()` would end
  up trying to add one chain to another where the two chains actually intersect. This will end up losing
  parts of the chain which is what happened in the above case where the `OC_GVSAVTARG` triples got lost

sr_port/f_incr.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -86,6 +86,19 @@ int f_incr(oprtype *a, opctype op)
 		}
 		setcurtchain(&targchain);
 		dqadd(&targchain, &incrchain, exorder);	/* dir before targ - this is a violation of info hiding */
+		if (NULL == triptr)
+		{	/* If "triptr" is NULL, it means "TREF(expr_start)" was NULL before the "expr()" call above.
+			 * If "expr()" involved a boolean expression evaluation, then "TREF(expr_start)" could have gotten
+			 * set to a non-NULL value. In that case, we need to reset "TREF(expr_start()" back to the original
+			 * value (i.e. NULL) as it would otherwise point to a triple in the "incrchain" (which has already
+			 * been merged into "targchain") and pose problems in the "dqadd(TREF(expr_start), &targchain, ...)"
+			 * call at the end of this function because "dqadd()" assumes that "TREF(expr_start)" and "targchain"
+			 * are non-intersecting chains. Since we are restoring "TREF(expr_start)", need to restore
+			 * "TREF(expr_start_orig)" as well at the same time as the two globals need to be in sync.
+			 */
+			TREF(expr_start) = triptr;	/* restore original shift chain */
+			TREF(expr_start_orig) = savptr;
+		}
 	}
 	coerce(increment, OCT_MVAL);
 	ins_triple(r);

sr_port/cdbg_dump.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -88,46 +88,40 @@ LITREF int4	sa_class_sizes[];
 STATICDEF char	*indent_str;
 STATICDEF int	last_indent = 0;
 
-/* Routine to dump all triples on "t_orig" chain - also callable from debugger */
-void cdbg_dump_t_orig(void)
+/* Routine to dump all triples on a generic chain starting at "chain" - also callable from debugger */
+void cdbg_dump_chain(triple *chain)
 {
 	triple	*ct;
 
-	dqloop(&t_orig, exorder, ct)
+	dqloop(chain, exorder, ct)
 	{
 		PRINTF("\n ************************ Triple Start **********************\n");
 		cdbg_dump_triple(ct, 0);
 	}
+}
+
+/* Routine to dump all triples on "t_orig" chain - also callable from debugger */
+void cdbg_dump_t_orig(void)
+{
+	cdbg_dump_chain(&t_orig);
 }
 
 /* Routine to dump all triples on "TREF(curtchain)" chain - also callable from debugger */
 void cdbg_dump_curtchain(void)
 {
-	triple	*ct, *chain;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
-	chain = TREF(curtchain);
-	dqloop(chain, exorder, ct)
-	{
-		PRINTF("\n ************************ Triple Start **********************\n");
-		cdbg_dump_triple(ct, 0);
-	}
+	cdbg_dump_chain(TREF(curtchain));
 }
 
 /* Routine to dump all triples on "TREF(boolchain_ptr)" chain - also callable from debugger */
 void cdbg_dump_boolchain(void)
 {
-	triple	*ct, *chain;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
-	chain = TREF(boolchain_ptr);
-	dqloop(chain, exorder, ct)
-	{
-		PRINTF("\n ************************ Triple Start **********************\n");
-		cdbg_dump_triple(ct, 0);
-	}
+	cdbg_dump_chain(TREF(boolchain_ptr));
 }
 
 /* Routine to dump a single triple and its followers */

sr_port/cdbg_dump.h
@@ -2,7 +2,7 @@
  *								*
  * Copyright 2001, 2013 Fidelity Information Services, Inc	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -21,6 +21,7 @@
 #define OP_DEST 2	/* destination is passed */
 
 void cdbg_dump_t_orig(void);
+void cdbg_dump_chain(triple *chain);
 void cdbg_dump_curtchain(void);
 void cdbg_dump_boolchain(void);
 void cdbg_dump_triple(triple *dtrip, int indent);

commit e30e2eb51c892dcf72a34ebe14b1d8e468cb647e

[#828] Fix SIG-11 when compiling a boolean expression with $INCREMENT(gvn) usages (regression in a3b15a64)
Background
----------
* This is an issue identified by fuzz testing.

* Below is a simple test case that demonstrates the problem.

  ```m
  YDB>write $Increment(x,0!1)&$Increment(^x)
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 76429 has been killed by a signal 11 at address 0x00007F2D392C1924 (vaddr 0x0000000000000008)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* This turns out to be a regression in a3b15a64.

* The fix done in that commit is pasted below from the commit message text.

  The fix is simple and is to reset `TREF(expr_start)` to NULL after evaluating the `boolexpr` argument
  in case it was NULL before evaluating the `boolexpr` argument. This is done in `sr_port/f_incr.c`.

* While this fix seemed right for the issue encountered in that commit (a `$INCR(@glvn,boolexpr)` usage),
  I now understand why that is not correct in case of a `$INCR(lvn,boolexpr)&$INCR(gvn)` usage.

* The global variable `TREF(expr_start)` maintains a pointer to the global chain of evaluations that need to
  happen before any boolean expression short-circuit evaluations happen. When we enter `f_incr.c` the first time
  to compile the `$INCR(lvn,boolexpr)` usage (in the `$INCR(lvn,boolexpr)&$INCR(gvn)` usage), the `expr()` call
  in `f_incr.c` would have set `TREF(expr_start)` to a non-NULL value in `eval_expr.c` to point to the earliest
  non-boolean-opcode triple. In that case, we should not be resetting this global pointer to NULL in `f_incr.c`
  after the `expr()` call as it would then disturb the additions that would later happen when the `$INCR(gvn)`
  call to `f_incr.c` happens. This would end up in a state where `&tmpexpr != tmpexpr.exorder.bl` is TRUE and
  so we would try to do a `dqadd(TREF(expr_start), &tmpexpr, exorder)` but would end up in a SIG-11 because
  `TREF(expr_start)` is NULL.

sr_port/f_incr.c
@@ -29,7 +29,7 @@ int f_incr(oprtype *a, opctype op)
 {
 	boolean_t	ok;
 	oprtype		*increment;
-	triple		incrchain, *oldchain, *r, *savptr, targchain, tmpexpr, *triptr;
+	triple		incrchain, *oldchain, *r, *savptr, targchain, tmpexpr, *triptr, *triptr2;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -86,19 +86,6 @@ int f_incr(oprtype *a, opctype op)
 		}
 		setcurtchain(&targchain);
 		dqadd(&targchain, &incrchain, exorder);	/* dir before targ - this is a violation of info hiding */
-		if (NULL == triptr)
-		{	/* If "triptr" is NULL, it means "TREF(expr_start)" was NULL before the "expr()" call above.
-			 * If "expr()" involved a boolean expression evaluation, then "TREF(expr_start)" could have gotten
-			 * set to a non-NULL value. In that case, we need to reset "TREF(expr_start()" back to the original
-			 * value (i.e. NULL) as it would otherwise point to a triple in the "incrchain" (which has already
-			 * been merged into "targchain") and pose problems in the "dqadd(TREF(expr_start), &targchain, ...)"
-			 * call at the end of this function because "dqadd()" assumes that "TREF(expr_start)" and "targchain"
-			 * are non-intersecting chains. Since we are restoring "TREF(expr_start)", need to restore
-			 * "TREF(expr_start_orig)" as well at the same time as the two globals need to be in sync.
-			 */
-			TREF(expr_start) = triptr;	/* restore original shift chain */
-			TREF(expr_start_orig) = savptr;
-		}
 	}
 	coerce(increment, OCT_MVAL);
 	ins_triple(r);
@@ -108,12 +95,21 @@ int f_incr(oprtype *a, opctype op)
 		dqadd(TREF(expr_start), &tmpexpr, exorder);	/* this is a violation of info hiding */
 		TREF(expr_start) = tmpexpr.exorder.bl;
 		assert(OC_GVSAVTARG == (TREF(expr_start))->opcode);
-		triptr = newtriple(OC_GVRECTARG);	/* restore the result of the last gvn to preserve $referece (the naked) */
-		triptr->operand[0] = put_tref(TREF(expr_start));
+		triptr2 = newtriple(OC_GVRECTARG);	/* restore the result of the last gvn to preserve $referece (the naked) */
+		triptr2->operand[0] = put_tref(TREF(expr_start));
 	}
 	if (!TREF(shift_side_effects) || (YDB_BOOL != TREF(ydb_fullbool)) || (OC_INDINCR != r->opcode)
-		|| (NULL == TREF(expr_start)))
-	{	/* put it on the end of the main chain as there's no reason to play more with the ordering */
+		|| (NULL == TREF(expr_start)) || (NULL == triptr))
+	{	/* Put it on the end of the main chain as there's no reason to play more with the ordering */
+		/* Note: The reason for the "(NULL == triptr)" check in the above "if" is the following.
+		 * If "triptr" is NULL, it means "TREF(expr_start)" was NULL before the "expr()" call above.
+		 * If "expr()" involved a boolean expression evaluation, then "TREF(expr_start)" could have gotten
+		 * set to a non-NULL value. In that case, "TREF(expr_start()" would point to a triple in the "incrchain"
+		 * (which has already been merged into "targchain") and so the "dqadd(TREF(expr_start), &targchain, ...)"
+		 * call in the "else" block below cannot be done as "dqadd()" assumes that "TREF(expr_start)" and "targchain"
+		 * are non-intersecting chains. Hence we treat a NULL value of "triptr" as if "TREF(expr_start)" is NULL
+		 * and fall through into the "if" block in that case.
+		 */
 		setcurtchain(oldchain);
 		triptr = (TREF(curtchain))->exorder.bl;
 		dqadd(triptr, &targchain, exorder);	/* this is a violation of info hiding */