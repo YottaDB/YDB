YottaDB/YDB

commit 3896dddb562d8519e871566ff5e780e3216f7161

[#860] Fix SIG-11 when .m file is opened read-write and ZLINK of same .m file is attempted
Background
----------
* Below is a simple test case obtained from a fuzz test failure in in-house testing.

  ```m
  $ cat test.m
   set fn="generated.m"
   open fn:new
   use fn
   write " z"
   Set $ZROUTINES=""
   zlink "generated.m"

  $ $ydb_dist/yottadb -run test
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 55439 has been killed by a signal 11 at address 0x00007F4F4F82EED7 (vaddr 0x0000000000000008)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  Segmentation fault (core dumped)
  ```

* This is a failure in both Release and Debug builds of YottaDB as well as the upstream GT.M.

Issue
-----
* Below is the stack trace from the core file.

  ```c
  (gdb) where
  #0  ins_errtriple (in_error=150373618) at sr_port/ins_errtriple.c:51
  #1  stx_error_va (in_error=150373618, args=0x7f6559aa53c0) at sr_port/stx_error.c:164
  #2  rts_error_va (csa=0x0, argcnt=1, var=0x7f6559aa54a0) at sr_unix/rts_error.c:179
  #3  rts_error_csa (csa=0x0, argcnt=1) at sr_unix/rts_error.c:99
  #4  iorm_wteol (x=1, iod=0x62d000004840) at sr_unix/iorm_wteol.c:87
  #5  iorm_cond_wteol (iod=0x62d000004840) at sr_unix/iorm_flush.c:42
  #6  iorm_close (iod=0x62d000004840, pp=0x7f6559aa63b0) at sr_unix/iorm_close.c:112
  #7  io_dev_close (d=0x62d000005ec0) at sr_port/io_rundown.c:102
  #8  io_rundown (rundown_type=0) at sr_port/io_rundown.c:60
  #9  gtm_exit_handler () at sr_unix/gtm_exit_handler.c:239
  #10 signal_exit_handler (exit_handler_name=0x7f6555366520 "generic_signal_handler", sig=11, info=0x7f6555881948 <stapi_signal_handler_oscontext+4424>, context=0x7f65558819c8 <stapi_signal_handler_oscontext+4552>, is_deferred_exit=0) at sr_unix/signal_exit_handler.c:78

sr_port/ins_errtriple.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -47,21 +47,31 @@ void ins_errtriple(int4 in_error)
 			return;
 		}
 		x = (TREF(pos_in_chain)).exorder.bl;
-		/* If first error in the current line/cmd, delete all triples and replace them with an OC_RTERROR triple. */
-		add_rterror_triple = (OC_RTERROR != x->exorder.fl->opcode);
-		if (!add_rterror_triple)
-		{	/* This is the second error in this line/cmd. Check for triples added after OC_RTERROR and remove them
-			 * as there could be dangling references amongst them which could later cause assertpro() in emit_code.
-			 */
-			x = x->exorder.fl;
-			assert(OC_RTERROR == x->opcode);/* corresponds to newtriple(OC_RTERROR) in previous ins_errtriple */
-			x = x->exorder.fl;
-			assert(OC_ILIT == x->opcode);	/* corresponds to put_ilit(in_error) in previous ins_errtriple */
-			x = x->exorder.fl;
-			assert(OC_ILIT == x->opcode);	/* corresponds to put_ilit(FALSE) in previous ins_errtriple */
+		/* Note that "x" can be NULL in case we encounter an error before we started processing the first line of
+		 * the M program (see https://gitlab.com/YottaDB/DB/YDB/-/issues/860#note_1056075754 for test case).
+		 */
+		assert((NULL == x) || (NULL != x->exorder.fl));
+		if (NULL != x)
+		{
+			/* If first error in the current line/cmd, delete all triples and replace them with an OC_RTERROR triple. */
+			add_rterror_triple = (OC_RTERROR != x->exorder.fl->opcode);
+			if (!add_rterror_triple)
+			{	/* This is the second error in this line/cmd. Check for triples added after OC_RTERROR and remove them
+				 * as there could be dangling references amongst them which could later cause assertpro() in emit_code.
+				 */
+				x = x->exorder.fl;
+				assert(OC_RTERROR == x->opcode);/* corresponds to newtriple(OC_RTERROR) in previous ins_errtriple */
+				x = x->exorder.fl;
+				assert(OC_ILIT == x->opcode);	/* corresponds to put_ilit(in_error) in previous ins_errtriple */
+				x = x->exorder.fl;
+				assert(OC_ILIT == x->opcode);	/* corresponds to put_ilit(FALSE) in previous ins_errtriple */
+			}
+			/* delete all trailing triples and if the first, replace them below with an OC_RTERROR triple */
+			dqdelchain(x, TREF(curtchain), exorder);
+		} else
+		{	/* We have not yet started processing the current line and so add OC_RTERROR triple */
+			add_rterror_triple = TRUE;
 		}
-		/* delete all trailing triples and if the first, replace them below with an OC_RTERROR triple */
-		dqdelchain(x, TREF(curtchain), exorder);
 		CHKTCHAIN(TREF(curtchain), exorder, FALSE);
 		assert(!add_rterror_triple || ((TREF(pos_in_chain)).exorder.bl->exorder.fl == TREF(curtchain)));
 		assert(!add_rterror_triple || ((TREF(curtchain))->exorder.bl == (TREF(pos_in_chain)).exorder.bl));

sr_port/show_source_line.c
@@ -126,19 +126,24 @@ void show_source_line(boolean_t warn)
 #			endif
 			}
 		}
-		dec_nofac = TRUE;
-		if (MAXLINESIZEFORDISPLAY > line_chwidth)
-			if (unable_to_complete_arrow)
-				dec_err(VARLSTCNT(6) ERR_SRCLIN, 4, (TREF(source_buffer)).len - 1, (TREF(source_buffer)).addr,
-					msgstr.len, msgstr.addr);
+		if (0 < line_chwidth)
+		{
+			dec_nofac = TRUE;
+			if (MAXLINESIZEFORDISPLAY > line_chwidth)
+				if (unable_to_complete_arrow)
+					dec_err(VARLSTCNT(6) ERR_SRCLIN, 4,
+						(TREF(source_buffer)).len - 1, (TREF(source_buffer)).addr,
+						msgstr.len, msgstr.addr);
+				else
+					dec_err(VARLSTCNT(6) ERR_SRCLIN, 4,
+						(TREF(source_buffer)).len - 1, (TREF(source_buffer)).addr,
+						b - buf, buf);
 			else
-				dec_err(VARLSTCNT(6) ERR_SRCLIN, 4, (TREF(source_buffer)).len - 1, (TREF(source_buffer)).addr,
-					b - buf, buf);
-		else
-			dec_err(VARLSTCNT(2) ERR_SRCLNNTDSP, 1, MAXLINESIZEFORDISPLAY);
-		if (!run_time)
-			dec_err(VARLSTCNT(6) ERR_SRCLOC, 4, TREF(last_source_column), TREF(source_line),
-				source_name_len, source_file_name);
-		dec_nofac = FALSE;
+				dec_err(VARLSTCNT(2) ERR_SRCLNNTDSP, 1, MAXLINESIZEFORDISPLAY);
+			if (!run_time)
+				dec_err(VARLSTCNT(6) ERR_SRCLOC, 4, TREF(last_source_column), TREF(source_line),
+					source_name_len, source_file_name);
+			dec_nofac = FALSE;
+		}
 	}
 }

sr_port/tripinit.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2022 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -69,5 +72,6 @@ void tripinit(void)
 	mline_tail = &mline_root;
 	TREF(block_level) = 0;
 	setcurtchain(&t_orig);
+	TREF(pos_in_chain) = *TREF(curtchain);
 	return;
 }

commit 22dad64c241e2bca626ba3452570d16c9aeae916

[DEBUG-ONLY] Fix placement of assert in sr_port/ins_errtriple.c (regression in 3896dddb)
Background
----------
* The `test-clang-tidy` pipeline job failed with an extra warning after 3896dddb got merged.
  Below is an example job output from https://gitlab.com/nars1/YDB/-/jobs/2840482716.

  ```diff
  # Running command : diff -Z ../ci/tidy_warnings_debug.ref warnings/sorted_warnings.txt >& differences.txt
   -> Expected warnings differ from actual warnings! diff output follows
   -> note: '<' indicates an expected warning, '>' indicates an actual warning
  143a144
  > ins_errtriple.c:warning: Dereference of null pointer [clang-analyzer-core.NullDereference]
  ```

* This is actually a debug-only code issue.

Issue
-----
* As part of 3896dddb, `add_rterror_triple` was set to TRUE in `sr_port/ins_errtriple.c` in case we
  find `(TREF(pos_in_chain)).exorder.bl` to be NULL. But pre-existing asserts that followed the
  newly introduced code did an access of `(TREF(pos_in_chain)).exorder.bl->exorder.fl`. And this
  could lead to a SIG-11 even though it is inside an assert (i.e. only in a Debug build).

Fix
---
* The debug code (including 2 asserts and one CHKTCHAIN call) are now moved into the `if` block of
  code where we are guaranteed that `(TREF(pos_in_chain)).exorder.bl` is non-NULL.

sr_port/ins_errtriple.c
@@ -68,19 +68,20 @@ void ins_errtriple(int4 in_error)
 			}
 			/* delete all trailing triples and if the first, replace them below with an OC_RTERROR triple */
 			dqdelchain(x, TREF(curtchain), exorder);
+			CHKTCHAIN(TREF(curtchain), exorder, FALSE);
+			assert(!add_rterror_triple || ((TREF(pos_in_chain)).exorder.bl->exorder.fl == TREF(curtchain)));
+			assert(!add_rterror_triple || ((TREF(curtchain))->exorder.bl == (TREF(pos_in_chain)).exorder.bl));
 		} else
 		{	/* We have not yet started processing the current line and so add OC_RTERROR triple */
 			add_rterror_triple = TRUE;
 		}
-		CHKTCHAIN(TREF(curtchain), exorder, FALSE);
-		assert(!add_rterror_triple || ((TREF(pos_in_chain)).exorder.bl->exorder.fl == TREF(curtchain)));
-		assert(!add_rterror_triple || ((TREF(curtchain))->exorder.bl == (TREF(pos_in_chain)).exorder.bl));
 		if ((ERR_DIVZERO == in_error) || (ERR_NEGFRACPWR == in_error) || (ERR_NUMOFLOW == in_error)
 					|| (ERR_PATNOTFOUND == in_error) || (ERR_BOOLEXPRTOODEEP == in_error))
 			TREF(rts_error_in_parse) = TRUE;	/* WARNING: fallthrough */
 	} else
-		/* For IS_STX_WARN errors (if not compiling a trigger), parsing continues, so dont strip the chain */
+	{	/* For IS_STX_WARN errors (if not compiling a trigger), parsing continues, so dont strip the chain */
 		add_rterror_triple = TRUE;
+	}
 	if (add_rterror_triple)
 	{	/* WARN error or first error in the current line */
 		triptr = newtriple(OC_RTERROR);