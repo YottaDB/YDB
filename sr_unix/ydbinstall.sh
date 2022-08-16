YottaDB/YDB

commit 49750ae43483fd3013f3da4262109eec7bf6ad45

[#891] Minor changes to ydbinstall.sh so all SUSE distributions use the SLE tarball
* Replaced a `sle` with `${osid}` inside a `sle)` case block. The two are equivalent
  but the latter usage is better.

* Also noticed a previous piece of code that used `sles` inside a `case` statement.
  This is no longer valid since the `osid` variable would get set to `sle` for all these 3 SUSE
  Linux distributions. Therefore fixed the `sles` to `sle`.

sr_unix/ydbinstall.sh
@@ -233,7 +233,7 @@ isvaluevalid()
 getosid()
 {
 	osid=`grep -w ID $1 | cut -d= -f2 | cut -d'"' -f2`
-	# Treat SLES (Server), SLED (Desktop), and openSUSE LEAP distributions as the same.
+	# Treat SLES (Server), SLED (Desktop) and OpenSUSE Leap distributions as the same.
 	if [ "sled" = "$osid" ] || [ "sles" = "$osid" ] || [ "opensuse-leap" = "$osid" ] ; then
 		osid="sle"
 	fi
@@ -1005,9 +1005,9 @@ else
 					# Hence the "rhel|centos" usage in the above "case" block.
 					;;
 				sle)
-					# Just like RHEL OS, we have SLE tarballs for both SLES/SLED based on the major version.
-					# Hence the use of "osmajorver" below in the "platform" variable.
-					platform="sle${osmajorver}"
+					# Just like RHEL OS, we have SLE tarballs for both SLES/SLED/OpenSUSE Leap based on the
+					# major version. Hence the use of "osmajorver" below in the "platform" variable.
+					platform="${osid}${osmajorver}"
 					;;
 				esac
 				;;
@@ -1036,7 +1036,7 @@ else
 				#
 				# To get the correct binary for CentOS, RHEL and SLES, we treat OS major version 7 as rhel and later versions as centos
 				case "${osid}" in
-				rhel|centos|sles|rocky)
+				rhel|centos|sle|rocky)
 					# CentOS-specific releases of YottaDB for x86_64 happened only after r1.26
 					if expr r1.26 \< "${ydb_version}" >/dev/null; then
 						# If the OS major version is later than 7, treat it as centos. Otherwise, treat it as rhel.

commit 8278ecafa8348eb1dbf9bb607c5279f3b132aefe

[#910] ydbinstall fails if you use the "--from-source" option due to uninitialized cmake variable
This was a regression introduced by
https://gitlab.com/YottaDB/DB/YDB/-/commit/49750ae43483fd3013f3da4262109eec7bf6ad45
due to a missing else clause, now fixed.

Tested using this command:

```
sudo ./ydbinstall.sh --from-source --verbose --branch r1.34 --overwrite-existing
```

sr_unix/ydbinstall.sh
@@ -592,6 +592,8 @@ if [ -n "$ydb_from_source" ] ; then
 		if [ "dbg" = `echo "$gtm_buildtype" | tr '[:upper:]' '[:lower:]'` ] ; then
 			# if --buildtype is dbg, tell CMake to make a dbg build
 			cmake_command="${cmakecmd} -D CMAKE_BUILD_TYPE=Debug"
+		else
+			cmake_command="${cmakecmd} -D CMAKE_BUILD_TYPE=RelWithDebInfo"
 		fi
 		if ! ${cmake_command} -D CMAKE_INSTALL_PREFIX:PATH=$PWD ../ ; then
 			echo "CMake failed. Exiting. Temporary directory $ydbinstall_tmp will not be deleted."