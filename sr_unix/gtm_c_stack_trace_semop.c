YottaDB/YDB

commit eb7e5dd119cee6b31a15b7be86caa99de5b1e549

[#560] SIGTERM/SIG-15 or Ctrl-C/SIGINT/SIG-2 can hang in some cases due to nested malloc()/free() invocation
Background
-----------
* The r126/ydb464 subtest tests Ctrl-C handling in a SimpleAPI program. Once in a while it hangs with
  two types of C-stack traces (pasted below). In both cases, the C program was in the middle of
  a glibc `malloc()` call when it got the Ctrl-C. The signal handler then ends up invoking some
  function (`sys_settimer()` in the first case below and `syslog()` in the second case below) that in
  turn requires a `malloc()` and that hangs because it requires an internal glibc/system lock that is
  currently held by the interrupted `malloc()`.

  ```c
  () where
  #0  __lll_lock_wait_private () from /usr/lib64/libc.so.6
  #1  _L_lock_17166 () from /usr/lib64/libc.so.6
  #2  malloc () from /usr/lib64/libc.so.6
  #3  timer_create@@GLIBC_2.3.3 () from /usr/lib64/librt.so.1
  #4  sys_settimer () at sr_unix/gt_timers.c:545
  #5  start_first_timer () at sr_unix/gt_timers.c:633
  #6  cancel_unsafe_timers () at sr_unix/gt_timers.c:1086
  #7  gtm_exit_handler () at sr_unix/gtm_exit_handler.c:212
  #8  generic_signal_handler () at sr_unix/generic_signal_handler.c:449
  #9  <signal handler called>
  #10 _int_malloc () from /usr/lib64/libc.so.6
  #11 malloc () from /usr/lib64/libc.so.6
  #12 runProc () at simpleapi/inref/randomWalk.c:212
  #13 runProc_driver () at simpleapi/inref/randomWalk.c:145
  #14 main () at simpleapi/inref/randomWalk.c:93

  () where
  #0  __lll_lock_wait_private () from /usr/lib64/libc.so.6
  #1  _L_lock_17166 () from /usr/lib64/libc.so.6
  #2  malloc () from /usr/lib64/libc.so.6
  #3  open_memstream () from /usr/lib64/libc.so.6
  #4  __vsyslog_chk () from /usr/lib64/libc.so.6
  #5  syslog () from /usr/lib64/libc.so.6
  #6  util_out_send_oper () at sr_unix/util_output.c:770
  #7  util_out_print_vaparm () at sr_unix/util_output.c:880
  #8  util_out_print () at sr_unix/util_output.c:913

sr_unix/gtm_c_stack_trace_semop.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2011-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2020 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -13,9 +13,12 @@
  *								*
  ****************************************************************/
 #include "mdef.h"
+
 #include "gtm_ipc.h"
+
 #include <sys/sem.h>
 #include <errno.h>
+
 #include "gtm_c_stack_trace.h"
 #include "gtm_c_stack_trace_semop.h"
 #include "semwt2long_handler.h"
@@ -24,6 +27,7 @@
 #include "gdsroot.h"
 #include "gdsbt.h"
 #include "gdsfhead.h"
+#include "eintr_wrappers.h"
 
 GBLREF uint4            process_id;
 #ifdef DEBUG
@@ -60,40 +64,42 @@ int try_semop_get_c_stack(int semid, struct sembuf sops[], int nsops)
 			start_timer((TID)semwt2long_handler, MAX_SEM_WAIT_TIME, semwt2long_handler, 0, NULL);
 		}
 		rc = semop(semid, sops, nsops);
-		if (-1 == rc)
+		if (-1 != rc)
+			break;
+		save_errno = errno;
+		if (EINTR != save_errno)
+			break;
+		EINTR_HANDLING_CHECK;
+		/* Timer popped, get C-stack trace */
+		if (TREF(semwait2long))
 		{
-			save_errno = errno;
-			/* Timer popped, get C-stack trace */
-			if ((EINTR == save_errno) && TREF(semwait2long))
+			stuckcnt++;
+			last_sem_trace = -1;
+			for (loopcount = 0; loopcount < nsops; loopcount++)
 			{
-				stuckcnt++;
-				last_sem_trace = -1;
-				for (loopcount = 0; loopcount < nsops; loopcount++)
-				{
-					if ((last_sem_trace != sops[loopcount].sem_num) && (0 == sops[loopcount].sem_op))
-					{	/* Do not take trace of the same process again, in a point of time */
-						last_sem_trace = sops[loopcount].sem_num;
-						semop_pid = semctl(semid, sops[loopcount].sem_num, GETPID);
-						if ((-1 != semop_pid) && (semop_pid != process_id))
-						{
-							GET_C_STACK_FROM_SCRIPT("SEMOP_INFO", process_id, semop_pid, stuckcnt);
-							/* Got stack trace signal the first process to continue */
+				if ((last_sem_trace != sops[loopcount].sem_num) && (0 == sops[loopcount].sem_op))
+				{	/* Do not take trace of the same process again, in a point of time */
+					last_sem_trace = sops[loopcount].sem_num;
+					semop_pid = semctl(semid, sops[loopcount].sem_num, GETPID);
+					if ((-1 != semop_pid) && (semop_pid != process_id))
+					{
+						GET_C_STACK_FROM_SCRIPT("SEMOP_INFO", process_id, semop_pid, stuckcnt);
+						/* Got stack trace signal the first process to continue */
 #							ifdef DEBUG
-							if (cnl)
-								GTM_WHITE_BOX_TEST(WBTEST_SEMTOOLONG_STACK_TRACE,
-									cnl->wbox_test_seq_num, 3);
+						if (cnl)
+							GTM_WHITE_BOX_TEST(WBTEST_SEMTOOLONG_STACK_TRACE,
+								cnl->wbox_test_seq_num, 3);
 #							endif
-						} else if (-1 == semop_pid)
-						{
-							rc = -1;
-							save_errno = errno;
-							break;
-						}
+					} else if (-1 == semop_pid)
+					{
+						rc = -1;
+						save_errno = errno;
+						break;
 					}
 				}
 			}
 		}
-	} while ((-1 == rc) && (EINTR == save_errno));
+	} while (TRUE);
 	cancel_timer((TID)semwt2long_handler);
 	return (-1 == rc) ? save_errno : 0;
 }

commit 1c3b726adde83ddf80a3970d33cfbee17c80f789

[#779] [V63014] [#560] Fix [clang-analyzer-deadcode.DeadStores] warning from clang-tidy in sr_unix/gtm_c_stack_trace_semop.c
Background
----------
* Below was a warning from the `test-clang-tidy` pipeline job that we noticed after merging GT.M V6.3-014.

  ```c
  sr_unix/gtm_c_stack_trace_semop.c:109:7: warning: Value stored to 'rc' is never read [clang-analyzer-deadcode.DeadStores]
                                rc = -1;
                                ^    ~~
  sr_unix/gtm_c_stack_trace_semop.c:109:7: note: Value stored to 'rc' is never read
                                rc = -1;
                                ^    ~~
  ```

Issue
-----
* Turns out this was a regression introduced in eb7e5dd1 where a `while ((-1 == rc) && (EINTR == save_errno))`
  at the end of a `do/while` loop was replaced with a `while (TRUE)` because checks for `rc` and `save_errno`
  were placed at the beginning of the loop (i.e. right after the `do`). But it was not realized then that
  there is a set of `rc` and `save_errno` variables towards the end of the `do/while` loop too. Therefore
  removing the `rc` check from the `while` meant the `rc` set towards the end of the loop did not break
  out of the `do/while` loop and this was unintended.

* GT.M V6.3-014 had rearranged the code enough for `clang-tidy` to expose this issue.

Fix
---
* The fix is to check if the inner `for` loop terminated prematurely (i.e. `loopcount < nsops`) and if so
  `break` out of the outer `do/while` loop too.

sr_unix/gtm_c_stack_trace_semop.c
@@ -121,6 +121,13 @@ int try_semop_get_c_stack(int semid, struct sembuf sops[], int nsops)
 					}
 				}
 			}
+			if (loopcount < nsops)
+			{	/* This means we did a "break" out of the inner "for" loop above due to an error in "semctl()".
+				 * Since that would have set "rc" and "save_errno" to appropriate values, "break" out of the
+				 * outer "do/while" loop as well and return.
+				 */
+				break;
+			}
 			new_timeout = TRUE; /* Start a new "semtimedop()" call for another MAX_SEM_WAIT_TIME_IN_SECONDS seconds */
 		}
 	} while (TRUE);