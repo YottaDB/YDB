f437dd38a0eb403d09efbe9ae8150156b32cd148
Narayanan Iyer <nars@yottadb.com>
2022-10-18 10:04:41 -0400

    [#491] Handle case where ttyname() can return NULL even if isatty() returns TRUE
    * @ztmr reported this issue.  See https://gitlab.com/YottaDB/DB/YDB/-/issues/491#note_895186879
      and surrounding discussion for more details on use cases (e.g. K8s, nix etc.) where `ttyname()`
      can return NULL even though `isatty()` returns TRUE.
    
    * In such cases, `sr_unix/io_init_name.c` would SIG-11 which is unfriendly. So we fix that by
      treating the device the same way we would if `isatty()` had returned FALSE in the first place.
    
    * This change should fix the SIG-11. Besides the name that we set here is not considered critical
      enough to affect anything else later in the code.

--- sr_unix/io_init_name.c
+++ sr_unix/io_init_name.c
@@ -2,7 +2,7 @@
  *								*
  * Copyright 2001, 2002 Sanchez Computer Associates, Inc.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -23,36 +23,44 @@
 GBLDEF mstr	sys_input;
 GBLDEF mstr	sys_output;
 
+/* We have seen "isatty()" return TRUE but "ttyname()" return NULL in some cases (e.g. K8s, nix etc.)
+ * See https://gitlab.com/YottaDB/DB/YDB/-/issues/491#note_895186879 and surrounding discussion for details.
+ * Hence we treat that case as if "isatty()" returned FALSE (i.e. it is not a terminal) and give it a default name.
+ */
+#define SET_TTYNAME(FD, DEVICE, NOTTY_NAME)				\
+{									\
+	boolean_t	ttyname_set;					\
+									\
+	ttyname_set = FALSE;						\
+	if (isatty(FD))							\
+	{								\
+		char    *temp;						\
+									\
+		temp = TTYNAME(FD);					\
+		if (NULL != temp)					\
+		{							\
+			int	i;					\
+			char    *c;					\
+									\
+			ttyname_set = TRUE;				\
+			for (i = 1, c = temp; *c != '\0'; i++)		\
+				c++;					\
+			DEVICE.addr = (char *)malloc(i);		\
+			memcpy(DEVICE.addr, temp, i);			\
+			DEVICE.len = i - 1;				\
+		}							\
+	}								\
+	if (!ttyname_set)						\
+	{								\
+		DEVICE.addr = NOTTY_NAME;				\
+		assert(1 == STRLEN(NOTTY_NAME));			\
+		DEVICE.len = 1;						\
+	}								\
+}
+
 void io_init_name(void)
 {
-	char    *temp, *c;
-	short   i;
-
-	if (isatty(0))
-	{
-		temp = TTYNAME(0);
-		for(i = 1, c = temp; *c != '\0'; i++)
-			c++;
-		sys_input.addr = (char*) malloc(i);
-		memcpy(sys_input.addr,temp, i);
-		sys_input.len = i - 1;
-	} else
-	{
-		sys_input.addr = "0";
-		sys_input.len = 1;
-	}
-	if (isatty(1))
-	{
-		temp = TTYNAME(1);
-		for(i = 1, c = temp; *c != '\0'; i++)
-			c++;
-		sys_output.addr = (char *)malloc(i);
-		memcpy(sys_output.addr,temp, i);
-		sys_output.len = i - 1;
-	} else
-	{
-		sys_output.addr = "&";
-		sys_output.len = 1;
-	}
+	SET_TTYNAME(0, sys_input, "0");
+	SET_TTYNAME(1, sys_output, "&");
 	return;
 }
