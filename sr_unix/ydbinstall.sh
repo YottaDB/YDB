YottaDB/YDB

commit 92daf751313be32175399d90c44c91a418fc1f9e

[#306][YottaDB/Util/YDBEncrypt#1] Move Encryption Plugin to YDBEncrypt repo
This commit makes the changes necessary to move the encryption plugin from the YDB repo to the YDBEncrypt repo.

* Deleting encryption plugin related files from the YDB repo unless they are needed for a YottaDB build.

* Modifying the ydbinstall script's `--encplugin` option to install the encryption plugin from the YDBEncrypt repo on gitlab

* Modifying the configure script to remove the commands that create an encryption plugin tarball in $ydb_dist

* Modifying CMakeLists to remove encryption plugin related targets

* Modifies the pre-commit hook to print an alert during the commit if changes in the YDBEncrypt repo are needed (such as during upstream version merges and changes to files needed by both YDB and YDBEncrypt)

sr_unix/ydbinstall.sh
@@ -260,20 +260,23 @@ install_plugins()
 	fi
 
 	if [ "Y" = $ydb_encplugin ] ; then
-		echo "Now installing YDBCrypt"
+		echo "Now installing YDBEncrypt"
 		cd $tmpdir	# Get back to top level temporary directory as the current directory
 		export ydb_icu_version=$ydb_icu_version
-		mkdir enc_tmp
-		cd enc_tmp
-		tar -xf ${ydb_installdir}/plugin/gtmcrypt/source.tar
-		ydb_dist=${ydb_installdir} make -j `grep -c ^processor /proc/cpuinfo`
-		if ydb_dist=${ydb_installdir} make install; then
-			# Save the build directory if the make install command returns a non-zero exit code. Otherwise, remove it.
-			cd ..
-			rm -R enc_tmp
+		mkdir enc_tmp && cd enc_tmp
+		url="https://gitlab.com/YottaDB/Util/YDBEncrypt.git"
+		if git clone -q ${url} .; then
+			ydb_dist=${ydb_installdir} make -j `grep -c ^processor /proc/cpuinfo`
+			if ydb_dist=${ydb_installdir} make install; then
+				# Save the build directory if the make install command returns a non-zero exit code. Otherwise, remove it.
+				cd ..
+				sudo rm -R enc_tmp
+			else
+				echo "Encryption plugin build failed. The build directory ($PWD/enc_tmp) has been saved."
+				remove_tmpdir=0
+			fi
 		else
-			echo "Encryption plugin build failed. The build directory ($PWD/enc_tmp) has been saved."
-			remove_tmpdir=0
+			echo "Failed to download YDBEncrypt. Check your internet connection. URL is ${url}"
 		fi
 		# rename gtmcrypt to ydbcrypt and create a symbolic link for backward compatibility
 		mv ${ydb_installdir}/plugin/gtmcrypt ${ydb_installdir}/plugin/ydbcrypt
@@ -669,7 +672,7 @@ if [ "Y" = "$ydb_plugins_only" ]; then
 	# without overwriting if --aim and/or --posix is already installed and --octo is selected.
 	if [ "Y" != "$gtm_overwrite_existing" ] ; then
 		if [ "Y" = $ydb_encplugin ] && [ -e $ydb_installdir/plugin/libgtmcrypt.so ] ; then
-			echo "YDBCrypt already installed and --overwrite-existing not specified. Exiting." ; err_exit
+			echo "YDBEncrypt already installed and --overwrite-existing not specified. Exiting." ; err_exit
 		fi
 		if [ "Y" = $ydb_posix ] && [ -e $ydb_installdir/plugin/libydbposix.so ] ; then
 			if [ "Y" = $ydb_octo ] ; then

CMakeLists.txt
@@ -233,16 +233,11 @@ set_source_list(gtcm_server      gtcm_server dlopen_libyottadb)
 set_source_list(gtcm_shmclean    gtcm_shmclean dlopen_libyottadb)
 set_source_list(gtmsecshr        gtmsecshr_wrapper)
 set_source_list(gtmsecshr_real   gtmsecshr)
-set_source_list(libgtmcrypt      gtmcrypt_ref gtmcrypt_pk_ref gtmcrypt_dbk_ref gtmcrypt_sym_ref gtmcrypt_util)
-set_source_list(libgtmcrypt      ydb_getenv minimal_gbldefs)
-set_source_list(libgtmtls        gtm_tls_impl gtmcrypt_util)
 set_source_list(libyottadb       gtm_main mupip_main mupip_cmd dse_main dse_cmd lke_main lke_cmd)
 set_source_list(libyottadb       gtcm_play_main omi_srvc_xct omi_extstr)
 set_source_list(libyottadb       gtcm_server_main gtcm_gnp_server_main gtcm_gnp_clitab gtcm_shmclean_main)
 set_source_list(libyottadb       dbcertify_main dbcertify_cmd ftok_main)
 set_source_list(lke              lke dlopen_libyottadb)
-set_source_list(maskpass         maskpass gtmcrypt_util)
-set_source_list(maskpass         ydb_getenv minimal_gbldefs)
 set_source_list(yottadb          gtm dlopen_libyottadb)
 set_source_list(mupip            mupip dlopen_libyottadb)
 set_source_list(semstat2         semstat2)
@@ -492,111 +487,6 @@ foreach(tlslib ssl crypto config)
   # Append the found library to the list
   set(TLS_LIBRARIES ${TLS_LIBRARIES} ${TLSLIB_${tlslib}})
   message(STATUS "TLSLIB_${tlslib}=${TLSLIB_${tlslib}}")
-endforeach()
-
-# Building the three encryption libraries could by a loop of some sort, but
-# manually creating each target is way easier.
-
-# Library=GCRYPT Algorithm=AES256CFB
-include_directories (/usr/local/ssl/include)
-add_library(libgtmcrypt_gcrypt_AES256CFB.so MODULE ${libgtmcrypt_SOURCES})
-set_target_properties(libgtmcrypt_gcrypt_AES256CFB.so PROPERTIES
-  OUTPUT_NAME gtmcrypt_gcrypt_AES256CFB
-  COMPILE_DEFINITIONS "USE_GCRYPT -DUSE_AES256CFB"
-  LIBRARY_OUTPUT_DIRECTORY ${YDB_BINARY_DIR}/plugin
-  )
-add_dependencies(libgtmcrypt_gcrypt_AES256CFB.so gen_gtm_threadgbl_deftypes)
-target_link_libraries(libgtmcrypt_gcrypt_AES256CFB.so ${GPG_LIBRARIES})
-install(TARGETS libgtmcrypt_gcrypt_AES256CFB.so DESTINATION ${YDB_INSTALL_DIR}/plugin)
-
-# Establish the default encryption link
-set(default_encr_link   ${YDB_BINARY_DIR}/plugin/libgtmcrypt.so)
-set(default_encr_target libgtmcrypt_gcrypt_AES256CFB.so)
-add_custom_command(
-  OUTPUT ${default_encr_link}
-  COMMAND ${CMAKE_COMMAND} -E create_symlink "${default_encr_target}" "${default_encr_link}"
-  DEPENDS ${YDB_BINARY_DIR}/plugin/${default_encr_target}
-  COMMENT "Generating default_encr symbolic link"
-)
-add_custom_target(install_default_encr ALL DEPENDS ${default_encr_link})
-install(FILES ${default_encr_link} DESTINATION ${YDB_INSTALL_DIR}/plugin)
-
-# Library=OPENSSL Algorithm=AES256CFB
-add_library(libgtmcrypt_openssl_AES256CFB MODULE ${libgtmcrypt_SOURCES})
-set_target_properties(libgtmcrypt_openssl_AES256CFB PROPERTIES
-  OUTPUT_NAME gtmcrypt_openssl_AES256CFB
-  COMPILE_DEFINITIONS "USE_OPENSSL -DUSE_AES256CFB"
-  LIBRARY_OUTPUT_DIRECTORY ${YDB_BINARY_DIR}/plugin
-  )
-target_link_libraries(libgtmcrypt_openssl_AES256CFB ${GPG_LIBRARIES} ${TLS_LIBRARIES})
-add_dependencies(libgtmcrypt_openssl_AES256CFB gen_gtm_threadgbl_deftypes)
-install(TARGETS libgtmcrypt_openssl_AES256CFB DESTINATION ${YDB_INSTALL_DIR}/plugin)
-
-add_library(libgtmtls MODULE ${libgtmtls_SOURCES})
-set_target_properties(libgtmtls PROPERTIES
-  OUTPUT_NAME gtmtls
-  COMPILE_DEFINITIONS "USE_OPENSSL"
-  LIBRARY_OUTPUT_DIRECTORY ${YDB_BINARY_DIR}/plugin
-  )
-target_link_libraries(libgtmtls ${TLS_LIBRARIES})
-add_dependencies(libgtmtls gen_gtm_threadgbl_deftypes)
-install(TARGETS libgtmtls DESTINATION ${YDB_INSTALL_DIR}/plugin)
-
-add_executable(maskpass ${maskpass_SOURCES})
-target_link_libraries(maskpass ${GPG_LIBRARIES} ${TLS_LIBRARIES})
-set_target_properties(maskpass PROPERTIES
-  COMPILE_DEFINITIONS "USE_GCRYPT -DUSE_SYSLIB_FUNCS"
-  RUNTIME_OUTPUT_DIRECTORY ${YDB_BINARY_DIR}/plugin/gtmcrypt
-  )
-install(TARGETS maskpass DESTINATION ${YDB_INSTALL_DIR}/plugin/gtmcrypt)
-
-# Always copy files into the plugin directory
-foreach(f_in
-      ${YDB_SOURCE_DIR}/sr_unix/Makefile.mk
-      ${YDB_SOURCE_DIR}/sr_unix/encrypt_sign_db_key.sh
-      ${YDB_SOURCE_DIR}/sr_unix/gen_keypair.sh
-      ${YDB_SOURCE_DIR}/sr_unix/gen_sym_hash.sh
-      ${YDB_SOURCE_DIR}/sr_unix/gen_sym_key.sh
-      ${YDB_SOURCE_DIR}/sr_unix/gtm_tls_impl.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtm_tls_impl.h
-      ${YDB_SOURCE_DIR}/sr_unix/ydb_tls_interface.h
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_dbk_ref.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_dbk_ref.h
-      ${YDB_SOURCE_DIR}/sr_unix/ydbcrypt_interface.h
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_pk_ref.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_pk_ref.h
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_ref.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_ref.h
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_sym_ref.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_sym_ref.h
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_util.c
-      ${YDB_SOURCE_DIR}/sr_unix/gtmcrypt_util.h
-      ${YDB_SOURCE_DIR}/sr_unix/import_and_sign_key.sh
-      ${YDB_SOURCE_DIR}/sr_unix/maskpass.c
-      ${YDB_SOURCE_DIR}/sr_port/minimal_gbldefs.c
-      ${YDB_SOURCE_DIR}/sr_unix/pinentry-gtm.sh
-      ${YDB_SOURCE_DIR}/sr_unix/pinentry.m
-      ${YDB_SOURCE_DIR}/sr_unix/show_install_config.sh
-      ${YDB_SOURCE_DIR}/sr_port/ydb_getenv.c
-      ${YDB_SOURCE_DIR}/sr_port/ydb_getenv.h
-      ${YDB_SOURCE_DIR}/sr_port/ydb_logicals.h
-      ${YDB_SOURCE_DIR}/sr_port/ydb_logicals_tab.h
-    )
-  get_filename_component(f ${f_in} NAME)
-  string(REGEX REPLACE ".mk$" "" f_mod "${f}")
-  set(f_out "${YDB_BINARY_DIR}/plugin/gtmcrypt/${f_mod}")
-  add_custom_command(
-    OUTPUT "${f_out}"
-    DEPENDS "${f_in}"
-    COMMAND ${CMAKE_COMMAND} -E copy "${f_in}" "${f_out}"
-    )
-  if("${f}" MATCHES "\\.sh$")
-    set(permissions PERMISSIONS ${install_permissions_script})
-  else()
-    set(permissions "")
-  endif()
-  install(FILES "${f_out}" DESTINATION ${YDB_INSTALL_DIR}/plugin/gtmcrypt ${permissions})
-  list(APPEND files_to_place "${f_out}")
 endforeach()
 
 install(TARGETS

ci/tidy_warnings_debug.ref
@@ -93,7 +93,6 @@ gtm_semutils.c:warning: The right operand of '!=' is a garbage value [clang-anal
 gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (182 padding bytes, where 6 is optimal).
 gtm_threadgbl_init.c:warning: Dereference of null pointer [clang-analyzer-core.NullDereference]
 gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (182 padding bytes, where 6 is optimal).
-gtm_tls_impl.c:warning: Value stored to 'rv' is never read [clang-analyzer-deadcode.DeadStores]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmrecv.c:warning: Assigned value is garbage or undefined [clang-analyzer-core.uninitialized.Assign]

ci/tidy_warnings_release.ref
@@ -129,14 +129,11 @@ gtm_putmsg_list.c:warning: Value stored to 'i' is never read [clang-analyzer-dea
 gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (166 padding bytes, where 6 is optimal).
 gtm_threadgbl_init.c:warning: Dereference of null pointer [clang-analyzer-core.NullDereference]
 gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (166 padding bytes, where 6 is optimal).
-gtm_tls_impl.c:warning: Value stored to 'rv' is never read [clang-analyzer-deadcode.DeadStores]
 gtm_trigger.c:warning: Dereference of null pointer [clang-analyzer-core.NullDereference]
 gtm_trigger.c:warning: Value stored to 'added' is never read [clang-analyzer-deadcode.DeadStores]
 gtm_trigger.c:warning: Value stored to 'rc' is never read [clang-analyzer-deadcode.DeadStores]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
-gtmcrypt_util.c:warning: The left operand of '^' is a garbage value [clang-analyzer-core.UndefinedBinaryOperatorResult]
-gtmcrypt_util.c:warning: Value stored to 'have_hash' is never read [clang-analyzer-deadcode.DeadStores]
 gtmrecv.c:warning: Assigned value is garbage or undefined [clang-analyzer-core.uninitialized.Assign]
 gtmrecv.c:warning: Value stored to 'log_init_status' is never read [clang-analyzer-deadcode.DeadStores]
 gtmrecv_comm_init.c:warning: Assigned value is garbage or undefined [clang-analyzer-core.uninitialized.Assign]

pre-commit
@@ -207,4 +207,22 @@ foreach file ($filelist)
 	endif
 end
 
+# -----------------------------------------------------------------
+# 4) Alerts if YDBEncrypt needs to be updated
+# -----------------------------------------------------------------
+set enclist = "sr_unix/Makefile.mk sr_unix/encrypt_sign_db_key.sh sr_unix/gen_keypair.sh sr_unix/gen_sym_hash.sh"
+set enclist = "$enclist sr_unix/gen_sym_key.sh sr_unix/gtm_tls_impl.c sr_unix/gtm_tls_impl.h sr_unix/gtmcrypt_dbk_ref.c"
+set enclist = "$enclist sr_unix/gtmcrypt_dbk_ref.h sr_unix/gtmcrypt_pk_ref.c sr_unix/gtmcrypt_pk_ref.h sr_unix/gtmcrypt_ref.c"
+set enclist = "$enclist sr_unix/gtmcrypt_ref.h sr_unix/gtmcrypt_sym_ref.c sr_unix/gtmcrypt_sym_ref.h sr_unix/gtmcrypt_util.c"
+set enclist = "$enclist sr_unix/gtmcrypt_util.h sr_unix/import_and_sign_key.sh sr_unix/maskpass.c sr_unix/pinentry-gtm.sh"
+set enclist = "$enclist sr_unix/pinentry.m sr_unix/show_install_config.sh sr_port/minimal_gbldefs.c sr_port/ydb_getenv.c"
+set enclist = "$enclist sr_port/ydb_getenv.h sr_port/ydb_logicals.h sr_port/ydb_logicals_tab.h sr_unix/ydb_tls_interface.h"
+set enclist = "$enclist sr_unix/ydbcrypt_interface.h"
+foreach file ($filelist)
+	foreach encfile ($enclist)
+		if ("$file" == "$encfile") then
+			echo "$file modified. Need to update equivalent file in YDBEncrypt accordingly."
+		endif
+	end
+end
 exit $exitstatus

sr_unix/Makefile.mk
@@ -1,251 +1,0 @@
-#################################################################
-#								#
-# Copyright (c) 2013-2019 Fidelity National Information		#
-# Services, Inc. and/or its subsidiaries. All rights reserved.	#
-#								#
-# Copyright (c) 2017-2021 YottaDB LLC and/or its subsidiaries.	#
-# All rights reserved.						#
-#								#
-# Copyright (c) 2017-2018 Stephen L Johnson.			#
-# All rights reserved.						#
-#								#
-#	This source code contains the intellectual property	#
-#	of its copyright holder(s), and is made available	#
-#	under a license.  If you do not know the terms of	#
-#	the license, please stop and do not read further.	#
-#								#
-#################################################################
-
-# Default configuration parameters. These values can be overridden by passing different values to build different build targets. For
-# the list of available build targets, see below.
-image = DEBUG
-thirdparty = gcrypt
-algo = AES256CFB
-# Add options for code scanning service
-scan = FALSE
-# If the machine has a libgcrypt version <= 1.4.1, then FIPS mode cannot be turned on.
-gcrypt_nofips = 0
-
-# Verify that $ydb_dist is defined
-ifndef ydb_dist
-$(error $$ydb_dist not defined!)
-endif
-
-DISTDIR = $(ydb_dist)
-PLUGINDIR = $(DISTDIR)/plugin
-GTMCRYPTDIR = $(PLUGINDIR)/gtmcrypt
-CURDIR = `pwd`
-
-# Find out whether we are already in $ydb_dist/plugin/gtmcrypt directory.
-NOT_IN_GTMCRYPTDIR = $(shell [ "$(CURDIR)" = "$(GTMCRYPTDIR)" ] ; echo $$?)
-
-# Users may install YottaDB without UTF-8 support
-HAVE_UTF8 = $(shell [ -d "$(DISTDIR)/utf8" ] ; echo $$?)
-
-# Determine machine and OS type.
-UNAMESTR = $(shell uname -a)
-MACHTYPE = $(shell uname -m)
-
-ifneq (,$(findstring Linux,$(UNAMESTR)))
-	FILEFLAG = -L
-endif
-# 64 bit version of YottaDB? 0 for yes!
-BIT64 = $(shell file $(FILEFLAG) $(DISTDIR)/yottadb | grep -q -E '64-bit|ELF-64'; echo $$?)
-
-# Determine if GPG 2.1+ is installed
-HAVE_GPG21   = 0
-HAVE_GPGCONF = $(shell which gpgconf 2> /dev/null)
-ifneq ($(HAVE_GPGCONF),)
-	GPGBIN     = $(shell gpgconf | grep 'gpg:' | cut -d: -f3)
-	GPGVER     = $(shell $(GPGBIN) --version | head -n1 | cut -d' ' -f3)
-	HAVE_GPG21 = $(shell expr $(GPGVER) \>= 2.1.12)
-endif
-ifeq ($(HAVE_GPG21),1)
-	USE_LOOPBACK = "-DUSE_GPGME_PINENTRY_MODE_LOOPBACK"
-endif
-
-# Determine if libgcrypt is installed.
-HAVE_GPGCRYPT = $(shell which libgcrypt-config 2> /dev/null)
-
-# Default installation target. This allows for the build system to randomize `thirdparty' and `algo' thereby changing the default
-# gtmcrypt install link.
-install_targ = libgtmcrypt_$(thirdparty)_$(algo).so
-
-# Setup build type -- debug or production.
-ifneq ($(image),DEBUG)
-	debug_flag =
-	optimize = -O2
-else
-	ifneq ($(scan),TRUE)
-		debug_flag = -g -DDEBUG
-	else
-		debug_flag = -g3 -gdwarf-2 -O0 -fno-builtin -DDEBUG -DBYPASS_MEMCPY_OVERRIDE -DSTATIC_ANALYSIS
-	endif
-	optimize =
-endif
-
-CC = cc
-
-# Setup common compiler flags
-CFLAGS = $(debug_flag) $(optimize) -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D_LARGEFILE64_SOURCE=1 $(USE_LOOPBACK)
-
-ifneq ($(gcrypt_nofips),0)
-	gcrypt_nofips_flag = -DGCRYPT_NO_FIPS
-else
-	gcrypt_nofips_flag =
-endif
-
-# Set default extra CFLAGS and LDFLAGS for building maskpass and libgtmcryptutil.so. Both of these use SHA512 for password
-# obfuscation which must come from either OpenSSL or Libgcrypt. Historically, on AIX we have always made OpenSSL the default
-# library and on non-AIX platforms, it has been libgcrypt.
-default_thirdparty_CFLAGS = -DUSE_GCRYPT
-default_thirdparty_LDFLAGS = -lgcrypt -lgpg-error
-
-# Platform specific compiler and linker flags
-LIBFLAGS =
-IFLAGS =
-# Linux
-ifneq (,$(findstring Linux,$(UNAMESTR)))
-	# -fPIC for Position Independent Code.
-	CFLAGS += -fPIC -std=c99 -D_GNU_SOURCE -D_XOPEN_SOURCE=600
-	LDFLAGS =
-	# So that dependent libraries are loaded from the parent library's load path at runtime
-	RPATHFLAGS = -Wl,-rpath,'$$ORIGIN'
-	# So that we can build shared library.
-	LDSHR = -shared
-	IFLAGS += -I /usr/local/ssl/include
-	ifeq ($(BIT64),0)
-		LIBFLAGS += -L /usr/local/ssl/lib -L /usr/lib/x86_64-linux-gnu
-	else ifneq (,$(findstring arm,$(MACHTYPE)))
-		IFLAGS += -I /usr/include/openssl
-		# Below is where Debian on ARM has ICU installed
-		IFLAGS += -I /usr/lib/arm-linux-gnueabi
-		LIBFLAGS += -L /usr/lib/arm-linux-gnueabi
-		# Below is where Raspbian on ARM has ICU installed
-		IFLAGS += -I /usr/lib/arm-linux-gnueabihf
-		LIBFLAGS += -L /usr/lib/arm-linux-gnueabihf
-	else
-		LIBFLAGS += -L /usr/local/ssl/lib -L /usr/lib/x86-linux-gnu
-	endif
-endif
-
-# AIX
-ifneq (,$(findstring AIX,$(UNAMESTR)))
-	# -qchars=signed forces `char' type to be treated as signed chars.
-	# -qsrcmsg enhances error reporting.
-	# -qmaxmem=-1 does not limit the amount of memory used for local tables of specific,
-	#	memory-intensive operations (in kilobytes).
-	CFLAGS += -qchars=signed -qsrcmsg -qmaxmem=-1 -D_TPARM_COMPAT
-
-	# -qro places string literals in read-only storage.
-	# -qroconst places constants in read-only storage.
-	# -q64 forces 64-bit object generation
-	CFLAGS += -qro -qroconst -q64 -qlanglvl=stdc99
-	# -q64 for 64-bit object generation
-	# -brtl for allowing both '.a' and '.so' to be searched at runtime.
-	# -bhalt:5 is to disable warnings about duplicate symbols that come from
-	#  libgtmcryptutil.so and other .so that need pulling the same object
-	#  file (/lib/crt0_64.o)
-	LDFLAGS = -q64 -brtl -bhalt:5
-	RPATHFLAGS =
-	# -G so that we can build shared library
-	# -bexpall exports all symbols from the shared library.
-	# -bnoentry to tell the linker that shared library has no entry point.
-	LDSHR = -Wl,-G -bexpall -bnoentry
-	# On AIX, build maskpass and libgtmcryptutil.so with OpenSSL's libcrypto instead of libgcrypt.
-	default_thirdparty_CFLAGS = -DUSE_OPENSSL
-	default_thirdparty_LDFLAGS = -lcrypto
-	# Set the default library
-	thirdparty = openssl
-	install_targ = libgtmcrypt_$(thirdparty)_$(algo).so
-endif
-
-# Common header and library paths
-IFLAGS += -I /usr/local/include -I /usr/include -I $(ydb_dist) -I $(CURDIR)
-ifeq ($(BIT64),0)
-	LIBFLAGS += -L /usr/local/lib64
-	LIBFLAGS += -L /usr/local/lib -L /usr/lib64 -L /usr/lib -L /lib64 -L /lib -L `pwd`
-else
-	LIBFLAGS += -L /usr/local/lib32
-	LIBFLAGS += -L /usr/local/lib -L /usr/lib32 -L /usr/lib -L /lib32 -L /lib -L `pwd`
-endif
-
-CFLAGS += $(IFLAGS)
-LDFLAGS += $(LIBFLAGS) -o
-
-COMMON_LIBS = -lgtmcryptutil -lconfig
-
-# Lists of all files needed for building the encryption plugin.
-crypt_util_srcfiles = gtmcrypt_util.c minimal_gbldefs.c ydb_getenv.c
-crypt_util_hdrfiles = gtmcrypt_util.h ydbcrypt_interface.h ydb_getenv.h ydb_logicals.h ydb_logicals_tab.h
-crypt_srcfiles = gtmcrypt_ref.c gtmcrypt_pk_ref.c gtmcrypt_dbk_ref.c gtmcrypt_sym_ref.c
-crypt_hrdfiles = gtmcrypt_ref.h gtmcrypt_pk_ref.h gtmcrypt_dbk_ref.h gtmcrypt_sym_ref.h ydbcrypt_interface.h
-tls_srcfiles = gtm_tls_impl.c
-tls_hdrfiles = gtm_tls_impl.h ydb_tls_interface.h
-
-all: libgtmcryptutil.so maskpass gcrypt openssl libgtmtls.so
-
-libgtmcryptutil.so: $(crypt_util_srcfiles) $(crypt_util_hdrfiles)
-	@echo ; echo "Compiling $@..."
-	$(CC) $(CFLAGS) $(default_thirdparty_CFLAGS) $(crypt_util_srcfiles) $(LDSHR) $(LDFLAGS) $@ $(default_thirdparty_LDFLAGS)
-
-# Since maskpass is a standalone utility, link it (implicitly) with gtmcrypt_util.o instead of libgtmcryptutil.so. This allows
-# maskpass to be run without setting LD_LIBRARY_PATH/LIBPATH to load libgtmcryptutil.so. As a standalone utility maskpass should
-# not depend on functions like `gtm_malloc' and `gtm_free', so we are compiling the executable with -DUSE_SYSLIB_FUNCS.
-maskpass: maskpass.c $(crypt_util_srcfiles) $(crypt_util_hdrfiles)
-	@echo ; echo "Compiling $@..."
-	$(CC) $(CFLAGS) -DUSE_SYSLIB_FUNCS $(default_thirdparty_CFLAGS) maskpass.c $(crypt_util_srcfiles)	\
-		$(LDFLAGS) $@ $(default_thirdparty_LDFLAGS)
-
-ifneq ($(HAVE_GPGCRYPT),)
-gcrypt: libgtmcrypt_gcrypt_AES256CFB.so
-
-libgtmcrypt_gcrypt_AES256CFB.so: $(crypt_srcfiles) $(crypt_hdrfiles) libgtmcryptutil.so
-	@echo ; echo "Compiling $@..."
-	$(CC) $(CFLAGS) -DUSE_GCRYPT -DUSE_AES256CFB $(gcrypt_nofips_flag) $(crypt_srcfiles) $(LDSHR)		\
-		$(RPATHFLAGS) $(LDFLAGS) $@ -lgcrypt -lgpgme -lgpg-error $(COMMON_LIBS)
-else
-gcrypt:
-endif
-
-openssl: libgtmcrypt_openssl_AES256CFB.so
-
-libgtmcrypt_openssl_AES256CFB.so: $(crypt_srcfiles) $(crypt_hdrfiles) libgtmcryptutil.so
-	@echo ; echo "Compiling $@..."
-	$(CC) $(CFLAGS) -DUSE_OPENSSL -DUSE_AES256CFB $(crypt_srcfiles) $(LDSHR) $(RPATHFLAGS) $(LDFLAGS)	\
-		$@ -lcrypto -lgpgme -lgpg-error $(COMMON_LIBS)
-
-libgtmtls.so: $(tls_srcfiles) $(tls_hdrfiles) libgtmcryptutil.so
-	@echo ; echo "Compiling $@..."
-	$(CC) $(CFLAGS) $(tls_srcfiles) $(LDSHR) $(RPATHFLAGS) $(LDFLAGS) $@ -lssl $(COMMON_LIBS)
-
-install: all
-	@echo ; echo "Installing shared libraries to $(PLUGINDIR) and maskpass to $(PLUGINDIR)/gtmcrypt..."
-	mkdir -p $(PLUGINDIR)/o/utf8 $(PLUGINDIR)/r
-	cp -f *.so $(PLUGINDIR)
-	echo "$(PLUGINDIR)/libgtmcryptutil.so"                                                      > $(PLUGINDIR)/gpgagent.tab
-	echo "unmaskpwd: gtm_status_t gc_mask_unmask_passwd(I:gtm_string_t*,O:gtm_string_t*[512])" >> $(PLUGINDIR)/gpgagent.tab
-	ln -fs ./$(install_targ) $(PLUGINDIR)/libgtmcrypt.so
-	cp -pf pinentry.m $(PLUGINDIR)/r
-	(cd $(PLUGINDIR)/o      && env ydb_chset=M     ${ydb_dist}/yottadb $(PLUGINDIR)/r/pinentry.m)
-ifeq ($(NOT_IN_GTMCRYPTDIR),1)
-	cp -pf *.sh *.m $(GTMCRYPTDIR)/
-	cp -f maskpass $(GTMCRYPTDIR)/
-endif
-ifeq ($(HAVE_UTF8),0)
-	@echo "UTF-8 mode library installation may fail if ydb_icu_version/gtm_icu_version (${ydb_icu_version}/${gtm_icu_version}) is not set"
-	(cd $(PLUGINDIR)/o/utf8 && env ydb_chset=UTF-8 && env ydb_icu_version=${ydb_icu_version} ${ydb_dist}/yottadb $(PLUGINDIR)/r/pinentry.m)
-endif
-
-uninstall:
-	@echo ; echo "Uninstalling shared libraries from $(PLUGINDIR) and maskpass from $(PLUGINDIR)/gtmcrypt..."
-	rm -f $(PLUGINDIR)/gpgagent.tab
-	rm -f $(PLUGINDIR)/libgtmcrypt*.so $(PLUGINDIR)/libgtmtls*.so
-	rm -f $(PLUGINDIR)/gtmcrypt/maskpass
-
-clean:
-	@echo ; echo "Removing generated files..."
-	rm -f *.so *.o
-ifeq ($(NOT_IN_GTMCRYPTDIR),1)
-	rm -f maskpass
-endif

sr_unix/configure.gtc
@@ -556,78 +556,11 @@ if [ -f /usr/bin/chcon ]; then
 	(chcon -t texrel_shlib_t $ydbdist/libyottadb$ext; exit 0)
 fi
 
-# Create $ydbdist/plugin/gtmcrypt directory if this platform supports encryption
-
-# Define variables to denote plugin and gtmcrypt directory names
-plugin="plugin"
-plugin_gtmcrypt="$plugin/gtmcrypt"
-
-# Gtmcrypt scripts
-gtmcryptscripts="gen_sym_key.sh encrypt_sign_db_key.sh gen_keypair.sh pinentry-gtm.sh"
-gtmcryptscripts="$gtmcryptscripts import_and_sign_key.sh gen_sym_hash.sh show_install_config.sh"
-
-# Gtmcrypt related M file
-gtmcryptmfile="pinentry.m"
-
-# Gtmcrypt source files
-gtmcryptsrcfiles="Makefile README gtmcrypt_ref.c gtmcrypt_ref.h ydbcrypt_interface.h maskpass.c"
-gtmcryptsrcfiles="$gtmcryptsrcfiles gtmcrypt_dbk_ref.c gtmcrypt_dbk_ref.h gtmcrypt_pk_ref.c gtmcrypt_pk_ref.h"
-gtmcryptsrcfiles="$gtmcryptsrcfiles gtmcrypt_sym_ref.h gtmcrypt_sym_ref.c ydb_tls_interface.h gtm_tls_impl.h"
-gtmcryptsrcfiles="$gtmcryptsrcfiles gtm_tls_impl.c gtmcrypt_util.c gtmcrypt_util.h $gtmcryptmfile"
-gtmcryptsrcfiles="$gtmcryptsrcfiles minimal_gbldefs.c ydb_getenv.c ydb_getenv.h ydb_logicals.h ydb_logicals_tab.h"
-
-dogtmcrypt=0
-if [ -d "$plugin_gtmcrypt" ]; then
-	dogtmcrypt=1
-	# Create plugin directory and gtmcrypt directory
-	mkdir -p $ydbdist/plugin/gtmcrypt
-	chmod 0755 $ydbdist/plugin
-	chown $owner $ydbdist/plugin
-	chgrp $bingroup $ydbdist/plugin
-	chmod 0755 $ydbdist/plugin/gtmcrypt
-	chown $owner $ydbdist/plugin/gtmcrypt/
-	chgrp $bingroup $ydbdist/plugin/gtmcrypt
-
-	# Tar the source files
-	cat > $plugin_gtmcrypt/README << EOF
-#
-# WARNING: This file was generated by the YottaDB install script at install time.
-#
-# Dependency information:
-#  Please install a compiler toolchain and libgcrypt, libgpgme, libconfig, and
-#  libssl development libraries. On Debian you can do:
-#    sudo apt-get install libgcrypt11-dev libgpgme11-dev libconfig-dev libssl-dev
-#
-# Installation instructions:
-#  You may need to edit the Makefile to add include (IFLAGS) and library paths
-#  (LIBFLAGS) for your system.
-#
-#  The commands below to compile, install and set the file permissions on the
-#  encryption plugin libraries make the following assumptions:
-#    - The shell is bourne shell compatible
-#    - The user is root
-#    - The permissions defined for the gtm installation are appropriate for the
-#      plug-in
-#
-#  You should be able to cut and paste the following commands
-#
-ydb_dist="$ydbdist"
-export ydb_dist
-make && make install && make clean
-find \$ydb_dist/plugin -type f -exec chown ${owner}:${bingroup} {} +
-EOF
-	if [ "$group" != "" ] ; then
-		echo "chmod -R o-rwx \$ydb_dist/plugin" >> $plugin_gtmcrypt/README
-	fi
-	(cd $plugin_gtmcrypt			&& \
-	 chmod 0644 $gtmcryptsrcfiles		&& \
-	 chmod 0755 $gtmcryptscripts 		&& \
-	 chown ${owner}:${bingroup} *		&& \
-	 tar -cvf $ydbdist/$plugin_gtmcrypt/source.tar $gtmcryptsrcfiles $gtmcryptscripts >/dev/null 2>&1)
-	chmod 0644 $ydbdist/$plugin_gtmcrypt/source.tar
-	chown ${owner}:${bingroup} $ydbdist/$plugin_gtmcrypt/source.tar
-	rm $plugin_gtmcrypt/README
-fi
+# Create $ydbdist/plugin directory
+mkdir $ydbdist/plugin
+chmod 0755 $ydbdist/plugin
+chown $owner $ydbdist/plugin
+chgrp $bingroup $ydbdist/plugin
 
 # Install GDE, GTMHELP, and all the percent routines
 cp -p *.o *.m $ydbdist
@@ -662,9 +595,7 @@ if [ "$doutf8" -ne 0 ]; then
 			fi
 		fi
 	done
-	if [ "$dogtmcrypt" -ne 0 ]; then
-		ln -s ../plugin $ydbdist/utf8/plugin
-	fi
+	ln -s ../plugin $ydbdist/utf8/plugin
 	cd ..
 fi
 

commit 2659605e5f5fa4d036827282d3d49c4aad59fcd9

[#943] ydbinstall.sh should install YDBEncrypt scripts with execute permission set
Background
----------
* The below is pasted from the issue description at https://gitlab.com/YottaDB/DB/YDB/-/issues/943#description

* See https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/1022#note_1146940691 for details but the issue
  is that a lot of encryption related scripts had the execute permission removed from them as part of #306
  (released as part of YottaDB r1.34).

* The list of the scripts that are missing the execute permission is pasted below.

  ```sh
  $ ls -1 $ydb_dist/plugin/gtmcrypt/*.sh
  encrypt_sign_db_key.sh
  gen_keypair.sh
  gen_sym_hash.sh
  gen_sym_key.sh
  import_and_sign_key.sh
  pinentry-gtm.sh*
  show_install_config.sh
  ```

* All those scripts should have the execute permission set. But only `pinentry-gtm.sh` has it above as seen
  by the `*` suffix. This is a regression in 92daf751 that needs to be fixed.

Fix
---
* The fix is simple and is to do a `chmod +x` on all `*.sh` scripts in `$ydb_dist/plugin/ydbcrypt`.

sr_unix/ydbinstall.sh
@@ -337,6 +337,8 @@ install_plugins()
 		# rename gtmcrypt to ydbcrypt and create a symbolic link for backward compatibility
 		mv ${ydb_installdir}/plugin/gtmcrypt ${ydb_installdir}/plugin/ydbcrypt
 		ln -s ${ydb_installdir}/plugin/ydbcrypt ${ydb_installdir}/plugin/gtmcrypt
+		# Enable execute permissions on .sh scripts in the YDBEncrypt plugin
+		chmod +x ${ydb_installdir}/plugin/ydbcrypt/*.sh
 	fi
 
 	if [ "Y" = $ydb_zlib ] ; then