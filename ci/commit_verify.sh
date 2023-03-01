YottaDB/YDB

commit d418add2fbd699261287b0e2ec09896c08b7b41b

Added check for copyright-only changes to `commit-verify.sh`
Any YottaDB file that is modified must have its copyright notice updated to reflect the year of the latest change. If, however, the code changes in such a case are reverted, it is possible that the copyright notice is omitted from reversions. This results in an orphaned copyright notice revision, such that the only modification to such files is the copyright notice itself.

Such copyright-only changes are undesirable and easy to overlook. So, this commit adds an automatic check for such changes to `commit-verify.sh`. Now, this script checks for files modified by new commits that contain only copyright notice changes and issues an error and fails the script when it finds any.

ci/commit_verify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #################################################################
 #								#
-# Copyright (c) 2020-2022 YottaDB LLC and/or its subsidiaries.	#
+# Copyright (c) 2020-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
 #								#
 #	This source code contains the intellectual property	#
@@ -132,23 +132,43 @@ do
 done
 
 # First check if there is a non-zero list of commits to verify. If the commit list is empty, skip this verification step.
+exit_status=0
 if [ -n "$commit_list" ]; then
 	# Get file list from all commits at once, rather than per-commit
 	filelist="$(git show --pretty="" --name-only $commit_list | sort -u)"
 	missing_files=""
+	copyright_only=""
 	for file in $filelist; do
+		if [[ ! -e $file ]]; then
+			continue
+		fi
+		changes=$(git diff upstream_repo/$target_branch..HEAD "$file")
+		if [[ "" != "$changes" ]]; then
+			changes=$(echo -n "$changes" | grep -v "+++" | grep "^+" || true)
+			num_changes=$(echo "$changes" | wc -l)
+			num_copyright_changes=$(echo "$changes" | grep -c 'Copyright (c)' || true)
+			if [[ $num_changes -eq $num_copyright_changes ]]; then
+				copyright_only="$copyright_only $file"
+			fi
+		fi
 		if $needs_copyright $file && ! grep -q 'Copyright (c) .*'$curyear' YottaDB LLC' $file; then
 			# Print these out only at the end so they're all shown at once
 			missing_files="$missing_files $file"
 		fi
 	done
-	if [ -n "$missing_files" ]; then
-		echo "  --> Error: some files are missing a YottaDB Copyright notice and/or current year $curyear"
-		# Don't give duplicate errors for the same file
-		for file in $(echo $missing_files | tr ' ' '\n' | sort -u); do
+	if [ -n "$copyright_only" ]; then
+		echo "  --> Error: some files contain only copyright changes"
+		for file in $copyright_only; do
 			echo "	$file"
 		done
-		exit 1
+		exit_status=1
+	fi
+	if [ -n "$missing_files" ]; then
+		echo "  --> Error: some files are missing a YottaDB Copyright notice and/or current year $curyear"
+		for file in $missing_files; do
+			echo "	$file"
+		done
+		exit_status=1
 	fi
 fi
-
+exit $exit_status

commit e912848f41fecc0f93681001cb708d925c3cd5cd

Fix issue with checking copyright-only changes in ci/commit_verify.sh script
In the YDBGUI project, a file which contains deletions only, and where
the copyright was correctly updated, was erroneously claimed to be a
copyright only change. This is a regression in the change in
d418add2fbd699261287b0e2ec09896c08b7b41b. That commit only looked for
additions as
signifying a non-copyright only change, but we can also have deletions.

This is fixed by using `git diff --numstat` to count the additions and
deletions, and comparing this against the Copyright line changes, which
must be counted manually.

ci/commit_verify.sh
@@ -142,12 +142,14 @@ if [ -n "$commit_list" ]; then
 		if [[ ! -e $file ]]; then
 			continue
 		fi
-		changes=$(git diff upstream_repo/$target_branch..HEAD "$file")
-		if [[ "" != "$changes" ]]; then
-			changes=$(echo -n "$changes" | grep -v "+++" | grep "^+" || true)
-			num_changes=$(echo "$changes" | wc -l)
-			num_copyright_changes=$(echo "$changes" | grep -c 'Copyright (c)' || true)
-			if [[ $num_changes -eq $num_copyright_changes ]]; then
+		# Get number of changes (addition deletion file_name)
+		changes=$(git diff --numstat upstream_repo/$target_branch..HEAD "$file")
+		if [[ -n "$changes" ]]; then
+			additions=$(echo $changes | awk '{print $1}')
+			deletions=$(echo $changes | awk '{print $2}')
+			num_copyright_additions=$(git diff upstream_repo/$target_branch..HEAD "$file" | grep -e '^+' | grep -c 'Copyright (c)' || true)
+			num_copyright_deletions=$(git diff upstream_repo/$target_branch..HEAD "$file" | grep -e '^-' | grep -c 'Copyright (c)' || true)
+			if [[ ( $deletions == $num_copyright_deletions ) && ( $additions == $num_copyright_additions ) ]]; then
 				copyright_only="$copyright_only $file"
 			fi
 		fi