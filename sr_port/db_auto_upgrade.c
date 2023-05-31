YottaDB/YDB

commit 1ccb6b3e66b3a4f4c7d3dd2c5f2c9a41f101fb69

[#388] Changed timers from milliseconds to nanoseconds

sr_port/db_auto_upgrade.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2019 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2020 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -202,8 +202,13 @@ void db_auto_upgrade(gd_region *reg)
 				/* YottaDB r122 introduced "reorg_sleep_nsec" to slow down reorg update rate by user */
 				csd->reorg_sleep_nsec = 0;
 			case GDSMV63007:	/* Note: This is also the case for GDSMR122 */
-				break;
 			case GDSMR126:
+				csd->flush_time = csd->flush_time * NANOSECS_IN_MSEC;
+				/* Note: This is a little-endian solution and will need to be modified if we
+				 * ever support big-endian.
+				 */
+				break;
+			case GDSMR130:
 		/* When adding a new minor version, the following template should be maintained
 		 * a) If there are any file header fields added in the new minor version, initialize the fields to default values
 		 *    in the last case (i.e. above this comment block). Do not add a "break" for the above "case" block.

commit 911add9b6949fd044b99c2172a643803410cb262

[#780] [V70000] Fix csd->flush_time auto upgrade issue (regression in 1ccb6b3e)
Background
----------
* Below issue description is from https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/696#note_1411319099

  While working on YDB!1339, I noticed an issue in !696 (#388 changes 1ccb6b3e). And traced it back to
  this particular line (see MR link above for the actual code change line).

  If I create a database using r1.28, I see `dse dump -file` shows the flush timer is `01:00` (i.e. 1 second).

  **r1.28 dse dump -file**
  ```
    Cache freeze id                0x00000000  Flush timer            00:00:01:00
  ```

  Then I switch to r1.30 and run the same dse command on the existing database. I see the flush timer is a
  garbage value.

  **r1.30 dse dump -file**
  ```
    Cache freeze id                0x00000000  Flush timer            -39:-01:-01
  ```

  This is because `csd->flush_time` is an array of 2 4-byte quantities in r1.28 (where only the first 4-byte
  quantity noted the flush time in milliseconds) and was changed to be 1 8-byte quantity (that noted the
  flush time in nanoseconds). Therefore, when converting the r1.28 flush_time to r1.30, we should ignore
  the second 4-byte quantity. It so happens that is the value -1 (i.e. `ff ff ff ff`) and so the
  multiplication in line 206 ends up creating a huge number resulting in a negative flush time (huge positive
  number ends up being treated as a negative number) showing up in the dse dump -file output above.

Fix
---
* The fix is simple and is to ignore the higher-significant 4-bytes in the 8-byte quantity as part of the
  multiplication. This is taken care of in this commit. There were 2 places with similar code and both
  of them are fixed similarly.

sr_port/db_auto_upgrade.c
@@ -147,8 +147,12 @@ void db_auto_upgrade(gd_region *reg)
 				/* YottaDB r122 introduced "reorg_sleep_nsec" to slow down reorg update rate by user */
 				csd->reorg_sleep_nsec = 0;
 				/* ----------------------------------- */
-				/* YottaDB r130 changed "flush_time" from milliseconds to nanoseconds to support nanosecond timers */
-				csd->flush_time = csd->flush_time * NANOSECS_IN_MSEC;
+				/* YottaDB r130 changed "flush_time" from milliseconds to nanoseconds to support nanosecond
+				 * timers. In GT.M, "flush_time" is an array of 2 4-byte quantities but only the first 4-byte
+				 * is used. Whereas in YottaDB, "flush_time" is an 8-byte quantity. Therefore, nullify the
+				 * second 4-byte before doing the below multiplication to arrive at the 8-byte value.
+				 */
+				csd->flush_time = (csd->flush_time & (((uint8)1 << 32) - 1)) * NANOSECS_IN_MSEC;
 				/* Note: This is a little-endian solution and will need to be modified if we
 				 * ever support big-endian.
 				 */
@@ -165,16 +169,19 @@ void db_auto_upgrade(gd_region *reg)
 				csd->max_procs.time = 0;
 				break;
 			case GDSMR200:
-		/* When adding a new minor version, the following template should be maintained
-		 * 1) Remove the penultimate 'break' (i.e. "break" in the PREVIOUS "case" block.
-		 * 2) If there are any file header fields added in the new minor version, initialize the fields to default values
-		 *    in THIS/CURRENT case (i.e. above this comment block). Do not add a "break" for THIS/CURRENT "case" block.
-		 * 3) Then, add a NEW "case" statement with the new minor version. The below 3 lines become part of that "case".
-		 * 4) For every GT.M minor ver added since the previous YottaDB release, duplicate the relevant auto upgrade
-		 *    code in this case block. This is needed as the GT.M GDSMV* (e.g. GDSMV63012) values will be way less
-		 *    than the older YottaDB GDSMVCURR value (e.g. in case of YottaDB r1.32) and so those GT.M switch/case
-		 *    code paths above will not be reached for upgrades from an older YottaDB release to a newer YottaDB release.
-		 */
+				/* When adding a new minor version, the following template should be maintained
+				 * 1) Remove the penultimate 'break' (i.e. "break" in the PREVIOUS "case" block.
+				 * 2) If there are any file header fields added in the new minor version, initialize the fields
+				 *    to default values in THIS/CURRENT case (i.e. above this comment block). Do not add a
+				 *    "break" for THIS/CURRENT "case" block.
+				 * 3) Then, add a NEW "case" statement with the new minor version. The below 3 lines become
+				 *    part of that "case".
+				 * 4) For every GT.M minor ver added since the previous YottaDB release, duplicate the relevant
+				 *    auto upgrade code in this case block. This is needed as the GT.M GDSMV* (e.g. GDSMV63012)
+				 *    values will be way less than the older YottaDB GDSMVCURR value (e.g. in case of YottaDB
+				 *    r1.32) and so those GT.M switch/case code paths above will not be reached for upgrades
+				 *    from an older YottaDB release to a newer YottaDB release.
+				 */
 				/* Nothing to do for this version since it is GDSMVCURR for now. */
 				assert(FALSE);		/* When this assert fails, it means a new GDSMV* was created, */
 				break;			/* 	so a new "case" needs to be added BEFORE the assert. */
@@ -395,8 +402,12 @@ void v6_db_auto_upgrade(gd_region *reg)
 				MOVE_GVSTATS_REC_FROM_OLD_HDR_TO_NODE_LOCAL(csa, csd);
 			case GDSMV63014:
 			case GDSMR126:
-				/* YottaDB r130 changed "flush_time" from milliseconds to nanoseconds to support nanosecond timers */
-				csd->flush_time = csd->flush_time * NANOSECS_IN_MSEC;
+				/* YottaDB r130 changed "flush_time" from milliseconds to nanoseconds to support nanosecond
+				 * timers. In GT.M, "flush_time" is an array of 2 4-byte quantities but only the first 4-byte
+				 * is used. Whereas in YottaDB, "flush_time" is an 8-byte quantity. Therefore, nullify the
+				 * second 4-byte before doing the below multiplication to arrive at the 8-byte value.
+				 */
+				csd->flush_time = (csd->flush_time & (((uint8)1 << 32) - 1)) * NANOSECS_IN_MSEC;
 				/* Note: This is a little-endian solution and will need to be modified if we
 				 * ever support big-endian.
 				 */