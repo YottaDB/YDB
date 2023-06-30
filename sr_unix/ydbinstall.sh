YottaDB/YDB

commit 6bbb033f4729cfc552500a16137f624ed6672356

[#780] [V70000] Address merge conflicts involving deleted files
* This commit addresses merge conflicts involving deleted files.

* The list of deleted files was identified using the following commands as part of the prior
  commit (i.e. after the `git cherry-pick` command was run but before the `git commit` command
  was run).

  ```sh
  $ git status | grep 'deleted'
        deleted:    sr_unix/buildaux_dbcertify.csh
        deleted:    sr_unix/buildaux_ftok.csh
        deleted:    sr_unix/buildaux_semstat2.csh
        deleted:    sr_unix/dbcertify_cmd.c
        deleted:    sr_unix/dbcertify_dbfilop.c
        deleted:    sr_unix/dbcertify_parse_and_dispatch.c
        deleted by us:   README
        deleted by us:   sr_i386/cmerrors_ctl.c
        deleted by us:   sr_i386/cmierrors_ctl.c
        deleted by us:   sr_i386/gdeerrors_ctl.c
        deleted by them: sr_port/dbcertify.c
        deleted by them: sr_port/dbcertify.h
        deleted by them: sr_port/dbcertify_base_ch.c
        deleted by them: sr_port/dbcertify_certify_phase.c
        deleted by them: sr_port/dbcertify_funcs.c
        deleted by them: sr_port/dbcertify_scan_phase.c
        deleted by them: sr_port/dsewrap.mpt
        deleted by us:   sr_port/get_spec.c
        deleted by them: sr_port/v5cbsu.m
        deleted by us:   sr_unix/Makefile.mk
        deleted by them: sr_unix/dbcertify_deferred_exit_handler.c
        deleted by them: sr_unix/dbcertify_signal_handler.c
        deleted by them: sr_unix/ftok.c
        deleted by us:   sr_unix/gtcmstub.c
        deleted by us:   sr_unix/gtm_startup_chk.c
        deleted by us:   sr_unix/gtm_test_install.csh
        deleted by us:   sr_unix/gtm_tls_impl.c
        deleted by us:   sr_unix/gtmcrypt_dbk_ref.c
        deleted by us:   sr_unix/gtmcrypt_dbk_ref.h
        deleted by us:   sr_unix/gtmcrypt_pk_ref.c
        deleted by us:   sr_unix/gtmcrypt_ref.c
        deleted by us:   sr_unix/gtmcrypt_util.c
        deleted by us:   sr_unix/gtmcrypt_util.h

sr_unix/ydbinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #################################################################
-# Copyright (c) 2014-2019 Fidelity National Information		#
-# Services, Inc. and/or its subsidiaries. All rights reserved.	#
+# Copyright (c) 2014-2020 Fidelity National Information         #
+# Services, Inc. and/or its subsidiaries. All rights reserved.  #
 #								#
 # Copyright (c) 2017-2023 YottaDB LLC and/or its subsidiaries.	#
 # All rights reserved.						#
@@ -74,7 +74,6 @@ if [ -z "$USER" ] ; then USER=`id -un` ; fi
 dump_info()
 {
 	set +x
-	if [ -n "$icu_version" ] ; then echo icu_version " : " $icu_version ; fi
 	if [ -n "$timestamp" ] ; then echo timestamp " : " $timestamp ; fi
 	if [ -n "$ydb_aim" ] ; then echo ydb_aim " : " $ydb_aim ; fi
 	if [ -n "$ydb_branch" ] ; then echo ydb_branch " : " $ydb_branch ; fi
@@ -94,6 +93,7 @@ dump_info()
 	if [ -n "$ydb_pkgconfig" ] ; then echo ydb_pkgconfig " : " $ydb_pkgconfig ; fi
 	if [ -n "$ydb_posix" ] ; then echo ydb_posix " : " $ydb_posix ; fi
 	if [ -n "$ydb_routines" ] ; then echo ydb_routines " : " $ydb_routines ; fi
+	if [ -n "$ydb_utf8" ] ; then echo ydb_utf8 " : " $ydb_utf8 ; fi
 	if [ -n "$ydb_version" ] ; then echo ydb_version " : " $ydb_version ; fi
 	if [ -n "$ydb_zlib" ] ; then echo ydb_zlib " : " $ydb_zlib ; fi
 	if [ -n "$gtm_arch" ] ; then echo gtm_arch " : " $gtm_arch ; fi
@@ -168,7 +168,7 @@ help_exit()
 	echo "--prompt-for-group	-> YottaDB installation script will prompt for group; default is yes for production releases V5.4-002 or later, no for all others"
 	echo "--ucaseonly-utils		-> install only upper case utility program names; defaults to both if not specified"
 	echo "--user username		-> user who should own YottaDB installation; default is root"
-	echo "--utf8 ICU_version	-> install UTF-8 support using specified major.minor ICU version; specify default to use version provided by OS as default"
+	echo "--utf8                    -> install UTF-8 support"
 	echo "--verbose			-> output diagnostic information as the script executes; default is to run quietly"
 	echo "--zlib			-> download and install the zlib plugin"
 	echo "Options that take a value (e.g, --group) can be specified as either --option=value or --option value."
@@ -178,8 +178,8 @@ help_exit()
 	echo ""
 	echo "Example usages are (assumes latest YottaDB release is r1.38 and latest GT.M version is V7.0-005)"
 	echo "	$0				# installs latest YottaDB release (r1.38) at /usr/local/lib/yottadb/r138"
-	echo "	$0 --utf8 default		# installs YottaDB release r1.38 with added support for UTF-8"
-	echo "	$0 --installdir /r138 r1.38 # installs YottaDB r1.38 at /r138"
+	echo "	$0 --utf8			# installs YottaDB release r1.38 with added support for UTF-8"
+	echo "	$0 --installdir /r138 r1.38	# installs YottaDB r1.38 at /r138"
 	echo "	$0 --gtm			# installs latest GT.M version (V7.0-005) at /usr/local/lib/fis-gtm/V7.0-005_x86_64"
 	echo ""
 	echo "As options are processed left to right, later options can override earlier options."
@@ -580,10 +580,27 @@ while [ $# -gt 0 ] ; do
 				fi
 			fi
 			shift ;;
-		--utf8*) tmp=`echo $1 | cut -s -d = -f 2- | tr DEFAULT default`
-			if [ -n "$tmp" ] ; then icu_version=$tmp
-			else retval=`isvaluevalid $# $2` ; if [ "$retval" -eq 0 ] ; then icu_version=`echo $2 | tr DEFAULT default`; shift
-				else echo "--utf8 needs a value" ; err_exit
+		--utf8*)
+			# We previously used to support "--utf8=default" or "--utf8 default" or "--utf8=7.1" or "--utf8 7.1" syntax
+			# where an ICU version of "default" or "7.1" (for example) needed to be specified. But in r1.40 (after merging
+			# GT.M V7.0-000 changes to sr_unix/gtminstall.sh), sr_unix/ydbinstall.sh started supporting "--utf8" with no value.
+			# To maintain backward compatibility, we allow for "--utf8=default" or "--utf8 default" syntax but issue an error
+			# if "--utf8=7.1" or "--utf8 7.1" syntax is specified.
+			tmp=`echo $1 | cut -s -d = -f 2- | tr DEFAULT default`
+			if [ -n "$tmp" ] ; then
+				if [ "default" != "$tmp" ] ; then
+				    echo "Only value permitted with --utf8= is [default]" ; err_exit
+				fi
+			fi
+			if [ 1 -lt $# ] ; then
+				tmp=`echo "$2" | grep -c '^--'`
+				if [ "$tmp" -eq 0 ] ; then
+					tmp=`echo $2 | tr DEFAULT default`; shift
+					if [ -n "$tmp" ] ; then
+						if [ "default" != "$tmp" ] ; then
+							echo "Only value permitted with --utf8 is [default]" ; err_exit
+						fi
+					fi
 				fi
 			fi
 			ydb_utf8="Y" ;
@@ -714,12 +731,8 @@ fi
 
 # Get actual ICU version if UTF-8 install was requested with "default" ICU version
 if [ "Y" = "$ydb_utf8" ] ; then
-	if [ "default" = $icu_version ] ; then
-		ydb_found_or_requested_icu_version=`icu_version`
-	else
-		ydb_found_or_requested_icu_version=$icu_version
-	fi
-	# At this point "ydb_found_or_requested_icu_version" holds the user specified or implicitly determined
+	ydb_found_or_requested_icu_version=`icu_version`
+	# At this point "ydb_found_or_requested_icu_version" holds the implicitly determined
 	# ICU version to use. Set and export the "ydb_icu_version" env var to reflect this value.
 	# This is used in various places (e.g. "install_plugins" call below, "cmake" call below).
 	ydb_icu_version=$ydb_found_or_requested_icu_version
@@ -808,7 +821,7 @@ if [ -n "$ydb_from_source" ] ; then
 	if [ "Y" = "$gtm_prompt_for_group" ] ; then install_options="${install_options} --prompt-for-group" ; fi
 	if [ "N" = "$gtm_lcase_utils" ] ; then install_options="${install_options} --ucaseonly-utils" ; fi
 	if [ -n "$gtm_user" ] ; then install_options="${install_options} --user ${gtm_user}" ; fi
-	if [ "Y" = "$ydb_utf8" ] ; then install_options="${install_options} --utf8 ${icu_version}" ; fi
+	if [ "Y" = "$ydb_utf8" ] ; then install_options="${install_options} --utf8" ; fi
 	if [ "Y" = "$gtm_verbose" ] ; then install_options="${install_options} --verbose" ; fi
 	if [ "Y" = "$ydb_zlib" ] ; then install_options="${install_options} --zlib" ; fi
 
@@ -880,7 +893,7 @@ if [ "Y" = "$ydb_plugins_only" ]; then
 			ydb_utf8="N"
 		fi
 	fi
-	# else use ydb_found_or_requested_icu_version determined from what the user had specified along with the --utf8 option
+	# else "$ydb_found_or_requested_icu_version" would already be set because the user had specified the --utf8 option
 	# Check that the plugins aren't already installed or that --overwrite-existing is selected
 	# Since selected --octo automatically selects --posix and --aim, we continue the install
 	# without overwriting if --aim and/or --posix is already installed and --octo is selected.
@@ -915,10 +928,9 @@ if [ "Y" = "$ydb_plugins_only" ]; then
 			echo "YDBOcto already installed and --overwrite-existing not specified. Exiting." ; err_exit
 		fi
 	fi
-	tmpdir=`mktmpdir`
-	ydb_routines="$tmp($ydb_installdir)" ; export ydb_routines
-	remove_tmpdir=1 # remove the tmpdir if the plugin installs are successful
-	install_plugins
+	tmpdir=`mktmpdir`	# used by the "install_plugins" function call below.
+	remove_tmpdir=1		# Assume the plugin installs will be successful by default.
+	install_plugins		# sets "remove_tmpdir" to 0 if there was an error installing one or more plugins
 	if [ 0 = "$remove_tmpdir" ] ; then exit 1; fi	# error seen while installing one or more plugins
 	rm -rf $tmpdir	# Now that we know it is safe to remove $tmpdir, do that before returning normal status
 	exit 0
@@ -1350,13 +1362,11 @@ fi
 	echo y			# Response to one of two possible questions
 				#	"Directory $ydb_dist exists. If you proceed with this installation then some files will be over-written. Is it ok to proceed?"
 				#	"Directory $ydb_dist does not exist. Do you wish to create it as part of this installation? (y or n)"
-	if [ -z "$icu_version" ] ; then echo n	# Response to : "Should UTF-8 support be installed?"
-	else echo y			# Response to : "Should UTF-8 support be installed?"
-		echo y			# Response to : "Should an ICU version other than the default be used?"
-		echo $ydb_found_or_requested_icu_version	# Response to : "Enter ICU version"
+	if [ "Y" != "$ydb_utf8" ] ; then echo n		# Response to : "Should UTF-8 support be installed?"
+	else echo y					# Response to : "Should UTF-8 support be installed?"
 	fi
-	if [ "Y" = $ydb_deprecated ] ; then echo y # Response to : "Should deprecated components be installed?"
-	else echo n			# Response to : "Should deprecated components be installed?"
+	if [ "Y" = $ydb_deprecated ] ; then echo y	# Response to : "Should deprecated components be installed?"
+	else echo n					# Response to : "Should deprecated components be installed?"
 	fi
 	echo $gtm_lcase_utils	# Response to : "Do you want uppercase and lowercase versions of the MUMPS routines?"
 	if [ "Y" = $gtm_shlib_support ] ; then echo $gtm_keep_obj ; fi	# Response to : "Object files of M routines placed in shared library $ydb_dist/libyottadbutil$ext. Keep original .o object files (y or n)?"

Dockerfile
@@ -100,7 +100,7 @@ RUN --mount=type=bind,from=ydb-release-builder,source=/tmp/yottadb-release,targe
                     wget \
                     netbase \
                     && \
-    /tmp/ydb-release/ydbinstall --utf8 default --installdir /opt/yottadb/current && \
+    /tmp/ydb-release/ydbinstall --utf8 --installdir /opt/yottadb/current && \
     apt-get remove -y wget && \
     apt-get autoclean -y && \
     apt-get clean && \

Dockerfile-debian
@@ -124,7 +124,7 @@ WORKDIR /data
 COPY --from=ydb-release-builder /tmp/yottadb-release /tmp/yottadb-release
 RUN cd /tmp/yottadb-release  \
  && ./ydbinstall \
-      --utf8 default \
+      --utf8 \
       --installdir /opt/yottadb/current \
       --octo \
       --gui \

Dockerfile-rocky
@@ -94,7 +94,7 @@ RUN --mount=type=bind,from=ydb-release-builder,source=/tmp/yottadb-release,targe
 		    nano \
 		    gzip \
 		    && \
-    /tmp/ydb-release/ydbinstall --utf8 default --installdir /opt/yottadb/current && \
+    /tmp/ydb-release/ydbinstall --utf8 --installdir /opt/yottadb/current && \
     yum remove -y wget && \
     yum clean all && \
     rm -rf /var/cache/yum && \

README
@@ -1,80 +1,0 @@
-All software in this package is part of FIS GT.M (http://fis-gtm.com)
-which is Copyright 2021 Fidelity Information Services, Inc., and
-provided to you under the terms of a license. If there is a COPYING
-file included in this package, it contains the terms of the license under
-which the package is provided to you. If there is not a COPYING file in
-the package, you must ensure that your use of FIS GT.M complies with the
-license under which it is provided. If you are unsure as to the terms of
-your license, please consult with the entity that provided you with the
-package.
-
-GT.M relies on CMake to generate the Makefiles to build GT.M from source. The
-prerequisites are CMake (at least 2.8.5), GNU make (at least 3.81), Linux
-(either x86 or x86_64), libraries and development files for libz, Unicode(R),
-OpenSSL and GPG. Debian 7, Ubuntu 12.04/14.04 LTS and RHEL 6.0 were used to do
-the test builds for this distribution. The default ICU and GPG packages were
-taken from the distribution repositories.
-
-To build GT.M for Linux, do the following steps:
-
-1. Fulfill the pre-requisites
-   Install developement libraries
-   cmake tcsh {libconfig,libelf,libgcrypt,libgpg-error,libgpgme11,libicu,libncurses,libssl,zlib1g}-dev
-   There may be other library dependencies or the packages may have different names
-   If CMake issues a NOTFOUND error, please see the FAQ below.
-
-2. Unpack the GT.M sources
-   The GT.M source tarball extracts to a directory with the version number in
-   the name, fis-gtm-V7.0-000
-   $ tar xfz fis-gtm-V7.0-000.tar.gz
-   $ cd fis-gtm-V7.0-000
-
-   You should find this README, LICENSE, COPYING and CMakeLists.txt file and
-   sr_* source directories.
-
-3. Building GT.M -
-   <fis-gtm-build> can be a sub directory of the source directory,
-   fis-gtm-V7.0-000, or any other valid path.
-
-   $ mkdir <fis-gtm-build>
-   $ cd <fis-gtm-build>
-
-   # [optional] If you installed GT.M, provide the directory path to cmake
-   #   -D GTM_DIST:PATH=$gtm_dist
-   #
-   # By default the build produces release versions of GT.M. To build a debug
-   # version of GT.M supply the following parameter to cmake
-   #     -D CMAKE_BUILD_TYPE=DEBUG
-   #
-   # Note that the cmake install does not create the final installed GT.M.
-   # Instead, it stages GT.M for distribution. Change the CMAKE_INSTALL_PREFIX
-   # to place the staged files in a local directory. To install GT.M, you must
-   # cd to that installed directory and execute the configure script.
-   #
-   #     -D CMAKE_INSTALL_PREFIX:PATH=${PWD}/package
-   #
-   $ cmake -D CMAKE_INSTALL_PREFIX:PATH=${PWD}/package <path to>/fis-gtm-V7.0-000
-
-   $ make
-
-   $ make install
-
-   $ cd package/lib/fis-gtm/V7.0-000_x86_64
-
-   # Now you are ready to install GT.M. Answer a few questions and install it.
-   # The recommended installation path is /opt/fis-gtm/V7.0-000_x86_64
-
-   $ sudo ./configure
-
-   $ make clean
-
-4. Packaging GT.M -
-   Create a tar file from the installed directory
-
-FAQ:
-- The CMake build fails with the following message followed by one or more cases.
-	CMake Error: The following variables are used in this project, but they are set to NOTFOUND.
-	Please set them or make sure they are set and tested correctly in the CMake files:
-  This indicates that required libraries are not found. Please consult the list
-  of libraries and check your distributions package manager.
-

sr_i386/cmerrors_ctl.c
@@ -1,44 +1,0 @@
-/****************************************************************
- *								*
- * Copyright (c) 2001-2021 Fidelity National Information 	*
- * Services, Inc. and/or its subsidiaries. All rights reserved.	*
- *								*
- *	This source code contains the intellectual property	*
- *	of its copyright holder(s), and is made available	*
- *	under a license.  If you do not know the terms of	*
- *	the license, please stop and do not read further.	*
- *								*
- ****************************************************************/
-#include "mdef.h"
-#include "error.h"
-
-LITDEF	err_msg cmerrors[] = {
-	{ "INVPROT", "Invalid protocol specified by remote partner", 0, 0 },
-	{ "REGNTFND", "Region referenced not initialized", 0, 0 },
-	{ "CMINTQUE", "Interlock failure accessing GT.CM server queue", 0, 0 },
-	{ "INVINTMSG", "Invalid interrupt message received.", 0, 0 },
-	{ "CMEXCDASTLM", "Exceeded AST limit. Cannot open database.", 0, 0 },
-	{ "CMSYSSRV", "Error doing system service, status:", 0, 0 },
-};
-
-LITDEF	int CMERR_INVPROT = 150568970;
-LITDEF	int CMERR_REGNTFND = 150568978;
-LITDEF	int CMERR_CMINTQUE = 150568988;
-LITDEF	int CMERR_INVINTMSG = 150568994;
-LITDEF	int CMERR_CMEXCDASTLM = 150569002;
-LITDEF	int CMERR_CMSYSSRV = 150569010;
-
-
-LITDEF	int cmerrors_undocarr[] = {
-};
-
-
-GBLDEF	err_ctl cmerrors_ctl = {
-	249,
-	"GTCM",
-	&cmerrors[0],
-	6,
-	&cmerrors_undocarr[0],
-	0
-};
-

sr_i386/cmierrors_ctl.c
@@ -1,82 +1,0 @@
-/****************************************************************
- *								*
- * Copyright (c) 2001-2021 Fidelity National Information 	*
- * Services, Inc. and/or its subsidiaries. All rights reserved.	*
- *								*
- *	This source code contains the intellectual property	*
- *	of its copyright holder(s), and is made available	*
- *	under a license.  If you do not know the terms of	*
- *	the license, please stop and do not read further.	*
- *								*
- ****************************************************************/
-#include "mdef.h"
-#include "error.h"
-
-LITDEF	err_msg cmierrors[] = {
-	{ "DCNINPROG", "Attempt to initiate operation while disconnect was in progress", 0, 0 },
-	{ "LNKNOTIDLE", "Attempt to initiate operation before previous operation completed", 0, 0 },
-	{ "ASSERT", "Assert failed !AD line !UL", 3, 0 },
-	{ "CMICHECK", "Internal CMI error. Report to your GT.M Support Channel.", 0, 0 },
-	{ "NETFAIL", "Failure of Net operation", 0, 0 },
-	{ "BADPORT", "Environment variable GTCM_TCP_PORT is not an integer", 0, 0 },
-	{ "NOTND", "tnd argument to cmi_init is NULL", 0, 0 },
-	{ "OVERRUN", "mbf argument in CLB is not large enough for packet", 0, 0 },
-	{ "NOSERVENT", "Sevices data lookup failure", 0, 0 },
-	{ "BADIPADDRPORT", "Bad specification of [ip address:port] in tnd", 0, 0 },
-	{ "REASON_CONNECT", "Incoming connection", 0, 0 },
-	{ "REASON_INTMSG", "Incoming urgent data", 0, 0 },
-	{ "REASON_DISCON", "Disconnect encountered", 0, 0 },
-	{ "REASON_ABORT", "Link aborted", 0, 0 },
-	{ "REASON_EXIT", "Exit", 0, 0 },
-	{ "REASON_PATHLOST", "Network path lost", 0, 0 },
-	{ "REASON_PROTOCOL", "Protocol error", 0, 0 },
-	{ "REASON_THIRDPARTY", "Thirdparty error", 0, 0 },
-	{ "REASON_TIMEOUT", "Network timeout", 0, 0 },
-	{ "REASON_NETSHUT", "Shutdown received", 0, 0 },
-	{ "REASON_REJECT", "Connection rejected", 0, 0 },
-	{ "REASON_IODONE", "I/O done", 0, 0 },
-	{ "REASON_OVERRUN", "Input overran buffer", 0, 0 },
-	{ "REASON_STATUS", "Status", 0, 0 },
-	{ "REASON_CONFIRM", "Confirm", 0, 0 },
-};
-
-LITDEF	int CMI_DCNINPROG = 150634508;
-LITDEF	int CMI_LNKNOTIDLE = 150634516;
-LITDEF	int CMI_ASSERT = 150634522;
-LITDEF	int CMI_CMICHECK = 150634532;
-LITDEF	int CMI_NETFAIL = 150634538;
-LITDEF	int CMI_BADPORT = 150634546;
-LITDEF	int CMI_NOTND = 150634556;
-LITDEF	int CMI_OVERRUN = 150634562;
-LITDEF	int CMI_NOSERVENT = 150634570;
-LITDEF	int CMI_BADIPADDRPORT = 150634578;
-LITDEF	int CMI_REASON_CONNECT = 150634586;
-LITDEF	int CMI_REASON_INTMSG = 150634594;
-LITDEF	int CMI_REASON_DISCON = 150634602;
-LITDEF	int CMI_REASON_ABORT = 150634610;
-LITDEF	int CMI_REASON_EXIT = 150634618;
-LITDEF	int CMI_REASON_PATHLOST = 150634626;
-LITDEF	int CMI_REASON_PROTOCOL = 150634634;
-LITDEF	int CMI_REASON_THIRDPARTY = 150634642;
-LITDEF	int CMI_REASON_TIMEOUT = 150634650;
-LITDEF	int CMI_REASON_NETSHUT = 150634658;
-LITDEF	int CMI_REASON_REJECT = 150634666;
-LITDEF	int CMI_REASON_IODONE = 150634674;
-LITDEF	int CMI_REASON_OVERRUN = 150634682;
-LITDEF	int CMI_REASON_STATUS = 150634690;
-LITDEF	int CMI_REASON_CONFIRM = 150634698;
-
-
-LITDEF	int cmierrors_undocarr[] = {
-};
-
-
-GBLDEF	err_ctl cmierrors_ctl = {
-	250,
-	"CMI",
-	&cmierrors[0],
-	25,
-	&cmierrors_undocarr[0],
-	0
-};
-

commit aed0e780a675f1d5a22c35830f09a04d5fed1974

[#780] [V70000] Fix ydbinstall --utf8 to work for r1.38 and older releases (regression in 6bbb033f)
Background
----------
* Below is a summary of the issue. Details can be seen from the discussion thread at
  https://gitlab.com/YottaDB/DB/YDB/-/issues/780#note_1453547732

* When the latest/master `ydbinstall.sh` is used to install binaries of an older release of YottaDB
  (e.g. r1.38 etc.), it does not work right in the UTF-8 install part.

  That is, it would say install successful, but if one tries to look at the `utf8` subdirectory under
  `$ydb_dist` (the installed directory) one would not find it clearly indicating the UTF-8 install
  did not succeed.

Issue
-----
* As part of merging GT.M V7.0-000, 6bbb033f made a change to `sr_unix/ydbinstall.sh` that removed
  support for example usages like `ydbinstall.sh --utf8 7.1` (to indicate ICU version 7.1 needs to be
  used etc.).

* This meant removing answers to 2 questions in the `ydbinstall.sh` script that corresponded to which
  ICU version needed to be installed as part of the `--utf8` install.

* But this caused a regression when installing older released versions of YottaDB using the latest
  `sr_unix/ydbinstall.sh` script since the latest script was not providing the answers to the 2 additional
  questions that the older release `sr_unix/configure.gtc` script was expecting.

Fix
---
* If the YottaDB version being installed is `r1.38` or older, `sr_unix/ydbinstall.sh` is changed to
  provide 2 additional answers in case of a `--utf8` install.

sr_unix/ydbinstall.sh
@@ -1378,6 +1378,21 @@ fi
 	if [ "Y" != "$ydb_utf8" ] ; then echo n		# Response to : "Should UTF-8 support be installed?"
 	else echo y					# Response to : "Should UTF-8 support be installed?"
 	fi
+	# YottaDB r1.38 and older releases had 2 additional questions in the "sr_unix/configure.gtc" script in case
+	# they were asked to install with "--utf8". Add answers to those questions. In such cases, prior releases
+	# allowed for an arbitrary ICU version to be installed. But the current ydbinstall.sh does not allow for such
+	# an option so the user cannot pass a specific ICU version to the older release configure script using the
+	# post-r1.38/latest ydbinstall script. Such an option was never used by anyone in our understanding. All users only
+	# cared for the latest ICU version to be installed so take care of that using "$ydb_found_or_requested_icu_version"
+	# variable.
+	if expr "r1.38" \>= "${ydb_version}" >/dev/null; then
+		# If --utf8 was not specified, there are no additional questions in the older release. So skip the below logic.
+		if [ "Y" = "$ydb_utf8" ] ; then
+			# Add answers to the additional 2 questions that older release configure.gtc script expects.
+			echo y			# Response to : "Should an ICU version other than the default be used?"
+			echo $ydb_found_or_requested_icu_version	# Response to : "Enter ICU version"
+		fi
+	fi
 	if [ "Y" = $ydb_deprecated ] ; then echo y	# Response to : "Should deprecated components be installed?"
 	else echo n					# Response to : "Should deprecated components be installed?"
 	fi