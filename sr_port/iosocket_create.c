YottaDB/YDB

commit 231f90a6de77afc8e0f23f803f97247cb5ad5eb7

[#996] Allow passing of LISTENING TCP sockets
Background
----------
* Currently, one is able to pass LISTENING LOCAL sockets, CONNECTED LOCAL sockets and
  CONNECTED TCP sockets to another process using `WRITE /PASS` and `WRITE /ACCEPT` syntax (see
  https://docs.yottadb.com/ProgrammersGuide/ioproc.html#write-command for details).

* But if one tries to pass LISTENING TCP sockets to another process, one gets the below error as part of
  the `WRITE /ACCEPT`.

  ```
  %YDB-E-GETSOCKNAMERR, Getting the socket name failed from getsockname(): (errno==107) Transport endpoint is not connected
  ```

Issue
-----
* As part of the `write /accept` command, we invoke the following function call graph
  - iosocket_iocontrol() -> iosocket_pass_local() -> iosocket_create()

* In `iosocket_create()`, if we find that the socket is already passed via a LOCAL socket and is a TCP socket,
  we do a `getpeername()` call to determine the remote side of the connection. And if that call returns -1,
  we issue a GETSOCKNAMERR error.

* This logic does not take into account that the socket could be a LISTENING socket (i.e. is not yet a CONNECTED
  socket). Therefore it is possible the `getpeername()` call returns -1 with a `ENOTCONN` errno.

Fix
---
* The fix is simple and is to allow for the `ENOTCONN` error from the `getpeername()` call and continue with the
  normal code flow (instead of issuing an error).

  There is pre-existing logic later in `iosocket_create()` that determines whether this is a LISTENING or CONNECTED
  socket before assigning the state of this socket. Therefore no other changes are needed to make this use case work.

sr_port/iosocket_create.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -320,11 +320,19 @@ socket_struct *iosocket_create(char *sockaddr, uint4 bfsize, int file_des, boole
 			if (-1 == getpeername(socketptr->sd, SOCKET_REMOTE_ADDR(socketptr), &tmp_addrlen))
 			{
 				save_errno = errno;
-				errptr = (char *)STRERROR(save_errno);
-				tmplen = STRLEN(errptr);
-				SOCKET_FREE(socketptr);
-				rts_error_csa(CSA_ARG(NULL) VARLSTCNT(5) ERR_GETSOCKNAMERR, 3, save_errno, tmplen, errptr);
-				return NULL;
+				/* Note that it is possible that even though the socket is inherited from a JOB command
+				 * or xinetd or passed through LOCAL socket, it is possible the socket is a LISTENING socket.
+				 * Therefore, it is possible to see a ENOTCONN errno. Allow for that. Issue error otherwise.
+				 */
+				if (ENOTCONN != save_errno)
+				{
+					assert(FALSE);
+					errptr = (char *)STRERROR(save_errno);
+					tmplen = STRLEN(errptr);
+					SOCKET_FREE(socketptr);
+					rts_error_csa(CSA_ARG(NULL) VARLSTCNT(5) ERR_GETSOCKNAMERR, 3, save_errno, tmplen, errptr);
+					return NULL;
+				}
 			}
 		} else if (socket_local == protocol)
 		{

commit 99c64bd463e33f1d25bf9bc6687fa0ac50bc1f56

[#996] Issue GETSOCKNAMERR error (not SIG-11) if LISTENING socket passed as STDIN of JOB (regression in 231f90a6)
Background
----------
* While trying out some examples of passing sockets through the JOB command, I tried out an example
  where I passed a LISTENING socket as the stdin/stdout of the child process.

* Below is a simple M program that implements the above use case.

  ```m
  $ cat listenjobpass
  listenjobpass;
          set s="socket"
          ; open a listening socket
          open s:(listen="19355:TCP":attach="handle1"):15:"socket"
          ; move listening socket to "socketpool" device
          use s:(detach="handle1")
          ; job a child and make it inherit the listening socket as its stdin/stdout
          job child:(INPUT="SOCKET:handle1":OUTPUT="SOCKET:handle1":ERROR="child.mje")
          quit
  child   ;
          quit
  ```

* When I ran this test, I got a SIG-11.

  ```sh
  $ yottadb -run listenjobpass
  %YDB-E-JOBFAIL, JOB command failure,%YDB-I-TEXT, Job error in fork
                  At M source location +7^tcp1

  $ cat child.mje
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 16008 has been killed by a signal 11 at address 0x00007FC9B23C345E (vaddr 0x0000000000000000)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* The SIG-11 happened at line 566 below because `socketptr->remote.saddr_ip` was NULL.

  **sr_port/iosocket_open.c**
  ```c
  565   if (socket_tcpip == socketptr->protocol)
  566           STRNCPY_STR(&ioptr->dollar.key[len], socketptr->remote.saddr_ip, DD_BUFLEN - len - 1);
  ```

* Before 231f90a6, the `iosocket_create()` call (which happens in `iosocket_open.c` before line 565)
  would have issued a `GETSOCKNAMERR` error and so it would not have let us get to line 565.

* The reason we got to the SIG-11 point is because 231f90a6 allowed for a LISTENING (i.e. not connected)

sr_port/iosocket_create.c
@@ -273,6 +273,8 @@ socket_struct *iosocket_create(char *sockaddr, uint4 bfsize, int file_des, boole
 		return socketptr;
 	} else
 	{	/* socket already setup by inetd or passed via JOB or LOCAL socket */
+		DEBUG_ONLY(boolean_t	is_enotconn_error);
+
 		SOCKET_ALLOC(socketptr);
 		socketptr->sd = file_des;
 		socketptr->temp_sd = FD_INVALID;
@@ -305,6 +307,7 @@ socket_struct *iosocket_create(char *sockaddr, uint4 bfsize, int file_des, boole
 		ai_ptr->ai_addrlen = tmp_addrlen;
 		ai_ptr->ai_family = SOCKET_LOCAL_ADDR(socketptr)->sa_family;
 		ai_ptr->ai_socktype = SOCK_STREAM;
+		DEBUG_ONLY(is_enotconn_error = FALSE);
 		if (socket_tcpip == protocol)
 		{	/* extract port information */
 			GETNAMEINFO(SOCKET_LOCAL_ADDR(socketptr), tmp_addrlen, host_buffer, NI_MAXHOST, port_buffer,
@@ -321,19 +324,20 @@ socket_struct *iosocket_create(char *sockaddr, uint4 bfsize, int file_des, boole
 			if (-1 == getpeername(socketptr->sd, SOCKET_REMOTE_ADDR(socketptr), &tmp_addrlen))
 			{
 				save_errno = errno;
-				/* Note that it is possible that even though the socket is inherited from a JOB command
-				 * or xinetd or passed through LOCAL socket, it is possible the socket is a LISTENING socket.
-				 * Therefore, it is possible to see a ENOTCONN errno. Allow for that. Issue error otherwise.
-				 */
-				if (ENOTCONN != save_errno)
+				/* Note: We allow LISTENING sockets to be passed through a LOCAL socket (YDB#996). In that
+				 * case, allow for a ENOTCONN error. Issue error otherwise. This is easily identifier by checking
+				 * for "sockaddr" being NULL. "iosocket_pass_local()" uses this special first parameter value
+				 * whereas other callers of "iosocket_create()" use a non-NULL value.
+                                 */
+				if ((NULL != sockaddr) || (ENOTCONN != save_errno))
 				{
-					assert(FALSE);
 					errptr = (char *)STRERROR(save_errno);
 					tmplen = STRLEN(errptr);
 					SOCKET_FREE(socketptr);
 					rts_error_csa(CSA_ARG(NULL) VARLSTCNT(5) ERR_GETSOCKNAMERR, 3, save_errno, tmplen, errptr);
 					return NULL;
 				}
+				DEBUG_ONLY(is_enotconn_error = TRUE);
 			}
 		} else if (socket_local == protocol)
 		{
@@ -360,6 +364,7 @@ socket_struct *iosocket_create(char *sockaddr, uint4 bfsize, int file_des, boole
 			socketptr->passive = TRUE;
 		} else
 		{
+			assert(!is_enotconn_error);
 			socketptr->state = socket_connected;
 			socketptr->passive = FALSE;
 		}