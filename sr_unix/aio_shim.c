bd75377bd291d2c74935b1affd31be91446f4a72
Narayanan Iyer <nars@yottadb.com>
2023-09-05 14:43:29 -0400

    [#835] [V70001] Remove copyright changes for files that had no changes after merge conflict resolution
    * This is needed to fix the commit-verify pipeline job failure.
    
    * See https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/1323#note_1396533670 for more details
      (note that discussion happened for GT.M V7.0-000 merge but is relevant even for the V7.0-001 merge).

--- sr_unix/aio_shim.c
+++ sr_unix/aio_shim.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2016-2021 Fidelity National Information	*
+ * Copyright (c) 2016-2020 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
  * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
