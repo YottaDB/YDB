YottaDB/YDB

commit 39cdd8abe17f5da3fc940491a431f68bfff6e935

Fix [-Werror,-Wstrict-prototypes] error in sr_unix/libyottadb.h when compiling with clang
* As part of YDBTest@0d51a044, installing plugins like YDBOcto etc. during `ydbinstall` was
  changed to use `clang` as the compiler in case `libyottadb.so` was built with CLANG + ASAN.

* After that change, it was noticed that the YDBGUI and YDBSodium plugins failed the install as
  follows.

  **YDBGUI**
  ```sh
  $ cat YDBGUI.err
  In file included from /tmp/tmp.RqX9D49GEX/YDBGUI_tmp/YDBGUI-master/build/_deps/ydbsodium-src/src/sodium_plugin.c:14:
  sudo_0/plugins/install/libyottadb.h:288:36: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    288 | typedef int             (*ydb_pointertofunc_t)();
        |                                               ^
        |                                                void
  sudo_0/plugins/install/libyottadb.h:289:39: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    289 | typedef void            (*ydb_funcptr_retvoid_t)();
        |                                                 ^
        |                                                  void
  sudo_0/plugins/install/libyottadb.h:347:37: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    347 | typedef uintptr_t       (*ydb_vplist_func)();           /* Indeterminate argument list for called variadic function */
        |                                           ^
        |                                            void
  ```

  **YDBSodium**
  ```sh
  $ cat YDBSodium.err
  In file included from /tmp/tmp.RqX9D49GEX/YDBSodium_tmp/YDBSodium-master/src/sodium_plugin.c:14:
  sudo_0/plugins/install/libyottadb.h:288:36: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]

sr_unix/libyottadb.h
@@ -285,8 +285,8 @@ typedef uint64_t	ydb_uint64_t;		/* Only currently usable in 64 bit mode */
 typedef	float		ydb_float_t;
 typedef	double		ydb_double_t;
 typedef	char		ydb_char_t;
-typedef	int		(*ydb_pointertofunc_t)();
-typedef	void		(*ydb_funcptr_retvoid_t)();
+typedef	int		(*ydb_pointertofunc_t)(void);
+typedef	void		(*ydb_funcptr_retvoid_t)(void);
 
 /* Structure for passing (non-NULL-terminated) character arrays whose length corresponds to the value of the
  * 'length' field. Note that for output-only ydb_string_t * arguments the 'length' field is set to the
@@ -344,7 +344,7 @@ typedef	ydb_char_t	ydb_jbig_decimal_t;
 /* Function pointer types for use in the APIs below */
 typedef	int		(*ydb_tpfnptr_t)(void *tpfnparm);					   /* For use in SimpleAPI */
 typedef	int		(*ydb_tp2fnptr_t)(uint64_t tptoken, ydb_buffer_t *errstr, void *tpfnparm); /* For use in SimpleThreadAPI */
-typedef	uintptr_t	(*ydb_vplist_func)();		/* Indeterminate argument list for called variadic function */
+typedef	uintptr_t	(*ydb_vplist_func)(void);	/* Indeterminate argument list for called variadic function */
 typedef void		(*GPCallback)(int sigtype);	/* Type of Go callback "panic" routine */
 
 #	ifdef GTM_PTHREAD

commit fe88980e5563a155e609863c484bc54cb4554050

Fix ydb_funcptr_retvoid_t prototype (regression in 39cdd8ab)
* 39cdd8ab changed the prototype of `ydb_funcptr_retvoid_t` to address a warning. But it did not fix it
  completely. The parameter list should not have been `void` but instead should have matched the parameter
  list at https://docs.yottadb.com/MultiLangProgGuide/cprogram.html#ydb-timer-start-ydb-timer-start-t.

  This prototype-mismatch caused test failures in the following subtests in the YDBTest project.
  - simpleapi/utilfuncs
  - simpleapi/ydb550
  - simplethreadapi/utilfuncs
  - simplethreadapi/ydb550

* This prototype mismatch is fixed in the current commit.

sr_unix/libyottadb.h
@@ -286,7 +286,7 @@ typedef	float		ydb_float_t;
 typedef	double		ydb_double_t;
 typedef	char		ydb_char_t;
 typedef	int		(*ydb_pointertofunc_t)(void);
-typedef	void		(*ydb_funcptr_retvoid_t)(void);
+typedef	void		(*ydb_funcptr_retvoid_t)(intptr_t tid, unsigned int hd_len, char *hd_data);
 
 /* Structure for passing (non-NULL-terminated) character arrays whose length corresponds to the value of the
  * 'length' field. Note that for output-only ydb_string_t * arguments the 'length' field is set to the