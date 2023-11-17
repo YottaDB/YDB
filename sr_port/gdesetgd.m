b12abc1fd63c7a742bb38f2292ae54e0ae74f350
Narayanan Iyer <nars@yottadb.com>
2023-11-17 10:56:03 -0500

    [#835] [V70001] [GTM-9410] Fix slow gld creation issue (takes 0.5 seconds always)
    Background
    ----------
    * @berwynhoyt noticed that .gld file creation times have slowed down in YDB master compared to r1.38.
    
    * Creation of a gld file now took close to 500 milliseconds whereas it used to be close to 10 milliseconds.
    
    Issue
    -----
    * This was an intentional change in GT.M V7.0-001 as seen in the below release note.
    
      http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V7.0-001_Release_Notes.html#GTM-9410
    
      _GDE attempts to avoid inappropriately creating a global directory by retrying its opening of an
      existing file a number of times; other components that read a global directory use the same technique
      to ensure a missing global directory is not a transient condition. These additional attempts take a
      fraction of a second, but one may perceive the additional time. Writing a revised global directory
      has a short gap between the removal of the prior file and the replacement by the new/revised file,
      during which another process might find the global directory missing; previously this was unlikely
      but has been encountered. (GTM-9410)_
    
    * And this got carried over into YDB as part of 5ae98ef3 and 95617101 when V7.0-001 got merged in.
    
    * The corresponding code change was in `sr_port/gdesetgd.m` where we now wait in a sleep loop starting
      at 30 milliseconds, and then 29 msec, 28 msec etc. until 1 msec waiting for the `.gld` file to exist
      and at the end of the sleep loop we give up and assume that the file does not exist and go ahead with
      creating it inside GDE. For a total sleep loop of 465 milliseconds.
    
    * This means whenever one tries to create a .gld for the first time, we will sleep almost 0.5 seconds
      unconditionally.
    
    * The reason for this change was to not catch another concurrent GDE in a small window in `gdeput.m`
      where it has created a `.gldinprogress` file and is writing stuff to it but BEFORE it gets renamed
      to the `.gld` file as part of a `close` command.
    
    * We did not want the current GDE to incorrectly conclude that the .gld file does not exist when actually
      it does exist in the form of the .gldinprogress file.
    
    Fix
    ---
    * We now check if the .gld file exists. If it does, then we move on like before.
    
    * If the .gld file does not exist, we now check if the .gldinprogress file exists. If so, we wait in a
      sleep loop for 500 milliseconds (just like before) until this file does not exist. If the file exists
      even at the end of the sleep loop, we give up and move on and assume the .gld does not exist.
    
    * The sleep in each iteration is now 1 millisecond as 30 milliseconds sleep (used in GT.M side) seems
      too much.
    
    * With these changes, in the most common case where only 1 GDE is running in the system at a point in time,
      we will not sleep at all. Just like in r1.38.
    
    Notes
    -----
    * Note that there is still a window where multiple GDE processes can be running and one could interfere
      with another at EXIT time and write to the same .gldinprogress and .gld file if both GDE processes come
      to `gdesetgd.m` at the same time and find the .gld file does not exist. But such an issue existed even
      before with the GT.M V7.0-001 changes (where both processes would wait 0.5 seconds unnecessarily before
      both assuming the file does not exist and going ahead to try create the file later).
    
      This window is not completely addressed in this commit. But the most common use case is speeded up and
      it is expected that the window that is not addressed is very unlikely to be encountered in practice.

--- sr_port/gdesetgd.m
+++ sr_port/gdesetgd.m
@@ -1,7 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2001-2021 Fidelity National Information		;
-; Services, Inc. and/or its subsidiaries. All rights reserved.	;
+;	Copyright 2001 Sanchez Computer Associates, Inc.	;
 ;								;
 ; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
@@ -20,12 +19,18 @@ GDESETGD
 	q
 GDFIND	s file=$zparse(tfile,"",defgldext)
 	i file="" s file=$ztrnlnm(tfile) s:file="" file=tfile d message^GDE(gdeerr("INVGBLDIR"),$zwrite(file)_":"_$zwrite(defgld)) s tfile=defgld
-	; 30 millisec is an arbitrarily chosen value yielding a wait that seems sufficient, but not too annoying
-	for i=.03:-.001:.0 do  q:'create  h i	; look long enough to ensure not in window of delete/rename from a concurrent edit
-	. i $zsearch("qfb")
+	s file=$zsearch($zparse(tfile,"",defgldext))
+	i file="" do
+	. ; .gld file does not exist. Check if .gldinprogress file exists. If so, we are in the small window in gdeput.m
+	. ; (of a concurrent GDE edit invocation) where we have create .gldinprogress file but have not yet renamed it to
+	. ; .gld. Wait for that to be done before moving on.
+	. n tfile2,file2
+	. s tfile2=tfile_"inprogress"
+	. ; Total sleep time in the loop is 500 milliseconds (1 msec for each iteration).
+	. ; If the .gldinprogress file exists but it has not been renamed in 500 msec, we give up on waiting
+	. ; and move on with our ".gld" file creation.
+	. f i=1:1:500 s file2=$zsearch($zparse(tfile2,"",defgldext)) q:file2=""  h 0.001
 	. s file=$zsearch($zparse(tfile,"",defgldext))
-	. i file="" s file=$zsearch($zparse(tfile,"",defgldext))
-	. i file="" s file=$zparse(tfile,"",defgldext),create=1
-	. e  s create=0 d message^GDE(gdeerr("LOADGD"),$zwrite(file))
-	d:create message^GDE(gdeerr("GDUSEDEFS"),$zwrite(file))
+	i file="" s file=$zparse(tfile,"",defgldext),create=1 d message^GDE(gdeerr("GDUSEDEFS"),$zwrite(file))
+	e  s create=0 d message^GDE(gdeerr("LOADGD"),$zwrite(file))
 	q
