YottaDB/YDB

commit fdeecd57cd122d1ee000ddb612fe8ae0b56dd6d5

[#1047] Fix SIG-11 in MUPIP INTEG -STATS and Assert failure in MUPIP TRIGGER
Background
----------
The below is pasted from https://gitlab.com/YottaDB/DB/YDB/-/issues/1047#description

* This is a YDB issue that I noticed while developing an automated test case in YDBTest!1770.

  **Test case (tcsh commands)**
  ```sh
  unsetenv ydb_app_ensures_isolation ydb_statsdir ydb_statshare
  setenv ydb_gbldir mumps.gld
  rm -f $ydb_gbldir
  $ydb_dist/mumps -run GDE exit
  rm -f mumps.dat
  $ydb_dist/mupip create
  setenv ydb_app_ensures_isolation "^x"
  setenv ydb_statsdir .
  setenv ydb_statshare 1
  echo '+^ax(1,2,2) -commands=K -xecute="set x=1"' | $ydb_dist/mupip trigger -stdin
  $ydb_dist/mumps -run %XCMD 'view "statshare" zsystem "mupip integ -reg DEFAULT -stats"'
  ```

  **mupip trigger output in Debug build**
  ```sh
  $ echo '+^ax(1,2,2) -commands=K -xecute="set x=1"' | $ydb_dist/mupip trigger -stdin
  .
  .
  %YDB-F-ASSERT, Assert failed in sr_port/gvnh_spanreg_init.c line 75 for expression ((reg >= reg_start) && (reg < &addr->regions[addr->n_regions]))
  ```

  **mupip integ output in Release build**
  ```sh
  $ $ydb_dist/mumps -run %XCMD 'view "statshare" zsystem "mupip integ -reg DEFAULT -stats"'
  .
  .
  Integ of region default
  %YDB-F-KILLBYSIGSINFO1, MUPIP process 7447 has been killed by a signal 11 at address 0x00007FF0BCE40C95 (vaddr 0x0000000000000018)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* The global variable `TREF(ok_to_see_statsdb_regs)` is set to `TRUE` in `sr_port/common_startup_init.c`
  for most binaries (e.g. yottadb, dse, lke etc.).

  There are a few exceptions though and they are
  - sr_port/mupip_create.c : MUPIP CREATE
  - sr_unix/mupip_cvtgbl.c : MUPIP LOAD
  - sr_unix/mupip_rundown.c : MUPIP RUNDOWN
  - sr_unix/mupip_trigger.c : MUPIP TRIGGER
  - sr_unix/mupip_integ.c : MUPIP INTEG
  - sr_port/updproc.c : Update process (in the receiver side of a replicated instance)

sr_port/dpgbldir.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -185,7 +185,7 @@ gd_addr *zgbldir_opt(mval *v, boolean_t env_translated, boolean_t force_load)
 	 * memory allocated for addr field of the mstr is needed as it has been copied over to "name->exp_name" */
 	free(tran_name);
 
-	if (gdr_name_head)
+	if (NULL != gdr_name_head)
 		name->link = (gdr_name *)gdr_name_head;
 	else
 		name->link = 0;

sr_port/gvinit.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2020 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2021-2023 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -14,6 +14,7 @@
  ****************************************************************/
 
 #include "mdef.h"
+
 #include "gdsroot.h"
 #include "gdsblk.h"
 #include "gtm_facility.h"
@@ -22,6 +23,10 @@
 #include "gdsfhead.h"
 #include "dpgbldir.h"
 #include "gvt_inline.h"
+#include "ydb_getenv.h"
+#include "op.h"
+
+#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLREF	gd_addr		*gd_header;
 
@@ -36,6 +41,23 @@ void gvinit(void)
 		v.str.len = 0;
 		v.str.addr = NULL;
 		gd_header = zgbldir(&v);
+		/* Now that the default global directory has been opened by the process, check if "ydb_app_ensures_isolation"
+		 * env var was specified. If so do needed initialization for that here. Cannot do this initialization before
+		 * here as it can cause STATSDB related issues (YDB#1047).
+		 */
+		char	*ptr;
+		mval	noiso_lit, gbllist;
+
+		if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
+		{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
+			noiso_lit.mvtype = MV_STR;
+			noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
+			noiso_lit.str.addr = NOISOLATION_LITERAL;
+			gbllist.mvtype = MV_STR;
+			gbllist.str.len = STRLEN(ptr);
+			gbllist.str.addr = ptr;
+			op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
+		}
 	}
 	/* May get in here after an extended ref call OR in mupip journal recover forward processing (with
 	 * function call graph "mur_output_record/gvcst_put/gvtr_init/gvtr_db_tpwrap/op_tstart").

sr_unix/gtm_startup.c
@@ -91,15 +91,12 @@
 #ifdef UTF8_SUPPORTED
 #include "utfcgr.h"
 #endif
-#include "ydb_getenv.h"
 #include "libyottadb_int.h"
 #include "mdq.h"
 #include "invocation_mode.h"
 #include "ydb_os_signal_handler.h"
 #include "deferred_events.h"
 #include "get_command_line.h"
-
-#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLDEF void			(*restart)() = &mum_tstart;
 #ifdef __MVS__
@@ -160,9 +157,7 @@ void gtm_startup(struct startup_vector *svec)
 	int4			temp_ydb_strpllim;
 	stack_frame 		*frame_pointer_lcl;
 	static char 		other_mode_buf[] = "OTHER";
-	char			*ptr;
 	int			i, status;
-	mval			noiso_lit, gbllist;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -351,17 +346,6 @@ void gtm_startup(struct startup_vector *svec)
 		ojchildparms(NULL, NULL, NULL);
 	if ((NULL != (TREF(mprof_env_gbl_name)).str.addr))
 		turn_tracing_on(TADR(mprof_env_gbl_name), TRUE, (TREF(mprof_env_gbl_name)).str.len > 0);
-	/* See if ydb_noisolation is specified */
-	if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
-	{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
-		noiso_lit.mvtype = MV_STR;
-		noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
-		noiso_lit.str.addr = NOISOLATION_LITERAL;
-		gbllist.mvtype = MV_STR;
-		gbllist.str.len = STRLEN(ptr);
-		gbllist.str.addr = ptr;
-		op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
-	}
 	return;
 }
 

commit b12bf46a66ff8ed05532cf3c05c29b5c59186802

[#1047] Fix $VIEW("NOISOLATION") result if SET $ZGBLDIR="" and ydb_app_ensures_isolation env var is set
Background
----------
* This is an issue noticed in the `dse/dse_cache` subtest in the YDBTest project.

* Below is a simple test case that demonstrates the same problem.

  ```m
  $ cat test.m
      set $zgbldir="mumps.gld"
      VIEW "NOISOLATION":"+^x"
      set $zgbldir=""
      set tmp=$get(^a)
      set $zgbldir="mumps.gld"
      set stat=$VIEW("NOISOLATION","^x")
      write:stat'=1 "TEST-E-NOISOL: Gld[mumps.gld] Gbl[^x] NOISOLATION[",stat,"]",!
      quit
  $ export ydb_app_ensures_isolation=""
  $ $ydb_dist/yottadb -run test
  TEST-E-NOISOL: Gld[mumps.gld] Gbl[^x] NOISOLATION[0]
  ```

* The `TEST-E-NOISOL` error is incorrect/unexpected. This error started happening after commit fdeecd57.

Issue
-----
* The issue is that as part of fdeecd57, `ydb_app_ensures_isolation` env var related processing was
  moved to `sr_port/gvinit.c` under a `if (NULL == gd_header)` code block.

  It was done under the assumption that this logic will be executed only. That is, `gd_header` would be
  NULL at process startup and would be non-NULL when the first global directory is opened and from then
  on it would stay `NULL`.

* But turns out that `gd_header` is set to NULL when one does a `set $zgbldir=""` (which the above test
  case does).

* This meant that the `ydb_app_ensures_isolation` env var related processing would happen again and
  again after every `set $zgbldir=""` done in the process. That means it will overwrite the effect of
  any prior `VIEW "NOISOLATION"` commands. And that is exactly what causes the later `$VIEW("NOISOLATION")`
  function to return the wrong results.

Fix
---
* The code block that does the `ydb_app_ensures_isolation` env var related processing is moved from
  `sr_port/gvinit.c` to `sr_port/dpgbldir.c` in the function `zgbldir_opt()` under a code block

sr_port/dpgbldir.c
@@ -42,6 +42,10 @@
 #include "trans_log_name.h"
 #include "gtm_env_xlate_init.h"
 #include "gtm_threadgbl.h"
+#include "ydb_getenv.h"
+#include "op.h"
+
+#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLREF	mstr		env_ydb_gbldir_xlate;
 GBLREF	gd_addr		*gd_header;
@@ -188,9 +192,28 @@ gd_addr *zgbldir_opt(mval *v, boolean_t env_translated, boolean_t force_load)
 	if (NULL != gdr_name_head)
 		name->link = (gdr_name *)gdr_name_head;
 	else
-		name->link = 0;
+		name->link = NULL;
 	gdr_name_head = name;
 	gdr_name_head->gd_ptr = gd_ptr;
+	if (NULL == name->link)
+	{	/* Now that the FIRST global directory has been opened by this process, check if "ydb_app_ensures_isolation"
+		 * env var was specified. If so do needed initialization for that here. Cannot do this initialization before
+		 * here as it can cause STATSDB related issues (YDB#1047).
+		 */
+		char	*ptr;
+		mval	noiso_lit, gbllist;
+
+		if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
+		{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
+			noiso_lit.mvtype = MV_STR;
+			noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
+			noiso_lit.str.addr = NOISOLATION_LITERAL;
+			gbllist.mvtype = MV_STR;
+			gbllist.str.len = STRLEN(ptr);
+			gbllist.str.addr = ptr;
+			op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
+		}
+	}
 	return gd_ptr;
 }
 

sr_port/gvinit.c
@@ -23,10 +23,6 @@
 #include "gdsfhead.h"
 #include "dpgbldir.h"
 #include "gvt_inline.h"
-#include "ydb_getenv.h"
-#include "op.h"
-
-#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLREF	gd_addr		*gd_header;
 
@@ -41,23 +37,6 @@ void gvinit(void)
 		v.str.len = 0;
 		v.str.addr = NULL;
 		gd_header = zgbldir(&v);
-		/* Now that the default global directory has been opened by the process, check if "ydb_app_ensures_isolation"
-		 * env var was specified. If so do needed initialization for that here. Cannot do this initialization before
-		 * here as it can cause STATSDB related issues (YDB#1047).
-		 */
-		char	*ptr;
-		mval	noiso_lit, gbllist;
-
-		if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
-		{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
-			noiso_lit.mvtype = MV_STR;
-			noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
-			noiso_lit.str.addr = NOISOLATION_LITERAL;
-			gbllist.mvtype = MV_STR;
-			gbllist.str.len = STRLEN(ptr);
-			gbllist.str.addr = ptr;
-			op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
-		}
 	}
 	/* May get in here after an extended ref call OR in mupip journal recover forward processing (with
 	 * function call graph "mur_output_record/gvcst_put/gvtr_init/gvtr_db_tpwrap/op_tstart").