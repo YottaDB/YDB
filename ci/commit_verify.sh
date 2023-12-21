YottaDB/YDB

commit 8ef68f1026b55c923f351b4cd30b2995eae1404a

Fix ci/commit_verify.sh to correctly handle master branch commit-verify pipeline job
* After a prior commit (SHA b3dceb1924ec47f8fe69aa41fc472ecda6a51620) changed `ci/commit_verify.sh`
  in the `YDB` repo, the master branch `commit-verify`/`gpgkey-verify` pipeline job in other dependent
  repos (e.g. `YDBTest`) ran fine but had messages like the following in the job console output.
  ```
  grep: create: No such file or directory
  grep: mode: No such file or directory
  grep: 100644: No such file or directory
  ```

* Turns out that the value of `curyear` variable got set to a list of words instead of just the current
  year (e.g. `2021`).  For example, it was set to the value `2021 create mode 100644 ...`.  This is because
  if there are newly created files in the commit, `git show` would produce extra lines of output for each
  such file with each line containing strings of the form `create mode 100644 ...`. This is now fixed
  by doing a `head -1` and removing an unnecessary `--oneline` usage.

ci/commit_verify.sh
@@ -82,7 +82,9 @@ if [ -z "$COMMIT_IDS" ]; then
 	# In this case, it is possible that when the master branch pipeline runs the date has moved forward by 1 year
 	# relative to when the latest 1 commit got merged. In that case, the copyright check should use the year of the
 	# commit instead of the current year. Hence the below code to retrieve that year from the latest commit.
-	curyear=$(git show --summary --oneline --pretty=format:'%cd' --date=format:%Y HEAD)
+	# If the commit contains some newly created files, there will be additional lines output of the form "create mode 100644"
+	# for each newly created file. We do not want that. Hence filter that out using "head -1".
+	curyear=$(git show --summary --pretty=format:'%cd' --date=format:%Y HEAD | head -1)
 else
 	# This is the normal case when the pipeline runs as part of an open MR in origin repo.
 	# Use the current year for copyright checks.

commit 78392e5d0bdb969cc7c2c2521d1572442ebec681

Fix occasional ci/commit_verify.sh script failure with exit status 141/SIGPIPE (regression in 8ef68f10)
* The YDBGUI `commit-verify` jobs started failing after merging YottaDB/UI/YDBGUI@7cb8da0
  See https://gitlab.com/YottaDB/UI/YDBGUI/-/jobs/5804936291 for example failure.

* The failure was at the following line.

  **ci/commit_verify.sh**
  ```
  curyear=$(git show --summary --pretty=format:'%cd' --date=format:%Y HEAD | head -1)
  ```

* The `git show` was exiting with an exit status of 141.

  ```
  $ set -o pipefail
  $ git show --summary --pretty=format:'%cd' --date=format:%Y HEAD | head -1
  2023
  $ echo ${PIPESTATUS[@]}
  141 0
  ```

* This is an infamous problem with `SIGPIPE` resulting in the exit status of 141.
  See https://stackoverflow.com/a/19120674 for more details.

* In this case, the `git show` had a lot of lines of output (it was a huge commit with lots of files
  that got created) and so it got a SIGPIPE signal when the `head -1` terminated the pipe after
  seeing just one line of output.

* Thanks to https://stackoverflow.com/a/19120674 for a solution which is to use `sed -n 1p` instead
  of `head -1`. That avoids the SIGPIPE error so that is what is done in this commit.

ci/commit_verify.sh
@@ -102,8 +102,10 @@ if [ -z "$COMMIT_IDS" ]; then
 	# relative to when the latest 1 commit got merged. In that case, the copyright check should use the year of the
 	# commit instead of the current year. Hence the below code to retrieve that year from the latest commit.
 	# If the commit contains some newly created files, there will be additional lines output of the form "create mode 100644"
-	# for each newly created file. We do not want that. Hence filter that out using "head -1".
-	curyear=$(git show --summary --pretty=format:'%cd' --date=format:%Y HEAD | head -1)
+	# for each newly created file. We do not want that. Hence filter that out using "sed -n 1p". Do not use "head -1" as
+	# it can cause SIGPIPE errors on the "git show" side (exit status 141) in case "git show" has a lot of output since
+	# we also have "set -o pipefail" set. See https://unix.stackexchange.com/a/738538 for the "sed" suggestion.
+	curyear=$(git show --summary --pretty=format:'%cd' --date=format:%Y HEAD | sed -n 1p)
 else
 	# This is the normal case when the pipeline runs as part of an open MR in origin repo.
 	# Use the current year for copyright checks.