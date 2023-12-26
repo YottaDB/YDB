YottaDB/YDB

commit b12abc1fd63c7a742bb38f2292ae54e0ae74f350

[#835] [V70001] [GTM-9410] Fix slow gld creation issue (takes 0.5 seconds always)
Background
----------
* @berwynhoyt noticed that .gld file creation times have slowed down in YDB master compared to r1.38.

* Creation of a gld file now took close to 500 milliseconds whereas it used to be close to 10 milliseconds.

Issue
-----
* This was an intentional change in GT.M V7.0-001 as seen in the below release note.

  http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V7.0-001_Release_Notes.html#GTM-9410

  _GDE attempts to avoid inappropriately creating a global directory by retrying its opening of an
  existing file a number of times; other components that read a global directory use the same technique
  to ensure a missing global directory is not a transient condition. These additional attempts take a
  fraction of a second, but one may perceive the additional time. Writing a revised global directory
  has a short gap between the removal of the prior file and the replacement by the new/revised file,
  during which another process might find the global directory missing; previously this was unlikely
  but has been encountered. (GTM-9410)_

* And this got carried over into YDB as part of 5ae98ef3 and 95617101 when V7.0-001 got merged in.

* The corresponding code change was in `sr_port/gdesetgd.m` where we now wait in a sleep loop starting
  at 30 milliseconds, and then 29 msec, 28 msec etc. until 1 msec waiting for the `.gld` file to exist
  and at the end of the sleep loop we give up and assume that the file does not exist and go ahead with
  creating it inside GDE. For a total sleep loop of 465 milliseconds.

* This means whenever one tries to create a .gld for the first time, we will sleep almost 0.5 seconds
  unconditionally.

* The reason for this change was to not catch another concurrent GDE in a small window in `gdeput.m`
  where it has created a `.gldinprogress` file and is writing stuff to it but BEFORE it gets renamed

sr_port/gdesetgd.m
@@ -1,7 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;								;
-; Copyright (c) 2001-2021 Fidelity National Information		;
-; Services, Inc. and/or its subsidiaries. All rights reserved.	;
+;	Copyright 2001 Sanchez Computer Associates, Inc.	;
 ;								;
 ; Copyright (c) 2023 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
@@ -20,12 +19,18 @@ GDESETGD
 	q
 GDFIND	s file=$zparse(tfile,"",defgldext)
 	i file="" s file=$ztrnlnm(tfile) s:file="" file=tfile d message^GDE(gdeerr("INVGBLDIR"),$zwrite(file)_":"_$zwrite(defgld)) s tfile=defgld
-	; 30 millisec is an arbitrarily chosen value yielding a wait that seems sufficient, but not too annoying
-	for i=.03:-.001:.0 do  q:'create  h i	; look long enough to ensure not in window of delete/rename from a concurrent edit
-	. i $zsearch("qfb")
+	s file=$zsearch($zparse(tfile,"",defgldext))
+	i file="" do
+	. ; .gld file does not exist. Check if .gldinprogress file exists. If so, we are in the small window in gdeput.m
+	. ; (of a concurrent GDE edit invocation) where we have create .gldinprogress file but have not yet renamed it to
+	. ; .gld. Wait for that to be done before moving on.
+	. n tfile2,file2
+	. s tfile2=tfile_"inprogress"
+	. ; Total sleep time in the loop is 500 milliseconds (1 msec for each iteration).
+	. ; If the .gldinprogress file exists but it has not been renamed in 500 msec, we give up on waiting
+	. ; and move on with our ".gld" file creation.
+	. f i=1:1:500 s file2=$zsearch($zparse(tfile2,"",defgldext)) q:file2=""  h 0.001
 	. s file=$zsearch($zparse(tfile,"",defgldext))
-	. i file="" s file=$zsearch($zparse(tfile,"",defgldext))
-	. i file="" s file=$zparse(tfile,"",defgldext),create=1
-	. e  s create=0 d message^GDE(gdeerr("LOADGD"),$zwrite(file))
-	d:create message^GDE(gdeerr("GDUSEDEFS"),$zwrite(file))
+	i file="" s file=$zparse(tfile,"",defgldext),create=1 d message^GDE(gdeerr("GDUSEDEFS"),$zwrite(file))
+	e  s create=0 d message^GDE(gdeerr("LOADGD"),$zwrite(file))
 	q

commit 0272e0aee37e90447a9901fc0de1f1e093fe4ea3

[#835] [V70001] [GTM-9410] Fix gld creation to sleep if mumps.gldinprogress exists (regression in b12abc1f)
* The commit message of b12abc1f has the following description of the change.

  _If the .gld file does not exist, we now check if the .gldinprogress file exists. If so, we wait in a
  sleep loop for 500 milliseconds (just like before) until this file does not exist. If the file exists
  even at the end of the sleep loop, we give up and move on and assume the .gld does not exist._

* But the actual change has a bug that was found when trying to test the above 500 millisecond sleep
  (as part of YDBTest#554).

* We do a `$zsearch(file)` in a for loop.

  ```m
  . f i=1:1:500 s file2=$zsearch($zparse(tfile2,"",defgldext)) q:file2=""  h 0.001
  ```

* The value of `file` stays the same for all iterations of the loop (for example the file name
  `mumps.gldinprogress`). But this means the second iteration would return `""` since `$zsearch()` of the
  same pattern/filename would reuse prior context and try finding other file names satisfying the pattern
  (which there won't be any as there is no regex in the parameter passed).

* Therefore, the loop will find `file2` variable to be `""` in the second iteration and quit out even
  though the `mumps.gldinprogress` file continues to exist.

* That is, we would be sleeping for just 1 millisecond instead of 500 milliseconds (original intent).

* This is fixed by resetting the `$zsearch` context in every iteration using `-1` as the stream number
  (second parameter of `$zsearch`).

sr_port/gdesetgd.m
@@ -29,7 +29,7 @@ GDFIND	s file=$zparse(tfile,"",defgldext)
 	. ; Total sleep time in the loop is 500 milliseconds (1 msec for each iteration).
 	. ; If the .gldinprogress file exists but it has not been renamed in 500 msec, we give up on waiting
 	. ; and move on with our ".gld" file creation.
-	. f i=1:1:500 s file2=$zsearch($zparse(tfile2,"",defgldext)) q:file2=""  h 0.001
+	. f i=1:1:500 s file2=$zsearch($zparse(tfile2,"",defgldext),-1) q:file2=""  h 0.001
 	. s file=$zsearch($zparse(tfile,"",defgldext))
 	i file="" s file=$zparse(tfile,"",defgldext),create=1 d message^GDE(gdeerr("GDUSEDEFS"),$zwrite(file))
 	e  s create=0 d message^GDE(gdeerr("LOADGD"),$zwrite(file))