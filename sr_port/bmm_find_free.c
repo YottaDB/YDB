83db1ef1b461f182e8f987e8f08e5030047f2b45
Narayanan Iyer <nars@yottadb.com>
2024-02-13 12:15:28 -0500

    [YDBTest#501] [DEBUG-ONLY] [PERFORMANCE] Add missing ydb_test_4g_db_blks env var handling in bmm_find_free()
    Background
    ----------
    * Commit 2b2eb4c2 added missing logic for the debug-only `ydb_test_4g_db_blks` env var handling
      in the `gtm_ffs()` function. And this fixed core files.
    
    Issue
    -----
    * But that commit did not fix `bmm_find_free()` (the caller of `gtm_ffs()`) to be aware of the
      env var.
    
    * This meant that `bmm_find_free()` would call `gtm_ffs()` for local bitmap blocks in the range
      occupied by the db hole.
    
    * This meant that even though `gtm_ffs()` had logic to identify blocks in the hole range, it would
      waste time being passed LOTS of blocks in the range of the hole (note that the hole would be
      95% or more of the size of the database file) and decide those as not being free blocks.
    
    * Such a stack trace was found while analyzing a `v62001/gtm7779` subtest hang (in the YDBTest project).
      See YDBTest@901bfadf commit message for more details.
    
    Fix
    ---
    * Although the unnecessary processing of blocks known to be in the hole range was not the eventual
      cause of the subtest hang, it is considered better to fix this unnecessary processing.
    
    * This commit therefore makes `bmm_find_free()` aware of the hole by checking if the `start` or `top`
      of a range of bits that it is going to be passed to `gtm_ffs()`, after going through a `for` loop,
      lies in the hole range. If so the `start` and `top` variables are adjusted so they fall outside
      the hole range thereby skipping any unnecessary calls to `gtm_ffs()`.

--- sr_port/bmm_find_free.c
+++ sr_port/bmm_find_free.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -20,6 +23,15 @@
 
 #define MAX_FFS_SIZE 32
 
+#ifdef DEBUG
+#include "gdsbt.h"
+#include "gtm_facility.h"
+#include "fileinfo.h"
+#include "gdsfhead.h"
+
+GBLREF	block_id	ydb_skip_bml_num;
+#endif
+
 /* Returns the location of the first set bit in the field.	*/
 /* The search starts at the hint and wraps if necessary.	*/
 
@@ -31,6 +43,15 @@ block_id bmm_find_free(block_id hint, uchar_ptr_t base_addr, block_id total_bits
 		hint = 0;
 	for (start = hint, top = total_bits;  top;  start = 0, top = hint, hint = 0)
 	{	/* one or two passes through outer loop; second is a wrap to the beginning */
+#		ifdef DEBUG
+		if (0 != ydb_skip_bml_num)
+		{	/* Do not waste time trying to find a free bit in bitmaps that lie in the db hole */
+			if ((1 <= start) && (start < (ydb_skip_bml_num / BLKS_PER_LMAP)))
+				start = (ydb_skip_bml_num / BLKS_PER_LMAP);
+			if ((1 <= top) && (top < (ydb_skip_bml_num / BLKS_PER_LMAP)))
+				top = 1;
+		}
+#		endif
 		for (width = MIN(top, ROUND_DOWN2(start, MAX_FFS_SIZE) + MAX_FFS_SIZE) - start;  width > 0;
 			start += width, width = MIN(top - start, MAX_FFS_SIZE))
 		{
