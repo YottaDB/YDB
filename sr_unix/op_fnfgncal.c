YottaDB/YDB

commit 9f916f63315c8d5298497fa2a8e5cbe87a4e7a5f

[#1049] Do not truncate 64-bit values returned from call-out on 32-bit architectures
* This commit takes care of an incomplete fix in YDB@fb70aab7 (i.e. YDB!1453) for 32-bit architectures.

**Problem**: On 32-bit architectures, the macro MV_FORCE_(U)LMVAL does not properly convert 64-bit values as it is designed to convert ydb_long_t which are 32-bits on 32-bit architectures. The diagnosis of this is discussed [here](https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/1453#note_1775543948).

**Solution**: This MR creates new macros MV_FORCE_(U)64MVAL and uses them for 64-bit values, instead.

Misc
----
* Additionally, a pre-existing `case ydb_jlong:` code block in `sr_unix/op_fnfgncal.c` was seen as
  benefitting from the new `MV_FORCE_64MVAL` macro so it was simplified to use this macro.

sr_unix/op_fnfgncal.c
@@ -353,15 +353,11 @@ STATICFNDEF void extarg2mval(void *src, enum ydb_types typ, mval *dst, boolean_t
 				MV_FORCE_MVAL(dst, s_int_num);
 				break;
 			case ydb_jlong:
-#				ifdef GTM64
 				if (starred)
-					s_long_num = *((ydb_long_t *)src);
+					s_int64_num = *((ydb_int64_t*)src);
 				else
-					s_long_num = (ydb_long_t)src;
-				MV_FORCE_LMVAL(dst, s_long_num);
-#				else
-				i82mval(dst, *(gtm_int64_t *)src);
-#				endif
+					s_int64_num = (ydb_int64_t)(intszofptr_t)src;
+				MV_FORCE_64MVAL(dst, s_int64_num);
 				break;
 			case ydb_jfloat:
 				float2mval(dst, *((float *)src));
@@ -441,18 +437,18 @@ STATICFNDEF void extarg2mval(void *src, enum ydb_types typ, mval *dst, boolean_t
 #		ifdef GTM64
 		case ydb_int64:
 			s_int64_num = (ydb_int64_t)src;
-			MV_FORCE_LMVAL(dst, s_int64_num);
+			MV_FORCE_64MVAL(dst, s_int64_num);
 			break;
 		case ydb_uint64:
 			uns_int64_num = (ydb_uint64_t)src;
-			MV_FORCE_ULMVAL(dst, uns_int64_num);
+			MV_FORCE_U64MVAL(dst, uns_int64_num);
 			break;
 #		endif
 		case ydb_int64_star:
-			VALIDATE_AND_CONVERT_PTR_TO_TYPE(LMVAL, ydb_int64_t, s_int64_num, dst, src);
+			VALIDATE_AND_CONVERT_PTR_TO_TYPE(64MVAL, ydb_int64_t, s_int64_num, dst, src);
 			break;
 		case ydb_uint64_star:
-			VALIDATE_AND_CONVERT_PTR_TO_TYPE(ULMVAL, ydb_uint64_t, uns_int64_num, dst, src);
+			VALIDATE_AND_CONVERT_PTR_TO_TYPE(U64MVAL, ydb_uint64_t, uns_int64_num, dst, src);
 			break;
 		case ydb_string_star:
 			sp = (struct extcall_string *)src;

sr_port/mdef.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2023 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  * Copyright (c) 2017-2018 Stephen L Johnson.			*
@@ -593,11 +593,13 @@ mval *underr_strict(mval *start, ...);
 				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(I)*MV_BIAS ))
 #define MV_FORCE_MVAL(M,I)	(((I) >= 1000000 || (I) <= -1000000) ? i2mval((M),(int)(I)) : \
 				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(I)*MV_BIAS ))
+#define MV_FORCE_U64MVAL(M,L)	(((L) >= 1000000) ? ui82mval((M),(gtm_uint64_t)(L)) : \
+				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(L)*MV_BIAS ))
+#define MV_FORCE_64MVAL(M,L)	(((L) >= 1000000 || (L) <= -1000000) ? i82mval((M),(gtm_int64_t)(L)) : \
+				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(L)*MV_BIAS ))
 #ifdef GTM64
-#define MV_FORCE_ULMVAL(M,L)	(((L) >= 1000000) ? ui82mval((M),(gtm_uint64_t)(L)) : \
-				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(L)*MV_BIAS ))
-#define MV_FORCE_LMVAL(M,L)	(((L) >= 1000000 || (L) <= -1000000) ? i82mval((M),(gtm_int64_t)(L)) : \
-				(void)( (M)->mvtype = MV_NM | MV_INT , (M)->m[1] = (int)(L)*MV_BIAS ))
+#define MV_FORCE_ULMVAL(M,L)	MV_FORCE_U64MVAL(M,L)
+#define MV_FORCE_LMVAL(M,L)	MV_FORCE_64MVAL(M,L)
 #else
 #define MV_FORCE_ULMVAL		MV_FORCE_UMVAL
 #define MV_FORCE_LMVAL		MV_FORCE_MVAL

commit b01270b586d89b701de2474734e61a8ebd7155c6

[#1049] Fix gtm_jlong_t return type handling on 32-bit armv6l (regression in 9f916f63)
Background
----------
* The `java/callouts` subtest failed on armv6l with the following diff.

  ```diff
  7a8,67
  > 16c16
  > < Job returned -336177440569076945
  > ---
  > > Job returned -1277316960
  .
  .
  ```

Issue
-----
* Before 9f916f63, on 32-bit armv6l, a `ydb_jlong` type return value was handled in `sr_unix/op_fnfgncal.c`
  by converting the 8-byte contents of the pointer to the return value. This was done whether `starred`
  was TRUE or not. That is, the handling was the same for `gtm_jlong_t` and `gtm_jlong_t *`.

* But after 9f916f63, on 32-bit armv6l, a `ydb_jlong` type return value was handled differently for
  `gtm_jlong_t` and `gtm_jlong_t *`. Just like it was done on 64-bit systems. While the different
  handling is correct and needed for 64-bit systems, it is incorrect for 32-bit systems.

Fix
---
* The fix is to restore the same handling for `gtm_jlong_t` and `gtm_jlong_t *` for 32-bit systems.
  That is, there should be no `if (starred)` logic in the `#ifndef GTM64` case for `case ydb_jlong:`
  in `sr_unix/op_fnfgncal.c` like is there for the `#ifdef GTM64` case..

sr_unix/op_fnfgncal.c
@@ -353,10 +353,18 @@ STATICFNDEF void extarg2mval(void *src, enum ydb_types typ, mval *dst, boolean_t
 				MV_FORCE_MVAL(dst, s_int_num);
 				break;
 			case ydb_jlong:
+				/* On 64-bit systems, "gtm_jlong_t *" is passed in using a pointer to the actual 8-byte value
+				 * whereas a "gtm_jlong_t" is passed in as is. On 32-bit systems though, both "gtm_jlong_t"
+				 * and "gtm_jlong_t *" are passed in using a pointer to the actual 8-byte value.
+				 */
+#				ifdef GTM64
 				if (starred)
-					s_int64_num = *((ydb_int64_t*)src);
+					s_int64_num = *((ydb_int64_t *)src);
 				else
-					s_int64_num = (ydb_int64_t)(intszofptr_t)src;
+					s_int64_num = (ydb_int64_t)src;
+#				else
+				s_int64_num = *((ydb_int64_t *)src);
+#				endif
 				MV_FORCE_64MVAL(dst, s_int64_num);
 				break;
 			case ydb_jfloat: