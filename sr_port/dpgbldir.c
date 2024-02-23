YottaDB/YDB

commit b12bf46a66ff8ed05532cf3c05c29b5c59186802

[#1047] Fix $VIEW("NOISOLATION") result if SET $ZGBLDIR="" and ydb_app_ensures_isolation env var is set
Background
----------
* This is an issue noticed in the `dse/dse_cache` subtest in the YDBTest project.

* Below is a simple test case that demonstrates the same problem.

  ```m
  $ cat test.m
      set $zgbldir="mumps.gld"
      VIEW "NOISOLATION":"+^x"
      set $zgbldir=""
      set tmp=$get(^a)
      set $zgbldir="mumps.gld"
      set stat=$VIEW("NOISOLATION","^x")
      write:stat'=1 "TEST-E-NOISOL: Gld[mumps.gld] Gbl[^x] NOISOLATION[",stat,"]",!
      quit
  $ export ydb_app_ensures_isolation=""
  $ $ydb_dist/yottadb -run test
  TEST-E-NOISOL: Gld[mumps.gld] Gbl[^x] NOISOLATION[0]
  ```

* The `TEST-E-NOISOL` error is incorrect/unexpected. This error started happening after commit fdeecd57.

Issue
-----
* The issue is that as part of fdeecd57, `ydb_app_ensures_isolation` env var related processing was
  moved to `sr_port/gvinit.c` under a `if (NULL == gd_header)` code block.

  It was done under the assumption that this logic will be executed only. That is, `gd_header` would be
  NULL at process startup and would be non-NULL when the first global directory is opened and from then
  on it would stay `NULL`.

* But turns out that `gd_header` is set to NULL when one does a `set $zgbldir=""` (which the above test
  case does).

* This meant that the `ydb_app_ensures_isolation` env var related processing would happen again and
  again after every `set $zgbldir=""` done in the process. That means it will overwrite the effect of
  any prior `VIEW "NOISOLATION"` commands. And that is exactly what causes the later `$VIEW("NOISOLATION")`
  function to return the wrong results.

Fix
---
* The code block that does the `ydb_app_ensures_isolation` env var related processing is moved from
  `sr_port/gvinit.c` to `sr_port/dpgbldir.c` in the function `zgbldir_opt()` under a code block

sr_port/dpgbldir.c
@@ -42,6 +42,10 @@
 #include "trans_log_name.h"
 #include "gtm_env_xlate_init.h"
 #include "gtm_threadgbl.h"
+#include "ydb_getenv.h"
+#include "op.h"
+
+#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLREF	mstr		env_ydb_gbldir_xlate;
 GBLREF	gd_addr		*gd_header;
@@ -188,9 +192,28 @@ gd_addr *zgbldir_opt(mval *v, boolean_t env_translated, boolean_t force_load)
 	if (NULL != gdr_name_head)
 		name->link = (gdr_name *)gdr_name_head;
 	else
-		name->link = 0;
+		name->link = NULL;
 	gdr_name_head = name;
 	gdr_name_head->gd_ptr = gd_ptr;
+	if (NULL == name->link)
+	{	/* Now that the FIRST global directory has been opened by this process, check if "ydb_app_ensures_isolation"
+		 * env var was specified. If so do needed initialization for that here. Cannot do this initialization before
+		 * here as it can cause STATSDB related issues (YDB#1047).
+		 */
+		char	*ptr;
+		mval	noiso_lit, gbllist;
+
+		if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
+		{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
+			noiso_lit.mvtype = MV_STR;
+			noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
+			noiso_lit.str.addr = NOISOLATION_LITERAL;
+			gbllist.mvtype = MV_STR;
+			gbllist.str.len = STRLEN(ptr);
+			gbllist.str.addr = ptr;
+			op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
+		}
+	}
 	return gd_ptr;
 }
 

sr_port/gvinit.c
@@ -23,10 +23,6 @@
 #include "gdsfhead.h"
 #include "dpgbldir.h"
 #include "gvt_inline.h"
-#include "ydb_getenv.h"
-#include "op.h"
-
-#define	NOISOLATION_LITERAL	"NOISOLATION"
 
 GBLREF	gd_addr		*gd_header;
 
@@ -41,23 +37,6 @@ void gvinit(void)
 		v.str.len = 0;
 		v.str.addr = NULL;
 		gd_header = zgbldir(&v);
-		/* Now that the default global directory has been opened by the process, check if "ydb_app_ensures_isolation"
-		 * env var was specified. If so do needed initialization for that here. Cannot do this initialization before
-		 * here as it can cause STATSDB related issues (YDB#1047).
-		 */
-		char	*ptr;
-		mval	noiso_lit, gbllist;
-
-		if (NULL != (ptr = ydb_getenv(YDBENVINDX_APP_ENSURES_ISOLATION, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)))
-		{	/* Call the VIEW "NOISOLATION" command with the contents of the <ydb_app_ensures_isolation> env var */
-			noiso_lit.mvtype = MV_STR;
-			noiso_lit.str.len = STR_LIT_LEN(NOISOLATION_LITERAL);
-			noiso_lit.str.addr = NOISOLATION_LITERAL;
-			gbllist.mvtype = MV_STR;
-			gbllist.str.len = STRLEN(ptr);
-			gbllist.str.addr = ptr;
-			op_view(VARLSTCNT(2) &noiso_lit, &gbllist);
-		}
 	}
 	/* May get in here after an extended ref call OR in mupip journal recover forward processing (with
 	 * function call graph "mur_output_record/gvcst_put/gvtr_init/gvtr_db_tpwrap/op_tstart").

commit c895a5a43b06f64d51c866f88d1bffeb63cbcd9d

[DEBUG-ONLY] Fix DBG build gtmsecshr executable size bloat (regression in b12bf46a)
Background
----------
Below is pasted from https://gitlab.com/YottaDB/DB/YDB/-/issues/1047#note_1781982593

* While trying to create the binary tarballs for YottaDB r2.00, I noticed that the `dbg` tarball
  (which is only internally created, but not made publicly available unlike the `pro` tarball) doubled in
  size. And most of the increase came from `gtmsecshr` which increased in size from 1Mib to 9Mib. After
  some experiments, narrowed down the commit that caused this bloat. And it is b12bf46a (i.e. YDB!1437).

Issue
-----
* The main issue is that `gtmsecshr` invokes the `get_next_gdr()` function. And that is in
  `sr_unix/dpgbldir.c` along with a bunch of other functions including `zgbldir_opt()` which
  was modified in b12bf46a to invoke `op_view()`.

* Because of the `get_next_gdr()` invocation, unnecessary functions like `zgbldir_opt()` and
  `op_view()` got pulled into the executable just because they were in the same file as the
  `get_next_gdr()` function.

* And pulling in `op_view()` caused the entire compiler to be pulled in causing the doubling of the
  `gtmsecshr` executable size in the DBG build.

* The same logic applied to the `gdr_name_head` global variable too. It is defined in `sr_port/dpgbldir.c`
  and is used inside `get_next_gdr()` which means just because gtmsecshr needs this global variable,
  it would be pulling in the entire `sr_port/dpgbldir.c` even though most of it is unnecessary.

Fix
---
* The fix is simple and is to
  - Move `get_next_gdr()` function to a separate file `sr_port/get_next_gdr.c`
  - Move a few global variables that were defined in `sr_port/dpgbldir.c` to `sr_port/gbldefs.c`

  These ensured that unnecessary functions do not get pulled into gtmsecshr just because a function
  or global variable that it relied on was defined in `sr_port/dpgbldir.c`.

sr_port/dpgbldir.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -50,11 +50,10 @@
 GBLREF	mstr		env_ydb_gbldir_xlate;
 GBLREF	gd_addr		*gd_header;
 GBLREF	gv_namehead	*gv_target_list;
+GBLREF	gdr_name	*gdr_name_head;
+GBLREF	gd_addr		*gd_addr_head;
 
 LITREF	char		gde_labels[GDE_LABEL_NUM][GDE_LABEL_SIZE];
-
-GBLDEF gdr_name	*gdr_name_head;
-GBLDEF gd_addr	*gd_addr_head;
 
 error_def(ERR_GDINVALID);
 
@@ -483,36 +482,6 @@ gd_addr *gd_load(mstr *v, boolean_t force_load)
 	/* Assert that all runtime-only fields are null-initialized by GDE */
 	assert(!table->ygs_map_entry_changed);
 	return table;
-}
-
-/*+
-Function:       GET_NEXT_GDR
-
-		This function returns the next entry in the list of open
-		global directories.  If the input parameter is zero, the
-		first entry is returned, otherwise the next entry in the
-		list is returned.  If the input parameter is not a member
-		of the list, then zero will be returned.
-
-Syntax:         gd_addr *get_next_gdr(gd_addr *prev)
-
-Prototype:      ?
-
-Return:         *gd_addr -- a pointer to the global directory structure
-
-Arguments:      The previous global directory accessed;
-
-Side Effects:   NONE
-
-Notes:          NONE
--*/
-gd_addr *get_next_gdr(gd_addr *prev)
-{
-	gd_addr	*ptr;
-
-	if (NULL == prev)
-		return gd_addr_head;
-	return prev->link;
 }
 
 /* Maintain list of regions for GTCM_SERVER */

sr_port/gbldefs.c
@@ -1468,3 +1468,11 @@ GBLDEF int			*vrl_catch_signals;
 GBLDEF rl_hook_func_t		**vrl_startup_hook;
 GBLDEF int			*vrl_already_prompted;
 GBLDEF rl_voidfunc_t 		**vrl_redisplay_function;
+
+#ifdef DEBUG
+GBLDEF	boolean_t		in_fake_enospc;	/* used by an assert in "send_msg.c" */
+#endif
+
+GBLDEF gdr_name	*gdr_name_head;
+GBLDEF gd_addr	*gd_addr_head;
+

sr_port/get_next_gdr.c
@@ -1,0 +1,57 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2001-2021 Fidelity National Information	*
+ * Services, Inc. and/or its subsidiaries. All rights reserved.	*
+ *								*
+ * Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "mdef.h"
+
+#include "gdsroot.h"
+#include "gdsblk.h"
+#include "gtm_facility.h"
+#include "fileinfo.h"
+#include "gdsbt.h"
+#include "gdsfhead.h"
+#include "dpgbldir.h"
+
+GBLREF gd_addr	*gd_addr_head;
+
+/*+
+Function:       GET_NEXT_GDR
+
+		This function returns the next entry in the list of open
+		global directories.  If the input parameter is zero, the
+		first entry is returned, otherwise the next entry in the
+		list is returned.  If the input parameter is not a member
+		of the list, then zero will be returned.
+
+Syntax:         gd_addr *get_next_gdr(gd_addr *prev)
+
+Prototype:      ?
+
+Return:         *gd_addr -- a pointer to the global directory structure
+
+Arguments:      The previous global directory accessed;
+
+Side Effects:   NONE
+
+Notes:          NONE
+-*/
+gd_addr *get_next_gdr(gd_addr *prev)
+{
+	gd_addr	*ptr;
+
+	if (NULL == prev)
+		return gd_addr_head;
+	return prev->link;
+}
+

sr_unix/fake_enospc.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -39,8 +39,7 @@
 #ifdef DEBUG
 GBLREF		jnlpool_addrs_ptr_t	jnlpool;
 GBLREF		volatile int4		gtmMallocDepth;
-
-GBLDEF		boolean_t		in_fake_enospc;	/* used by an assert in "send_msg.c" */
+GBLREF		boolean_t		in_fake_enospc;
 
 STATICDEF	uint4		syslog_deferred = 0;
 