7c14b6bdafa5c49ada97e66eb120873e43b52a5e
Narayanan Iyer <nars@yottadb.com>
2024-03-19 15:49:55 -0400

    [#863] [V70002] Fix r136/ydb901 subtest failure due to moving start_fetches() across files
    Background
    ----------
    * The `r136/ydb901` subtest started failing with SIG-11s after merging GT.M V7.0-002.
    
    Issue
    -----
    * Commits 68517ff6 and b8c2680b enhanced the `start_fetches()` function in `sr_port/start_fetches.c`
      to fix SIG-11s. See those commit messages for more details.
    
    * As part of GT.M V7.0-002, the `start_fetches()` function was moved from `sr_port/start_fetches.c`
      into `sr_port/start_fetches.h`.
    
    * The deletion of `sr_port/start_fetches.c` was flagged as a `deleted by them` conflict by `git`
      but it was resolved by just deleting it from the YottaDB side. This was where the issue arose.
    
      What this caused is that the file `sr_port/start_fetches.c` was removed from the YottaDB side
      leaving us with the `sr_port/start_fetches.h` version of `start_fetches()` (from GT.M V7.0-002)
      which did not have the fixes from 68517ff6 and b8c2680b.
    
    Fix
    ---
    * The fix is simple and is to retrofit the YottaDB fixes from those 2 commits back into the code.
      And that is what is done in this commit.

--- sr_port/start_fetches.h
+++ sr_port/start_fetches.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2022 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -13,16 +16,32 @@
 #ifndef START_FETCHES_H_INCLUDED
 #define START_FETCHES_H_INCLUDED
 
+#include "cmd_qlf.h"
+
+GBLREF command_qualifier	cmd_qlf;
+GBLREF boolean_t		cur_line_entry;	/* TRUE if control can reach this line in a -NOLINE_ENTRY compilation */
+
 #define START_FETCHES(OP) start_fetches(OP)
 
 static inline void start_fetches(opctype op)
 {
+	boolean_t	noline_entry_point;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
-	if (!(TREF(fetch_control)).curr_fetch_count)
-		(TREF(fetch_control)).curr_fetch_trip->opcode = ((TREF(fetch_control)).curr_fetch_trip->opcode == OC_LINEFETCH)
-			? OC_LINESTART : OC_NOOP;
+	/* If the compilation happens with -NOLINE_ENTRY and control can reach the current M line from an external entry point,
+	 * then make sure the current OC_LINEFETCH accounts for ALL local variable references in this frame (not just the
+	 * ones seen till now in the parse, but also the ones that are going to follow under this entryref). A simple way
+	 * of ensuring this is by setting "(TREF(fetch_control))curr_fetch_count" to 0 in this case. Not doing so can
+	 * cause SIG-11 (YDB#901).
+	 */
+	noline_entry_point = ((OC_LINEFETCH == (TREF(fetch_control)).curr_fetch_trip->opcode)
+					&& !(cmd_qlf.qlf & CQ_LINE_ENTRY) && cur_line_entry);
+	if (noline_entry_point)
+		(TREF(fetch_control)).curr_fetch_count = 0;
+	if (!(TREF(fetch_control)).curr_fetch_count && !noline_entry_point)
+		(TREF(fetch_control)).curr_fetch_trip->opcode =
+			((TREF(fetch_control)).curr_fetch_trip->opcode == OC_LINEFETCH) ? OC_LINESTART : OC_NOOP;
 	else
 	{
 		assert((OC_LINEFETCH == (TREF(fetch_control)).curr_fetch_trip->opcode)
