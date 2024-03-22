YottaDB/YDB

commit 222054bec34378da48ba412da3a8c69ad6d98ef6

[#863] [V70002] Fix r134/ydb828 subtest failure due to regression in GT.M V7.0-002
Background
----------
* After GT.M V7.0-002 was merged, the `r134/ydb828` subtest failed.

* Below is a simple test case demonstrating the failure.

  ```sh
  $ $ydb_dist/yottadb -direct
  YDB> new:0  write i
  %YDB-F-ASSERT, Assert failed in sr_port/start_fetches.h line 29 for expression ((OC_LINEFETCH == (TREF(fetch_control)).curr_fetch_trip->opcode) || (OC_FETCH == (TREF(fetch_control)).curr_fetch_trip->opcode))
  ```

Issue
-----
* GT.M V7.0-002 introduced logic in `sr_port/cmd.c` to implement an optimization when a literal
  postconditional evaluates to `0`. It added logic to restore the `OC_LINEFETCH` opcode with whatever
  fetch opcode was there before the command that was thrown away was parsed.

  It did this by resetting `(TREF(fetch_control)).curr_fetch_trip`.

* But this is not correct as this can lead to lines like the above example  not having any `OC_LINEFETCH`
  opcode at all. And that is not correct. Because even though the `new` had a postconditional that
  evaluated to 0, there is a variable `i` that is accessed later in the `write` command that follows
  and that happens without a postconditional.

Fix
---
* The fix is simple and is to remove the restore of `(TREF(fetch_control)).curr_fetch_trip` that
  GT.M introduced (along with 2 other related variables.

* YottaDB code already has logic to handle this a few lines later where it checks if
  `fetch0 != (TREF(fetch_control)).curr_fetch_trip` and if so it does a call to
  `ins_triple((TREF(fetch_control)).curr_fetch_trip)` which ensures a `OC_FETCH` happens even
  in that case (even if it is not a `OC_LINEFETCH`).

Notes
-----
* I verified that the above simple case produces a SIG-11 even in GT.M V7.1-003 (the latest GT.M).

sr_port/cmd.c
@@ -324,9 +324,6 @@ int cmd(void)
 	}
 	if (NULL != oldchain)
 	{	/* for a literal 0 postconditional, we just throw the command & args away and return happiness */
-		(TREF(fetch_control)).curr_fetch_trip = fetch0;
-		(TREF(fetch_control)).curr_fetch_opr = fetch1;
-		(TREF(fetch_control)).curr_fetch_count = fetch_cnt;
 		setcurtchain(oldchain);
 		if (fetch0 != (TREF(fetch_control)).curr_fetch_trip)
 		{

commit 5f5996545b4ac6514e79616310d79cf7b5609697

[#863] [V70002] Fix v63001/gtm8573 subtest failure (OC_LINESTART vs OC_LINEFETCH)
Background
----------
* The `v63001/gtm8573` subtest failed with the following symptom.

  ```diff
  51c51
  < OC_LINESTART
  ---
  > OC_LINEFETCH
  ```

* Below is a simple M command that demonstrates the issue.

  ```m
   set:0 x=1
  ```

* If one copies this into an M program and does a `mumps -machine -lis` using a Debug
  build, one would see a `OC_LINEFETCH` being generated whereas the `v63001/gtm8573` subtest
  expects a `OC_LINESTART` to be generated. See commit in the `YDBTest` project with the
  following title for more details on this expectation.

  ```
  [YDB#863] [V70002] Fix v63001/outref/gtm8573.txt due to literal FALSE post-conditional optimization changes in GTM-DE201386
  ```

Issue
-----
* Commit a65c6220 (`[#863] [V70002] Resolve conflicts during merge of GT.M V7.0-002 into YottaDB master`
  is the title) did pull in the following GT.M V7.0-002 optimization.

  http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V7.0-002_Release_Notes.html#GTM-DE197637465

  _The GT.M compiler appropriately handles optimization of literal FALSE post conditionals; previously
  these could cause segmentation violations (SIG-11)._

  After that, the above M command started generating `OC_LINESTART` which was right.

* But commit 222054be (`[#863] [V70002] Fix r134/ydb828 subtest failure due to regression in GT.M V7.0-002`
  is the title) broke the optimization while trying to fix another issue.

Fix
---
* There are 2 contrasting needs. One is to try keep the GT.M V7.0-002 optimization which reset
  `(TREF(fetch_control)).curr_fetch_trip`. Another is to ensure the reset does not happen in case
  of exceptions like `NEW` when used with a post-conditional (like the r134/ydb828 subtest exercises).

* This leads the way to the solution. And that is to check if it is not the exceptions (like `new` and

sr_port/cmd.c
@@ -329,6 +329,11 @@ int cmd(void)
 		{
 			assert(OC_FETCH == (TREF(fetch_control)).curr_fetch_trip->opcode);
 			ins_triple((TREF(fetch_control)).curr_fetch_trip);
+		} else
+		{
+			(TREF(fetch_control)).curr_fetch_trip = fetch0;
+			(TREF(fetch_control)).curr_fetch_opr = fetch1;
+			(TREF(fetch_control)).curr_fetch_count = fetch_cnt;
 		}
 		return TRUE;
 	}