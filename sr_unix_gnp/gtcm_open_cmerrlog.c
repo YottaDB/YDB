485d45baa0f54fcc1eefc815cf2d54814f3411a3
Narayanan Iyer <nars@yottadb.com>
2024-04-11 14:38:28 -0400

    [#963] [V70005] Remove copyright changes for files that had no changes after merge conflict resolution
    * This is needed to fix the commit-verify pipeline job failure.
    
    * See https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/1323#note_1396533670 for more details
      (note that discussion happened for GT.M V7.0-000 merge but is relevant even for the V7.0-005 merge).

--- sr_unix_gnp/gtcm_open_cmerrlog.c
+++ sr_unix_gnp/gtcm_open_cmerrlog.c
@@ -1,9 +1,9 @@
 /****************************************************************
  *								*
- * Copyright (c) 2001-2022 Fidelity National Information	*
+ * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2018 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
