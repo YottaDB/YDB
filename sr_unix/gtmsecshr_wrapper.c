YottaDB/YDB

commit 9af7f58919660b936638381716221dffc7c43c2f

[#209] Add support for ydb_* environment variables corresponding to all gtm*/GTM* environment variables
* In YottaDB r1.20, support for only a few ydb* environment variables was added
  (gtm_dist -> ydb_dist, gtmgbldir -> ydb_gbldir, gtm_zmaxtptime -> ydb_maxtptime and
  gtmdbglvl -> ydb_dbglvl). But there are 100+ environment variables that need similar
  support (e.g. gtmroutines -> ydb_routines, gtm_chset -> ydb_chset,
  gtm_repl_instance -> ydb_repl_instance, etc.). That is, YottaDB should recognize all
  the gtm* environment variables (for backward compatibility) but also recognize the
  new ydb_* environment variables. If both types of variables are defined, the ydb_*
  definition should override the gtm* definition. That recognition of all env vars
  is the purpose of this commit.

* A new file ydb_logicals_tab.h now defines all the gtm* environment variables and
  the corresponding ydb_* environment variables that YottaDB cares about. GT.M
  environment variables could start with gtm, gtm_, GTM or GTM_. They could be lower
  case or upper case. But the new YottaDB environment variables always start with ydb_
  and are always lower case. And because of this new file, gtm_logicals.h is no longer
  needed and is nixed.

* As part of this, previously unused env var names like gtm_jnl_statsdb,
  gtm_gvdupsetnoop are now removed.

* gtm_env_init.c, gtm_env_init_sp.c (and a few others like mstack_size_init.c) are
  now reworked to call new functions ydb_trans_numeric(), ydb_logical_truth_value(),
  ydb_trans_log_name() which are passed an index into the environment variable name
  table instead of the actual name of the env var; This way the functions will then
  know the corresponding ydb and gtm prefixed env var names for the passed in index
  and first check the ydb* name if it exists and if not use the gtm* env var.

* In ydb_trans_logname(), the ydb* env var is set to the gtm* env var if only the latter
  is defined.

sr_unix/gtmsecshr_wrapper.c
@@ -54,6 +54,7 @@
 #include "wbox_test_init.h"
 #include "gtm_limits.h"
 #include "get_syslog_flags.h"
+#include "ydb_getenv.h"
 
 #define ROOTUID 0
 #define ROOTGID 0
@@ -61,8 +62,7 @@
 #define MAX_ALLOWABLE_LEN 256
 #define ASCIICTLMAX	32  /* space character */
 #define ASCIICTLMIN	0   /* NULL character */
-#define GTM_TMP		"gtm_tmp"
-#define YDB_DIST	"ydb_dist"
+#define YDB_TMP		"ydb_tmp"
 #define	SUB_PATH_TO_GTMSECSHRDIR "/gtmsecshrdir"
 #define	REL_PATH_TO_CURDIR "."
 #define	REL_PATH_TO_GTMSECSHR "./gtmsecshr"
@@ -77,7 +77,7 @@
 #define TZLOCATOR			"TZ="
 #define NEWLINE				0x0a
 #define GTM_WHITE_BOX_TEST_CASE_ENABLE	"gtm_white_box_test_case_enable"
-#define GTM_WHITE_BOX_TEST_CASE_NUMBER	"gtm_white_box_test_case_number"
+#define GTM_WHITE_BOX_TEST_CASE_NUMBER	"ydb_white_box_test_case_number"
 #define GTMETCDIRPATH			"/etc"
 #define BADGTMETCDIRPATH		"/bogusdirnotetc"
 #define GTMENVIRONFILE			"environment"
@@ -101,8 +101,8 @@ extern	char	**environ;
 	"%%YDB-E-SECSHREXECLFAILED, execl of %s failed\n"
 #define ERR_SECSHRYDBDIST2LONG			\
 	"%%YDB-E-SECSHRYDBDIST2LONG, ydb_dist env var too long. gtmsecshr will not be started\n"
-#define ERR_SECSHRGTMTMP2LONG			\
-	"%%YDB-E-SECSHRGTMTMP2LONG, gtm_tmp env var too long. gtmsecshr will not be started\n"
+#define ERR_SECSHRYDBTMP2LONG			\
+	"%%YDB-E-SECSHRYDBTMP2LONG, ydb_tmp/gtm_tmp env var too long. gtmsecshr will not be started\n"
 #define ERR_SECSHRNOYDBDIST			\
 	"%%YDB-E-SECSHRNOYDBDIST, ydb_dist env var does not exist. gtmsecshr will not be started\n"
 #define ERR_SECSHRNOTOWNEDBYROOT		\
@@ -113,8 +113,8 @@ extern	char	**environ;
 	"%%YDB-E-SECSHRPERMINCRCT, %s permissions incorrect (%04o). gtmsecshr will not be started\n"
 #define ERR_SECSHRSETYDBDISTFAILED		\
 	"%%YDB-E-SECSHRSETYDBDISTFAILED, setenv for ydb_dist failed. gtmsecshr will not be started\n"
-#define ERR_SECSHRSETGTMTMPFAILED		\
-	"%%YDB-E-SECSHRSETGTMTMPFAILED, setenv for gtm_tmp failed. gtmsecshr will not be started\n"
+#define ERR_SECSHRSETYDBTMPFAILED		\
+	"%%YDB-E-SECSHRSETYDBTMPFAILED, setenv for ydb_tmp failed. gtmsecshr will not be started\n"
 #define ERR_SECSHRSETUIDFAILED			\
 	"%%YDB-E-SECSHRSETUIDFAILED, setuid failed. gtmsecshr will not be started\n"
 #define ERR_SECSHRSTATFAILED			\
@@ -135,13 +135,13 @@ SECSHRCHDIRFAILED2	<chdir failed on !AD, errno !UL. gtmsecshr will be started wi
 SECSHRCLEARENVFAILED	<clearenv failed. gtmsecshr will not be started>/error/fao=0!/ansi=0
 SECSHREXECLFAILED	<execl of !AD failed>/error/fao=2!/ansi=0
 SECSHRYDBDIST2LONG	<ydb_dist env var too long. gtmsecshr will not be started>/error/fao=0!/ansi=0
-SECSHRGTMTMP2LONG	<gtm_tmp env var too long. gtmsecshr will not be started>/error/fao=0!/ansi=0
+SECSHRYDBTMP2LONG	<ydb_tmp/gtm_tmp env var too long. gtmsecshr will not be started>/error/fao=0!/ansi=0
 SECSHRNOYDBDIST		<ydb_dist env var does not exist. gtmsecshr will not be started>/error/fao=0!/ansi=0
 SECSHRNOTOWNEDBYROOT	<!AD not owned by root. gtmsecshr will not be started>/error/fao=3!/ansi=0
 SECSHRNOTSETUID		<!AD not set-uid. gtmsecshr will not be started>/error/fao=2!/ansi=0
 SECSHRPERMINCRCT	<!AD permissions incorrect (0!UL). gtmsecshr will not be started>/error/fao=3!/ansi=0
 SECSHRSETYDBDISTFAILED	<setenv for ydb_dist failed. gtmsecshr will not be started>/error/fao=0!/ansi=0
-SECSHRSETGTMTMPFAILED	<setenv for gtm_tmp failed. gtmsecshr will not be started>/error/fao=0!/ansi=0
+SECSHRSETYDBTMPFAILED	<setenv for ydb_tmp failed. gtmsecshr will not be started>/error/fao=0!/ansi=0
 SECSHRSETUIDFAILED	<setuid failed. gtmsecshr will not be started>/error/fao=0!/ansi=0
 SECSHRSTATFAILED	<stat failed on !AD, errno !UL. gtmsecshr will not be started>/error/fao=3!/ansi=0
 SECSHRTZFAIL		<!AD !UL. gtmsecshr will start with TZ set to GMT>/warning/fao=3!/ansi=0
@@ -174,13 +174,13 @@ int main()
 	struct stat	gtm_secshrdir_stat;
 	struct stat	gtm_secshr_stat;
 	char 		ydb_dist_val[MAX_ENV_VAR_VAL_LEN];
-	char 		gtm_tmp_val[MAX_ENV_VAR_VAL_LEN];
+	char 		ydb_tmp_val[MAX_ENV_VAR_VAL_LEN];
 	char 		gtm_secshrdir_path[MAX_ENV_VAR_VAL_LEN];
 	char 		gtm_secshrdir_path_display[MAX_ENV_VAR_VAL_LEN];
 	char 		gtm_secshr_path[MAX_ENV_VAR_VAL_LEN];
 	char 		gtm_secshr_path_display[MAX_ENV_VAR_VAL_LEN];
 	char 		gtm_secshr_orig_path[MAX_ENV_VAR_VAL_LEN];
-	boolean_t	gtm_tmp_exists = FALSE;
+	boolean_t	ydb_tmp_exists = FALSE;
 	int		rc;
 	sigset_t	mask;
 #	ifdef _AIX
@@ -205,15 +205,15 @@ int main()
 	 * numerically as best as possible. For the boolean enable flag, If it's non-zero - it's true else false. No errors raised
 	 * here for conversions.
 	 */
-	env_var_ptr = getenv(GTM_WHITE_BOX_TEST_CASE_ENABLE);
+	env_var_ptr = ydb_getenv(YDBENVINDX_WHITE_BOX_TEST_CASE_ENABLE, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH);
 	if (NULL != env_var_ptr)
 	{
-		gtm_white_box_test_case_enabled = atoi(env_var_ptr);
-		if (gtm_white_box_test_case_enabled)
+		ydb_white_box_test_case_enabled = atoi(env_var_ptr);
+		if (ydb_white_box_test_case_enabled)
 		{
-			env_var_ptr = getenv(GTM_WHITE_BOX_TEST_CASE_NUMBER);
+			env_var_ptr = ydb_getenv(YDBENVINDX_WHITE_BOX_TEST_CASE_NUMBER, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH);
 			if (NULL != env_var_ptr)
-				gtm_white_box_test_case_number = atoi(env_var_ptr);
+				ydb_white_box_test_case_number = atoi(env_var_ptr);
 		}
 	}
 #	endif /* DEBUG */
@@ -306,7 +306,7 @@ int main()
 #	endif /* _AIX */
 	ret = 0; /* start positive */
 	/* get the ones we need */
-	if (env_var_ptr = getenv(YDB_DIST))		/* Warning - assignment */
+	if (env_var_ptr = ydb_getenv(YDBENVINDX_DIST_ONLY, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH))	/* Warning - assignment */
 	{
 		if (MAX_ALLOWABLE_LEN < (strlen(env_var_ptr) + STR_LIT_LEN(SUB_PATH_TO_GTMSECSHRDIR)
 		    + STR_LIT_LEN(GTMSECSHR_BASENAME)))
@@ -331,16 +331,16 @@ int main()
 		SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRNOYDBDIST);
 		ret = -1;
 	}
-	if (env_var_ptr = getenv(GTM_TMP))		/* Warning - assignment */
+	if (env_var_ptr = ydb_getenv(YDBENVINDX_TMP, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH))	/* Warning - assignment */
 	{
 		if (MAX_ALLOWABLE_LEN < strlen(env_var_ptr))
 		{
-			SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRGTMTMP2LONG);
+			SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRYDBTMP2LONG);
 			ret = -1;
 		} else
 		{
-			gtm_tmp_exists = TRUE;
-			strcpy(gtm_tmp_val, env_var_ptr);
+			ydb_tmp_exists = TRUE;
+			strcpy(ydb_tmp_val, env_var_ptr);
 		}
 	}
 	if (!ret)
@@ -357,18 +357,18 @@ int main()
 			ret = -1;
 		}
 		/* add the ones we need */
-		status = setenv(YDB_DIST, ydb_dist_val, TRUE);
+		status = setenv(ydbenvname[YDBENVINDX_DIST] + 1, ydb_dist_val, TRUE);	/* + 1 to skip leading $ */
 		if (status)
 		{
 			SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRSETYDBDISTFAILED);
 			ret = -1;
 		}
-		if (gtm_tmp_exists)
+		if (ydb_tmp_exists)
 		{
-			status = setenv(GTM_TMP, gtm_tmp_val, TRUE);
+			status = setenv(YDB_TMP, ydb_tmp_val, TRUE);
 			if (status)
 			{
-				SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRSETGTMTMPFAILED);
+				SYSLOG(LOG_USER | LOG_INFO, ERR_SECSHRSETYDBTMPFAILED);
 				ret = -1;
 			}
 		}

commit 6a2bc5b967f8c49911aa9b917a71299de9990d07

Fix gtmsecshr_wrapper to examine gtm_dist env var if ydb_dist is unset (regression in 9af7f589)
Background
----------
* As part of YDBTest@a70b72b3 (see that commit message for more details), the `secshr/gtm7617`
  subtest was enhanced to test 2 scenarios.

  - ydb_dist env var is set to the same tampered value (with control characters) as `gtm_dist`
  - ydb_dist env var is unset and `gtm_dist` had the tampered value

* In either case, I expected the test to behave the same. But the test ran fine only in the
  first case and not in the second case.

Issue
-----
* Commit 9af7f589 made the following change.

  ```diff
  $ git show -U1 9af7f589 sr_unix/gtmsecshr_wrapper.c
  .
  .
  @@ -308,3 +308,3 @@ int main()
          /* get the ones we need */
  -       if (env_var_ptr = getenv(YDB_DIST))             /* Warning - assignment */
  +       if (env_var_ptr = ydb_getenv(YDBENVINDX_DIST_ONLY, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH)) /* Warning - assignment */
          {
  .
  .
  ```

* Note that it replaced a `getenv(YDB_DIST)` call with `ydb_getenv(YDBENVINDX_DIST_ONLY)` call.

* `YDBENVINDX_DIST_ONLY` meant only look at `ydb_dist` env var. Whereas `YDBENVINDX_DIST` would have
  looked at `ydb_dist` and `gtm_dist` env vars in that order.

Fix
---
* I don't see any reason (security or otherwise) why we cannot honor `gtm_dist` too in case `ydb_dist`
  is unset.

* Therefore, the fix is simple and is to replace `YDBENVINDX_DIST_ONLY` with `YDBENVINDX_DIST`.

Notes
-----
* Although this is a user-visible change, it is not expected to be encountered in practice so no new
  YDB issue was created for this.

sr_unix/gtmsecshr_wrapper.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2008-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2023 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -305,7 +305,7 @@ int main()
 #	endif /* _AIX */
 	ret = 0; /* start positive */
 	/* get the ones we need */
-	if (env_var_ptr = ydb_getenv(YDBENVINDX_DIST_ONLY, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH))	/* Warning - assignment */
+	if (env_var_ptr = ydb_getenv(YDBENVINDX_DIST, NULL_SUFFIX, NULL_IS_YDB_ENV_MATCH))	/* Warning - assignment */
 	{
 		if (MAX_ALLOWABLE_LEN <
 				SECSHR_PARENT_DIR_LEN(strlen(env_var_ptr)) + STR_LIT_LEN(GTMSECSHR_DIR_SUFFIX) + STR_LIT_LEN(GTMSECSHR_BASENAME))