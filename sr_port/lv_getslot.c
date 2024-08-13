YottaDB/YDB

commit 49f59d9bd31f990f330d3336d05fab7f76163790

[NARS1] [estess] Eliminate longcpy usages with memcpy; Fix edge case in lv_getslot #80
These changes address review comments

1) All longcpy usages need to be replaced with memcpy. Since there were only a few remaining in the codebase, I replaced all of them.
2) lv_getslot.c : In function lvtreenode_getslot(), as soon as numElems is > (MAXINT4 / 2) but is < MAXINT4, we would end up passing a number that is > MAXINT4 but < MAXUINT4 to lvtreenode_newblock(). The latter expects a signed int though and so is going to see this as a negative number. Fix it so we never pass more than MAXINT4 to lvtreenode_newblock(). While in this module, fix a few assignments to be DEBUG_ONLY and move them to their proper scope and add a few missing asserts in lv_getslot() similar to the other two (lvtree_getslot() and lvtreenode_getslot()).

sr_port/lv_getslot.c
@@ -42,13 +42,13 @@ lv_val *lv_getslot(symval *sym)
 	lv_val		*lv;
 	unsigned int	numElems, numUsed;
 
-	numElems = MAXUINT4;	/* maximum value */
 	if (lv = sym->lv_flist)
 	{
 		assert(NULL == LV_PARENT(lv));		/* stp_gcol relies on this for correct garbage collection */
 		sym->lv_flist = (lv_val *)lv->ptrs.free_ent.next_free;
 	} else
 	{
+		DEBUG_ONLY(numElems = MAXUINT4);	/* maximum value */
 		for (p = sym->lv_first_block; ; p = p->next)
 		{
 			if (NULL == p)
@@ -71,6 +71,8 @@ lv_val *lv_getslot(symval *sym)
 				p->numUsed++;
 				break;
 			}
+			assert(numElems >= p->numAlloc);
+			DEBUG_ONLY(numElems = p->numAlloc);
 		}
 	}
 	assert(lv);
@@ -85,13 +87,13 @@ lvTree *lvtree_getslot(symval *sym)
 	lvTree		*lvt;
 	unsigned int	numElems, numUsed;
 
-	numElems = MAXUINT4;	/* maximum value */
 	if (lvt = sym->lvtree_flist)
 	{
 		assert(NULL == LVT_GET_PARENT(lvt));
 		sym->lvtree_flist = (lvTree *)lvt->avl_root;
 	} else
 	{
+		DEBUG_ONLY(numElems = MAXUINT4);	/* maximum value */
 		for (p = sym->lvtree_first_block; ; p = p->next)
 		{
 			if (NULL == p)
@@ -126,13 +128,13 @@ lvTreeNode *lvtreenode_getslot(symval *sym)
 	lvTreeNode	*lv;
 	unsigned int	numElems, numUsed;
 
-	numElems = MAXUINT4;	/* maximum value */
 	if (lv = sym->lvtreenode_flist)
 	{
 		assert(NULL == LV_PARENT(lv));	/* stp_gcol relies on this for correct garbage collection */
 		sym->lvtreenode_flist = (lvTreeNode *)lv->sbs_child;
 	} else
 	{
+		DEBUG_ONLY(numElems = MAXUINT4);	/* maximum value */
 		for (p = sym->lvtreenode_first_block; ; p = p->next)
 		{
 			if (NULL == p)
@@ -141,7 +143,7 @@ lvTreeNode *lvtreenode_getslot(symval *sym)
 					numElems = p->numAlloc;
 				else
 					numElems = LV_NEWBLOCK_INIT_ALLOC;
-				lvtreenode_newblock(sym, (numElems < MAXINT4) ? (numElems * 2) : MAXINT4);
+				lvtreenode_newblock(sym, (numElems <= (MAXINT4 / 2)) ? (numElems * 2) : MAXINT4);
 				p = sym->lvtreenode_first_block;
 				assert(NULL != p);
 			}

sr_port/lke_show.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -33,7 +36,6 @@
 #include "hashtab_mname.h"	/* needed for cmmdef.h */
 #include "cmmdef.h"	/* for gtcmtr_protos.h */
 #include "util.h"
-#include "longcpy.h"
 #include "gtcmtr_protos.h"
 #include "lke.h"
 #include "lke_getcli.h"
@@ -113,7 +115,7 @@ void	lke_show(void)
 				/* Prevent any modification of the lock space while we make a local copy of it */
 				if (!nocrit)
 					GRAB_LOCK_CRIT(csa, reg, was_crit);
-				longcpy((uchar_ptr_t)ctl, (uchar_ptr_t)csa->lock_addrs[0], ls_len);
+				memcpy((uchar_ptr_t)ctl, (uchar_ptr_t)csa->lock_addrs[0], ls_len);
 				assert((ctl->max_blkcnt > 0) && (ctl->max_prccnt > 0) && ((ctl->subtop - ctl->subbase) > 0));
 				if (!nocrit)
 					REL_LOCK_CRIT(csa, reg, was_crit);

sr_port/longcpy.h
@@ -1,28 +1,0 @@
-/****************************************************************
- *								*
- *	Copyright 2001, 2003 Sanchez Computer Associates, Inc.	*
- *								*
- *	This source code contains the intellectual property	*
- *	of its copyright holder(s), and is made available	*
- *	under a license.  If you do not know the terms of	*
- *	the license, please stop and do not read further.	*
- *								*
- ****************************************************************/
-
-#ifndef LONGCPY_INCLUDED
-#define LONGCPY_INCLUDED
-
-/*
-
-To be one day eliminated when usage is totally replaced by the memcpy
-calls it should be calling now instead. Since this is a stop-gap measure,
-it is ok that this include pulls in gtm_string.h if necessary.
-
-void longcpy(uchar_ptr_t a, uchar_ptr_t b, int4 len);
-
- */
-
-#include "gtm_string.h"
-#define longcpy(dst, src, len) memcpy(dst, src, len)
-
-#endif /* LONGCPY_INCLUDED */

sr_port/stp_expand_array.c
@@ -13,10 +13,12 @@
  ****************************************************************/
 
 #include "mdef.h"
+
+#include "gtm_string.h"
+
 #include "compiler.h"
 #include "stringpool.h"
 #include "stp_parms.h"
-#include "longcpy.h"
 
 GBLREF mstr		**stp_array;
 GBLREF gtm_uint64_t	stp_array_size;
@@ -30,7 +32,7 @@ void stp_expand_array(void)
 	stp_array_size = ((MAXINT4 > n) ? (n * 2) : (n + MAXINT4));
 	a = stp_array;
 	stp_array = (mstr **)malloc(stp_array_size * SIZEOF(mstr *));
-	longcpy((uchar_ptr_t)stp_array, (uchar_ptr_t)a, n * SIZEOF(mstr *));
+	memcpy((uchar_ptr_t)stp_array, (uchar_ptr_t)a, n * SIZEOF(mstr *));
 	free(a);
 	return;
 }

sr_port/stp_gcol_src.h
@@ -56,7 +56,6 @@
 #include "zshow.h"
 #include "zwrite.h"
 #include "error.h"
-#include "longcpy.h"
 #include "stpg_sort.h"
 #include "hashtab_objcode.h"
 #include "hashtab_str.h"

sr_port/tp_hist.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -13,6 +16,7 @@
 #include "mdef.h"
 
 #include "gtm_string.h"
+
 #include "gdsroot.h"
 #include "gdskill.h"
 #include "gdsblk.h"
@@ -25,7 +29,6 @@
 #include "copy.h"
 #include "jnl.h"
 #include "buddy_list.h"		/* needed for tp.h */
-#include "longcpy.h"
 #include "hashtab_int4.h"	/* needed for tp.h and cws_insert.h */
 #include "tp.h"
 #include "longset.h"		/* needed for cws_insert.h */
@@ -496,7 +499,7 @@ void	gds_tp_hist_moved(sgm_info *si, srch_hist *hist1)
 	assert(si->cur_tp_hist_size < si->tp_hist_size);
 	si->cur_tp_hist_size <<= 1;
 	new_first_tp_hist = (srch_blk_status *)malloc(SIZEOF(srch_blk_status) * si->cur_tp_hist_size);
-	longcpy((uchar_ptr_t)new_first_tp_hist, (uchar_ptr_t)si->first_tp_hist,
+	memcpy((uchar_ptr_t)new_first_tp_hist, (uchar_ptr_t)si->first_tp_hist,
 		(sm_uc_ptr_t)si->last_tp_hist - (sm_uc_ptr_t)si->first_tp_hist);
 	delta = (sm_long_t)((sm_uc_ptr_t)new_first_tp_hist - (sm_uc_ptr_t)si->first_tp_hist);
 	for (tabent = si->blks_in_use->base, topent = si->blks_in_use->top; tabent < topent; tabent++)

sr_port_cm/gtcmtr_lke_showrep.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -18,6 +21,8 @@
  */
 
 #include "mdef.h"
+
+#include "gtm_string.h"
 
 #include <stddef.h>
 
@@ -39,7 +44,6 @@
 #include "iosp.h"
 #include "gtcm_find_region.h"
 #include "gvcmz.h"
-#include "longcpy.h"
 #include "interlock.h"
 #include "rel_quant.h"
 
@@ -68,7 +72,7 @@ char gtcmtr_lke_showrep(struct CLB *lnk, show_request *sreq)
 		lke_ctl = (mlk_ctldata *)malloc(ls_len);
 		/* Prevent any modification of the lock space while we make a local copy of it */
 		GRAB_LOCK_CRIT(csa, gv_cur_region, was_crit);
-		longcpy((uchar_ptr_t)lke_ctl, csa->lock_addrs[0], ls_len);
+		memcpy((uchar_ptr_t)lke_ctl, csa->lock_addrs[0], ls_len);
 		REL_LOCK_CRIT(csa, gv_cur_region, was_crit);
 		util_cm_print(lnk, 0, NULL, RESET);
 		dnode.len = sreq->nodelength;

sr_port_cm/gtcmtr_lke_showreq.c
@@ -1,6 +1,9 @@
 /****************************************************************
  *								*
- *	Copyright 2001, 2009 Fidelity Information Services, Inc	*
+ * Copyright 2001, 2009 Fidelity Information Services, Inc	*
+ *								*
+ * Copyright (c) 2017 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
@@ -40,7 +43,6 @@
 #include "iosp.h"
 #include "gtcm_find_region.h"
 #include "gvcmz.h"
-#include "longcpy.h"
 
 #define FLUSH	1
 

commit e7c707371df29ba95d32ec912df049dc8fd39748

[YDB#1088] [DEBUG-ONLY] Fix incorrect assert in lv_getslot.c (regression in 49f59d9b)
* See https://gitlab.com/YottaDB/DB/YDB/-/issues/1088#description for issue description
  that describes an assert failure in `lv_getslot.c`.

* See newly added comment (as part of this commit) in `lv_getslot.c` for a description
  of why the assert is incorrect.

* This is a regression in 49f59d9b where the failing assert was added because it exists
  in 2 other similar functions per the below pasted text from that commit message.

  _add a few missing asserts in lv_getslot() similar to the other two (lvtree_getslot()
  and lvtreenode_getslot())_

* The fix is simple and is to remove the assert.

sr_port/lv_getslot.c
@@ -4,7 +4,7 @@
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
 
  *								*
- * Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -49,7 +49,6 @@ lv_val *lv_getslot(symval *sym)
 		sym->lv_flist = (lv_val *)lv->ptrs.free_ent.next_free;
 	} else
 	{
-		DEBUG_ONLY(numElems = MAXUINT4);	/* maximum value */
 		for (p = sym->lv_first_block; ; p = p->next)
 		{
 			if (NULL == p)
@@ -72,8 +71,22 @@ lv_val *lv_getslot(symval *sym)
 				p->numUsed++;
 				break;
 			}
-			assert(numElems >= p->numAlloc);
-			DEBUG_ONLY(numElems = p->numAlloc);
+			/* Note: The following assert exists in "lvtree_getslot()" and "lvtreenode_getslot()".
+			 *	assert(numElems >= p->numAlloc);
+			 * This assert basically says that the "sym->lv_first_block" linked list has the property
+			 * that earlier nodes in the linked list have a "numAlloc" that is greater or equal to
+			 * later nodes. This is guaranteed to be true since the only allocations for lvtree and
+			 * lvtreenode happen inside "lvtree_getslot()" and "lvtreenode_getslot()" respectively
+			 * where the allocation is incrementally done and doubled at every node until it is maxed
+			 * out at 128.
+			 *
+			 * One would be tempted to have a similar assert here. But it is not guaranteed
+			 * to be true since "lv_newblock()" is called not just in "lv_getslot()" but also in
+			 * "sr_port/symbinit.c" with a bulk allocation based on the number of local variables used
+			 * in a given M program. This means that the initial allocation could be greater than 128
+			 * (see YDB#1088 issue description for example). In that case, later allocations would get
+			 * maxed out at "128" and so the assert would fail. Hence the assert is commented out here.
+			 */
 		}
 	}
 	assert(lv);