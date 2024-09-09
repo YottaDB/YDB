d9855f6f4152438001d41461b534a20b8abdca3e
Narayanan Iyer <nars@yottadb.com>
2024-09-09 15:08:50 -0400

    [#1097] Issue FALLINTOFLST error even when falling through dotted DO lines
    Background
    ----------
    Below is pasted from https://gitlab.com/YottaDB/DB/YDB/-/issues/1097#description.
    
    * This bug report is from https://discord.com/channels/1137126843774931055/1139991195179831297/1281217899125538860
    
      A little more about the formallist:
      Simple program
    
      ```m
      zzz ; Test
       d sub1
       w !,a
       q
      sub1
       if 1 do
       . s a=1
      sub2(arg)
       s a=2
       q
      ```
    
    * The procedure `sub1` missed an exit. Works well, gives an error.
    
      ```m
      %YDB-E-FALLINTOFLST, Fall-through to a label with formallist is not allowed
              At M source location sub1+2^zzz
      ```
    
    * However, if we change the condition, there will be no error, and the code after the label will be executed.
    
      ```m
      zzz ; Test
       d sub1
       w !,a
       q
      sub1
       if 0 do
       . s a=1
      sub2(arg)
       s a=2
       q
      ```
    
      ```m
      YDB>d ^zzz
    
      2
      ```
    
    Issue
    -----
    * The issue is in `sr_port/resolve_blocks.c` where a `OC_JMP` triple is inserted as part of
      dotted DO line processing.
    
    * If the jump target of this `OC_JMP` happens to be a line containing a label with a formal list,
      then we miss out on issuing the error because the `OC_RTERROR` triple happens to be one triple
      BEFORE the first triple corresponding to the target M line.
    
    * If there are no dotted DO lines before the line containing the label with a formal list, then
      there is no issue.
    
    Fix
    ---
    * The fix is simple and is to check if the target of the `OC_JMP` is a triple that is preceded
      by a `OC_RTERROR` triple whose first operand is the `ERR_FALLINTOFLST` message number. If so,
      we reset the jump target to be the `OC_RTERROR` triple.

--- sr_port/resolve_blocks.c
+++ sr_port/resolve_blocks.c
@@ -2,6 +2,9 @@
  *								*
  *	Copyright 2001 Sanchez Computer Associates, Inc.	*
  *								*
+ * Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -28,11 +31,26 @@ void resolve_blocks(void)
 		{
 			if (mlx->sibling)
 			{
+				triple	*t2, *t3;
+
 				ref = maketriple(OC_JMP);
-				ref->operand[0] = put_tjmp(mlx->sibling->externalentry);
+				t1 = mlx->sibling->externalentry;
+				/* We are about to jump from this line to its sibling line (which is a few M lines later
+				 * because of intervening dotted DO lines). Check if the sibling line had a FALLINTOFLST
+				 * error triple added before it (in line.c). If so, reset the jump target to that triple
+				 * as this jump should result in the same fall-through error (YDB#1097).
+				 */
+				t2 = t1->exorder.bl;
+				if (OC_RTERROR == t2->opcode)
+				{
+					t3 = t2->operand[0].oprval.tref;
+					if ((ILIT_REF == t3->operand[0].oprclass)
+							&& (ERR_FALLINTOFLST == t3->operand[0].oprval.ilit))
+						t1 = t2;
+				}
+				ref->operand[0] = put_tjmp(t1);
 			} else
-			{	ref = maketriple(OC_RET);
-			}
+				ref = maketriple(OC_RET);
 			t1 = mly->externalentry;
 			t1 = t1->exorder.bl;
 			ref->src = t1->src;
