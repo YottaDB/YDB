YottaDB/YDB

commit c6d1a9a2b04e9ada081a06c3be391cca205cc67f

[#828] Fix SIG-11 and heap-use-after-free errors if current device has been opened more than once and gets closed
Background
----------
* This is an issue identified by fuzz testing.

* Below is a simple test case demonstrating the SIG-11.

  ```m
  $ unsetenv ydb_dist
  $ rm -f x.m x.o
  $ echo "a\nb c" > x.m
  $ echo ' set file="x.m" open file use file\n set $zro=""\n do ^x\n set x=;' | $ydb_dist/yottadb -direct

  YDB>

  YDB>

  YDB>
          b c
             ^-----
                  At column 4, line 2, source module x.m
  %YDB-E-SPOREOL, Either a space or an end-of-line was expected but not found
  %YDB-E-SPOREOL, Either a space or an end-of-line was expected but not found
                  At M source location b^x
  %YDB-F-KILLBYSIGSINFO1, YottaDB process 55827 has been killed by a signal 11 at address 0x00007F4CD31B4BD2 (vaddr 0x0000000000000010)
  %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

* Below is the same test case with a build of YottaDB that has ASAN enabled. This shows a `heap-use-after-free` error which
  happens reliably (the SIG-11 shown above in a non-ASAN build of YottaDB might not show up as reliably).

  ```m
  $ rm -f x.o
  $ $ydb_dist/yottadb -direct

  YDB>set file="x.m" open file use file

  YDB>set $zro=""

  YDB>do ^x
  %YDB-E-ZYSQLNULLNOTVALID, $ZYSQLNULL cannot be used as an integer, numeric, gvn subscript/value or lock subscript
                  At M source location +1^x
  =================================================================
  ==49387==ERROR: AddressSanitizer: heap-use-after-free on address 0x6120000004d0 at pc 0x7f94f1229e1c bp 0x7fffce5cfe70 sp 0x7fffce5cfe60
  READ of size 8 at 0x6120000004d0 thread T0
      #0 0x7f94f1229e1b in op_break sr_port/op_break.c:115:24
      #1 0x7f94f06b4b70  sr_x86_64/opp_break.s:30

  0x6120000004d0 is located 16 bytes inside of 320-byte region [0x6120000004c0,0x612000000600)
  freed by thread T0 here:
      #0 0x4b90a8 in free (yottadb+0x4b90a8)

sr_unix/source_file.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2019-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -330,6 +330,7 @@ void	close_source_file (void)
 	mval		val;
 	mval		pars;
 	unsigned char	no_param;
+	boolean_t	in_is_curr_device, out_is_curr_device;
 
 	no_param = (unsigned char)iop_eol;
 	pars.mvtype = MV_STR;
@@ -338,7 +339,9 @@ void	close_source_file (void)
 	val.mvtype = MV_STR;
 	val.str.len = source_name_len;
 	val.str.addr = (char *)source_file_name;
+	SAVE_IN_OUT_IS_CURR_DEVICE(tmp_list_dev, in_is_curr_device, out_is_curr_device);
 	op_close(&val, &pars);
-	io_curr_device = tmp_list_dev;	/*	not dev_in_use to make sure list file works	*/
+	/* Note: Use tmp_list_dev and not dev_in_use below to make sure list file works */
+	RESTORE_IO_CURR_DEVICE(tmp_list_dev, in_is_curr_device, out_is_curr_device);
 	return;
 }

sr_port/io.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -606,5 +606,44 @@ MBSTART {											\
 		MSTR->addr = IOD->dollar.devicebuffer;						\
 		MSTR->len = STRLEN(IOD->dollar.devicebuffer);					\
 	}											\
+} MBEND
+
+/* The below macro is called before an "op_close" that is followed by a restore of "io_curr_device".
+ * In that case, we need to make sure the restored "io_curr_device" after the "op_close()" does not point
+ * to the just now closed/freed device. The "IN_IS_CURR_DEVICE" and "OUT_IS_CURR_DEVICE" output parameters
+ * are used by the caller after the "op_close()" to adjust "io_curr_device" as appropriate.
+ */
+#define SAVE_IN_OUT_IS_CURR_DEVICE(SAVE_DEVICE, IN_IS_CURR_DEVICE, OUT_IS_CURR_DEVICE)	\
+MBSTART {										\
+	GBLREF io_pair		io_curr_device;						\
+											\
+	IN_IS_CURR_DEVICE = (io_curr_device.in == SAVE_DEVICE.in);			\
+	OUT_IS_CURR_DEVICE = (io_curr_device.out == SAVE_DEVICE.out);			\
+} MBEND
+
+/* The below macro is invoked after an "op_close()" that is preceded by a SAVE_IN_OUT_IS_CURR_DEVICE macro call.
+ * After restoring "io_curr_device" to the "SAVE_DEVICE" input macro parameter, we check the other 2 parameters.
+ * If they are TRUE it means "SAVE_DEVICE.in" and/or "SAVE_DEVICE.out" were closed as part of the just recently
+ * done "op_close()" call and so we adjust "io_curr_device.in" and "io_curr_device.out" respectively to point to
+ * $PRINCIPAL (i.e. "io_std_device") as we do not want "io_curr_device" to point to a closed/freed device. (YDB#828)
+ */
+#define	RESTORE_IO_CURR_DEVICE(SAVE_DEVICE, IN_IS_CURR_DEVICE, OUT_IS_CURR_DEVICE)	\
+MBSTART {										\
+	GBLREF io_pair		io_curr_device, io_std_device;				\
+											\
+	/* Assert that if IN_IS_CURR_DEVICE is TRUE, that "io_curr_device.in" got	\
+	 * automatically adjusted to point to "io_std_device.in" (by "op_close()").	\
+	 */										\
+	assert(!IN_IS_CURR_DEVICE || (io_curr_device.in == io_std_device.in));		\
+	/* Similar assert for "OUT_IS_CURR_DEVICE" */					\
+	assert(!OUT_IS_CURR_DEVICE || (io_curr_device.out == io_std_device.out));	\
+	/* Restore "io_curr_device" first */						\
+	io_curr_device = SAVE_DEVICE;							\
+	/* Adjust "io_curr_device.in" if it points to a closed device. */		\
+	if (IN_IS_CURR_DEVICE)								\
+		io_curr_device.in = io_std_device.in;					\
+	/* Adjust "io_curr_device.in" if it points to a closed device. */		\
+	if (OUT_IS_CURR_DEVICE)								\
+		io_curr_device.out = io_std_device.out;					\
 } MBEND
 #endif /* IO_H */

sr_port/jobexam_process.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -117,7 +117,7 @@ void jobexam_process(mval *dump_file_name, mval *zshowcodes, mval *dump_file_spe
 	sigaction(SIGBUS, &new_action, &prev_action_sigbus);
 	sigaction(SIGSEGV, &new_action, &prev_action_sigsegv);
 	*dump_file_spec = empty_str_mval;
-	dev_in_use = io_curr_device;		/* Save current IO device */
+	dev_in_use = io_curr_device;            /* Save current IO device */
 	/* Save text in util_outbuff which can be detrimentally overwritten by ZSHOW.
 	 * NOTE: The following code needs to be eventually moved to jobinterrupt_process.c and replaced with
 	 * SAVE/RESTORE_UTIL_OUT_BUFFER macros, as follows:
@@ -143,14 +143,17 @@ void jobexam_process(mval *dump_file_name, mval *zshowcodes, mval *dump_file_spe
 		assert(saved_util_outbuff_len <= SIZEOF(saved_util_outbuff));
 		memcpy(saved_util_outbuff, TREF(util_outbuff_ptr), saved_util_outbuff_len);
 	}
-	jobexam_dump(input_dump_file_name, dump_file_spec, save_dump_file_name_buff, zshowcodes);
-	/* If any errors occur in job_exam_dump, the condition handler will unwind the stack to this point and return.  */
+	/* See comment inside "jobexam_dump()" for why "&dev_in_use" needs to be passed as a parameter */
+	jobexam_dump(input_dump_file_name, dump_file_spec, save_dump_file_name_buff, zshowcodes, &dev_in_use);
+	io_curr_device = dev_in_use;
+	/* If any errors occur in job_exam_dump, the condition handler will unwind the stack to this point
+	 * in the caller frame and continue execution from here.
+	 */
 	if (0 != saved_util_outbuff_len)
 	{	/* Restore util_outbuff values */
 		memcpy(TREF(util_outbuff_ptr), saved_util_outbuff, saved_util_outbuff_len);
 		TREF(util_outptr) = TREF(util_outbuff_ptr) + saved_util_outbuff_len;
 	}
-	io_curr_device = dev_in_use;		/* Restore IO device */
 	/* If we saved an mval on our stack, we need to pop it off. If there was an error while doing the
 	 * jobexam dump, zshow may have left some other mv_stent entries on the stack. Pop them all off with
 	 * just a regular POP_MV_STENT macro rather than unw_mv_ent() call because the mv_stent entries
@@ -176,10 +179,12 @@ void jobexam_process(mval *dump_file_name, mval *zshowcodes, mval *dump_file_spe
 /* This routine is broken out as another ep so we can do cleanup processing in jobexam_process if
  * we trigger the condition handler and unwind.
  */
-void jobexam_dump(mval *dump_filename_arg, mval *dump_file_spec, char *fatal_file_name_buff, mval *zshowcodes)
+void jobexam_dump(mval *dump_filename_arg, mval *dump_file_spec, char *fatal_file_name_buff, mval *zshowcodes, io_pair *dev_in_use)
 {
 	unsigned char		dump_file_name[DEFAULT_DUMP_FILE_TOTSIZE], *dump_file_name_ptr;
 	mval			def_file_name, parms, zshowops;
+	io_pair			lcl_dev_in_use;
+	boolean_t		in_is_curr_device, out_is_curr_device;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -215,6 +220,7 @@ void jobexam_dump(mval *dump_filename_arg, mval *dump_file_spec, char *fatal_fil
 	parms.mvtype = MV_STR;
 	parms.str.addr = (char *)dumpable_error_dump_file_parms;
 	parms.str.len = SIZEOF(dumpable_error_dump_file_parms);
+	lcl_dev_in_use = io_curr_device;		/* Save current IO device */
 	/* Open, use, and zshow into new file, then close and reset current io device */
 	op_open(dump_file_spec, &parms, (mval *)&literal_zero, 0);
 	op_use(dump_file_spec, &parms);
@@ -231,7 +237,16 @@ void jobexam_dump(mval *dump_filename_arg, mval *dump_file_spec, char *fatal_fil
 	op_zshow(&zshowops, ZSHOW_DEVICE, NULL);
 	parms.str.addr = (char *)dumpable_error_dump_file_noparms;
 	parms.str.len = SIZEOF(dumpable_error_dump_file_noparms);
+	SAVE_IN_OUT_IS_CURR_DEVICE(lcl_dev_in_use, in_is_curr_device, out_is_curr_device);
 	op_close(dump_file_spec, &parms);
+	RESTORE_IO_CURR_DEVICE(lcl_dev_in_use, in_is_curr_device, out_is_curr_device);
+	/* Now that "op_close()" is done and "io_curr_device" has been adjusted (if needed) by the above macro,
+	 * use this to set "*dev_in_use" as this will be used later by caller to reset "io_curr_device".
+	 * Caller needs that line because it needs to switch back the current device even if there are any errors
+	 * in say "op_zshow()" call above (in which case the condition handler "jobexam_dump_ch()" will transfer
+	 * control back to caller without having done the "op_close()" above).
+	 */
+	*dev_in_use = io_curr_device;
 	/* Notify operator dump was taken */
 	send_msg_csa(CSA_ARG(NULL) VARLSTCNT(5) ERR_JOBEXAMDONE, 3, process_id, dump_file_spec->str.len, dump_file_spec->str.addr);
 	REVERT;

sr_port/jobexam_process.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2015 Fidelity National Information 	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -16,7 +16,9 @@
 #ifndef JOBEXAM_PROCESS_INCLUDED
 #define JOBEXAM_PROCESS_INCLUDED
 
+#include "io.h"
+
 void jobexam_process(mval *dump_file_name, mval *zshowcodes, mval *dump_file_spec);
-void jobexam_dump(mval *dump_file_name, mval *dump_file_spec, char *fatal_file_name_buff, mval *zshowcodes);
+void jobexam_dump(mval *dump_file_name, mval *dump_file_spec, char *fatal_file_name_buff, mval *zshowcodes, io_pair *dev_in_use);
 
 #endif

sr_unix/list_file.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2020 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -121,8 +121,9 @@ void open_list_file(void)
 
 void close_list_file(void)
 {
-	mval param,list_file;
-	char charspace;
+	mval		param,list_file;
+	char		charspace;
+	boolean_t	in_is_curr_device, out_is_curr_device;
 
 	param.str.len = 1;
 	charspace = (char) iop_eol;
@@ -130,8 +131,9 @@ void close_list_file(void)
 	list_file.mvtype = param.mvtype = MV_STR;
 	list_file.str.len = io_curr_device.in->trans_name->len;
 	list_file.str.addr = &io_curr_device.in->trans_name->dollar_io[0];
+	SAVE_IN_OUT_IS_CURR_DEVICE(dev_in_use, in_is_curr_device, out_is_curr_device);
 	op_close(&list_file, &param);
-	io_curr_device = dev_in_use;
+	RESTORE_IO_CURR_DEVICE(dev_in_use, in_is_curr_device, out_is_curr_device);
 }
 
 void list_head(bool newpage)

sr_unix/trigger_select.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2010-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2021 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2022 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -687,14 +687,16 @@ boolean_t trigger_select_tpwrap(char *select_list, uint4 select_list_len, char *
 	TREF(ztrig_use_io_curr_device) = FALSE;
 	if (0 != file_name_len)
 	{
+		boolean_t	in_is_curr_device, out_is_curr_device;
+
 		op_val.mvtype = op_pars.mvtype = MV_STR;
 		op_val.str.addr = (char *)file_name;;
 		op_val.str.len = file_name_len;
 		op_pars.str.len = SIZEOF(no_param);
 		op_pars.str.addr = (char *)&no_param;
+		SAVE_IN_OUT_IS_CURR_DEVICE(save_io_curr_device, in_is_curr_device, out_is_curr_device);
 		op_close(&op_val, &op_pars);
-		/* Return back to the current device */
-		io_curr_device = save_io_curr_device;
+		RESTORE_IO_CURR_DEVICE(save_io_curr_device, in_is_curr_device, out_is_curr_device);
 	}
 	return (TRIG_FAILURE == select_status);
 }

sr_unix/trigger_trgfile.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2010-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2021 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -101,6 +101,7 @@ STATICFNDEF boolean_t trigger_trgfile_tpwrap_helper(char *trigger_filename, uint
 	char			*trigptr;
 	char			*values[NUM_SUBS];
 	unsigned short		value_len[NUM_SUBS];
+	boolean_t		in_is_curr_device, out_is_curr_device;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -151,8 +152,9 @@ STATICFNDEF boolean_t trigger_trgfile_tpwrap_helper(char *trigger_filename, uint
 				util_out_print_gtmio("File !AD, Line !UL: Line too long", FLUSH, trigger_filename_len, trigger_filename, ++record_num);
 		io_curr_device = io_trigfile_device;
 	}
+	SAVE_IN_OUT_IS_CURR_DEVICE(io_save_device, in_is_curr_device, out_is_curr_device);
 	file_input_close();
-	io_curr_device = io_save_device;
+	RESTORE_IO_CURR_DEVICE(io_save_device, in_is_curr_device, out_is_curr_device);
 	if (trigger_error)
 	{
 		util_out_print_gtmio("=========================================", FLUSH);

commit 4d509b3e6a75683df81b7dab4d58926f73b024d3

[DEBUG-ONLY] Fix assert failure in MUMPS -MACHINE -LIS if more than 128 errors (regression in c6d1a9a2)
Background
----------
* This is an issue I noticed while trying to come up with an automated test case for YDB#1091.
  As part of that, the M program generated by the test case had a syntax error. And it had more
  than 128 such errors. Running `mumps -machine -lis` on such an M program resulted in an assert
  failure.

* Below is a simple test case demonstrating the failure. This requires a Debug build of YottaDB.

  ```sh
  $ $gtm_dist/mumps -run %XCMD 'for i=1:1:129 write " s x=\'a\'",!' > test.m
  $ $gtm_dist/mumps -machine -lis=test.lis test.m
  .
  .
           s x='a'
                 ^-----
                  At column 8, line 129, source module /tmp/test.m
  %YDB-E-SPOREOL, Either a space or an end-of-line was expected but not found
  %YDB-F-ASSERT, Assert failed in sr_unix/source_file.c line 344 for expression (!in_is_curr_device || (io_curr_device.in == io_std_device.in))
  ```

Issue
-----
* This is a regression introduced in c6d1a9a2.

* As part of that commit, we use `tmp_list_dev` in `close_source_file()` (in `sr_unix/source_file.c`)
  as the device passed to the `SAVE_IN_OUT_IS_CURR_DEVICE` and `RESTORE_IO_CURR_DEVICE` macros
  around a `op_close()` call.

* In the case of a `mumps -machine -lis` compilation where there are more than 128 errors, the compilation
  is abruptly terminated and in this case `tmp_list_dev` would point to the `test.lis` file (source file)
  while we are actually closing the source file `test.m` in `close_source_file()`. Therefore, the
  `tmp_list_dev` parameter passed to SAVE_IN_OUT_IS_CURR_DEVICE is not correct. And this causes
  the `in_is_curr_device` and `out_is_curr_device` variables to be incorrectly set and later
  cause an assert failure when these are used in the `RESTORE_IO_CURR_DEVICE` call.

Fix
---
* In `close_source_file()`, we are guaranteed to be closing the `.m` file. There is already a global

sr_unix/source_file.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2019-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2019-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -338,9 +338,15 @@ void	close_source_file (void)
 	val.mvtype = MV_STR;
 	val.str.len = source_name_len;
 	val.str.addr = (char *)source_file_name;
-	SAVE_IN_OUT_IS_CURR_DEVICE(tmp_list_dev, in_is_curr_device, out_is_curr_device);
+	/* Restore current device to "tmp_list_dev" (which would point to the current device BEFORE the M source file
+	 * was opened for compilation). It is possible this device is closed too as part of the above "op_close()"
+	 * call (see commit message of YDB@c6d1a9a2 for example). In that case, "op_close()" would take care of resetting
+	 * "io_curr_device.in" and "io_curr_device.out" to the principal device (io_std_device.in and .out respectively).
+	 */
+	io_curr_device = tmp_list_dev;
 	op_close(&val, &pars);
-	/* Note: Use tmp_list_dev and not dev_in_use below to make sure list file works */
-	RESTORE_IO_CURR_DEVICE(tmp_list_dev, in_is_curr_device, out_is_curr_device);
+	/* Ensure that after the "op_close()" call, "io_curr_device" points to open input and output devices */
+	assert(dev_open == io_curr_device.in->state);
+	assert(dev_open == io_curr_device.out->state);
 	return;
 }