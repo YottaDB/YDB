672171342579d959ca5f460a5777f455f16e76a0
Narayanan Iyer <nars@yottadb.com>
2024-09-18 16:03:25 -0400

    [#1091] Remove is_equ() code duplication without affecting performance
    * Commit dd69a2c5 had introduced the following files and the logic of `is_equ()` was copy/pasted
      into each of those files to avoid the overhead of a function call for performance reasons.
      - sr_port/op_equ_retbool.c
      - sr_port/op_equ_retmval.c
      - sr_port/op_nequ_retbool.c
      - sr_port/op_nequ_retmval.c
    
    * This commit removes the code duplication and takes care of not affecting performance by
      instead using macros.
    
    * The `is_equ()` function body has now been moved into a `IS_EQU()` macro. And this is now
      invoked by the `is_equ()` function as well as the above 4 C files.
    
    * I verified that after this change, the performance of `x=y` operations did not get affected
      negatively.

--- sr_port/is_equ.h
+++ sr_port/is_equ.h
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2020-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -13,6 +13,55 @@
 #ifndef IS_EQU_INCLUDED
 #define IS_EQU_INCLUDED
 
+#include "gtm_string.h"
+
+/* This is kept as a macro to avoid overhead of a "is_equ()" function call from performance sensitive
+ * callers like "op_equ_retbool.c".
+ */
+#define	IS_EQU(u, v, result)													\
+{																\
+	int	land, lor, utyp, vtyp;												\
+																\
+	utyp = u->mvtype;													\
+	vtyp = v->mvtype;													\
+	land = utyp & vtyp;													\
+	lor = utyp | vtyp;													\
+	for ( ; ; )	/* have a dummy for loop to be able to use "break" for various codepaths below */			\
+	{															\
+		if ((land & MV_NM) != 0 && (lor & MV_NUM_APPROX) == 0)								\
+		{														\
+			/* at this point, the mval's are both exact numbers, we can do a numeric comparison */			\
+			/* If they are both integers, compare only the relevant cells */					\
+			if (land & MV_INT)											\
+			{													\
+				result = (u->m[1] == v->m[1]);									\
+				break;												\
+			}													\
+			/* If one is an integer and the other is not, the two values cannot be equal */				\
+			if (lor & MV_INT)											\
+			{													\
+				result = 0;											\
+				break;												\
+			}													\
+			/* They are both decimal floating numbers, do a full comparison */					\
+			result = ((((mval_b *)u)->sgne == ((mval_b *)v)->sgne) && (u->m[1] == v->m[1]) && (u->m[0] == v->m[0]));\
+			break;													\
+		}														\
+		/* At least one of the numbers is not in numeric form or is not a canonical number, do a string compare */	\
+		MV_FORCE_STR(u);												\
+		MV_FORCE_STR(v);												\
+		if ((u->str.len != v->str.len)											\
+				|| (u->str.len && (u->str.addr != v->str.addr) && memcmp(u->str.addr, v->str.addr, u->str.len)))\
+			result = 0;												\
+		else														\
+			result = 1;												\
+		break;														\
+	}															\
+}
+
+/* The function is also made available (inspite of the above macro) for callers that are not performance sensitive.
+ * The rest of callers use the above IS_EQU macro to avoid the overhead of a function call.
+ */
 boolean_t is_equ(mval *u, mval *v);
 
 #endif /* IS_EQU_INCLUDED */
--- sr_port/op_nequ_retbool.c
+++ sr_port/op_nequ_retbool.c
@@ -12,63 +12,24 @@
 
 #include "mdef.h"
 
-#include "gtm_string.h"
-
 #include "op.h"
+#include "is_equ.h"
 
 /* Note: This C function is called by "opp_nequ_retbool.s". */
 int	op_nequ_retbool(mval *u, mval *v)
 {
-	int		land, lor, utyp, vtyp;
 	boolean_t	result;
 
-	utyp = u->mvtype;
-	vtyp = v->mvtype;
-	if (MVTYPE_IS_SQLNULL(utyp))
+	if (MV_IS_SQLNULL(u))
 	{
 		MV_FORCE_DEFINED(v);
 		return FALSE;
 	}
-	if (MVTYPE_IS_SQLNULL(vtyp))
+	if (MV_IS_SQLNULL(v))
 	{
 		MV_FORCE_DEFINED(u);
 		return FALSE;
 	}
-	/* Note: The below code is similar to that in "is_equ.c" (invoked from bx_relop_operator.c for OC_NEQU case).
-	 * We do not directly call "is_equ()" for performance reasons (to avoid function call overhead).
-	 */
-	for ( ; ; )	/* have a dummy for loop to be able to use "break" for various codepaths below */
-	{
-		land = utyp & vtyp;
-		lor = utyp | vtyp;
-		if ((land & MV_NM) != 0 && (lor & MV_NUM_APPROX) == 0)
-		{
-			/* at this point, the mval's are both exact numbers, we can do a numeric comparison */
-			/* If they are both integers, compare only the relevant cells */
-			if (land & MV_INT)
-			{
-				result = (u->m[1] != v->m[1]);
-				break;
-			}
-			/* If one is an integer and the other is not, the two values cannot be equal */
-			if (lor & MV_INT)
-			{
-				result = 1;
-				break;
-			}
-			/* They are both decimal floating numbers, do a full comparison */
-			result = ((((mval_b *)u)->sgne != ((mval_b *)v)->sgne) || (u->m[1] != v->m[1]) || (u->m[0] != v->m[0]));
-			break;
-		}
-		/* At least one of the numbers is not in numeric form or is not a canonical number, do a string compare */
-		MV_FORCE_STR(u);
-		MV_FORCE_STR(v);
-		if ((u->str.len != v->str.len)
-				|| (u->str.len && (u->str.addr != v->str.addr) && memcmp(u->str.addr, v->str.addr, u->str.len)))
-			result = 1;
-		else
-			result = 0;
-		break;
-	}
-	return result;
+	IS_EQU(u, v, result);	/* sets "result" to 1 if u == v and 0 otherwise */
+	return !result;
 }
