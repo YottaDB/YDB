YottaDB/YDB

commit 57006c9f5956c84184d84b0d410daaac5fff79b6

[YDBTest#550] Fix MUPIP REORG to prevent MUPIP STOP from causing KILLABANDONED (fixes GTM-9400 for real)
Background
----------
The below is pasted from https://gitlab.com/YottaDB/DB/YDBTest/-/issues/550#note_1733171439

* While trying to test YDBTest#550, I noticed that the KILLABANDONED error happens even with V7.0-001
  whereas the GTM-9400 release note in GT.M V7.0-001 indicates this as being fixed in V7.0-001.

* Below is the test case (using `tcsh`, not `sh`) that stops after a few seconds with a `KILLABANDONED`
  error with V7.0-000 as well as with V7.0-001.

  ```sh
  cat > kill.csh << CAT_EOF
  while (1)
          k15 reorg
          if (-e STOP) then
                  break
          endif
  end
  CAT_EOF

  rm -f STOP
  unsetenv ydb_gbldir
  setenv gtmgbldir mumps.gld
  rm -f mumps.gld mumps.dat
  $gtm_dist/mumps -run GDE exit
  $gtm_dist/mupip create
  $gtm_dist/mumps -run %XCMD 'for i=1:1:100000 set ^x(i)=$j(i,200)'
  source kill.csh &
  while (1)
          foreach fillfactor (50 10 90)
                  $gtm_dist/mupip reorg -fill=$fillfactor -region DEFAULT
                  $gtm_dist/mupip integ -reg "*"
                  if ($status) then
                          touch STOP
                          break
                  endif
          end
          if (-e STOP) then
                  break
          endif
  end
  ```

Issue
-----
* I added an assert in `secshr_db_clnup.c` where it invoked the `INCR_ABANDONED_KILLS` macro and ran
  the above test to see the cause of the above `KILLABANDONED` error.

* With that change, I got an assert failure (in line 446 below) and the core file showed the below
  stack trace.

  ```c
  (gdb) where
  #0  __pthread_kill_implementation (no_tid=0, signo=3, threadid=140185231378240) at ./nptl/pthread_kill.c:44
  #1  __pthread_kill_internal (signo=3, threadid=140185231378240) at ./nptl/pthread_kill.c:78
  #2  __GI___pthread_kill (threadid=140185231378240, signo=3) at ./nptl/pthread_kill.c:89

sr_port/deferred_signal_handler.c
@@ -1,6 +1,6 @@
 /****************************************************************
  *								*
- * Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -16,10 +16,19 @@
 #include "gtm_stdio.h"
 #include "gtm_unistd.h"
 #include "sig_init.h"
+#include "gdsroot.h"
+#include "gtm_facility.h"
+#include "gdskill.h"
+#include "fileinfo.h"
+#include "gdsbt.h"
+#include "gdsblk.h"
+#include "gdsfhead.h"
 
-GBLREF	int		process_exiting;
-GBLREF	VSIG_ATOMIC_T	forced_exit;
-GBLREF	volatile int	in_os_signal_handler;
+GBLREF	int			process_exiting;
+GBLREF	VSIG_ATOMIC_T		forced_exit;
+GBLREF	volatile int		in_os_signal_handler;
+GBLREF	boolean_t		mu_reorg_process;
+GBLREF	sgmnt_data_ptr_t	cs_data;
 
 void	deferred_signal_handler(void)
 {
@@ -70,6 +79,13 @@ void	deferred_signal_handler(void)
 	 */
 	if (forced_exit)
 	{
+		if (mu_reorg_process && cs_data->kill_in_prog)
+		{	/* This is a MUPIP REORG process and the database has a kill-in-progress.
+			 * Avoid KILLABANDONED state of the database by deferring handling of the MUPIP STOP
+			 * till a later DEFERRED_EXIT_REORG_CHECK call.
+			 */
+			return;
+		}
 		if (GET_DEFERRED_EXIT_CHECK_NEEDED)
 			deferred_exit_handler();
 	} else

sr_port/secshr_db_clnup.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2018 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2024 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -81,6 +81,7 @@ GBLREF	sgmnt_addrs		*cs_addrs;
 GBLREF	volatile boolean_t	in_wcs_recover; /* TRUE if in "wcs_recover" */
 GBLREF	uint4			update_trans;
 GBLREF	boolean_t		dse_running;
+GBLREF	boolean_t		mu_reorg_process;
 #endif
 
 error_def(ERR_WCBLOCKED);
@@ -430,6 +431,8 @@ void secshr_db_clnup(enum secshr_db_state secshr_state)
 					 */
 					if (si && (NULL != si->kill_set_head) && (NULL != si->kip_csa))
 					{
+						/* Assert that MUPIP REORG never leaves the database with an abandoned kill */
+						assert(!mu_reorg_process);
 						assert(csa == si->kip_csa);
 						DECR_KIP(csd, csa, si->kip_csa);
 						INCR_ABANDONED_KILLS(csd, csa);
@@ -439,6 +442,8 @@ void secshr_db_clnup(enum secshr_db_state secshr_state)
 				{
 					if ((NULL != kip_csa) && (csa == kip_csa))
 					{
+						/* Assert that MUPIP REORG never leaves the database with an abandoned kill */
+						assert(!mu_reorg_process);
 						assert(0 < kip_csa->hdr->kill_in_prog);
 						DECR_KIP(csd, csa, kip_csa);
 						INCR_ABANDONED_KILLS(csd, csa);

commit 53cbbb4e66fa76d0c50a94130614a68117df89c0

[YDBTest#313] Fix SIG-11 in mupip reorg -encrypt on a mupip stop (regression in 57006c9f)
Background
----------
* The `encryption/eotf_resume` subtest failed once in a while with the following symptom
  when run with `-encrypt`.

  ```diff
  > ##TEST_PATH##/encryption_0/eotf_resume/mupip_reorg_encrypt0.out
  > %YDB-F-KILLBYSIGSINFO1, MUPIP process 864 has been killed by a signal 11 at address 0x00007F311F975E9F (vaddr 0x0000000000000130)
  > %YDB-F-SIGMAPERR, Signal was caused by an address not mapped to an object
  ```

Issue
-----
* The core had the following stack trace.

  ```c
  Core was generated by `mupip reorg -encrypt=...

  (gdb) where
  #0  __pthread_kill_implementation (threadid=<optimized out>, signo=3, no_tid=<optimized out>) at ./nptl/pthread_kill.c:44
  #1  gtm_dump_core () at sr_unix/gtm_dump_core.c:74
  #2  gtm_fork_n_core () at sr_unix/gtm_fork_n_core.c:163
  #3  generic_signal_handler (sig=11, info=0x7f3121623d98, context=0x7f3121623e18, is_os_signal_handler=1) at sr_unix/generic_signal_handler.c:521
  #4  ydb_os_signal_handler (sig=11, info=0x7f31228e9af0, context=0x7f31228e99c0) at sr_unix/ydb_os_signal_handler.c:92
  #5  <signal handler called>
  #6  deferred_signal_handler () at sr_port/deferred_signal_handler.c:87
  #7  gvcst_init (reg=0x62d000005970) at sr_port/gvcst_init.c:1201
  #8  mupip_reorg_encrypt () at sr_unix/mupip_reorg_encrypt.c:277
  #9  mupip_reorg () at sr_port/mupip_reorg.c:186
  #10 mupip_main (argc=5, argv=0x7fff1ef6af88, envp=0x7fff1ef6afb8) at sr_unix/mupip_main.c:132
  #11 dlopen_libyottadb (argc=5, argv=0x7fff1ef6af88, envp=0x7fff1ef6afb8, main_func=0x5580ce07a020 "mupip_main") at sr_unix/dlopen_libyottadb.c:151
  #12 main (argc=5, argv=0x7fff1ef6af88, envp=0x7fff1ef6afb8) at sr_unix/mupip.c:21
  ```

* The SIG-11 happened at line 87 below because `cs_data` was NULL.

  **sr_port/deferred_signal_handler.c**
  ```c
     85         if (forced_exit)
     86         {

sr_port/deferred_signal_handler.c
@@ -84,7 +84,7 @@ void	deferred_signal_handler(void)
 	 */
 	if (forced_exit)
 	{
-		if (mu_reorg_process && cs_data->kill_in_prog)
+		if (mu_reorg_process && (NULL != cs_data) && cs_data->kill_in_prog)
 		{	/* This is a MUPIP REORG process and the database has a kill-in-progress.
 			 * Avoid KILLABANDONED state of the database by deferring handling of the MUPIP STOP
 			 * till a later DEFERRED_EXIT_REORG_CHECK call.