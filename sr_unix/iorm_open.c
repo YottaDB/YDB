YottaDB/YDB

commit ddbf9cd0f9d0de2e355e57a9ac4a74cd857e4d18

[#120] READ command on files in /proc filesystem now work

sr_unix/iorm_open.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -220,7 +223,8 @@ short	iorm_open(io_log_name *dev_name, mval *pp, int fd, mval *mspace, int4 time
 					}
 				}
 			}
-			if (size == statbuf.st_size)
+			/* Note that for /proc files, statbuf.st_size is 0. We should not incorrectly set $ZEOF=1 in that case. */
+			if ((size == statbuf.st_size) && size)
 				iod->dollar.zeof = TRUE;
 		} else
 		{

commit 47df3f8120df8bc35807bd7555e8c94454b8ee41

[#120] [YDBTest#313] Fix incorrect NOTTOEOFONPUT error while writing to encrypted file (regression in ddbf9cd0)
Background
----------
* The `v62000/jobcmdDollarxTest` subtest in the YDBTest project failed with the following symptom.

  ```diff
  5a6,9
  > %YDB-E-NOTTOEOFONPUT, Not positioned to EOF on write (sequential organization only)
  >               At M source location parent+11^jobcmdDollarxTest
  > %YDB-W-NOTPRINCIO, Output currently directed to device SD.encr
  ```

* Below is a simple test case that demonstrates the same error as above (after ensuring the
  encryption related env vars `gtm_passwd`, `gtmcrypt_config` and `GNUPGHOME` are set appropriately
  like they were when the subtest ran with `-encrypt`).

  ```m
  $ cat test.m
   set outfile="SD.encr"
   open outfile:(okey="key":newversion)
   use outfile
   write "abcd",!

  $ $gtm_dist/mumps -run test
  ```

Issue
-----
* The error was issued at line 398 below.

  **sr_unix/iorm_write.c**
  ```c
    393         if ((!rm_ptr->fifo) && (!rm_ptr->is_pipe) && rm_ptr->output_encrypted)
    394         {
    395                 if (!iod->dollar.zeof)
    396                 {
    397                         iod->dollar.za = ZA_IO_ERR;
    398                         RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(1) ERR_NOTTOEOFONPUT);
    399                 } else
  ```

* The issue turned out to be that the file was empty and it was an encrypted file (due to the
  `okey` device parameter usage above) and so `rm_ptr->output_encrypted` was set to TRUE and
  because the file size was 0, we incorrectly skipped setting `dollar.zeof` to TRUE at line
  233 below because we saw `size` was 0 and thought this was a file under the `/proc` filesystem.

  **sr_unix/iorm_open.c**
  ```c
    231     /* Note that for /proc files, statbuf.st_size is 0. We should not incorrectly set $ZEOF=1 in that case. */
    232     if ((size == statbuf.st_size) && size)
    233             iod->dollar.zeof = TRUE;
  ```

sr_unix/iorm_open.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2024 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -50,6 +50,8 @@ error_def(ERR_CRYPTNOAPPEND);
 error_def(ERR_DEVOPENFAIL);
 error_def(ERR_TEXT);
 error_def(ERR_IOERROR);
+
+#define PROCPATH_PREFIX	"/proc/"
 
 LITREF	mstr		chset_names[];
 LITREF unsigned char	io_params_size[];
@@ -228,9 +230,17 @@ short	iorm_open(io_log_name *dev_name, mval *pp, int fd, mval *mspace, uint8 tim
 					}
 				}
 			}
-			/* Note that for /proc files, statbuf.st_size is 0. We should not incorrectly set $ZEOF=1 in that case. */
-			if ((size == statbuf.st_size) && size)
+			/* Note that for /proc files, statbuf.st_size is 0. We should not incorrectly set $ZEOF=1 in that case.
+			 * But it is also possible for a non-/proc regular file to be empty in which case too statbuf.st_size
+			 * would be 0. Unfortunately, there is no accurate way to differentiate a 0-byte regular file vs
+			 * a /proc file. Therefore, we check for /proc in the file name and if so we skip setting $ZEOF.
+			 */
+			if ((size == statbuf.st_size)
+				&& (size || (STR_LIT_LEN(PROCPATH_PREFIX) > dev_name->len)
+					|| MEMCMP_LIT(dev_name->dollar_io, PROCPATH_PREFIX)))
+			{
 				iod->dollar.zeof = TRUE;
+			}
 		} else
 		{
 			pipe_buff_size = fpathconf(fd, _PC_PIPE_BUF);