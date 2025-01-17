cb4e8bd9e3c47c29489f0c4f6026e23bf760eb49
Narayanan Iyer <nars@yottadb.com>
2025-01-17 13:47:35 -0500

    [#1000] [V71000] [YDBTest#644] Revert d9855f6f (#1097) and pick GT.M V7.1-000 dotted-DO FALLINTOFLST handling
    * See https://gitlab.com/YottaDB/DB/YDBTest/-/issues/644#note_2303447509 and surrounding discussion
      for more background on this commit.
    
    * Because GT.M V7.1-000 inserts an implicit QUIT at the end of a dotted DO block, there is no way
      for one to fall through from a dotted DO line to a line with a formal list. And so the changes
      done in d9855f6f are no longer necessary and are reverted as part of this commit.

--- sr_port/resolve_blocks.c
+++ sr_port/resolve_blocks.c
@@ -2,7 +2,7 @@
  *								*
  *	Copyright 2001 Sanchez Computer Associates, Inc.	*
  *								*
- * Copyright (c) 2024 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -31,26 +31,11 @@ void resolve_blocks(void)
 		{
 			if (mlx->sibling)
 			{
-				triple	*t2, *t3;
-
 				ref = maketriple(OC_JMP);
-				t1 = mlx->sibling->externalentry;
-				/* We are about to jump from this line to its sibling line (which is a few M lines later
-				 * because of intervening dotted DO lines). Check if the sibling line had a FALLINTOFLST
-				 * error triple added before it (in line.c). If so, reset the jump target to that triple
-				 * as this jump should result in the same fall-through error (YDB#1097).
-				 */
-				t2 = t1->exorder.bl;
-				if (OC_RTERROR == t2->opcode)
-				{
-					t3 = t2->operand[0].oprval.tref;
-					if ((ILIT_REF == t3->operand[0].oprclass)
-							&& (ERR_FALLINTOFLST == t3->operand[0].oprval.ilit))
-						t1 = t2;
-				}
-				ref->operand[0] = put_tjmp(t1);
+				ref->operand[0] = put_tjmp(mlx->sibling->externalentry);
 			} else
-				ref = maketriple(OC_RET);
+			{	ref = maketriple(OC_RET);
+			}
 			t1 = mly->externalentry;
 			t1 = t1->exorder.bl;
 			ref->src = t1->src;
