YottaDB/YDB

commit 5f21ebc99e9977e35364b47466078551542350e6

[PERFORMANCE] Convert a lot of change_reg() calls to CHANGE_REG_IF_NEEDED macro
Main changes
------------
* Commit e56a6971 sped up `gv_bind_name()` by converting `change_reg()` calls to use the
  `CHANGE_REG_IF_NEEDED` macro.

* It had a mention that other callers of `change_reg()` will be replaced with the macro
  in a future commit. This is that future commit.

* Various callers of `change_reg()` were examined and only those that can be safely replaced
  were done.

* As an example, `op_gvorder.c` was enhanced in the section where it does `name-level $order`.
  A simple `$order(^x)` was seen to reduce the cpu cycles taken by `op_gvorder()` from
  166 cycles to 140 cycles (when observed through the `pin` binary instrumentation tool).

* There are various other files that were changed. Particularly the `gvcst_spr_*` files. Those
  involve database operations (e.g. $data, $query, $order etc.) in globals that span across
  multiple regions. I did not test each of them but expect them to also benefit even if only
  by a small amount.

Misc changes
------------
* In `op_gvorder.c` and `op_zprevious.c`, I noticed a call to `change_reg()` after the
  `for ( ; map < map_top; ++map)` loop. The loop does the `change_reg()` calls already inside
  and those were changed to the new CHANGE_REG_IF_NEEDED macro. I did not see the need for
  the `change_reg()` at the end of the loop so removed it.

* In `sr_port/tp_set_sgm.c`, there was a `just in case` if check after an assert. This assert
  has not failed in all these years of testing so removed the following `if` check to save
  a cpu cycle.

sr_port/gdsfhead.h
@@ -34,6 +34,7 @@
 #include "gtm_reservedDB.h"
 #include "proc_wait_stat.h"
 #include "region_freeze_multiproc.h"
+#include "tp_set_sgm.h"
 
 #define CACHE_STATE_OFF SIZEOF(que_ent)
 
@@ -990,41 +991,48 @@ MBSTART {													\
  * TP_CHANGE_REG_IF_NEEDED macro, it can be used only if the region passed is not
  * NULL, is open and if gv_cur_region, cs_addrs and cs_data are known to be in sync.
  */
-#define	CHANGE_REG_IF_NEEDED(reg)										\
-MBSTART {													\
-	GBLREF	sgmnt_data_ptr_t	cs_data;								\
-	GBLREF	sgmnt_addrs		*cs_addrs;								\
-	GBLREF	jnlpool_addrs_ptr_t	jnlpool;								\
-	GBLREF	uint4			dollar_tlevel;								\
-	GBLREF	sgm_info		*sgm_info_ptr;								\
-														\
-	assert(NULL != reg);											\
-	assert(reg->open);											\
-	if (reg != gv_cur_region)										\
-	{													\
-		gv_cur_region = reg;										\
-		if (IS_REG_BG_OR_MM(reg))									\
-		{												\
-			assert(reg->open);									\
-			cs_addrs = &FILE_INFO(reg)->s_addrs;							\
-			cs_data = cs_addrs->hdr;								\
-			assert((&FILE_INFO(reg)->s_addrs == cs_addrs) && cs_addrs->hdr == cs_data);		\
-		} else												\
-		{												\
-			assert(REG_ACC_METH(reg) == dba_cm);							\
-			cs_addrs = NULL;									\
-			cs_data = NULL;										\
-		}												\
-	} else													\
-	{													\
-		assert((REG_ACC_METH(reg) != dba_cm) || (NULL == cs_addrs));					\
-		assert((REG_ACC_METH(reg) == dba_cm) || (cs_addrs == &FILE_INFO(reg)->s_addrs));		\
-		assert((NULL == cs_addrs) || (cs_data == cs_addrs->hdr));					\
-	}													\
-	if (dollar_tlevel && (NULL != cs_addrs))								\
-		tp_set_sgm();											\
-	if ((NULL != cs_addrs) && (NULL != cs_addrs->jnlpool))							\
-		jnlpool = cs_addrs->jnlpool;									\
+#define	CHANGE_REG_IF_NEEDED(REG)									\
+MBSTART {												\
+	GBLREF	sgmnt_data_ptr_t	cs_data;							\
+	GBLREF	sgmnt_addrs		*cs_addrs;							\
+	GBLREF	jnlpool_addrs_ptr_t	jnlpool;							\
+	GBLREF	uint4			dollar_tlevel;							\
+	GBLREF	struct sgm_info_struct	*sgm_info_ptr;							\
+													\
+	/* Copy REG into a local variable in case it is a complex expression.				\
+	 * Don't want to recompute that expression multiple times inside this macro.			\
+	 */												\
+	gd_region	*lclReg;									\
+													\
+	lclReg = REG;											\
+	assert(NULL != lclReg);										\
+	assert(lclReg->open);										\
+	if (lclReg != gv_cur_region)									\
+	{												\
+		gv_cur_region = lclReg;									\
+		if (IS_REG_BG_OR_MM(lclReg))								\
+		{											\
+			assert(lclReg->open);								\
+			cs_addrs = &FILE_INFO(lclReg)->s_addrs;						\
+			cs_data = cs_addrs->hdr;							\
+			assert((&FILE_INFO(lclReg)->s_addrs == cs_addrs)				\
+				&& cs_addrs->hdr == cs_data);						\
+		} else											\
+		{											\
+			assert(REG_ACC_METH(lclReg) == dba_cm);						\
+			cs_addrs = NULL;								\
+			cs_data = NULL;									\
+		}											\
+	} else												\
+	{												\
+		assert((REG_ACC_METH(lclReg) != dba_cm) || (NULL == cs_addrs));				\
+		assert((REG_ACC_METH(lclReg) == dba_cm) || (cs_addrs == &FILE_INFO(lclReg)->s_addrs));	\
+		assert((NULL == cs_addrs) || (cs_data == cs_addrs->hdr));				\
+	}												\
+	if (dollar_tlevel && (NULL != cs_addrs))							\
+		tp_set_sgm();										\
+	if ((NULL != cs_addrs) && (NULL != cs_addrs->jnlpool))						\
+		jnlpool = cs_addrs->jnlpool;								\
 } MBEND
 
 #define PUSH_GV_CUR_REGION(REG, SAV_REG, SAV_CS_ADDRS, SAV_CS_DATA, SAV_JNLPOOL)		\
@@ -4962,10 +4970,10 @@ MBSTART {										\
 	}										\
 	gv_target = gvt;								\
 	/* Even though gv_cur_region might already be equal to "REG", need to invoke	\
-	 * "change_reg" in order to do the "tp_set_sgm" in case of TP.			\
+	 * "change_reg" in order to do the "tp_set_sgm" in case of TP. The below macro	\
+	 * does exactly that for us.							\
 	 */										\
-	gv_cur_region = REG;								\
-	change_reg();									\
+	CHANGE_REG_IF_NEEDED(REG);							\
 	DEBUG_ONLY(acc_meth = REG_ACC_METH(gv_cur_region);)				\
 	assert(IS_ACC_METH_BG_OR_MM(acc_meth));						\
 	GVCST_ROOT_SEARCH;								\

sr_port/gv_bind_subsname.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2013-2020 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -27,6 +30,8 @@
 #include "gtmimagename.h"
 #include "io.h"
 #include "gvt_inline.h"
+#include "repl_msg.h"	/* for "gtmsource.h" */
+#include "gtmsource.h"	/* for CHANGE_REG_IF_NEEDED macro invoked inside GV_BIND_SUBSREG macro */
 
 GBLREF gd_region	*gv_cur_region;
 GBLREF gv_key		*gv_currkey;

sr_port/gvnh_spanreg_subs_gvt_init.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2013-2017 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -33,6 +33,8 @@
 #include "process_gvt_pending_list.h"	/* for "is_gvt_in_pending_list" prototype used in ADD_TO_GVT_PENDING_LIST_IF_REG_NOT_OPEN */
 #include "gtmimagename.h"
 #include "io.h"
+#include "repl_msg.h"	/* for "gtmsource.h" */
+#include "gtmsource.h"	/* for CHANGE_REG_IF_NEEDED macro invoked inside GV_BIND_SUBSREG macro */
 
 GBLREF	gd_region			*gv_cur_region;
 

sr_port/muextr.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2019 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2020 YottaDB LLC and/or its subsidiaries.      *
+ * Copyright (c) 2020-2025 YottaDB LLC and/or its subsidiaries.      *
  * All rights reserved.                                         *
  *								*
  *	This source code contains the intellectual property	*
@@ -53,14 +53,16 @@ typedef	struct glist_struct
 		&GBLSTAT.recknt, GBLSTAT.keylen, GBLSTAT.datalen, GBLSTAT.reclen);			\
 }
 
+#include "repl_msg.h"	/* for "gtmsource.h" */
+#include "gtmsource.h"	/* for CHANGE_REG_IF_NEEDED macro */
+
 #define	DO_OP_GVNAME(GL_PTR)								\
 {											\
 	GBLREF	gv_namehead		*gv_target;					\
 	GBLREF	gd_region		*gv_cur_region;					\
 											\
 	gv_target = GL_PTR->gvt;							\
-	gv_cur_region = GL_PTR->reg;							\
-	change_reg();									\
+	CHANGE_REG_IF_NEEDED(GL_PTR->reg);						\
 	assert((NULL != gv_target) && (DIR_ROOT != gv_target->root));			\
 	SET_GV_CURRKEY_FROM_GVT(gv_target);	/* needed by gvcst_root_search */	\
 	GVCST_ROOT_SEARCH;								\

sr_port/op_gvorder.c
@@ -171,8 +171,7 @@ void op_gvorder(mval *v)
 				if (!reg->open)
 					gv_init_reg(reg);
 				/* Entries in directory tree of region could have empty GVT in which case move on to next entry */
-				gv_cur_region = map->reg.addr;
-				change_reg();
+				CHANGE_REG_IF_NEEDED(map->reg.addr);
 				acc_meth = REG_ACC_METH(gv_cur_region);
 				for ( ; ; )
 				{
@@ -317,7 +316,6 @@ void op_gvorder(mval *v)
 				GVKEY_DECREMENT_ORDER(gv_currkey); /* back off 1 spot from map */
 			}
 		}
-		change_reg();
 		v->mvtype = 0; /* so stp_gcol (if invoked below) can free up space currently occupied by (BYPASSOK)
 				* this to-be-overwritten mval */
 		if (found)

sr_port/op_zprevious.c
@@ -177,8 +177,7 @@ void op_zprevious(mval *v)
 				if (!reg->open)
 					gv_init_reg(reg);
 				/* Entries in directory tree could have empty GVT in which case move on to previous entry */
-				gv_cur_region = map->reg.addr;
-				change_reg();
+				CHANGE_REG_IF_NEEDED(map->reg.addr);
 				acc_meth = REG_ACC_METH(gv_cur_region);
 				for ( ; ; )
 				{
@@ -294,7 +293,6 @@ void op_zprevious(mval *v)
 				}
 			}
 		}
-		change_reg();
 		v->mvtype = 0; /* so stp_gcol (if invoked below) can free up space currently occupied (BYPASSOK)
 				* by this to-be-overwritten mval */
 		if (found)

sr_port/region_init.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -26,6 +26,8 @@
 #include "error.h"
 #include "change_reg.h"
 #include "min_max.h"
+#include "repl_msg.h"	/* for "gtmsource.h" */
+#include "gtmsource.h"	/* for CHANGE_REG_IF_NEEDED macro */
 
 GBLREF gd_addr		*gd_header;
 GBLREF gd_region	*gv_cur_region;
@@ -94,8 +96,7 @@ boolean_t region_init(bool cm_regions)
 			}
 		}
 	}
-	gv_cur_region = first_nonstatsdb_reg;
-	change_reg();
+	CHANGE_REG_IF_NEEDED(first_nonstatsdb_reg);
 	return all_files_open;
 }
 

sr_port/tp_set_sgm.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -55,8 +58,6 @@ void tp_set_sgm(void)
 	assert(csa == &FILE_INFO(gv_cur_region)->s_addrs);
 	si = csa->sgm_info_ptr;
 	assert(NULL != si);
-	if (NULL == si)
-		return;		/* not expected but be safe */
 	assert(si->tp_csa == csa);
 	assert(si->tp_csd == cs_data);
 	assert(csa->hdr == cs_data);

commit 3beba0c7c18e0bfd123cfa10c4f0228521d6f846

Fix CHANGE_REG_IF_NEEDED inconsistency with change_reg() (regression in 5f21ebc99)
Background
----------
* After commit 5f21ebc99 was merged, the `triggers_1/symmetry` subtest failed in very rare
  test runs with the following symptom.

  ```
  %YDB-F-ASSERT, Assert failed in sr_port/tp_restart.c line 431 for expression (jnlpool)
  ```

Issue
-----
* The core had the following stack

  ```c
  (gdb) where
  #0  __pthread_kill_implementation (no_tid=0, signo=3, threadid=<optimized out>) at ./nptl/pthread_kill.c:44
  #1  __pthread_kill_internal (signo=3, threadid=<optimized out>) at ./nptl/pthread_kill.c:78
  #2  __GI___pthread_kill (threadid=<optimized out>, signo=3) at ./nptl/pthread_kill.c:89
  #3  gtm_dump_core () at sr_unix/gtm_dump_core.c:74
  #4  gtm_fork_n_core () at sr_unix/gtm_fork_n_core.c:163
  #5  ch_cond_core () at sr_unix/ch_cond_core.c:80
  #6  rts_error_va (csa=0x0, argcnt=7, var=0x7ffd2acf6420) at sr_unix/rts_error.c:199
  #7  rts_error_csa (csa=0x0, argcnt=7) at sr_unix/rts_error.c:100
  #8  assertfail (testlen=7, teststr=0x749cf6040f80 "jnlpool", flen=47, fstr=0x749cf60403c0 "sr_port/tp_restart.c", line=431) at sr_port/mdef.h:828
  #9  tp_restart (newlevel=1, handle_errors_internally=0) at sr_port/tp_restart.c:431
  #10 trigger_tpwrap_ch (arg=150376098) at sr_unix/trigger_tpwrap_ch.c:50
  #11 rts_error_va (csa=0x0, argcnt=1, var=0x7ffd2acf8ff0) at sr_unix/rts_error.c:199
  #12 rts_error_csa (csa=0x0, argcnt=1) at sr_unix/rts_error.c:100
  #13 t_retry (failure=cdb_sc_needcrit) at sr_port/t_retry.c:595
  #14 insert_region (reg=0x51d000003db0, reg_list=0x749cf6611260 <tp_reg_list>, reg_free_list=0x749cf6611220 <tp_reg_free_list>, size=40) at sr_port/insert_region.c:211
  #15 tp_set_sgm () at sr_port/tp_set_sgm.c:71
  #16 dump_all_triggers () at sr_unix/trigger_select.c:564
  #17 trigger_select (select_list=0x749cf805e473 "*\031", select_list_len=1) at sr_unix/trigger_select.c:788

sr_port/gdsfhead.h
@@ -1029,10 +1029,10 @@ MBSTART {												\
 		assert((REG_ACC_METH(lclReg) == dba_cm) || (cs_addrs == &FILE_INFO(lclReg)->s_addrs));	\
 		assert((NULL == cs_addrs) || (cs_data == cs_addrs->hdr));				\
 	}												\
-	if (dollar_tlevel && (NULL != cs_addrs))							\
-		tp_set_sgm();										\
 	if ((NULL != cs_addrs) && (NULL != cs_addrs->jnlpool))						\
 		jnlpool = cs_addrs->jnlpool;								\
+	if (dollar_tlevel && (NULL != cs_addrs))							\
+		tp_set_sgm();										\
 } MBEND
 
 #define PUSH_GV_CUR_REGION(REG, SAV_REG, SAV_CS_ADDRS, SAV_CS_DATA, SAV_JNLPOOL)		\