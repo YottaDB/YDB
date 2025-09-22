YottaDB/YDB

commit b4ee544e926b581a9f9d063ce693504fd641ed09

[PERFORMANCE] Optimize history/bitmap validation in t_end() (e.g. remove bt_get() calls)
Background
----------
* This commit started out as a performance enhancement attempt to see if `bt_get()` calls
  can be removed from `t_end()` as those calls happen while we are holding crit and doing
  history (gvt blocks) validation or bitmap block validation.

Main Change
-----------
* Validation in `t_end()` does 2 checks for the BG access method (which is what this commit
  is concerned about at this point).
  - a) Whether the block was updated since we read the block outside of crit.
  - b) Whether the buffer/cache-record holding the block was recycled to hold a different block
       since we read the block outside of crit.

* The previous logic in `t_end.c` achieved these checks by first obtaining the `bt` record for
  a given block number using a `bt_get()` call. And then obtaining the cache-record `cr` from
  the `bt->cache_index`.

* `a)` is achieved by comparing `bt->tn` against `t1->tn` (the history's tn).

* `b)` is achieved by comparing `cr->cycle` against `t1->cycle` (the history's cycle) where
  `cr->cycle` is incremented every time the global buffer corresponding to `cr` is used to
  hold the contents of a different block number.

* But we already have the `cr` in `t1->cr`. And from `cr` we can obtain the `bt` directly
  using `cr->bt_index`. So we can avoid the `bt_get()` call altogether.

* The only issue is if `cr->bt_index` is 0 and twinning is in effect. This is possible if the
  database has ASYNCIO turned on. In that case, if a dirty block is being written out
  asynchronously to disk, we create a twin cache-record and perform any future updates to that
  block in this new cache-record while letting the background IO still run fine without issues.

  In this case, `cr->bt_index` would be `0` as the `bt` for `cr->blk` would point to the newer
  twin whereas `t1->cr` (the history cache-record) would point to the older twin which was the

sr_port/t_end.c
@@ -160,8 +160,6 @@ error_def(ERR_NOTREPLICATED);
 error_def(ERR_TEXT);
 error_def(ERR_TNTOOLARGE);
 
-#define BLOCK_FLUSHING(x) (csa->hdr->clustered && x->flushing && !CCP_SEGMENT_STATE(cs_addrs->nl,CCST_MASK_HAVE_DIRTY_BUFFERS))
-
 #define	RESTORE_CURRTN_IF_NEEDED(csa, cti, write_inctn, decremented_currtn)				\
 {													\
 	if (write_inctn && decremented_currtn)								\
@@ -335,7 +333,6 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 	if (!update_trans)
 	{	/* Take a fast path for read transactions. We do not need crit; we simply need to verify our blocks have not been
 		 * modified since we read them. The read is valid if neither the cycles nor the tns have changed. Otherwise restart.
-		 * Note: if, as with updates, we validated via bt_get/db_csh_get, we would restart under the same conditions.
 		 */
 		SHM_READ_MEMORY_BARRIER;
 		same_db_state = (start_tn == cti->early_tn);
@@ -917,38 +914,43 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 				t1->cse = NULL;	/* reset for next transaction */
 			} else
 			{
-				bt = bt_get(t1->blk_num);
-				if (NULL == bt)
+				cr = t1->cr;
+				if (0 != cr->bt_index)
 				{
+					bt = (bt_rec_ptr_t)GDS_REL2ABS(cr->bt_index);
+					assert(NULL != bt);
+					assert(cr->blk == bt->blk);
+					if (cr->cycle != t1->cycle)
+					{
+						assert(CDB_STAGNATE > t_tries);
+						status = cdb_sc_lostcr;
+						goto failed;
+					}
+				} else if (0 != cr->twin)
+				{	/* Update "cr" to point to newer twin */
+					cr = (cache_rec_ptr_t)GDS_ANY_REL2ABS(csa, cr->twin);
+					assert(0 != cr->bt_index);
+					bt = (bt_rec_ptr_t)GDS_REL2ABS(cr->bt_index);
+					assert(NULL != bt);
+					assert(cr->blk == bt->blk);
+				} else
+				{
+					bt = NULL;
 					if (t1->tn <= oldest_hist_tn)
 					{
 						assert(CDB_STAGNATE > t_tries);
 						status = cdb_sc_losthist;
 						goto failed;
 					}
-					cr = db_csh_get(t1->blk_num);
-				} else
-				{
-					if (BLOCK_FLUSHING(bt))
+					if (cr->cycle != t1->cycle)
 					{
 						assert(CDB_STAGNATE > t_tries);
-						status = cdb_sc_blockflush;
+						status = cdb_sc_lostcr;
 						goto failed;
 					}
-					if (CR_NOTVALID == bt->cache_index)
-						cr = db_csh_get(t1->blk_num);
-					else
-					{
-						cr = (cache_rec_ptr_t)GDS_REL2ABS(bt->cache_index);
-						if (cr->blk != bt->blk)
-						{
-							assert(FALSE);
-							SET_TRACEABLE_VAR(cnl->wc_blocked, WC_BLOCK_RECOVER);
-							BG_TRACE_PRO_ANY(csa, wc_blocked_t_end_crbtmismatch1);
-							status = cdb_sc_crbtmismatch;
-							goto failed;
-						}
-					}
+				}
+				if (NULL != bt)
+				{
 					assert(bt->killtn <= bt->tn);
 					if (t1->tn <= bt->tn)
 					{
@@ -991,28 +993,7 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 						}
 					}
 				}
-				if ((cache_rec_ptr_t)CR_NOTVALID == cr)
-				{
-					SET_TRACEABLE_VAR(cnl->wc_blocked, WC_BLOCK_RECOVER);
-					BG_TRACE_PRO_ANY(csa, wc_blocked_t_end_hist);
-					SET_CACHE_FAIL_STATUS(status, csd);
-					goto failed;
-				}
-				if ((NULL == cr) || (cr->cycle != t1->cycle)
-					|| ((sm_long_t)GDS_REL2ABS(cr->buffaddr) != (sm_long_t)t1->buffaddr))
-				{
-					if ((NULL != cr) && (NULL != bt) && (cr->blk != bt->blk))
-					{
-						assert(FALSE);
-						SET_TRACEABLE_VAR(cnl->wc_blocked, WC_BLOCK_RECOVER);
-						BG_TRACE_PRO_ANY(csa, wc_blocked_t_end_crbtmismatch2);
-						status = cdb_sc_crbtmismatch;
-						goto failed;
-					}
-					assert(CDB_STAGNATE > t_tries);
-					status = cdb_sc_lostcr;
-					goto failed;
-				}
+				assert((sm_long_t)GDS_REL2ABS(cr->buffaddr) == (sm_long_t)t1->buffaddr);
 				/* MUPIP REORG -UPGRADE enters t_end without any of the usual concurrency checks. It is possible
 				 * for a process to be in bg_update_phase2 modifying the same CR. Since UPGRADE can be deferred
 				 * stop the update and let it t_retry() the GVT */
@@ -1101,22 +1082,16 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 			}
 		} else
 		{
-			bt = bt_get(cs->blk);
-			if (NULL == bt)
+			cr = cs->cr;
+			if (0 != cr->bt_index)
 			{
-				if (cs->tn <= oldest_hist_tn)
+				bt = (bt_rec_ptr_t)GDS_REL2ABS(cr->bt_index);
+				assert(NULL != bt);
+				assert(cr->blk == bt->blk);
+				if (cr->cycle != cs->cycle)
 				{
 					assert(CDB_STAGNATE > t_tries);
-					status = cdb_sc_lostbmlhist;
-					goto failed;
-				}
-				cr = db_csh_get(cs->blk);
-			} else
-			{
-				if (BLOCK_FLUSHING(bt))
-				{
-					assert(CDB_STAGNATE > t_tries);
-					status = cdb_sc_blockflush;
+					status = cdb_sc_lostbmlcr;
 					goto failed;
 				}
 				if (cs->tn <= bt->tn)
@@ -1125,35 +1100,28 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 					status = cdb_sc_bmlmod;
 					goto failed;
 				}
-				if (CR_NOTVALID == bt->cache_index)
-					cr = db_csh_get(cs->blk);
-				else
-				{
-					cr = (cache_rec_ptr_t)GDS_REL2ABS(bt->cache_index);
-					if (cr->blk != bt->blk)
-					{
-						assert(FALSE);
-						SET_TRACEABLE_VAR(cnl->wc_blocked, WC_BLOCK_RECOVER);
-						BG_TRACE_PRO_ANY(csa, wc_blocked_t_end_crbtmismatch3);
-						status = cdb_sc_crbtmismatch;
-						goto failed;
-					}
-				}
-			}
-			if ((cache_rec_ptr_t)CR_NOTVALID == cr)
-			{
-				SET_TRACEABLE_VAR(cnl->wc_blocked, WC_BLOCK_RECOVER);
-				BG_TRACE_PRO_ANY(csa, wc_blocked_t_end_bitmap_nullbt);
-				SET_CACHE_FAIL_STATUS(status, csd);
-				goto failed;
-			}
-			if ((NULL == cr)  || (cr->cycle != cs->cycle) ||
-				((sm_long_t)GDS_REL2ABS(cr->buffaddr) != (sm_long_t)cs->old_block))
+			} else if (0 != cr->twin)
 			{
 				assert(CDB_STAGNATE > t_tries);
-				status = cdb_sc_lostbmlcr;
+				status = cdb_sc_bmlmod;
 				goto failed;
+			} else
+			{
+				bt = NULL;
+				if (cs->tn <= oldest_hist_tn)
+				{
+					assert(CDB_STAGNATE > t_tries);
+					status = cdb_sc_lostbmlhist;
+					goto failed;
+				}
+				if (cr->cycle != cs->cycle)
+				{
+					assert(CDB_STAGNATE > t_tries);
+					status = cdb_sc_lostbmlcr;
+					goto failed;
+				}
 			}
+			assert((sm_long_t)GDS_REL2ABS(cr->buffaddr) == (sm_long_t)cs->old_block);
 			PIN_CACHE_RECORD(cr, cr_array, cr_array_index);
 		}
 	}

sr_port/gvincr_recompute_upd_array.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2023 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2019 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2017-2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -105,6 +105,26 @@ enum cdb_sc	gvincr_recompute_upd_array(srch_blk_status *bh, struct cw_set_elemen
 		assert(CDB_STAGNATE > t_tries);
 		return cdb_sc_blkmod;
 	}
+	if (bh->cr != cr)
+	{	/* History is not in sync with cr. Possible in case cr is a newer twin and history points to older twin.
+		 * Assert accordingly. And update history to be in sync with newer twin in the hope that the recomputation
+		 * will succeed and the history validation in t_end() (caller) will otherwise succeed.
+		 */
+		assert(csa->hdr->asyncio);
+		assert(0 != cr->twin);
+		assert(0 != cr->bt_index);
+		bh->cr = cr;
+		bh->cycle = cr->cycle;
+		bh->tn = csa->ti->curr_tn;
+		assert(bh->buffaddr != (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
+		cse->old_block = bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
+	} else
+	{
+		assert(bh->cycle == cr->cycle);	/* t_end() (caller) would have restarted otherwise */
+		assert(bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
+	}
+	/* assert that cse->old_block is indeed pointing to the buffer that the cache-record is pointing to */
+	assert(cse->old_block == (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
 	buffaddr = bh->buffaddr;
 	target_key_size = gv_currkey->end + 1;
 	if (cdb_sc_normal != (status = gvcst_search_blk(gv_currkey, bh)))
@@ -178,9 +198,6 @@ enum cdb_sc	gvincr_recompute_upd_array(srch_blk_status *bh, struct cw_set_elemen
 		return cdb_sc_mkblk;
 	}
 	cse->upd_addr.blk = bs1;
-	/* assert that cse->old_block is indeed pointing to the buffer that the cache-record is pointing to.
-	 * While this code no longer copies the cache record's "ondsk_blkver" onto the cse, we leave this assert in place*/
-	assert((cse->old_block == (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr)) || (bh->cycle != cr->cycle) || (bh->cr != cr));
 	cse->done = FALSE;
 	if (cse->ondsk_blkver < ((blk_hdr_ptr_t)cse->old_block)->bver)
 	{	/* Adjust the ondsk_blkver as needed */

sr_port/t_end_sysops.c
@@ -1568,13 +1568,49 @@ enum cdb_sc	t_recompute_upd_array(srch_blk_status *bh, struct cw_set_element_str
 			BG_TRACE_PRO_ANY(csa, recompute_upd_array_in_tend);
 			return cdb_sc_blkmod;
 		}
-		bh->cr = cr;
-		bh->cycle = cr->cycle;
-		cse->old_block = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
-		/* old_block needs to be repointed to the NEW buffer but the fact that this block was free does not change in this
-		 * entire function. So cse->blk_prior_state's free_status can stay as it is.
-		 */
-		bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
+		if (!dollar_tlevel)
+		{	/* Caller is t_end(). It has already done the "cycle" check (i.e. cr->cycle == bh->cycle is TRUE) */
+			if (bh->cr != cr)
+			{	/* History is not in sync with cr. Possible in case cr is a newer twin and history points
+				 * to older twin. Assert accordingly. And update history to be in sync with newer twin in
+				 * the hope that the recomputation will succeed and the history validation in t_end()
+				 * (caller) will otherwise succeed.
+				 */
+				assert(csa->hdr->asyncio);
+				assert(0 != cr->twin);
+				assert(0 != cr->bt_index);
+				bh->cr = cr;
+				bh->cycle = cr->cycle;
+				bh->tn = csa->ti->curr_tn;
+				assert(bh->buffaddr != (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
+				/* old_block needs to be repointed to the NEW buffer but the fact that this block was free
+				 * does not change in this entire function. So cse->blk_prior_state's free_status can stay as it is.
+				 */
+				cse->old_block = bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
+			} else
+			{
+				assert(bh->cycle == cr->cycle);	/* t_end() (caller) would have restarted otherwise */
+				assert(bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
+			}
+			/* assert that cse->old_block is indeed pointing to the buffer that the cache-record is pointing to */
+			assert(cse->old_block == (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr));
+		} else
+		{	/* Caller is tp_tend(). In this case, the "cycle" check is not already done (will be done only
+			 * after this call). So if "bh->cr == cr", it is not guaranteed that "bh->cycle" and "bh->buffaddr"
+			 * are the same as "cr->cycle" and "cr->buffaddr" like was the case when "t_end()" was the caller
+			 * (in the "if" block above). So fix all those fields explicitly for now.
+			 *
+			 * NOTE: One might hope that "tp_tend()" can also be fixed to be just like "t_end()" at which point,
+			 * the below code can be removed and the "if" block above can be made common to both t_end and tp_tend.
+			 * But that is unlikely because "tp_tend()" validation skips the "cycle" check for blocks that were
+			 * only part of the read-set. Only blocks that are part of the read-write set go through the "cycle"
+			 * check. Therefore, this else block will stay on.
+			 */
+			bh->cr = cr;
+			bh->cycle = cr->cycle;
+			cse->old_block = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
+			bh->buffaddr = (sm_uc_ptr_t)GDS_REL2ABS(cr->buffaddr);
+		}
 	}
 	buffaddr = bh->buffaddr;
 	if ((NULL != cse->old_block) && (cse->ondsk_blkver < ((blk_hdr_ptr_t)cse->old_block)->bver))

sr_port/tab_bg_trc_rec.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2005-2020 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2022 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -59,11 +59,11 @@ TAB_BG_TRC_REC("  WcBlocked tptend t1   ", wc_blocked_tp_tend_t1)
 TAB_BG_TRC_REC("  WcBlocked tp bitmap   ", wc_blocked_tp_tend_bitmap)
 TAB_BG_TRC_REC("  WcBlocked tp jnl cwset", wc_blocked_tp_tend_jnl_cwset)
 TAB_BG_TRC_REC("  WcBlocked tp jnl wcflu", wc_blocked_tp_tend_jnl_wcsflu)
-TAB_BG_TRC_REC("  WcBlocked tend hist   ", wc_blocked_t_end_hist)
-TAB_BG_TRC_REC("  WcBlocked hist1 nul bt", wc_blocked_t_end_hist1_nullbt)
-TAB_BG_TRC_REC("  WcBlocked hist1 no nul", wc_blocked_t_end_hist1_nonnullbt)
-TAB_BG_TRC_REC("  WcBlocked bitmp nul bt", wc_blocked_t_end_bitmap_nullbt)
-TAB_BG_TRC_REC("  WcBlocked bitmp no nul", wc_blocked_t_end_bitmap_nonnullbt)
+TAB_BG_TRC_REC("  WcBlocked tend hist   ", wc_blocked_t_end_hist)		/* currently unused in YottaDB */
+TAB_BG_TRC_REC("  WcBlocked hist1 nul bt", wc_blocked_t_end_hist1_nullbt)	/* currently unused in YottaDB and GT.M */
+TAB_BG_TRC_REC("  WcBlocked hist1 no nul", wc_blocked_t_end_hist1_nonnullbt)	/* currently unused in YottaDB and GT.M */
+TAB_BG_TRC_REC("  WcBlocked bitmp nul bt", wc_blocked_t_end_bitmap_nullbt)	/* currently unused in YottaDB */
+TAB_BG_TRC_REC("  WcBlocked bitmp no nul", wc_blocked_t_end_bitmap_nonnullbt)	/* currently unused in YottaDB and GT.M */
 TAB_BG_TRC_REC("  WcBlocked jnl cwset   ", wc_blocked_t_end_jnl_cwset)
 TAB_BG_TRC_REC("  WcBlocked jnl wcflu   ", wc_blocked_t_end_jnl_wcsflu)
 TAB_BG_TRC_REC("  UNUSED                ", unused_filler_0052)	/* db_csh_get_too_many_loops => gvstats n_buffer_scarce BUS*/
@@ -97,9 +97,9 @@ TAB_BG_TRC_REC("  DbCshGetn_WrtLtchStuck", db_csh_getn_wrt_latch_stuck)
 TAB_BG_TRC_REC("  WcBlocked secshrclnup1", wcb_secshr_db_clnup_now_crit)
 TAB_BG_TRC_REC("  WcBlocked t_qread_bt1 ", wc_blocked_t_qread_bad_bt_index1)
 TAB_BG_TRC_REC("  WcBlocked t_qread_bt2 ", wc_blocked_t_qread_bad_bt_index2)
-TAB_BG_TRC_REC("  WcBlocked tend crbt1  ", wc_blocked_t_end_crbtmismatch1)
-TAB_BG_TRC_REC("  WcBlocked tend crbt2  ", wc_blocked_t_end_crbtmismatch2)
-TAB_BG_TRC_REC("  WcBlocked tend crbt3  ", wc_blocked_t_end_crbtmismatch3)
+TAB_BG_TRC_REC("  WcBlocked tend crbt1  ", wc_blocked_t_end_crbtmismatch1)	/* currently unused in YottaDB */
+TAB_BG_TRC_REC("  WcBlocked tend crbt2  ", wc_blocked_t_end_crbtmismatch2)	/* currently unused in YottaDB */
+TAB_BG_TRC_REC("  WcBlocked tend crbt3  ", wc_blocked_t_end_crbtmismatch3)	/* currently unused in YottaDB */
 TAB_BG_TRC_REC("  WcBlocked tptend crbt1", wc_blocked_tp_tend_crbtmismatch1)
 TAB_BG_TRC_REC("  WcBlocked tptend crbt2", wc_blocked_tp_tend_crbtmismatch2)
 TAB_BG_TRC_REC("  WcBlocked tptend crbt3", wc_blocked_tp_tend_crbtmismatch3)

commit ae7cdde07e7ac1f3f549017d7dbc68d9ac4c488c

Fix missing cycle check in t_end() validation of history and bitmap (regression in b4ee544e9)
Background
----------
Below is pasted from https://gitlab.com/YottaDB/DB/YDB/-/merge_requests/1734#note_2769422381.

* While testing the YDB!1750 changes, I noticed a rare failure of the `64bittn_1/rolling_upgrade` sub
  test with the following symptom.

  ```
  %YDB-F-ASSERT, Assert failed in sr_port/t_end.c line 1451 for expression (cs->blk == cache_start[bufindx].blk)
  ```

* The assert failure indicates that we have pinned a cache-record for a history block `34825` while inside
  `t_end()`. But the cache-record says the corresponding global buffer holds a different block `28476`.

  ```c
  (gdb) p cs->blk
  $74 = 34825

  (gdb) p cache_start[bufindx].blk
  $75 = 28476

  (gdb) p process_id
  $80 = 11016

  (gdb) p cs->cr->in_cw_set
  $1068 = 11016

  (gdb) p gv_target->hist.h[0].blk_num
  $1069 = 34825
  ```

* After some analysis of the failure, I have come to the conclusion that this is most likely a subtle
  bug in YDB!1734 (commit b4ee544e9) that was exposed by the YDB!1750 changes (which changed the db
  critical section mutex manager).

Issue
-----
* As suspected, it did turn out to be a bug in YDB!1734 (commit b4ee544e9). As part of those changes,
  a lot of the validation logic in `t_end.c` was simplified.

* Before that commit there was a `cr->cycle != t1->cycle` check (line 984 below) that was part of
  the validation logic and applied for the `if` code path in line 904 as well as the `else` code
  path of line 914.

  ```c
    903        bt = bt_get(t1->blk_num);
    904        if (NULL == bt)
    905        {
      .
      .
    913        } else
    914        {
      .
      .
    976        }
      .
      .
    984        if ((NULL == cr) || (cr->cycle != t1->cycle)
    985                || ((sm_long_t)GDS_REL2ABS(cr->buffaddr) != (sm_long_t)t1->buffaddr))
  ```

* As part of commit b4ee544e9 the above logic was reworked as follows.

  ```c

sr_port/t_end.c
@@ -914,17 +914,18 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 			} else
 			{
 				cr = t1->cr;
+				assert(t1->cycle <= cr->cycle);
+				if (cr->cycle != t1->cycle)
+				{
+					assert(CDB_STAGNATE > t_tries);
+					status = cdb_sc_lostcr;
+					goto failed;
+				}
 				if (0 != cr->bt_index)
 				{
 					bt = (bt_rec_ptr_t)GDS_REL2ABS(cr->bt_index);
 					assert(NULL != bt);
 					assert(cr->blk == bt->blk);
-					if (cr->cycle != t1->cycle)
-					{
-						assert(CDB_STAGNATE > t_tries);
-						status = cdb_sc_lostcr;
-						goto failed;
-					}
 				} else if (0 != cr->twin)
 				{	/* Update "cr" to point to newer twin */
 					cr = (cache_rec_ptr_t)GDS_ANY_REL2ABS(csa, cr->twin);
@@ -941,13 +942,8 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 						status = cdb_sc_losthist;
 						goto failed;
 					}
-					if (cr->cycle != t1->cycle)
-					{
-						assert(CDB_STAGNATE > t_tries);
-						status = cdb_sc_lostcr;
-						goto failed;
-					}
 				}
+				assert(t1->blk_num == cr->blk);
 				if (NULL != bt)
 				{
 					assert(bt->killtn <= bt->tn);
@@ -1088,17 +1084,18 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 		} else
 		{
 			cr = cs->cr;
+			assert(cs->cycle <= cr->cycle);
+			if (cr->cycle != cs->cycle)
+			{
+				assert(CDB_STAGNATE > t_tries);
+				status = cdb_sc_lostbmlcr;
+				goto failed;
+			}
 			if (0 != cr->bt_index)
 			{
 				bt = (bt_rec_ptr_t)GDS_REL2ABS(cr->bt_index);
 				assert(NULL != bt);
 				assert(cr->blk == bt->blk);
-				if (cr->cycle != cs->cycle)
-				{
-					assert(CDB_STAGNATE > t_tries);
-					status = cdb_sc_lostbmlcr;
-					goto failed;
-				}
 				if (cs->tn <= bt->tn)
 				{
 					assert(CDB_STAGNATE > t_tries);
@@ -1119,13 +1116,8 @@ trans_num t_end(srch_hist *hist1, srch_hist *hist2, trans_num ctn)
 					status = cdb_sc_lostbmlhist;
 					goto failed;
 				}
-				if (cr->cycle != cs->cycle)
-				{
-					assert(CDB_STAGNATE > t_tries);
-					status = cdb_sc_lostbmlcr;
-					goto failed;
-				}
 			}
+			assert(cs->blk == cr->blk);
 			assert((sm_long_t)GDS_REL2ABS(cr->buffaddr) == (sm_long_t)cs->old_block);
 			PIN_CACHE_RECORD(cr, cr_array, cr_array_index);
 			/* Note down "bt" in "cs->bt" to avoid a "bt_put()" call in "bg_update_phase1()".