YottaDB/YDB

commit 71c234032c330098bf58da638b71ef055830ef60

[#1181] Fix ~5x JOB command slowdown in V6.3-007 (as part of GTM-9058)
Background
----------
Below is pasted from the issue description at https://gitlab.com/YottaDB/DB/YDB/-/issues/1181

* This is something I noticed while trying to start thousands of processes for benchmarks as part of YDB#1178.

* Starting 10000 processes took a lot longer from r1.26 onwards whereas with r1.24 it was noticeably faster.

* Some more analysis showed the slowdown happened in GT.M V6.3-007.

* Below is a simple test case demonstrating the slowdown.

  ```m
  $ cat lotsofjobs.m
          set njobs=+$zcmdline
          set start=$zut
          for i=1:1:njobs job child set child(i)=$zjob
          set mid=$zut
          write "Starting ",njobs," processes took ",(mid-start)/(10**6)," seconds",!
          for i=1:1:njobs set pid=child(i) for  quit:'$zgetjpi(pid,"ISPROCALIVE")  hang 0.001
          quit
  child   ;
          quit
  ```

  **V6.3-006 output**
  ```sh
  $ $gtm_dist/mumps -run lotsofjobs 10000
  Starting 10000 processes took 6.71812 seconds
  ```

  **V6.3-007 output**
  ```sh
  $ $gtm_dist/mumps -run lotsofjobs 10000
  Starting 10000 processes took 31.942112 seconds
  ```

* Notice how starting 10,000 processes took 6.71 seconds in V6.3-006 but took 31.94 seconds in V6.3-007.

* Finally narrowed down the slowdown to the following change in GT.M V6.3-007.

  [GTM-9058](http://tinco.pair.com/bhaskar/gtm/doc/articles/GTM_V6.3-007_Release_Notes.html#GTM-9058)

  > GT.M handles JOB errors differently. Errors associated with the specified routine, label, or offset
  > appear in the error file of the JOBbed process in detail in addition to the JOBFAIL error received
  > by the original process. Previously GT.M did not report the more specific errors and did not start
  > the JOBbed process. GT.M detects and reports JOBLVN2LONG errors in the original process; GT.M does
  > not start the JOBbed process in this case. Previously the JOBbed process would report JOBLVN2LONG

sr_unix/ojstartchild.c
@@ -126,6 +126,7 @@ LITREF gtmImageName	gtmImageNames[];
 	joberr = joberr_io_setup_op_write;								\
 	job_errno = errno;										\
 	DOWRITERC(pipe_fd, &job_errno, SIZEOF(job_errno), pipe_status);					\
+	ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */	\
 	assert(FALSE);											\
 	UNDERSCORE_EXIT(joberr);									\
 }
@@ -136,9 +137,20 @@ LITREF gtmImageName	gtmImageNames[];
 	joberr = joberr_io_setup_write;									\
 	job_errno = errno;										\
 	DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);				\
+	ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */	\
 	assert(FALSE);											\
 	UNDERSCORE_EXIT(joberr);									\
 }
+
+#ifdef AUTORELINK_SUPPORTED
+#define	RELINKCTL_RUNDOWN_MIDDLE_PARENT(NEED_RTNOBJ_SHM_FREE, RTNHDR)			\
+{											\
+	if (NEED_RTNOBJ_SHM_FREE)							\
+		rtnobj_shm_free(RTNHDR, LATCH_GRABBED_FALSE);				\
+	relinkctl_rundown(TRUE, FALSE);							\
+}
+#endif
+
 
 error_def(ERR_JOBFAIL);
 error_def(ERR_JOBLVN2LONG);
@@ -168,6 +180,7 @@ static CONDITION_HANDLER(middle_child)
 	 */
 	assert(joberr == joberr_rtn);
 	DOWRITERC(pipe_fd, &job_errno, SIZEOF(job_errno), pipe_status);
+	ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(FALSE, NULL);)	/* decrement refcnts for relinkctl shm */
 	UNDERSCORE_EXIT(joberr);
 }
 
@@ -191,6 +204,18 @@ static CONDITION_HANDLER(ojlvzwr_ch)
 	maxstr_stack_level--;
 
 	NEXTCH;
+}
+
+/* The only purpose of this condition handler is to catch any error in a call to "job_addr()" and return to the caller
+ * "ojstartchild()" so it can move on as if the "job_addr()" call did not happen. This is needed because we want the
+ * grandchild to also do the "job_addr()" call (which it will only if the middle child moved on to a later point and then
+ * passed the routine name arguments of the JOB command to the grandchild) and issue an error in its "*.mje" file.
+ * And so we do not want to stop the middle child at this point in case of an error in "job_addr()".
+ */
+static CONDITION_HANDLER(middle_child_job_addr_ch)
+{
+	START_CH(TRUE);
+	UNWIND(NULL, NULL);	/* Return back to ESTABLISH_NORET() in caller "ojstartchild() */
 }
 
 /* This is to close the window of race condition where the timeout occurs and actually by that time, the middle process had already
@@ -377,6 +402,9 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 	char			*argv[4];
 	char			**env_ary, **env_ind;
 	char			**new_env_cur, **new_env_top, **old_env_cur, **old_env_top, *env_end;
+	boolean_t		need_rtnobj_shm_free;
+	boolean_t		job_addr_status;
+	boolean_t		error_encountered;
 	DCL_THREADGBL_ACCESS;
 
 	SETUP_THREADGBL_ACCESS;
@@ -487,11 +515,54 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 			UNDERSCORE_EXIT(status);
 		}
+
+		/* We are the middle child.  Check if job parameters could have caused an error in the grandchild.
+		 * Instead of waiting for the grandchild to let us know this (would slow the JOB command runtime
+		 * down noticeably, particularly when a huge number of processes are being jobbed off), we simulate
+		 * it by doing the "job_addr()" call (which the grandchild would be doing in "jobchild_init()")
+		 * in the middle child right here. And assume that if we get an error, the grandchild would
+		 * have also gotten an error and vice versa. Note down if an error occurred in "job_addr()" in the
+		 * "job_addr_status" variable and use this at a LATER point (after we have sent over the JOB command
+		 * parameters to the grandchild) so the grandchild will also record this same error in its .mje file.
+		 * If the middle child did this call BEFORE sending the job parameters to the grandchild, it would
+		 * return an error to the parent process (JOB command) but the .mje file would not contain the error.
+		 *
+		 * Record the fact that this process is interested in the relinkctl files inherited from the parent by
+		 * incrementing the linkctl->hdr->nattached count. This is required by the relinkctl_rundown(TRUE, FALSE)
+		 * call done as part of the RELINKCTL_RUNDOWN_MIDDLE_PARENT macro in the middle child or the grandchild.
+		 * Do this BEFORE the call to job_addr. In case that does a relinkctl_attach of a new relinkctl file,
+		 * we would increment the counter automatically so don't want that to go through a double-increment.
+		 */
+		ARLINK_ONLY(relinkctl_incr_nattached(RTNOBJ_REFCNT_INCR_CNT_FALSE));
+		/* We are the middle child. It is possible the below call to job_addr loads an object into shared memory
+		 * using "rtnobj_shm_malloc". In that case, as part of halting we need to do a "rtnobj_shm_free" to keep
+		 * the rtnobj reference counts in shared memory intact. The variable "need_rtnobj_shm_free" serves this purpose.
+		 * Note that we cannot safely call relinkctl_rundown since we do not want to decrement reference counts for
+		 * routines that have already been loaded by our parent process (which we inherited due to the fork) since
+		 * the parent process is the one that will do the decrement later. We should decrement the count only for
+		 * routines that we load ourselves. And the only one possible is due to the below call to "job_addr".
+		 * Note that the balancing decrement/free happens in the middle child until the grandchild is forked.
+		 * Once the grandchild fork succeeds, the grandchild is incharge of doing this cleanup. This flow is needed
+		 * so the decrement (and potential removal of rtnobj shmid) only happens AFTER when it is needed.
+		 */
+		MSTR_DEF(routine_mstr, jparms->params.routine.len, jparms->params.routine.buffer);
+		MSTR_DEF(label_mstr, jparms->params.label.len, jparms->params.label.buffer);
+		/* Surround the "job_addr()" call with a condition handler as it can issue "rts_error_csa" calls.
+		 * See comment before "middle_child_job_addr_ch()" function definition for more details on why this is needed.
+		 */
+		ESTABLISH_NORET(middle_child_job_addr_ch, error_encountered)
+		need_rtnobj_shm_free = FALSE;
+		if (!error_encountered)
+			job_addr_status = job_addr(&routine_mstr, &label_mstr, jparms->params.offset,
+							(char **)&rtnhdr, &transfer_addr, &need_rtnobj_shm_free);
+		REVERT;
+
 		joberr = joberr_sid;
 		if (-1 == setsid())
 		{
 			job_errno = errno;
 			DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			assert(FALSE);
 			UNDERSCORE_EXIT(joberr);
 		}
@@ -505,6 +576,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 				job_errno = errno;
 				DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 				assert(FALSE);	/* core the middle child toward diagnosing why the white box case failed */
+				ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			}
 		}
 #		endif
@@ -514,6 +586,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			job_errno = errno;
 			DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 			assert(WBTEST_ENABLED(WBTEST_JOBFAIL_FILE_LIM));
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			UNDERSCORE_EXIT(joberr);
 		}
 		assert(!WBTEST_ENABLED(WBTEST_JOBFAIL_FILE_LIM));
@@ -528,6 +601,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			job_errno = errno;
 			DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 			assert(FALSE);
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			UNDERSCORE_EXIT(joberr);
 		}
 		if (child_pid)
@@ -561,6 +635,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 						kill(child_pid, SIGTERM);
 					DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 					assert(FALSE);
+					ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */
 					UNDERSCORE_EXIT(joberr);
 				}
 			}
@@ -574,6 +649,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 				job_errno = errno;
 				DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 				assert(FALSE);
+				ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */
 				UNDERSCORE_EXIT(joberr);
 			}
 			/* send job parameters and arguments to final mumps process over setup socket */
@@ -584,16 +660,23 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			SEND(setup_fds[0], &jparms->params, SIZEOF(jparms->params), 0, rc);
 			if (rc < 0)
 				SETUP_DATA_FAIL();
-			/* Read status to catch any basic errors */
-			DOREADRC(setup_fds[0], &joberr, SIZEOF(joberr), rc);
-			if (rc < 0)
+			/* Now that we have sent over the job parameters to the grandchild, this is the LATER point
+			 * (referenced in a previous comment, search for "job_addr_status" above). Check if the "job_addr()"
+			 * call done above encountered an error and if so issue that error now.
+			 */
+			if (!job_addr_status || error_encountered)
 			{
-				joberr = joberr_rtn;	/* Assume routine error if there is a problem getting the report */
-				job_errno = errno;
-			}
-			if (joberr_ok != joberr)
-			{
-				DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
+				/* Grandchild process is going to encounter an error in its "job_addr()" call as well.
+				 * Wait for that to terminate before exiting the middle child as we do not want a situation
+				 * where a separate process has been jobbed off but the JOB command is returning an error
+				 * (as that would mean the parent process would then have to wait for the grandchild to
+				 * terminate using $zjob but that is not set since middle child exited with an error).
+				 * Ignore the wait status.
+				 */
+				WAITPID(child_pid, &wait_status, 0, done_pid);
+				assert(done_pid == child_pid);
+				ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */
+				joberr = joberr_rtn;
 				UNDERSCORE_EXIT(joberr);
 			}
 			setup_op = job_set_parm_list;
@@ -613,6 +696,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 					job_errno = ERR_JOBPARTOOLONG;
 					DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);
 					assert(FALSE);
+					ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */
 					UNDERSCORE_EXIT(joberr);
 				}
 				if (0 == jp->parm->mvtype)
@@ -670,6 +754,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			 * to report to.
 			 */
 			DOWRITERC(pipe_fds[1], &child_pid, SIZEOF(child_pid), pipe_status);
+			ARLINK_ONLY(relinkctl_rundown(FALSE, FALSE));	/* do not decrement counters, just shmdt */
 			UNDERSCORE_EXIT(EXIT_SUCCESS);
 		}
 		/* This is now the grandchild process (actual Job process) -- an orphan as soon as the EXIT(EXIT_SUCCESS) above
@@ -689,6 +774,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 		DOREADRC(mproc_fds[0], &decision, SIZEOF(decision), pipe_status);
 		if (pipe_status)	 /* We failed to read the communication from middle process */
 		{
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(7) ERR_JOBFAIL, 0, ERR_TEXT, 2,
 				LEN_AND_LIT("Error reading from pipe"), errno);
 		} else
@@ -743,6 +829,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 		string_len = STRLEN("%s=%d") + STRLEN(CHILD_FLAG_ENV) + MAX_NUM_LEN - 4;
 		if (string_len >= MAX_YOTTADB_EXE_PATH_LEN)
 		{
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(1) ERR_JOBPARTOOLONG);
 		}
 		c1 = (char *)malloc(string_len + 1);
@@ -853,6 +940,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 			 * This is impossible hence the below assert.
 			 */
 			assert(string_len);
+			ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 			RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(5) ERR_LOGTOOLONG, 3, string_len, c1,
 				SIZEOF(tbuff) - strlen(exe_str));
 		}
@@ -869,13 +957,21 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 		{
 			if (jparms->cmdline.len >= TEMP_BUFF_SIZE)
 			{
+				ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
 				RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(1) ERR_JOBPARTOOLONG);
 			}
 			memcpy(cmdbuff, jparms->cmdline.buffer, jparms->cmdline.len);
 			*(cmdbuff + jparms->cmdline.len) = 0;
 		} else
 			memset(cmdbuff, 0, TEMP_BUFF_SIZE);
-		/* Do common cleanup in child. */
+		/* Now that all job parameters (which could potentially be in relinkctl rtnobj shared memory) have been
+		 * copied over, we can get rid of our relinkctl files. Note that the below macro decrements
+		 * linkctl->hdr->nattached in the grandchild on behalf of the middle child (who did the increment).
+		 */
+		ARLINK_ONLY(RELINKCTL_RUNDOWN_MIDDLE_PARENT(need_rtnobj_shm_free, rtnhdr));
+		/* Do common cleanup in child. Note that the below call to "ojchildioclean" invokes "relinkctl_rundown"
+		 * but that is a no-op since we have already done it in the above RELINKCTL_RUNDOWN_MIDDLE_PARENT invocation.
+		 */
 		ojchildioclean();
 
 #ifdef KEEP_zOS_EBCDIC

sr_port/job_addr.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -24,11 +24,10 @@
 
 GBLREF mident_fixed     zlink_mname;
 
-error_def(ERR_JOBLABOFF);
 error_def(ERR_ZLINKFILE);
 error_def(ERR_ZLMODULE);
 
-boolean_t job_addr(mstr *rtn, mstr *label, int4 offset, char **hdr, char **labaddr)
+boolean_t job_addr(mstr *rtn, mstr *label, int4 offset, char **hdr, char **labaddr, boolean_t *need_rtnobj_shm_free)
 {
 	rhdtyp		*rt_hdr;
 	int4		*lp;
@@ -45,8 +44,10 @@ boolean_t job_addr(mstr *rtn, mstr *label, int4 offset, char **hdr, char **labad
 		if (NULL == rt_hdr)
 			RTS_ERROR_CSA_ABT(NULL, VARLSTCNT(8) ERR_ZLINKFILE, 2, rtn->len, rtn->addr,
 				ERR_ZLMODULE, 2, STRLEN(&zlink_mname.c[0]), &zlink_mname);
+		*need_rtnobj_shm_free = ARLINK_ONLY(rt_hdr->shared_object) NON_ARLINK_ONLY(FALSE);
 		*hdr = (char *)rt_hdr;
-	}
+	} else
+		*need_rtnobj_shm_free = FALSE;
 	lp = NULL;
 	if ((rt_hdr->compiler_qlf & CQ_LINE_ENTRY) || (0 == offset))
 		/* Label offset with routine compiled with NOLINE_ENTRY should cause error. */

sr_port/job_addr.h
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2018 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -13,6 +16,6 @@
 #ifndef JOB_ADDR_H_INCLUDED
 #define JOB_ADDR_H_INCLUDED
 
-boolean_t job_addr(mstr *rtn, mstr *label, int4 offset, char **hdr, char **labaddr);
+boolean_t job_addr(mstr *rtn, mstr *label, int4 offset, char **hdr, char **labaddr, boolean_t *need_rtnobj_shm_free);
 
 #endif

sr_port/libydberrors.h
@@ -108,7 +108,7 @@
 #define YDB_ERR_MUPIPINFO -150373099
 #define YDB_ERR_UNUSEDMSG277 -150373106
 #define YDB_ERR_JOBFAIL -150373114
-#define YDB_ERR_JOBLABOFF -150373122
+#define YDB_ERR_RTNLABOFF -150373122
 #define YDB_ERR_JOBPARNOVAL -150373130
 #define YDB_ERR_JOBPARNUM -150373138
 #define YDB_ERR_JOBPARSTR -150373146

sr_port/merrors.msg
@@ -276,7 +276,7 @@ IONOTOPEN	<Attempt to USE an I/O device which has not been opened>/error/fao=0!/
 MUPIPINFO	<!AD>/info/fao=2!/ansi=0
 UNUSEDMSG277	<IVTIME nixed in r1.20 because it is a VMS only error>/error/fao=0!/ansi=0
 JOBFAIL		<JOB command failure>/error/fao=0!/ansi=0
-JOBLABOFF	<Label and offset not found in created process>/error/fao=0!/ansi=13
+RTNLABOFF	<Label !AD and/or offset !UL not found in routine !AD>/error/fao=5!/ansi=13
 JOBPARNOVAL	<This job parameter cannot take a value>/error/fao=0!/ansi=0
 JOBPARNUM	<The value of this job parameter must be an integer>/error/fao=0!/ansi=0
 JOBPARSTR	<The value of this job parameter must be a string>/error/fao=0!/ansi=0

sr_port/merrors_ansi.h
@@ -110,7 +110,7 @@ const static readonly int error_ansi[] = {
 	   0,	/* MUPIPINFO */
 	   0,	/* UNUSEDMSG277 */
 	   0,	/* JOBFAIL */
-	  13,	/* JOBLABOFF */
+	  13,	/* RTNLABOFF */
 	   0,	/* JOBPARNOVAL */
 	   0,	/* JOBPARNUM */
 	   0,	/* JOBPARSTR */

sr_port/merrors_ctl.c
@@ -112,7 +112,7 @@ LITDEF	err_msg merrors[] = {
 	{ "MUPIPINFO", "!AD", 2, 0 },
 	{ "UNUSEDMSG277", "IVTIME nixed in r1.20 because it is a VMS only error", 0, 0 },
 	{ "JOBFAIL", "JOB command failure", 0, 0 },
-	{ "JOBLABOFF", "Label and offset not found in created process", 0, 0 },
+	{ "RTNLABOFF", "Label !AD and/or offset !UL not found in routine !AD", 5, 0 },
 	{ "JOBPARNOVAL", "This job parameter cannot take a value", 0, 0 },
 	{ "JOBPARNUM", "The value of this job parameter must be an integer", 0, 0 },
 	{ "JOBPARSTR", "The value of this job parameter must be a string", 0, 0 },

sr_port/ydbmerrors.h
@@ -108,7 +108,7 @@
 #define ERR_MUPIPINFO 150373099
 #define ERR_UNUSEDMSG277 150373106
 #define ERR_JOBFAIL 150373114
-#define ERR_JOBLABOFF 150373122
+#define ERR_RTNLABOFF 150373122
 #define ERR_JOBPARNOVAL 150373130
 #define ERR_JOBPARNUM 150373138
 #define ERR_JOBPARSTR 150373146

commit 11d7e6d10790d25e4b48aa16dff6e33ab474f0b1

[#1181] Fix rare FORCEDHALT message in stdout of JOB command child (regression in 71c234032)
Background
----------
* The nightly YDBOcto pipeline jobs started failing on a regular basis since the past 5 days.

* The failures happened in a variety of subtests. And randomly too.

* @shabiel analyzed the failures and came up with a simple set of steps to reproduce the failure.

  ```sh
  $ parallel -N0 -j 128 bats -f TOBW09 bats_tests/test_order_by_where.bats ::: {1..1000}
  ```

* Out of 1000 tests started in parallel above, a few of them failed with a symptom where the
  `FORCEDHALT` message (due to a SIGTERM sent by YDBAIM to the 2 children it had JOBbed off)
  is issued in very rare scenarios to the stdout file of the JOB command, not the stderr.

* Below is a sample failure diff.

  ```diff
  $ diff outref/TOBW09.ref output.txt
  0a1
  > %YDB-F-FORCEDHALT, Image HALTed by MUPIP STOP
  ```

* The way AIM works when it starts indexing is:
  - Start two jobs (one forward, one backwards)
  - Record $ZJOB into a global. It does not WAIT for any of the jobs to actually set a global.
  - See if one job is done with indexing the entire data.
  - If so, AIM terminates the other job by sending a SIGTERM/SIG15.

* Since we see that all FORCEDHALT messages are in the .out for the reverse job (the -1), this
  is probably what's happening here. In other words, once the first job is started, it actually
  finishes, and the second job never actually starts before it get signal 15 to stop.

Issue
-----
* Using `strace`, I took a look at the system calls invoked by the process issuing the JOB command,
  the middle child and the grandchild.

* And found that the FORCEDHALT message was written by the grandchild when it had the following stack.

  ```c
  (gdb) where
  #3  util_out_print_vaparm () at sr_unix/util_output.c:894
  #4  util_out_print (message=<optimized out>, flush=<optimized out>) at sr_unix/util_output.c:936

sr_unix/ojstartchild.c
@@ -79,6 +79,7 @@ GBLREF	mstr_len_t		sizeof_pat_everything;
 GBLREF	io_pair			io_curr_device;
 GBLREF	boolean_t		exit_handler_active;
 GBLREF	boolean_t		skip_exit_handler;
+GBLREF	boolean_t		err_same_as_out;
 
 static  joberr_t		joberr = joberr_gen;
 static	int			pipe_fd;
@@ -510,6 +511,7 @@ int ojstartchild (job_params_type *jparms, int argcnt, boolean_t *non_exit_retur
 		/* attempt to open output files. This also redirects stdin/out/err, so any error messages by this process during the
 		 * creation of the Job will get redirected.
 		 */
+		err_same_as_out = FALSE;
 		if ((status = ojchildioset(jparms)))
 		{
 			DOWRITERC(pipe_fds[1], &job_errno, SIZEOF(job_errno), pipe_status);