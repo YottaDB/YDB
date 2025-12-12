YottaDB/YDB

commit 5580099ac30020e276bb3d788eba6785238723d0

[#1178] [PERFORMANCE] Speed up database crit performance; Allow 3 mutex options : YDB, PTHREAD or ADAPTIVE
Background
----------
* See https://yottadb.net/critical-section-performance-in-r2-04/ for a high level description
  of the changes in this commit.

High level changes
------------------
* The custom/internal mutex manager with a mix of hard spin, yields, and sleeps (henceforth referred to
  as the YDB mutex manager) which was in use till GT.M V6.3-006 was replaced in GT.M V6.3-007 by the
  PTHREAD mutex manager (which instead used `pthread_mutex_trylock()` etc.).

* During in-house benchmarks of the 3n+1 program, we noticed that the YDB mutex manager performed
  noticeably better than the PTHREAD mutex manager when the number of processes contending for
  database crit was in the hundreds.

* But if the number of processes contending for database crit went in the thousdands, the PTHREAD
  mutex manager performed noticeably better than the YDB mutex manager.

* Additional benchmarks were also developed to test a variety of workloads. While they exhibited
  different behavior in the hundreds of processes (PTHREAD was better than YDB in some cases but
  not as good as YDB in a lot more cases), all of them did show that with thousands of processes
  PTHREAD became better than YDB.

* Therefore this commit resurrects the YDB mutex manager code from V6.3-006, and then trims it down
  even more (to remove a lot of what I thought was unnecessary code) and hopefully makes it even
  better compared to the V6.3-006 mutex code.

* This commit then also changed the mutex structures in shared memory to allow YDB and PTHREAD
  mutex managers to co-exist.

* And then came in a new ADAPTIVE mutex manager algorithm which starts out with YDB mutex and then
  switches to PTHREAD mutex if there is a lot of contention. And switches back to YDB mutex if
  the contention drops. This way it is hoped this new mutex manager will give users the best of

sr_unix/wcs_clean_dbsync.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2023 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -125,14 +128,9 @@ void	wcs_clean_dbsync(TID tid, int4 hd_len, sgmnt_addrs **csaptr)
 		 *	Note that the function "mutex_deadlock_check" resets crit_count to 0 temporarily even though we
 		 *	might actually be in the midst of acquiring crit. Therefore we should not interrupt mainline code
 		 *	if we are in the "mutex_deadlock_check" as otherwise it presents reentrancy issues.
-		 *   2) We have crit on any region/jnlpool OR are in the middle of commit for this region (even though
+		 *   2) We have crit on the current region OR are in the middle of commit for this region (even though
 		 *	we dont hold crit) OR are in wcs_wtstart (potentially holding write interlock and keeping another
-		 *	process in crit waiting) OR we need to wait to obtain crit. At least one reason why we should not wait
-		 *	to obtain crit is because the timeout mechanism for the critical section is currently (as of 2004 May)
-		 *	driven by heartbeat on Tru64, AIX, Solaris and HPUX. The periodic heartbeat handler cannot pop as
-		 *	it is a SIGALRM handler and cannot nest while we are already in a SIGALRM handler for the wcs_clean_dbsync.
-		 *   	Were this to happen, we could end up waiting for crit, not being able to interrupt the wait
-		 *   	with a timeout resulting in a hang until crit became available.
+		 *	process in crit waiting) OR we need to wait to obtain crit.
 		 *   3) We are in a "fast lock".
 		 *   4) We are in gtm_malloc. Don't want to recurse on malloc.
 		 * Other deadlock causing conditions that need to be taken care of
@@ -148,9 +146,8 @@ void	wcs_clean_dbsync(TID tid, int4 hd_len, sgmnt_addrs **csaptr)
 			&& (0 == fast_lock_count)
 			&& (!db_fsync_in_prog)
 			&& (!jpc || (LOCK_AVAILABLE == jbp->fsync_in_prog_latch.u.parts.latch_pid))
-			&& (0 == TREF(crit_reg_count))
-			&& ((NULL == check_csaddrs) || !T_IN_COMMIT_OR_WRITE(check_csaddrs))
-			&& !T_IN_COMMIT_OR_WRITE(csa)
+			&& ((NULL == check_csaddrs) || (!T_IN_COMMIT_OR_WRITE(check_csaddrs) && !check_csaddrs->now_crit))
+			&& (!T_IN_COMMIT_OR_WRITE(csa) && !csa->now_crit)
 			&& (FALSE != grab_crit_immediate(reg, OK_FOR_WCS_RECOVER_FALSE, NOT_APPLICABLE)))
 		{ 	/* Note that if we are here, we have obtained crit using grab_crit_immediate. */
 			assert(csa->ti->early_tn == csa->ti->curr_tn);

ci/tidy_warnings_debug_x86_64.ref
@@ -16,8 +16,8 @@ gt_timers.c:warning: Uninitialized va_list is copied [clang-analyzer-valist.Unin
 gtm_ci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtm_cip.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtm_putmsg_list.c:warning: Uninitialized va_list is copied [clang-analyzer-valist.Uninitialized]
-gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (190 padding bytes, where 6 is optimal).
-gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (190 padding bytes, where 6 is optimal).
+gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (194 padding bytes, where 2 is optimal).
+gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (194 padding bytes, where 2 is optimal).
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmsecshr_wrapper.c:warning: multiple statement macro used without braces; some statements will be unconditionally executed [bugprone-multiple-statement-macro]

ci/tidy_warnings_release_x86_64.ref
@@ -17,8 +17,8 @@ gt_timers.c:warning: Uninitialized va_list is copied [clang-analyzer-valist.Unin
 gtm_ci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtm_cip.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtm_putmsg_list.c:warning: Uninitialized va_list is copied [clang-analyzer-valist.Uninitialized]
-gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (186 padding bytes, where 2 is optimal).
-gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (186 padding bytes, where 2 is optimal).
+gtm_threadgbl_deftypes.c:warning: Excessive padding in 'gtm_threadgbl_def_t' (190 padding bytes, where 6 is optimal).
+gtm_threadgbl_init.c:warning: Excessive padding in 'gtm_threadgbl_true_t' (190 padding bytes, where 6 is optimal).
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gtmci.c:warning: Initialized va_list 'var' is leaked [clang-analyzer-valist.Unterminated]
 gvcst_init_sysops.c:warning: redundant condition 'db_do_crypt_init' [bugprone-redundant-branch-condition]

sr_port/db_auto_upgrade.c
@@ -207,8 +207,14 @@ void db_auto_upgrade(gd_region *reg)
 			case GDSMR204_V71001:
 				/* GT.M V7.1-002 implemented index reserved bytes and changed default to zero */
 				csd->i_reserved_bytes = 0;
-				break;		/* so a new "case" needs to be added BEFORE the assert. */
 			case GDSMR204_V71002:
+				/* YottaDB r2.04 added new "mutex_type" field */
+				assert(!IS_STATSDB_REG(reg));	/* statsdb should never come through auto upgrade logic.
+								 * but just to be safe handle it below as it is easy to do so.
+								 */
+				csd->mutex_type = IS_STATSDB_REG(reg) ? mutex_type_ydb : mutex_type_adaptive_ydb;
+				break;		/* so a new "case" needs to be added BEFORE the assert. */
+			case GDSMR204:
 				/* When adding a new minor version, the following template should be maintained
 				 * 1) Remove the penultimate 'break' (i.e. "break" in the PREVIOUS "case" block.
 				 * 2) If there are any file header fields added in the new minor version, initialize the fields
@@ -719,6 +725,11 @@ void v6_db_auto_upgrade(gd_region *reg)
 				csd->problksplit = DEFAULT_PROBLKSPLIT;
 				/* GT.M V7.1-002 implemented index reserved bytes and changed default to zero */
 				csd->i_reserved_bytes = 0;
+				/* YottaDB r2.04 added new "mutex_type" field */
+				assert(!IS_STATSDB_REG(reg));	/* statsdb should never come through auto upgrade logic.
+								 * but just to be safe handle it below as it is easy to do so.
+								 */
+				csd->mutex_type = IS_STATSDB_REG(reg) ? mutex_type_ydb : mutex_type_adaptive_ydb;
 				break;
 			case GDSMV63015:
 				assert(FALSE);	/* if this should come to pass, add appropriate code above the assert */

sr_port/dse_crit.c
@@ -3,6 +3,9 @@
  * Copyright (c) 2001-2021 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
  *	This source code contains the intellectual property	*
  *	of its copyright holder(s), and is made available	*
  *	under a license.  If you do not know the terms of	*
@@ -93,9 +96,7 @@ void dse_crit(void)
 		if (gv_cur_region->read_only)
 			RTS_ERROR_CSA_ABT(cs_addrs, VARLSTCNT(4) ERR_DBRDONLY, 2, DB_LEN_STR(gv_cur_region));
 		cs_addrs->hdr->image_count = 0;
-#		ifdef CRIT_USE_PTHREAD_MUTEX
 		crash = TRUE;
-#		endif
 		gtm_mutex_init(gv_cur_region, NUM_CRIT_ENTRY(cs_addrs->hdr), crash);
 		cs_addrs->nl->in_crit = 0;
 		cs_addrs->now_crit = FALSE;
@@ -111,9 +112,8 @@ void dse_crit(void)
 			util_out_print("!/The write critical section is unowned!/", TRUE);
 			return;
 		}
-#		ifndef CRIT_USE_PTHREAD_MUTEX
-		assert(LOCK_AVAILABLE != cs_addrs->critical->semaphore.u.parts.latch_pid);
-#		endif
+		assert(!IS_MUTEX_TYPE_YDB(cs_addrs->critical->curr_mutex_type)
+			|| (LOCK_AVAILABLE != cs_addrs->critical->semaphore.u.parts.latch_pid));
 		cs_addrs->now_crit = TRUE;
 		cs_addrs->nl->in_crit = process_id;
 		UPDATE_CRASH_COUNT(cs_addrs, crash_count);
@@ -143,15 +143,16 @@ void dse_crit(void)
 	}
 	if (crash)
 	{
-#		ifndef CRIT_USE_PTHREAD_MUTEX
-		memcpy(util_buff, "!/Critical section crash count is ", 34);
-		util_len = 34;
-		util_len += i2hex_nofill(cs_addrs->critical->crashcnt, (uchar_ptr_t)&util_buff[util_len], 8);
-		memcpy(&util_buff[util_len], "!/", 2);
-		util_len += 2;
-		util_buff[util_len] = 0;
-		util_out_print(util_buff, TRUE);
-#		endif
+		if (IS_MUTEX_TYPE_YDB(cs_addrs->critical->curr_mutex_type))
+		{
+			memcpy(util_buff, "!/Critical section crash count is ", 34);
+			util_len = 34;
+			util_len += i2hex_nofill(cs_addrs->critical->crashcnt, (uchar_ptr_t)&util_buff[util_len], 8);
+			memcpy(&util_buff[util_len], "!/", 2);
+			util_len += 2;
+			util_buff[util_len] = 0;
+			util_out_print(util_buff, TRUE);
+		}
 		return;
 	}
 	if (cli_present("ALL") == CLI_PRESENT)

sr_port/dse_dmp_fhead.c
@@ -112,6 +112,7 @@ void dse_dmp_fhead (void)
 	time_t			backup_start_tmp;
 	struct tm		backup_start_tm_tmp;
 	char			backup_start_buf[100];
+	mutex_type_t		curr_mutex_type;
 
 	is_dse_all = dse_all_dump;
 	dse_all_dump = FALSE;
@@ -263,7 +264,23 @@ void dse_dmp_fhead (void)
 		util_out_print("  Index Reserved Bytes          !12UL", TRUE, csd->i_reserved_bytes);
 		util_out_print("  Max conc proc time         !22UL", FALSE, csd->max_procs.time);
 		util_out_print("  Max Concurrent processes         !9UL", TRUE, csd->max_procs.cnt);
-		util_out_print("  Reorg Sleep Nanoseconds         !17UL", TRUE, csd->reorg_sleep_nsec);
+		util_out_print("  Reorg Sleep Nanoseconds         !17UL", FALSE, csd->reorg_sleep_nsec);
+		assert(mutex_type_adaptive_pthread != csd->mutex_type);
+		assert((mutex_type_adaptive_ydb == csd->mutex_type)
+			|| (mutex_type_pthread == csd->mutex_type) || (mutex_type_ydb == csd->mutex_type));
+		curr_mutex_type = csa->critical->curr_mutex_type;
+		assert((mutex_type_adaptive_ydb != csd->mutex_type) || (mutex_type_adaptive_ydb == curr_mutex_type));
+		assert((mutex_type_adaptive_pthread != csd->mutex_type) || (mutex_type_adaptive_pthread == curr_mutex_type));
+		assert((mutex_type_adaptive_ydb != csd->mutex_type)
+			|| (mutex_type_adaptive_ydb == curr_mutex_type) ||(mutex_type_adaptive_pthread == curr_mutex_type));
+		util_out_print("  Mutex Manager Type      !AZ", TRUE,
+				((mutex_type_adaptive_ydb == curr_mutex_type)
+					? "    ADAPTIVE (YDB)"
+					: ((mutex_type_adaptive_pthread == curr_mutex_type)
+						? "ADAPTIVE (PTHREAD)"
+						: ((mutex_type_pthread == csd->mutex_type)
+							? "           PTHREAD"
+							: "               YDB"))));
 	}
 	if (CLI_PRESENT == cli_present("ALL"))
 	{	/* Only dump these if -/ALL as if part of above display */

sr_port/gbldefs.c
@@ -397,14 +397,6 @@ GBLDEF	int			gtmrecv_srv_count;
 GBLDEF	volatile int4		db_fsync_in_prog;
 GBLDEF	volatile int4		jnl_qio_in_prog;
 GBLDEF	gtmsiginfo_t		signal_info;
-#ifndef MUTEX_MSEM_WAKE
-GBLDEF	int			mutex_sock_fd = FD_INVALID;
-GBLDEF	struct sockaddr_un	mutex_sock_address;
-GBLDEF	struct sockaddr_un	mutex_wake_this_proc;
-GBLDEF	int			mutex_wake_this_proc_len;
-GBLDEF	int			mutex_wake_this_proc_prefix_len;
-GBLDEF	fd_set			mutex_wait_on_descs;
-#endif
 GBLDEF	void			(*call_on_signal)();
 GBLDEF	enum gtmImageTypes	image_type;	/* initialized at startup i.e. in dse.c, lke.c, gtm.c, mupip.c, gtmsecshr.c etc. */
 
@@ -794,7 +786,6 @@ GBLDEF	boolean_t	holds_sem[NUM_SEM_SETS][NUM_SRC_SEMS];	/* whether a particular 
 GBLDEF	boolean_t	detail_specified;	/* Set to TRUE if -DETAIL is specified in MUPIP REPLIC -JNLPOOL or -EDITINST */
 GBLDEF	boolean_t	in_mupip_ftok;		/* Used by an assert in repl_inst_read */
 GBLDEF	uint4		section_offset;		/* Used by PRINT_OFFSET_PREFIX macro in repl_inst_dump.c */
-GBLDEF	uint4		mutex_per_process_init_pid;	/* pid that invoked "mutex_per_process_init" */
 GBLDEF	boolean_t	ydb_quiet_halt;		/* Suppress FORCEDHALT message */
 #ifdef UTF8_SUPPORTED
 /* UTF8 line terminators.  In addition to the following

sr_port/gdsbt.h
@@ -18,13 +18,7 @@
 /* this requires gdsroot.h */
 
 #include <sys/types.h>
-#ifdef MUTEX_MSEM_WAKE
-#ifdef POSIX_MSEM
-#  include "gtm_semaphore.h"
-#else
-#  include <sys/mman.h>
-#endif
-#endif
+#include "gtm_semaphore.h"	/* needed only with ydb mutex (not pthread mutex) */
 
 #include <gtm_limits.h>		/* for _POSIX_HOST_NAME_MAX */
 #if defined(SUNOS) && !defined(_POSIX_HOST_NAME_MAX)
@@ -122,15 +116,6 @@ typedef struct th_rec_struct
 	sm_off_t	cache_index;
 } th_rec;
 
-/* This structure is used to maintain all cache records.  The BT queue contains
- * a history of those blocks that have been updated.
- */
-
-/*
- *	Definitions for GT.M Mutex Control
- *
- *	See MUTEX.MAR for VMS functional implementation
- */
 typedef struct
 {
 	struct
@@ -139,21 +124,12 @@ typedef struct
 			bl;
 	}		que;
 	int4		pid;
-	void		*super_crit;
+	int4		filler_8byte_align;
 	/*
 	 * Make sure that the size of mutex_que_entry is a multiple of 8 bytes
 	 * for quadword alignment requirements of remqhi and insqti.
 	 */
-	int4		mutex_wake_instance;
-	int4		filler1; /* for dword alignment 		 */
-#ifdef MUTEX_MSEM_WAKE
-# ifdef POSIX_MSEM
 	sem_t		mutex_wake_msem; /* Not two ints .. somewhat larger */
-# else
-	msemaphore	mutex_wake_msem; /* Two ints (incidentally two int4s) */
-# endif
-
-#endif
 } mutex_que_entry;
 
 typedef struct
@@ -166,30 +142,67 @@ typedef struct
 	global_latch_t	latch;
 } mutex_que_head;
 
-typedef struct
+typedef enum
 {
-	FILL8DCL(uint4, crit_cycle, 1);
-	global_latch_t	semaphore;
-	CACHELINE_PAD(8 + SIZEOF(global_latch_t), 2);	/* 8 for the FILL8DCL */
-	FILL8DCL(latch_t, crashcnt, 3);
-	global_latch_t	crashcnt_latch;
-	CACHELINE_PAD(8 + SIZEOF(global_latch_t), 4);	/* 8 for the FILL8DCL */
-	compswap_time_field	stuckexec;
-	CACHELINE_PAD(SIZEOF(compswap_time_field), 5);
-	FILL8DCL(latch_t, queslots, 6);
-	CACHELINE_PAD(SIZEOF(latch_t) + SIZEOF(latch_t), 7);
-	mutex_que_head	prochead;
-	CACHELINE_PAD(SIZEOF(mutex_que_head), 8);
-	mutex_que_head	freehead;
-	CACHELINE_PAD(SIZEOF(mutex_que_head), 9);
-} mutex_struct;
+	mutex_type_adaptive_ydb     = 0,/* Adaptive mutex manager (with ydb mutex being current setting).
+					 * enum set to 0 so this is the default mutex type value for new databases.
+					 */
+	mutex_type_adaptive_pthread = 1,/* Adaptive mutex manager (with pthread mutex being current setting) */
+	mutex_type_ydb              = 2,/* ONLY ydb mutex manager */
+	mutex_type_pthread          = 3,/* ONLY pthread mutex manager */
+} mutex_type_t;
+
+/* Note: The first macro below is faster than (mutex_type_pthread == MUTEX_TYPE) || (mutex_type_adaptive_pthread == MUTEX_TYPE).
+ * The other macros are also structured in similar fashion. There are asserts in "gtm_mutex_init()" that verify their correctness.
+ */
+#define	IS_MUTEX_TYPE_PTHREAD(MUTEX_TYPE)	(MUTEX_TYPE & 1)
+#define	IS_MUTEX_TYPE_YDB(MUTEX_TYPE)		(!IS_MUTEX_TYPE_PTHREAD(MUTEX_TYPE))
+#define	IS_MUTEX_TYPE_ADAPTIVE(MUTEX_TYPE)	!(MUTEX_TYPE & 2)
+
+/* Uncomment the following line to enable mutex switch debugging */
+/* #define DEBUG_YDB_MUTEX */
+
+#ifdef DEBUG_YDB_MUTEX
+#	define	DEBUG_YDB_MUTEX_ONLY(X)		X
+#else
+#	define	DEBUG_YDB_MUTEX_ONLY(X)
+#endif
 
 typedef struct
 {
-	FILL8DCL(uint4, crit_cycle, 1);
-	FILL8DCL(uint4, stuck_cycle, 2);
+	mutex_type_t		curr_mutex_type;	/* can be "mutex_type_ydb" or "mutex_type_pthread".
+							 * Initial value is "mutex_type_ydb".
+							 */
+	CACHELINE_PAD(SIZEOF(mutex_type_t), 1);
+	/* ------------- The below is used by BOTH the ydb mutex AND pthread mutex logic ---------- */
+	uint4			crit_cycle;			/* used by both ydb mutex AND pthread mutex logic */
+	CACHELINE_PAD(SIZEOF(uint4), 2);
+	/* ------------- The below is only used by the pthread mutex logic ---------- */
 	pthread_mutex_t		mutex;
-} pth_mutex_struct;
+	CACHELINE_PAD(SIZEOF(pthread_mutex_t), 3);
+	/* ------------- The below is used only by the ydb mutex logic ---------- */
+	uint4			stuck_cycle;
+	CACHELINE_PAD(SIZEOF(uint4), 4);
+	global_latch_t		semaphore;
+	CACHELINE_PAD(SIZEOF(global_latch_t), 5);
+	latch_t			crashcnt;
+	CACHELINE_PAD(SIZEOF(latch_t), 6);
+	compswap_time_field	stuckexec;
+	CACHELINE_PAD(SIZEOF(compswap_time_field), 7);
+	DEBUG_YDB_MUTEX_ONLY(int8	n_crit_waiters);	/* number of processes waiting to get crit */
+	DEBUG_YDB_MUTEX_ONLY(CACHELINE_PAD(SIZEOF(int8), 8));
+	int8			n_msem_waiters;	/* number of processes waiting in sem_wait() */
+	CACHELINE_PAD(SIZEOF(int8), 9);
+	latch_t			queslots;
+	CACHELINE_PAD(SIZEOF(latch_t), 10);
+	mutex_que_head		prochead;
+	CACHELINE_PAD(SIZEOF(mutex_que_head), 11);
+	/* Note: The following 2 lines need to be the last lines in this structure.
+	 * "q_free_entry" initializion in "clean_initialize()" relies on this assumption.
+	 */
+	mutex_que_head		freehead;
+	CACHELINE_PAD(SIZEOF(mutex_que_head), 12);
+} mutex_struct;
 
 typedef struct {
 	int4	mutex_hard_spin_count;
@@ -742,6 +755,9 @@ typedef struct node_local_struct
 	global_latch_t		lock_crit;		/* mutex for LOCK processing */
 	volatile block_id	tp_hint;
 	char			max_procs[MAX_PROCS_ARRAY_SIZE];
+	/* The following 2 fields are used by logic that determines whether the ydb mutex needs to be switched to pthread mutex */
+	uint8			prev_n_crit_que_slps, prev_n_crit_yields, prev_n_crit_failed;
+	uint8			switch_streak;
 } node_local;
 
 #define UPDATE_MAX_PROCS_STRING(NODE, MPS)	SNPRINTF(NODE->max_procs, MAX_PROCS_ARRAY_SIZE, "%d,%lu", MPS.cnt, MPS.time)
@@ -1000,11 +1016,7 @@ MBSTART {								\
 #define MIN_CRIT_ENTRY				64		/* keep this in sync with gdeinit.m minseg("MUTEX_SLOTS") */
 #define MAX_CRIT_ENTRY				32768		/* keep this in sync with gdeinit.m maxseg("MUTEX_SLOTS") */
 #define DEFAULT_NUM_CRIT_ENTRY			1024		/* keep this in sync with gdeget.m tmpseg("MUTEX_SLOTS") */
-#ifdef CRIT_USE_PTHREAD_MUTEX
-#define CRIT_SPACE(ENTRIES)			SIZEOF(pth_mutex_struct)
-#else
 #define CRIT_SPACE(ENTRIES)			((ENTRIES) * SIZEOF(mutex_que_entry) + SIZEOF(mutex_struct))
-#endif
 #define NUM_CRIT_ENTRY(CSD)			(CSD)->mutex_spin_parms.mutex_que_entry_space_size
 #define JNLPOOL_CRIT_SPACE			CRIT_SPACE(DEFAULT_NUM_CRIT_ENTRY)
 #define NODE_LOCAL_SIZE				(ROUND_UP(SIZEOF(node_local), OS_PAGE_SIZE))
@@ -1120,17 +1132,10 @@ typedef bt_rec	*bt_rec_ptr_t;
 typedef th_rec	*th_rec_ptr_t;
 typedef th_index *th_index_ptr_t;
 typedef mutex_struct *mutex_struct_ptr_t;
-typedef pth_mutex_struct *pth_mutex_struct_ptr_t;
 
 typedef mutex_spin_parms_struct *mutex_spin_parms_ptr_t;
 typedef mutex_que_entry	*mutex_que_entry_ptr_t;
 typedef node_local *node_local_ptr_t;
-
-#ifdef CRIT_USE_PTHREAD_MUTEX
-typedef pth_mutex_struct_ptr_t	CRIT_PTR_T;
-#else
-typedef mutex_struct_ptr_t	CRIT_PTR_T;
-#endif
 
 #define OLDEST_HIST_TN(CSA)		(DBG_ASSERT(CSA->hdr) DBG_ASSERT(CSA->hdr->acc_meth != dba_mm)	\
 						((th_rec_ptr_t)((sm_uc_ptr_t)CSA->th_base + CSA->th_base->tnque.fl))->tn)

commit c67d65be96dca23ce5c569e4d6737b9e1cf85a45

[#1178] Fix rare stress/concurr subtest failure due to wcs_clean_dbsync.c regression in 5580099ac
Background
----------
* During in-house testing, the above subtest failed in a rare case with the following symptom.

  ```diff
  > ##HOST##:##TEST_PATH##/stress_1_2/concurr/stress.mjo10
  > %YDB-F-ASSERT, Assert failed in sr_port/jnl_write_attempt.c line 258 for expression (was_crit || (0 == have_crit(CRIT_HAVE_ANY_REG)))
  ```

Issue
-----
* Below is the C-stack from the assert failure.

  ```c
  (gdb) where
  #0  __pthread_kill_implementation (no_tid=0, signo=3, threadid=<optimized out>) at ./nptl/pthread_kill.c:44
  #1  __pthread_kill_internal (signo=3, threadid=<optimized out>) at ./nptl/pthread_kill.c:78
  #2  __GI___pthread_kill (threadid=<optimized out>, signo=3) at ./nptl/pthread_kill.c:89
  #3  gtm_dump_core () at sr_unix/gtm_dump_core.c:74
  #4  ch_cond_core () at sr_unix/ch_cond_core.c:77
  #5  rts_error_va () at sr_unix/rts_error.c:199
  #6  rts_error_csa () at sr_unix/rts_error.c:100
  #7  assertfail () at sr_port/mdef.h:827
  #8  jnl_write_attempt () at sr_port/jnl_write_attempt.c:258
  #9  jnl_wait (reg=0x52a0000023d0) at sr_port/jnl_wait.c:37
  #10 wcs_clean_dbsync () at sr_unix/wcs_clean_dbsync.c:210
  #11 timer_handler () at sr_unix/gt_timers.c:978
  #12 ydb_stm_invoke_deferred_signal_handler () at sr_unix/ydb_stm_invoke_deferred_signal_handler.c:148
  #13 deferred_signal_handler () at sr_port/deferred_signal_handler.c:78
  #14 grab_crit (reg=0x52a0000028e0, state=WS_53) at sr_unix/grab_crit.c:161
  #15 tp_crit_all_regions () at sr_port/tp_tend.c:187
  #16 tp_restart () at sr_port/tp_restart.c:603
  #17 ydb_simpleapi_ch () at sr_unix/ydb_simpleapi_ch.c:154
  #18 rts_error_va () at sr_unix/rts_error.c:199
  #19 rts_error_csa () at sr_unix/rts_error.c:100
  #20 t_retry () at sr_port/t_retry.c:595
  #21 gvcst_kill2 () at sr_port/gvcst_kill.c:884
  #22 gvcst_kill () at sr_port/gvcst_kill.c:149
  #23 gvcst_spr_kill () at sr_unix/gvcst_spr_kill.c:90

sr_port/jnl.h
@@ -1885,11 +1885,12 @@ MBSTART {								\
  * Note that we can do the db fsync only if we already have the journal file open. If we do not, we will end
  * up doing this later after grabbing crit. This just minimizes the # of times db fsync happens while inside crit.
  */
-#define	DO_JNL_FSYNC_OUT_OF_CRIT_IF_NEEDED(REG, CSA, JPC, JBP)			\
-MBSTART {									\
-	assert(!CSA->now_crit);							\
-	if ((NULL != JPC) && JBP->need_db_fsync)				\
-		jnl_wait(REG);	/* Try to do db fsync outside crit */		\
+#define	DO_JNL_FSYNC_OUT_OF_CRIT_IF_NEEDED(REG, CSA, JPC, JBP)					\
+MBSTART {											\
+	assert(!CSA->now_crit);									\
+	assert(0 == have_crit(CRIT_HAVE_ANY_REG)); /* caller should have ensured this */	\
+	if ((NULL != JPC) && JBP->need_db_fsync)						\
+		jnl_wait(REG);	/* Try to do db fsync outside crit */				\
 } MBEND
 
 /* The below macro is currently used by the source server but is placed here in case others want to avail it later.

sr_unix/wcs_clean_dbsync.c
@@ -146,8 +146,9 @@ void	wcs_clean_dbsync(TID tid, int4 hd_len, sgmnt_addrs **csaptr)
 			&& (0 == fast_lock_count)
 			&& (!db_fsync_in_prog)
 			&& (!jpc || (LOCK_AVAILABLE == jbp->fsync_in_prog_latch.u.parts.latch_pid))
-			&& ((NULL == check_csaddrs) || (!T_IN_COMMIT_OR_WRITE(check_csaddrs) && !check_csaddrs->now_crit))
-			&& (!T_IN_COMMIT_OR_WRITE(csa) && !csa->now_crit)
+			&& (0 == have_crit(CRIT_HAVE_ANY_REG))
+			&& ((NULL == check_csaddrs) || !T_IN_COMMIT_OR_WRITE(check_csaddrs))
+			&& !T_IN_COMMIT_OR_WRITE(csa)
 			&& (FALSE != grab_crit_immediate(reg, OK_FOR_WCS_RECOVER_FALSE, NOT_APPLICABLE)))
 		{ 	/* Note that if we are here, we have obtained crit using grab_crit_immediate. */
 			assert(csa->ti->early_tn == csa->ti->curr_tn);