YottaDB/YDB

commit 3b35ef5d697a7f7112fb1ede8887048df4c92320

[PERFORMANCE] Avoid grab_crit/rel_crit in loop during kill_in_prog wait in region_freeze()
Background
----------
* As part of analyzing YDB#1231, I noticed that there are 4 code paths that wait for
  `cs_data->kill_in_prog` to become 0.

  ```c
  $ cd YDB
  $ rg MAX_CRIT_TRY
  sr_unix/ss_initiate.c
  521:            wait_for_zero_kip = csd->kill_in_prog && (MAX_CRIT_TRY > crit_counter); /* only do kip wait once */
  541:                    while (csd->kill_in_prog && (MAX_CRIT_TRY > crit_counter++))
  543:                            GET_C_STACK_FOR_KIP(kip_pids_arr_ptr, crit_counter, MAX_CRIT_TRY, 1, MAX_KIP_PID_SLOTS);

  sr_port/region_freeze.c
  198:                    } while (MAX_CRIT_TRY > sleep_counter++);
  211:                            assert(!csd->kill_in_prog || (MAX_CRIT_TRY <= sleep_counter));
  219:            if (MAX_CRIT_TRY <= sleep_counter)

  sr_port/mu_int_maps.c
  103:    while (((cs_data && cs_data->kill_in_prog) || mu_int_data.kill_in_prog) && (MAX_CRIT_TRY > crit_counter++))
  106:                    GET_C_STACK_FOR_KIP(cs_addrs->nl->kip_pid_array, crit_counter, MAX_CRIT_TRY, 1, MAX_KIP_PID_SLOTS);

  sr_port/mupip_backup.c
  1019:                   while (cs_data->kill_in_prog && (MAX_CRIT_TRY > crit_counter++))
  1021:                           GET_C_STACK_FOR_KIP(kip_pids_arr_ptr, crit_counter, MAX_CRIT_TRY, 1, MAX_KIP_PID_SLOTS);
  1030:                   if (MAX_CRIT_TRY < crit_counter)
  1050:                           if (MAX_CRIT_TRY < crit_counter)
  ```

Issue
-----
* But I noticed that `region_freeze()` was an exception in the above in that it had calls to
  `grab_crit()` and `rel_crit()` in the wait loop (lines 192 and 196 below).

  **sr_port/region_freeze.c**
  ```c
    147      INCR_INHIBIT_KILLS(cnl);
      .
      .
    178      if (!override && wait_for_kip && (0 < csd->kill_in_prog))
    179      {
      .
      .
    189              do
    190              {
    191                      if (!was_crit)

sr_port/region_freeze.c
@@ -188,12 +188,12 @@ freeze_status	region_freeze_main(gd_region *region, boolean_t freeze, boolean_t 
 			}
 			do
 			{
-				if (!was_crit)
-					grab_crit(region, WS_80);
+				/* Note: We don't hold crit here so one might wonder if "csd->kill_in_prog" can become
+				 * non-zero after we find it zero below and do a "break". That is not possible because
+				 * we have already inhibited the start of new kills (INCR_INHIBIT_KILLS call above).
+				 */
 				if (!csd->kill_in_prog)
 					break;
-				if (!was_crit)
-					rel_crit(region);
 				wcs_sleep(sleep_counter);
 			} while (MAX_CRIT_TRY > sleep_counter++);
 			if (debug_mupip)
@@ -201,6 +201,14 @@ freeze_status	region_freeze_main(gd_region *region, boolean_t freeze, boolean_t 
 				GET_CUR_TIME(time_str);
 				util_out_print("!/MUPIP INFO: !AD : Done with kill-in-prog wait on region", TRUE,
 					       CTIME_BEFORE_NL, time_str);
+			}
+			if (!was_crit)
+			{
+				grab_crit(region, WS_80);
+				/* Assert that "csd->kill_in_prog" did not change since we found it to be 0 in the
+				 * do/while loop above outside of crit (see INCR_INHIBIT_KILLS comment above for why).
+				 */
+				assert(!csd->kill_in_prog || (MAX_CRIT_TRY <= sleep_counter));
 			}
 		}
 		if (pfrms)

sr_unix/ss_initiate.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2009-2023 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2018-2025 YottaDB LLC and/or its subsidiaries.	*
+ * Copyright (c) 2018-2026 YottaDB LLC and/or its subsidiaries.	*
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -528,7 +528,7 @@ boolean_t	ss_initiate(gd_region *reg,			/* Region in which snapshot has to be st
 		 * be rare.
 		 */
 		if (wait_for_zero_kip)
-		{	/* after inhibitting kills wait once for up to a minute; if that's insufficient, it's probably abandoned */
+		{	/* after inhibiting kills wait once for up to a minute; if that's insufficient, it's probably abandoned */
 			/* Release crit before going into the wait loop */
 			if (csa->now_crit)	/* In MUPIP INTEG and instance freeze, it is possible we don't have crit */
 				rel_crit(reg);

commit 18f63d2501f2127da99776397f021caf711f0033

Fix rare assert failure in region_freeze() (regression in 3b35ef5d6)
Background
----------
A `dbg` build failed once in the `v63003`/`gtm8850` subtest. That subtest runs 8
processes doing `set ^x($j)=$j kill ^x($j)` in a loop concurrently with a loop of
`mupip freeze -online -on -autorelease "*"` and `mupip freeze -off "*"`.

```
%YDB-F-ASSERT, Assert failed in sr_port/region_freeze.c line 211 for expression
(!csd->kill_in_prog || (MAX_CRIT_TRY <= sleep_counter))
```

```
#6  assertfail (...) at sr_port/mdef.h:827
#7  region_freeze_main (region=..., freeze=1, override=0, wait_for_kip=1,
        online=3, flush_sync=1, pfrms=0x0) at sr_port/region_freeze.c:211
#8  mupip_freeze () at sr_port/mupip_freeze.c:374
```

The core of the failing `mupip freeze` process showed the following.

- `sleep_counter` was 2, i.e. the wait loop had done exactly one `wcs_sleep(1)`
  before it found `csd->kill_in_prog` to be 0 and broke out.
- `csd->kill_in_prog` was 1 and `cnl->kip_pid_array` held one pid.
- `cnl->inhibit_kills` was 1 (the `mupip freeze` process's own increment).
- `csd->abandoned_kills` was 0 and `csd->freeze` was 0.

Analysis
--------
Commit 3b35ef5d6 removed the `grab_crit()`/`rel_crit()` calls from the
kill-in-progress wait loop in `region_freeze_main()`, on the assumption that the
`INCR_INHIBIT_KILLS()` done a few lines above guarantees that no new KILL can
start, and therefore that a `csd->kill_in_prog` value of 0 observed outside crit
stays 0 until the later `DECR_INHIBIT_KILLS()`.

That assumption is incorrect. `cnl->inhibit_kills` only defers a KILL until its
final retry. Both commit paths deliberately ignore it once `t_tries` reaches
`CDB_STAGNATE`:

- `sr_port/t_end.c` restarts with `cdb_sc_inhibitkills` only under
  `need_kip_incr && (0 < cnl->inhibit_kills) && (CDB_STAGNATE > t_tries)`, and
  otherwise falls through to the `INCR_KIP()` later in `t_end()`.
- `sr_port/tp_tend.c` has the same guard and the same fall-through to
  `INCR_KIP()`.

sr_port/mupip_backup.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2001-2023 Fidelity National Information	*
  * Services, Inc. and/or its subsidiaries. All rights reserved.	*
  *								*
- * Copyright (c) 2017-2025 YottaDB LLC and/or its subsidiaries. *
+ * Copyright (c) 2017-2026 YottaDB LLC and/or its subsidiaries. *
  * All rights reserved.						*
  *								*
  *	This source code contains the intellectual property	*
@@ -983,7 +983,7 @@ repl_inst_bkup_done1:
 	for (crit_counter = 1; online && kip_count; )
 	{	/* The purpose of this loop is to wait for kip to clear on all regions, waiting for a maximum of about a minute.
 		 * This is complicated by the fact that in spite of setting INCR_INHIBIT_KILLS above, a process in crit for a
-		 * "fourth" retry may ignote that, and set the kip flag while we are waiting
+		 * "fourth" retry may ignore that, and set the kip flag while we are waiting
 		 */
 		DEBUG_ONLY(nocritrptr = NULL;)
 		for (rptr = (backup_reg_list *)(grlist); NULL != rptr; rptr = rptr->fPtr)

sr_port/region_freeze.c
@@ -190,11 +190,25 @@ freeze_status	region_freeze_main(gd_region *region, boolean_t freeze, boolean_t 
 			do
 			{
 				/* Note: We don't hold crit here so one might wonder if "csd->kill_in_prog" can become
-				 * non-zero after we find it zero below and do a "break". That is not possible because
-				 * we have already inhibited the start of new kills (INCR_INHIBIT_KILLS call above).
+				 * non-zero after we find it to be zero below. One might think that is not possible
+				 * because we have already inhibited the start of new kills (INCR_INHIBIT_KILLS call
+				 * above). But that is not true. A KILL that is in its final retry (i.e. "t_tries" is
+				 * CDB_STAGNATE) goes ahead with the kill in spite of "cnl->inhibit_kills" being
+				 * non-zero (see "cdb_sc_inhibitkills" usages in "t_end.c" and "tp_tend.c") and so
+				 * can do an INCR_KIP after we find "csd->kill_in_prog" to be zero. Since that INCR_KIP
+				 * happens while holding crit, we grab crit and recheck "csd->kill_in_prog" below and
+				 * go back to waiting in case it is non-zero. Note that "mupip_backup.c" does a similar
+				 * recheck of "cs_data->kill_in_prog" after grabbing crit for the same reason.
 				 */
 				if (!csd->kill_in_prog)
-					break;
+				{
+					if (was_crit)
+						break;
+					grab_crit(region, WS_80);
+					if (!csd->kill_in_prog)
+						break;	/* Found it to be zero while holding crit. Safe to proceed. */
+					rel_crit(region);	/* A final-retry KILL slipped in. Wait some more. */
+				}
 				wcs_sleep(sleep_counter);
 			} while (MAX_CRIT_TRY > sleep_counter++);
 			if (debug_mupip)
@@ -203,13 +217,13 @@ freeze_status	region_freeze_main(gd_region *region, boolean_t freeze, boolean_t 
 				util_out_print("!/MUPIP INFO: !AD : Done with kill-in-prog wait on region", TRUE,
 					       CTIME_BEFORE_NL, time_str);
 			}
-			if (!was_crit)
-			{
-				grab_crit(region, WS_80);
-				/* Assert that "csd->kill_in_prog" did not change since we found it to be 0 in the
-				 * do/while loop above outside of crit (see INCR_INHIBIT_KILLS comment above for why).
+			if (!csa->now_crit)
+			{	/* We get here without crit only if the above do/while loop timed out (in which case
+				 * the "MAX_CRIT_TRY <= sleep_counter" check below returns REG_HAS_KIP). Grab crit like
+				 * the rest of the code below expects.
 				 */
-				assert(!csd->kill_in_prog || (MAX_CRIT_TRY <= sleep_counter));
+				assert(!was_crit && (MAX_CRIT_TRY <= sleep_counter));
+				grab_crit(region, WS_80);
 			}
 		}
 		if (pfrms)